61d01a1be3e1cc587ed5a0a4180a9439d3f66ac6
Mateusz Kocz <mateusz@kocz.pl>
2019-11-08 09:55:48 +0100

    Add Storybook configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,30 +1,59 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+module.exports = ({ config }) => {
+  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+  config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+  // use @babel/preset-react for JSX and env (instead of staged presets)
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve("@babel/preset-react"),
+    require.resolve("@babel/preset-env"),
+  ]
+  config.module.rules[0].use[0].options.plugins = [
+    // use @babel/plugin-proposal-class-properties for class arrow functions
+    require.resolve("@babel/plugin-proposal-class-properties"),
+  ]
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ["browser", "module", "main"]
 
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-//
-// module.exports = {
-//   plugins: [
-//     // your custom plugins
-//   ],
-//   module: {
-//     rules: [
-//       // add your custom rules.
-//     ],
-//   },
-// };
+  // Allow SVGs.
+  handleSVGs(config)
 
-const path = require('path');
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
-module.exports = (baseConfig, env, config) => {
+  // TypeScript config
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader'),
-  });
-  config.plugins.push(new TSDocgenPlugin()); // optional
-  config.resolve.extensions.push('.ts', '.tsx');
-  return config;
-};
+    loader: require.resolve("babel-loader"),
+    options: {
+      presets: [["react-app", { flow: false, typescript: true }]],
+      plugins: [
+        require.resolve("@babel/plugin-proposal-class-properties"),
+      ],
+    },
+  })
+  config.resolve.extensions.push(".ts", ".tsx")
+  return config
+}
+
+// SVGs are broken in the default config, so have to apply
+// this fix: https://github.com/storybookjs/storybook/issues/6188#issuecomment-487705465
+function handleSVGs(config) {
+  // Remove svg from existing rule.
+  config.module.rules = config.module.rules.map(rule => {
+    if (
+      String(rule.test) ===
+      String(
+        /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/
+      )
+    ) {
+      return {
+        ...rule,
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
+      }
+    }
+
+    return rule
+  })
+
+  // Use svgr for svg files.
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ["svg-react-loader"],
+  })
+}
---
+++ app/home/components/home/home-view.component.tsx
@@ -0,0 +1,53 @@
+import * as React from "react"
+import styled from "styled-components"
+import FunctionComponent from "../../../types/function-component.interface"
+
+const HomeWrapper = styled.div`
+  font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans,
+    sans-serif;
+  width: 80%;
+  margin: auto;
+  border: 1px #ccc dotted;
+`
+
+const HomeTitle = styled.div`
+  padding: 10px;
+  margin: 10px 0px;
+`
+
+const HomeContent = styled.div`
+  padding: 10px;
+  margin: 10px 0px;
+`
+
+interface Props {
+  readonly onTitleClick: () => void
+  readonly count: number
+}
+
+const HomeView: FunctionComponent<Props> = ({ onTitleClick, count }) => {
+  return (
+    <HomeWrapper>
+      <HomeTitle onClick={onTitleClick}> Title {count} </HomeTitle>
+      <HomeContent>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean orci
+        dolor, rutrum at quam sed, aliquet dapibus ante. Maecenas id commodo
+        justo. Suspendisse potenti. Pellentesque ut tristique tortor, non
+        ultrices ante. Maecenas vel orci arcu. Duis ut nunc blandit, vestibulum
+        dui sed, dignissim lorem. Curabitur eleifend nisi faucibus massa
+        elementum ultricies. Integer sed quam felis. Aenean tincidunt, velit
+        tempus elementum pellentesque, risus erat maximus mi, nec iaculis nisl
+        dolor elementum tortor. Nunc sollicitudin sem vitae auctor gravida.
+        Vestibulum vel commodo velit. Cras id pellentesque lectus, et viverra
+        enim. Ut vel mauris tempor, consequat purus at, fringilla ipsum. Sed
+        enim diam, vestibulum vitae dui et, tempor faucibus tortor. Nam eget
+        nisi lacinia, condimentum turpis id, iaculis sem. Phasellus porttitor
+        elementum elit, egestas pellentesque nulla. Sed aliquet nec lectus non
+        sollicitudin. Morbi feugiat sollicitudin ullamcorper. Donec nec congue
+        neque, sed volutpat enim.
+      </HomeContent>
+    </HomeWrapper>
+  )
+}
+
+export default HomeView
