b557bfd996b5a690d58c65e1b7577eb99ea18bdf
Andrzej Ziolek <a.j.ziolek@gmail.com>
2019-11-18 11:36:18 +0100

    fix: prettier

--- src/renderer/wrappers/root.wrapper.tsx
+++ src/renderer/wrappers/root.wrapper.tsx
@@ -1,17 +1,17 @@
 import { ConnectedRouter } from "connected-react-router"
 import { History } from "history"
 import * as React from "react"
+import { IntlProvider } from "react-intl"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "styled-components"
 import { Normalize } from "styled-normalize"
-import { IntlProvider } from "react-intl";
 
 import BaseRoutes from "Renderer/routes/base-routes"
 import GlobalStyle from "Renderer/styles/global-style.component"
 import theme from "Renderer/styles/theming/theme"
 import FunctionComponent from "Renderer/types/function-component.interface"
 
-import { LANGUAGE } from "Renderer/constants/languages";
+import { LANGUAGE } from "Renderer/constants/languages"
 
 interface Props {
   store: any
