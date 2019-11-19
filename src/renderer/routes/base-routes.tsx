dce6c1ff904fc646cb52bff42d369ebdc05ad2ca
Andrzej Ziolek <a.j.ziolek@gmail.com>
2019-11-19 11:15:08 +0100

    style: wrappers renamed

--- src/renderer/routes/base-routes.tsx
+++ src/renderer/routes/base-routes.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Route, Switch } from "react-router"
 
 import HomePage from "Renderer/modules/home/home.page"
-import AppWrapper from "Renderer/wrappers/app.wrapper"
+import AppWrapper from "Renderer/wrappers/app-wrapper"
 
 import { URL_MAIN } from "Renderer/constants/urls"
 
