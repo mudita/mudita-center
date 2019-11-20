a896780de92a3f6065d86f3b8b4b64bb7a359a28
Kamil Staszewski <kamilstaszewski95@gmail.com>
2019-11-20 14:40:47 +0100

    Update .storybook/config.js
    Co-Authored-By: Andrzej Ziółek <a.j.ziolek@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@ import { action } from "@storybook/addon-actions"
 
 import GlobalStyle from "Renderer/styles/global-style.component"
 import theme from "Renderer/styles/theming/theme"
-import "../src/renderer/fonts/fonts.css"
+import "Renderer/fonts/fonts.css"
 
 import localeEn from "Renderer/locales/main/en-US.json"
 import { IntlProvider } from "react-intl"
