cdee2e6c1444ece7e74d564bdbaa852944d7aca1
staszewski <kamilstaszewski95@gmail.com>
2019-11-21 11:37:28 +0100

    PDA-25 Spreading also src as Mateusz suggested

--- src/renderer/components/image/image.component.tsx
+++ src/renderer/components/image/image.component.tsx
@@ -3,9 +3,8 @@ import * as React from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
 
 const Image: FunctionComponent<ImgHTMLAttributes<HTMLImageElement>> = ({
-  src,
   alt = "",
   ...rest
-}) => <img src={src} alt={alt} {...rest} />
+}) => <img alt={alt} {...rest} />
 
 export default Image
--- src/renderer/components/image/image.stories.tsx
+++ src/renderer/components/image/image.stories.tsx
@@ -2,6 +2,6 @@ import { storiesOf } from "@storybook/react"
 import * as React from "react"
 import Image from "./image.component"
 
-storiesOf("Components|Image", module).add("Text", () => {
+storiesOf("Components|Image", module).add("Image", () => {
   return <Image src="http://placekitten.com/g/200/300" alt="Kitku" />
 })
