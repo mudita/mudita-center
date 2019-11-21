913196fb63c3dec92ac60e1ac8a47a97408c69f4
staszewski <kamilstaszewski95@gmail.com>
2019-11-21 12:18:41 +0100

    PDA-32 Add input

---
+++ src/renderer/components/input-radio/input-radio.component.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import FunctionComponent from "Renderer/types/function-component.interface"
+
+const InputRadio: FunctionComponent = () => {
+  return <div>cos</div>
+}
+
+export default InputRadio
