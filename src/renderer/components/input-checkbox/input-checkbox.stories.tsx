60fe671ad6c403f3a2ace1cfe143a639001171a5
staszewski <kamilstaszewski95@gmail.com>
2019-11-26 08:06:04 +0100

    PDA-33 Show both states of checkbox in sb

--- src/renderer/components/input-checkbox/input-checkbox.stories.tsx
+++ src/renderer/components/input-checkbox/input-checkbox.stories.tsx
@@ -1,17 +1,19 @@
 import { storiesOf } from "@storybook/react"
 import * as React from "react"
 import InputCheckbox from "Renderer/components/input-checkbox/input-checkbox.component"
+import InputRadio from "Renderer/components/input-radio/input-radio.component"
 
-storiesOf("Components|Input", module).add("Checkbox", () => {
-  return (
-    <form action="">
+storiesOf("Components|InputCheckbox", module)
+  .add("Checked", () => {
+    return (
       <InputCheckbox
         name={"Example1"}
         value={"value1"}
         id={"id1"}
         defaultChecked
       />
-      <InputCheckbox name={"Example1"} value={"value2"} id={"id2"} />
-    </form>
-  )
-})
+    )
+  })
+  .add("Unchecked", () => {
+    return <InputCheckbox name={"Example1"} value={"value2"} id={"id2"} />
+  })
