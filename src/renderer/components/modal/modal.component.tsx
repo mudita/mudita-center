f0f570aa1ee3080e53abc70a5c9d2b8e79838fb0
Michał Kurczewski <michalkurczewski94@gmail.com>
2019-11-26 19:22:25 +0100

    Cleaned up modal component

--- src/renderer/components/modal/modal.component.tsx
+++ src/renderer/components/modal/modal.component.tsx
@@ -1,6 +1,3 @@
-import * as React from "react"
-import { ReactNode, useState } from "react"
-import ReactDOM from "react-dom"
 import styled from "styled-components"
 
 export const ModalWrapper = styled.section`
@@ -16,26 +13,4 @@ export const ModalWrapper = styled.section`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 `
 
-const useModal = (content: ReactNode) => {
-  const [visible, setVisibility] = useState(false)
-
-  const open = () => {
-    console.log("open")
-    setVisibility(true)
-
-    ReactDOM.createPortal(<ModalWrapper>{content}</ModalWrapper>, document.body)
-  }
-
-  const close = () => {
-    console.log("close")
-    setVisibility(false)
-  }
-
-  return {
-    visible,
-    open,
-    close,
-  }
-}
-
-export default useModal
+export default ModalWrapper
