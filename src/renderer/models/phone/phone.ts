7f9a3ced5c838d21821fc22b6503c8ed3e047667
staszewski <kamilstaszewski95@gmail.com>
2019-12-04 09:52:43 +0100

    PDA-49 Clean functions and previous version of fake data

--- src/renderer/models/phone/phone.ts
+++ src/renderer/models/phone/phone.ts
@@ -1,7 +1,9 @@
 import Faker from "faker"
+
 const initialContactList = {
   contactList: [],
 }
+
 const generateLetters = (contactList: any) => {
   const alphabet = new Array(26)
     .fill(1)
@@ -16,56 +18,31 @@ const generateLetters = (contactList: any) => {
   return contactList
 }
 
-const generateNames = (state: any) => {
-  const fakeNames = Array(50)
+const generateFakeState = (state: any) => {
+  const fakeData = Array(50)
     .fill(0)
-    .map(_ => Faker.name.firstName())
+    .map(_ => ({
+      id: Faker.random.uuid(),
+      firstName: Faker.name.firstName(),
+      lastName: Faker.name.lastName(),
+      phoneNumber: Faker.phone.phoneNumber(),
+      favourite: Faker.random.boolean(),
+    }))
 
   for (const [i] of state.entries()) {
     const letter = state[i].letter
     const contacts = state[i].contacts
-    for (const name of fakeNames) {
-      if (name.charAt(0) === letter) {
-        contacts.push({
-          firstName: name.charAt(0),
-        })
+    for (const item of fakeData) {
+      if (item.firstName.charAt(0) === letter) {
+        contacts.push(item)
       }
     }
   }
-  return state
-}
-
-console.log(generateNames(generateLetters(initialContactList.contactList)))
-
-const initialState = {
-  contactList: [
-    {
-      letter: "A",
-      contacts: [
-        {
-          firstName: "Adam",
-          lastName: "Smiths",
-          phoneNumber: Faker.phone.phoneNumber(),
-          id: 1,
-          favourite: true,
-        },
-      ],
-    },
-    {
-      letter: "B",
-      contacts: [
-        {
-          name: "Bob",
-          lastName: "Smiths",
-          phoneNumber: Faker.phone.phoneNumber(),
-          id: 1,
-          favourite: false,
-        },
-      ],
-    },
-  ],
+  return {
+    contactList: state,
+  }
 }
 
 export default {
-  state: initialState,
+  state: generateFakeState(generateLetters(initialContactList.contactList)),
 }
