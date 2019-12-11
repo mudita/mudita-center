e30c55f1717647a74fdd260cb4a14aac6283c740
Michał Kurczewski <michalkurczewski94@gmail.com>
2019-12-11 11:53:54 +0100

    Added indeterminate state for checkbox

--- src/renderer/interfaces/input.interface.ts
+++ src/renderer/interfaces/input.interface.ts
@@ -4,4 +4,5 @@ import { TextDisplayStyle } from "Renderer/components/core/text/text.component"
 export interface InputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
   labelDisplayStyle?: TextDisplayStyle
+  indeterminate?: boolean
 }
