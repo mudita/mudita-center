397f043ce1aaf4f584f50577b84b581e57c19abc
staszewski <kamilstaszewski95@gmail.com>
2019-12-16 12:13:36 +0100

    PDA-50 Fix height with margins.

--- src/renderer/components/rest/header/header.component.tsx
+++ src/renderer/components/rest/header/header.component.tsx
@@ -12,7 +12,7 @@ import styled from "styled-components"
 
 const HeaderWrapper = styled.div`
   display: flex;
-  align-items: center;
+  justify-content: center;
   position: relative;
   border-bottom: 0.1rem solid ${borderColor("dark")};
 `
@@ -22,7 +22,7 @@ interface HeaderProps {
 }
 
 const HeaderText = styled(Text)`
-  margin: 0;
+  margin: 2.4rem 0 1.5rem 0;
   position: absolute;
   left: 4rem;
 `
--- src/renderer/components/rest/header/tab.component.tsx
+++ src/renderer/components/rest/header/tab.component.tsx
@@ -16,15 +16,11 @@ const TabIcon = styled(Svg)`
 const TabWrapper = styled.div`
   display: flex;
   align-items: center;
-  height: calc(100% - 0.02rem);
+  margin: 2.6rem 0 1.5rem 0;
 
   &:not(:last-child) {
     margin-right: 4rem;
   }
-`
-
-const TabText = styled(Text)`
-  margin: 1.6rem 0 1.3rem 0;
 `
 
 interface TabProps {
@@ -36,7 +32,7 @@ const Tab: FunctionComponent<TabProps> = ({ icon, tabText }) => {
   return (
     <TabWrapper data-testid="tab">
       <TabIcon Image={icon} />
-      <TabText displayStyle={TextDisplayStyle.MediumText}>{tabText}</TabText>
+      <Text displayStyle={TextDisplayStyle.MediumText}>{tabText}</Text>
     </TabWrapper>
   )
 }
