c1e83e48897a3b48079af81319dd29f6605ef4a1
Michał Kurczewski <michalkurczewski94@gmail.com>
2019-12-18 20:19:23 +0100

    Test implementation of table in real app

--- src/renderer/modules/news/news.component.tsx
+++ src/renderer/modules/news/news.component.tsx
@@ -1,6 +1,20 @@
 import React from "react"
+import Table from "Renderer/components/core/table/table.component"
+import {
+  fakeTableLabels,
+  fakeTableRows,
+  labelsLayout,
+  Sidebar,
+} from "Renderer/components/core/table/table.stories"
 import FunctionComponent from "Renderer/types/function-component.interface"
 
-const News: FunctionComponent = () => <div>News</div>
+const News: FunctionComponent = () => (
+  <Table
+    rows={fakeTableRows}
+    labels={fakeTableLabels}
+    sidebar={<Sidebar />}
+    labelsLayout={labelsLayout}
+  />
+)
 
 export default News
