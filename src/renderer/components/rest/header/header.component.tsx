e45de1e0771da55edaa015e171ae36689ac877b1
staszewski <kamilstaszewski95@gmail.com>
2019-12-19 13:06:26 +0100

    PDA-50 Change middle components type to any

--- src/renderer/components/rest/header/header.component.tsx
+++ src/renderer/components/rest/header/header.component.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { ReactNode, useState } from "react"
+import { useState } from "react"
 import { useEffect } from "react"
 import { useLocation } from "react-router"
 import Text, {
@@ -18,7 +18,7 @@ const HeaderWrapper = styled.div`
 `
 
 interface HeaderProps {
-  middleComponent?: ReactNode
+  middleComponent?: any
 }
 
 const HeaderText = styled(Text)`
