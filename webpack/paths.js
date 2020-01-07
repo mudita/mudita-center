2681d94e7280af1ed6ce4b90a36711c6b25f65dc
Mateusz Kocz <mateusz@kocz.pl>
2020-01-07 07:27:37 +0100

    Add backend path configuration

--- webpack/paths.js
+++ webpack/paths.js
@@ -1,9 +1,10 @@
 module.exports = {
-  src: './src',
-  dist: './dist',
-  tmp: './tmp/',
-  publicStatic: './public',
-  storybook: './.storybook',
-  cypress: './cypress',
-  renderer: './src/renderer',
-};
+  src: "./src",
+  dist: "./dist",
+  tmp: "./tmp/",
+  publicStatic: "./public",
+  storybook: "./.storybook",
+  cypress: "./cypress",
+  renderer: "./src/renderer",
+  backend: "./src/backend",
+}
