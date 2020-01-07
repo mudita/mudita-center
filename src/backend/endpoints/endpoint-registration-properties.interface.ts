4950805a169822a4b19e170a5b1d5ae57aaacfbd
Mateusz Kocz <mateusz@kocz.pl>
2020-01-07 11:08:08 +0100

    Rename the data to give it more meaning

--- src/backend/endpoints/endpoint-registration-properties.interface.ts
+++ src/backend/endpoints/endpoint-registration-properties.interface.ts
@@ -1,10 +1,10 @@
 import Adapters from "Backend/adapters/adapters.interface"
 import { IpcRequest } from "Common/requests/ipc-request.enum"
 
-export default interface EndpointRegistrationProperties<Params, Response> {
+export default interface EndpointRegistrationProperties<CallerProps, Response> {
   name: IpcRequest
   handler: (
     adapters: Adapters,
-    data: Params
+    callerProps: CallerProps
   ) => Response | PromiseLike<Response>
 }
