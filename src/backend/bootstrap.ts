5bccca1121d072cf9d8f6472b8adb1459ddb7c9a
Mateusz Kocz <mateusz@kocz.pl>
2020-01-07 10:57:01 +0100

    Create a temporary device info request

--- src/backend/bootstrap.ts
+++ src/backend/bootstrap.ts
@@ -1,3 +1,13 @@
-const bootstrap = () => console.log("Backend running")
+import createElectronAppAdapter from "Backend/adapters/electron-app/electron-app.adapter"
+import registerDeviceInfoRequest from "Backend/requests/get-device-info.request"
+
+const bootstrap = () => {
+  const appAdapter = createElectronAppAdapter()
+  ;[registerDeviceInfoRequest].forEach(register =>
+    register({
+      app: appAdapter,
+    })
+  )
+}
 
 export default bootstrap
