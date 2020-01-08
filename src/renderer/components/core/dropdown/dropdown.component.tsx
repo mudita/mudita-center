7fe50bd182b1a8b7369d34adf3bea4423c49bbfa
staszewski <kamilstaszewski95@gmail.com>
2020-01-08 13:33:37 +0100

    PDA-37 Add animation, update tests

--- src/renderer/components/core/dropdown/dropdown.component.tsx
+++ src/renderer/components/core/dropdown/dropdown.component.tsx
@@ -1,22 +1,40 @@
 import * as React from "react"
-import { ReactNode, useRef, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import {
   backgroundColor,
   boxShadowColor,
 } from "Renderer/styles/theming/theme-getters"
 import FunctionComponent from "Renderer/types/function-component.interface"
 import useOutsideClick from "Renderer/utils/hooks/useOutsideClick"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 interface Props {
   toggler: ReactNode
 }
 
+const fadeEnter = css`
+  @keyframes fadeEnter {
+    from {
+      opacity: 0;
+    }
+  }
+  animation: fadeEnter 0.5s both ease-in;
+`
+
+const fadeLeave = css`
+  @keyframes fadeLeave {
+    to {
+      opacity: 0;
+    }
+  }
+  animation: fadeLeave 0.5s both ease-out;
+`
+
 const DropdownWrapper = styled.div`
   position: relative;
 `
 
-const DropdownList = styled.ul`
+const DropdownList = styled.ul<{ showAnimation: boolean }>`
   position: absolute;
   right: 0;
   list-style-type: none;
@@ -26,23 +44,43 @@ const DropdownList = styled.ul`
   border-radius: 0.6rem 0.2rem;
   box-shadow: 0 1rem 5.5rem -0.5rem ${boxShadowColor("grey")};
   min-width: 17rem;
+  ${({ showAnimation }) => (showAnimation ? fadeEnter : fadeLeave)}
 `
 
 const Dropdown: FunctionComponent<Props> = ({ toggler, children }) => {
-  const [open, setOpen] = useState(false)
+  const [show, setShow] = useState(false)
+  const [render, setRender] = useState(show)
   const ref = useRef(null)
   useOutsideClick(ref, () => {
-    if (open) {
-      setOpen(false)
+    if (show) {
+      setShow(false)
     }
   })
+
+  useEffect(() => {
+    if (show) {
+      setRender(true)
+    }
+  }, [show])
+
+  const onAnimationEnd = () => {
+    if (!show) {
+      setRender(false)
+    }
+  }
+
   return (
     <DropdownWrapper>
       {React.cloneElement(toggler as React.ReactElement, {
-        onClick: () => setOpen(!open),
+        onClick: () => setShow(!show),
       })}
-      {open && (
-        <DropdownList ref={ref} data-testid="dropdown">
+      {render && (
+        <DropdownList
+          ref={ref}
+          showAnimation={show}
+          data-testid="dropdown"
+          onAnimationEnd={onAnimationEnd}
+        >
           {children}
         </DropdownList>
       )}
--- src/renderer/components/core/dropdown/dropdown.test.tsx
+++ src/renderer/components/core/dropdown/dropdown.test.tsx
@@ -1,11 +1,12 @@
 import { fireEvent } from "@testing-library/dom"
 import "@testing-library/jest-dom/extend-expect"
+import { wait } from "@testing-library/react"
 import React from "react"
 import ButtonComponent from "Renderer/components/core/button/button.component"
-import DropdownItem from "Renderer/components/core/dropdown/dropdown-item.component"
 import Dropdown from "Renderer/components/core/dropdown/dropdown.component"
 import Upload from "Renderer/svg/upload.svg"
 import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-intl"
+import Button, { DisplayStyle } from "../button/button.component"
 
 test("matches snapshot", () => {
   const { container } = renderWithThemeAndIntl(
@@ -22,29 +23,49 @@ test("renders toggler passed to component", () => {
   expect(getByText(buttonText)).toBeInTheDocument()
 })
 
-test("renders dropdown", () => {
+test("renders dropdown", async () => {
   const buttonText = "Example"
   const { getByTestId, getByText, container } = renderWithThemeAndIntl(
-    <Dropdown toggler={<ButtonComponent label={buttonText} />} />
-  )
-
-  fireEvent.click(getByText(buttonText))
-
-  expect(getByTestId("dropdown")).toBeInTheDocument()
-  expect(container).toMatchSnapshot()
-})
-
-test("renders children", () => {
-  const buttonText = "Example"
-  const childText = "childText"
-  const { getByText } = renderWithThemeAndIntl(
     <Dropdown toggler={<ButtonComponent label={buttonText} />}>
-      <DropdownItem Icon={Upload} text={childText} />
+      <Button
+        displayStyle={DisplayStyle.Link1}
+        label="I open Google in new tab"
+        href="http://www.google.pl"
+        target="_blank"
+        Icon={Upload}
+        dropdown
+      />
     </Dropdown>
   )
 
   fireEvent.click(getByText(buttonText))
 
-  expect(getByText(childText)).toBeInTheDocument()
-  expect(getByText(childText)).toHaveTextContent(childText)
+  await wait(() => {
+    expect(getByTestId("dropdown")).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+})
+
+test("renders children", async () => {
+  const buttonText = "Example"
+  const childText = "childText"
+  const { getByText } = renderWithThemeAndIntl(
+    <Dropdown toggler={<ButtonComponent label={buttonText} />}>
+      <Button
+        displayStyle={DisplayStyle.Link1}
+        label={childText}
+        href="http://www.google.pl"
+        target="_blank"
+        Icon={Upload}
+        dropdown
+      />
+    </Dropdown>
+  )
+
+  fireEvent.click(getByText(buttonText))
+
+  await wait(() => {
+    expect(getByText(childText)).toBeInTheDocument()
+    expect(getByText(childText)).toHaveTextContent(childText)
+  })
 })
