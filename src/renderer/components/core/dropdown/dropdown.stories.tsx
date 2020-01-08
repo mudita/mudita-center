82938ff7a1b69d972d91befb116a6cc436ea1cac
staszewski <kamilstaszewski95@gmail.com>
2020-01-08 14:01:15 +0100

    PDA-37 Change story name

--- src/renderer/components/core/dropdown/dropdown.stories.tsx
+++ src/renderer/components/core/dropdown/dropdown.stories.tsx
@@ -12,38 +12,35 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
-storiesOf("Components|Dropdown", module).add(
-  "Dropdown with dropdown specific link",
-  () => {
-    return (
-      <Wrapper>
-        <Dropdown toggler={<ButtonComponent label={"Click dropdown"} />}>
-          <Button
-            displayStyle={DisplayStyle.Link1}
-            label="I open Google in new tab"
-            href="http://www.google.pl"
-            target="_blank"
-            Icon={Upload}
-            dropdown
-          />
-          <Button
-            displayStyle={DisplayStyle.Link1}
-            label="I open Google in new tab"
-            href="http://www.google.pl"
-            target="_blank"
-            Icon={Upload}
-            dropdown
-          />
-          <Button
-            displayStyle={DisplayStyle.Link1}
-            label="I open Google in new tab"
-            href="http://www.google.pl"
-            target="_blank"
-            Icon={Upload}
-            dropdown
-          />
-        </Dropdown>
-      </Wrapper>
-    )
-  }
-)
+storiesOf("Components|Dropdown", module).add("dropdown specific link", () => {
+  return (
+    <Wrapper>
+      <Dropdown toggler={<ButtonComponent label={"Click dropdown"} />}>
+        <Button
+          displayStyle={DisplayStyle.Link1}
+          label="I open Google in new tab"
+          href="http://www.google.pl"
+          target="_blank"
+          Icon={Upload}
+          dropdown
+        />
+        <Button
+          displayStyle={DisplayStyle.Link1}
+          label="I open Google in new tab"
+          href="http://www.google.pl"
+          target="_blank"
+          Icon={Upload}
+          dropdown
+        />
+        <Button
+          displayStyle={DisplayStyle.Link1}
+          label="I open Google in new tab"
+          href="http://www.google.pl"
+          target="_blank"
+          Icon={Upload}
+          dropdown
+        />
+      </Dropdown>
+    </Wrapper>
+  )
+})
