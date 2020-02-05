9703926761a886593797a259101000abe63bca9d
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-02-05 13:01:33 +0100

    Stories and tests

--- src/renderer/components/rest/overview/system/system.stories.tsx
+++ src/renderer/components/rest/overview/system/system.stories.tsx
@@ -5,42 +5,73 @@ import Text, {
   TextDisplayStyle,
 } from "Renderer/components/core/text/text.component"
 import getFakeAdapters from "App/tests/get-fake-adapters"
-import { SystemProps } from "Renderer/components/rest/overview/system/system.interface"
+import { action } from "@storybook/addon-actions"
+import styled from "styled-components"
 
 const fakeSystemInfo = getFakeAdapters().purePhone
+const osVersion = fakeSystemInfo.getOsVersion()
+const updated = new Date(fakeSystemInfo.getOsUpdateDate()).toLocaleDateString(
+  "en-US"
+)
 
-export const CustomizableSystem = ({
-  osVersion = fakeSystemInfo.getOsVersion(),
-  lastUpdate,
-  onUpdatesCheck,
-}: Partial<SystemProps>) => {
-  return (
-    <System
-      osVersion={osVersion}
-      lastUpdate={lastUpdate}
-      onUpdatesCheck={onUpdatesCheck}
-    />
-  )
-}
+const Part = styled.div`
+  padding: 2rem;
+  > p {
+    margin-bottom: 2rem;
+  }
+`
 
-storiesOf("Overview/System", module).add("Basic", () => {
+storiesOf("Overview|System", module).add("System", () => {
   return (
-    <div style={{ margin: "2rem", maxWidth: "59rem" }}>
-      <Text displayStyle={TextDisplayStyle.SmallText}>Not updated yet</Text>
-      <br />
-      <CustomizableSystem />
-      <br />
-      <br />
-      <Text displayStyle={TextDisplayStyle.SmallText}>Updated month ago</Text>
-      <br />
-      <CustomizableSystem lastUpdate={fakeSystemInfo.getOsUpdateDate()} />
-      <br />
-      <br />
-      <Text displayStyle={TextDisplayStyle.SmallText}>
-        New update available
-      </Text>
-      <br />
-      <CustomizableSystem lastUpdate={fakeSystemInfo.getOsUpdateDate()} />
+    <div style={{ maxWidth: "59rem" }}>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>Not updated yet</Text>
+        <System
+          osVersion={osVersion}
+          onUpdateCheck={action("checked for update")}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          Updated some time ago
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={updated}
+          onUpdateCheck={action("checked for update")}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          New update available
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={updated}
+          onUpdate={action("update requested")}
+          updateAvailable
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          New (first) update available
+        </Text>
+        <System
+          osVersion={osVersion}
+          onUpdateCheck={action("checked for update")}
+          updateAvailable
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          Updated just now (custom text)
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={"just now"}
+          onUpdateCheck={action("checked for update")}
+        />
+      </Part>
     </div>
   )
 })
--- src/renderer/components/rest/overview/system/system.test.tsx
+++ src/renderer/components/rest/overview/system/system.test.tsx
@@ -4,18 +4,19 @@ import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-int
 import { SystemProps } from "Renderer/components/rest/overview/system/system.interface"
 import getFakeAdapters from "App/tests/get-fake-adapters"
 import { intl } from "Renderer/utils/intl"
-import { CustomizableSystem } from "Renderer/components/rest/overview/system/system.stories"
+import System from "Renderer/components/rest/overview/system/system.component"
+import { fireEvent } from "@testing-library/dom"
+import { wait } from "@testing-library/react"
 
 const fakeSystemInfo = getFakeAdapters().purePhone
-const osVersion = fakeSystemInfo.getOsVersion()
 const lastUpdate = fakeSystemInfo.getOsUpdateDate()
+const fakeOsVersion = fakeSystemInfo.getOsVersion()
 
-const renderSystem = ({ ...props }: Partial<SystemProps> = {}) => {
-  const outcome = renderWithThemeAndIntl(<CustomizableSystem {...props} />)
-  return {
-    ...outcome,
-    getButton: () => outcome.queryByRole("button"),
-  }
+const renderSystem = ({
+  osVersion = fakeOsVersion,
+  ...props
+}: Partial<SystemProps> = {}) => {
+  return renderWithThemeAndIntl(<System osVersion={osVersion} {...props} />)
 }
 
 test("matches snapshot", () => {
@@ -24,23 +25,79 @@ test("matches snapshot", () => {
 })
 
 test("renders os version properly", () => {
-  const { queryByText } = renderSystem()
-  expect(queryByText(osVersion)).toBeInTheDocument()
+  const { getByText } = renderSystem()
+  expect(getByText(fakeOsVersion)).toBeInTheDocument()
   expect(
-    queryByText(intl.formatMessage({ id: "view.name.overview.system.version" }))
+    getByText(intl.formatMessage({ id: "view.name.overview.system.version" }))
   ).toBeInTheDocument()
 })
 
 test("renders last update info properly", () => {
-  const { queryByText } = renderSystem({
-    lastUpdate,
-  })
+  const { getByText } = renderSystem({ lastUpdate })
   expect(
-    queryByText(
+    getByText(
       intl.formatMessage(
         { id: "view.name.overview.system.lastUpdate" },
-        { date: new Date(lastUpdate).toLocaleDateString() }
+        { date: lastUpdate }
       )
     )
   ).toBeInTheDocument()
+})
+
+test("renders available update info properly", () => {
+  const { getByText } = renderSystem({ updateAvailable: true })
+  expect(
+    getByText(
+      intl.formatMessage({ id: "view.name.overview.system.updateAvailable" })
+    )
+  ).toBeInTheDocument()
+})
+
+test("renders 'check for updates' button properly", () => {
+  const { queryByRole } = renderSystem()
+  expect(queryByRole("button")).toHaveTextContent(
+    intl.formatMessage({ id: "view.name.overview.system.checkForUpdates" })
+  )
+})
+
+test("renders 'update now' button properly", () => {
+  const { queryByRole } = renderSystem({ updateAvailable: true })
+  expect(queryByRole("button")).toHaveTextContent(
+    intl.formatMessage({ id: "view.name.overview.system.updateAction" })
+  )
+})
+
+test("checks for update after button click", async () => {
+  const onUpdateCheck = jest.fn()
+  const onUpdate = jest.fn()
+
+  const { getByRole } = renderSystem({
+    onUpdateCheck,
+    onUpdate,
+  })
+
+  fireEvent.click(getByRole("button"))
+
+  await wait(() => {
+    expect(onUpdateCheck).toHaveBeenCalled()
+    expect(onUpdate).not.toHaveBeenCalled()
+  })
+})
+
+test("triggers update after button click", async () => {
+  const onUpdateCheck = jest.fn()
+  const onUpdate = jest.fn()
+
+  const { getByRole } = renderSystem({
+    updateAvailable: true,
+    onUpdateCheck,
+    onUpdate,
+  })
+
+  fireEvent.click(getByRole("button"))
+
+  await wait(() => {
+    expect(onUpdateCheck).not.toHaveBeenCalled()
+    expect(onUpdate).toHaveBeenCalled()
+  })
 })
