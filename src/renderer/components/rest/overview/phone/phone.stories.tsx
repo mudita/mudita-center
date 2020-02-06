6720c84edf9a9aa13e1b367f257dc97c5a44303b
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-02-06 13:50:41 +0100

    Added phone info card

---
+++ src/renderer/components/rest/overview/phone/phone.stories.tsx
@@ -0,0 +1,26 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+import Phone from "Renderer/components/rest/overview/phone/phone.component"
+import { action } from "@storybook/addon-actions"
+import styled from "styled-components"
+
+const Part = styled.div`
+  padding: 2rem;
+  > p {
+    margin-bottom: 2rem;
+  }
+`
+
+storiesOf("Overview|Phone", module).add("Phone", () => {
+  return (
+    <div style={{ maxWidth: "31.5rem" }}>
+      <Part>
+        <Phone
+          onDisconnect={action("disconnect phone")}
+          batteryLevel={0.75}
+          network={"Play"}
+        />
+      </Part>
+    </div>
+  )
+})
---
+++ src/renderer/components/rest/overview/phone/phone.test.tsx
@@ -0,0 +1,49 @@
+import "@testing-library/jest-dom/extend-expect"
+import React from "react"
+import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-intl"
+import { PhoneProps } from "Renderer/components/rest/overview/phone/phone.interface"
+import Phone from "Renderer/components/rest/overview/phone/phone.component"
+import { noop } from "Renderer/utils/noop"
+import { fireEvent } from "@testing-library/dom"
+import { wait } from "@testing-library/react"
+import { intl } from "Renderer/utils/intl"
+
+const renderPhone = ({
+  onDisconnect = noop,
+  batteryLevel = 0.75,
+  network = "Play",
+}: Partial<PhoneProps> = {}) => {
+  const outcome = renderWithThemeAndIntl(
+    <Phone
+      onDisconnect={onDisconnect}
+      batteryLevel={batteryLevel}
+      network={network}
+    />
+  )
+  return {
+    ...outcome,
+    disconnectButton: () => outcome.getByRole("button"),
+  }
+}
+
+test("phone info renders properly", () => {
+  const { disconnectButton, getByText } = renderPhone()
+  expect(getByText("75 %")).toBeInTheDocument()
+  expect(getByText("Play")).toBeInTheDocument()
+  expect(
+    getByText(intl.formatMessage({ id: "view.name.overview.phone.battery" }))
+  ).toBeInTheDocument()
+  expect(disconnectButton()).toBeInTheDocument()
+})
+
+test("disconnect button works properly", async () => {
+  const onDisconnect = jest.fn()
+
+  const { disconnectButton } = renderPhone({ onDisconnect })
+
+  fireEvent.click(disconnectButton())
+
+  await wait(() => {
+    expect(onDisconnect).toHaveBeenCalled()
+  })
+})
