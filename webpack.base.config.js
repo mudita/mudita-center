e69c00d77671065a7e8fcbd0d765af11a939cb1b
Mateusz Kocz <mateusz@kocz.pl>
2020-02-13 13:00:06 +0100

    Fix the images loading in the rednerere config

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -55,11 +55,6 @@ module.exports = {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
         use: "file-loader",
       },
-      // Common Image Formats
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-        use: "url-loader",
-      },
     ],
   },
 
