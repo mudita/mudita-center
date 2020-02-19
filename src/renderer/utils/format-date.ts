669b7aeca6157abd9bb06de85f5c8f18defc1dcf
staszewski <kamilstaszewski95@gmail.com>
2020-02-19 13:29:05 +0100

    PDA-70 Add better abstraction of formatting to MM/DD/YYYY

--- src/renderer/utils/format-date.ts
+++ src/renderer/utils/format-date.ts
@@ -3,13 +3,5 @@ export const formatDateToMMDDYYYY = (standarizedDate?: string) => {
     console.warn("date is undefined")
     return 0
   }
-  const date = new Date(standarizedDate)
-  const year = date.getFullYear()
-  const month = (1 + date.getMonth()).toString().padStart(2, "0")
-  const day = date
-    .getDate()
-    .toString()
-    .padStart(2, "0")
-
-  return month + "." + day + "." + year
+  return new Date(standarizedDate).toLocaleDateString("en-US")
 }
