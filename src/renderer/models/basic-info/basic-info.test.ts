3805d7a673d698b88027c67be6c858c15ecc023f
staszewski <kamilstaszewski95@gmail.com>
2020-02-27 14:29:01 +0100

    PDA-70 Extract common call to seperate file

--- src/renderer/models/basic-info/basic-info.test.ts
+++ src/renderer/models/basic-info/basic-info.test.ts
@@ -4,6 +4,7 @@ import { ipcRenderer } from "electron-better-ipc"
 import { IpcRequest } from "Common/requests/ipc-request.enum"
 import { SimCard } from "Renderer/models/basic-info/interfaces"
 import { DeviceResponseStatus } from "Backend/adapters/device-response.interface"
+import { commonCalls } from "Renderer/models/basic-info/utils/test-helpers"
 
 afterEach(() => {
   for (const property in (ipcRenderer as any).__rendererCalls) {
@@ -30,67 +31,7 @@ test("mock calls update state", async () => {
     models: { basicInfo },
   })
   ;(ipcRenderer as any).__rendererCalls = {
-    [IpcRequest.GetDeviceInfo]: Promise.resolve({
-      name: "Ziemniaczek",
-      modelName: "U12300000",
-      modelNumber: "A1239999",
-      serilaNumber: "a-b-3d",
-      osVersion: "0.123v",
-      osUpdateDate: "12-12-2003",
-    }),
-    [IpcRequest.GetNetworkInfo]: Promise.resolve({
-      simCards: [
-        {
-          active: true,
-          carrier: "AAAAAAAAAAAA",
-          iccid: 1234,
-          imei: 5678,
-          meid: 8765,
-          network: "Y-Mobile",
-          networkLevel: 0.5,
-          number: 12345678,
-          seid: "1234",
-          slot: 1,
-        },
-        {
-          active: false,
-          carrier: "BBBBBBBBBBBB",
-          iccid: 412,
-          imei: 42,
-          meid: 1410,
-          network: "X-Mobile",
-          networkLevel: 0.69,
-          number: 7001234523,
-          seid: "x123",
-          slot: 2,
-        },
-      ],
-    }),
-    [IpcRequest.GetStorageInfo]: Promise.resolve({
-      capacity: 9001,
-      available: 99999999999999,
-      categories: [
-        { label: "music", filesCount: 1233333, size: 999999999 },
-        { label: "storage", filesCount: 100000, size: 999999999 },
-      ],
-    }),
-    [IpcRequest.GetBatteryInfo]: Promise.resolve({
-      level: 9001,
-      charging: false,
-      maximumCapacity: 99999,
-    }),
-    [IpcRequest.GetBackupsInfo]: Promise.resolve({
-      backups: [
-        {
-          createdAt: "20-11-15T07:35:01.562Z20",
-          size: 99999,
-        },
-        {
-          createdAt: "20-01-30T07:35:01.562Z20",
-          size: 1234567,
-        },
-      ],
-    }),
+    ...commonCalls,
   }
 
   await store.dispatch.basicInfo.loadData()
@@ -167,67 +108,7 @@ test("change sim switches active property on sim cards", async () => {
     models: { basicInfo },
   })
   ;(ipcRenderer as any).__rendererCalls = {
-    [IpcRequest.GetDeviceInfo]: Promise.resolve({
-      name: "Ziemniaczek",
-      modelName: "U12300000",
-      modelNumber: "A1239999",
-      serilaNumber: "a-b-3d",
-      osVersion: "0.123v",
-      osUpdateDate: "12-12-2003",
-    }),
-    [IpcRequest.GetNetworkInfo]: Promise.resolve({
-      simCards: [
-        {
-          active: true,
-          carrier: "AAAAAAAAAAAA",
-          iccid: 1234,
-          imei: 5678,
-          meid: 8765,
-          network: "Y-Mobile",
-          networkLevel: 0.5,
-          number: 12345678,
-          seid: "1234",
-          slot: 1,
-        },
-        {
-          active: false,
-          carrier: "BBBBBBBBBBBB",
-          iccid: 412,
-          imei: 42,
-          meid: 1410,
-          network: "X-Mobile",
-          networkLevel: 0.69,
-          number: 7001234523,
-          seid: "x123",
-          slot: 2,
-        },
-      ],
-    }),
-    [IpcRequest.GetStorageInfo]: Promise.resolve({
-      capacity: 9001,
-      available: 99999999999999,
-      categories: [
-        { label: "music", filesCount: 1233333, size: 999999999 },
-        { label: "storage", filesCount: 100000, size: 999999999 },
-      ],
-    }),
-    [IpcRequest.GetBatteryInfo]: Promise.resolve({
-      level: 9001,
-      charging: false,
-      maximumCapacity: 99999,
-    }),
-    [IpcRequest.GetBackupsInfo]: Promise.resolve({
-      backups: [
-        {
-          createdAt: "20-11-15T07:35:01.562Z20",
-          size: 99999,
-        },
-        {
-          createdAt: "20-01-30T07:35:01.562Z20",
-          size: 1234567,
-        },
-      ],
-    }),
+    ...commonCalls,
     [IpcRequest.ChangeSim]: Promise.resolve({
       status: DeviceResponseStatus.Ok,
     }),
