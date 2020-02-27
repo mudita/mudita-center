f512cef0d0083c5cd52a7c7e7741e23346d2eafa
staszewski <kamilstaszewski95@gmail.com>
2020-02-27 10:36:37 +0100

    PDA-70 Update snapshots

--- src/renderer/models/basic-info/basic-info.test.ts
+++ src/renderer/models/basic-info/basic-info.test.ts
@@ -12,19 +12,7 @@ test("store returns initial state", () => {
 
   expect(store.getState()).toMatchInlineSnapshot(`
     Object {
-      "basicInfo": Object {
-        "batteryLevel": 0,
-        "disconnectedDevice": false,
-        "lastBackup": "10.11.2019",
-        "memorySpace": Object {
-          "free": 0,
-          "full": 16000000000,
-        },
-        "networkName": "",
-        "osUpdateDate": "",
-        "osVersion": "1.0",
-        "simCards": Array [],
-      },
+      "basicInfo": Object {},
     }
   `)
 })
@@ -100,47 +88,45 @@ test("mock calls update state", async () => {
   await store.dispatch.basicInfo.loadData()
 
   expect(store.getState()).toMatchInlineSnapshot(`
-Object {
-  "basicInfo": Object {
-    "batteryLevel": 9001,
-    "disconnectedDevice": false,
-    "lastBackup": "20-01-30T07:35:01.562Z20",
-    "memorySpace": Object {
-      "free": 99999999999999,
-      "full": 9001,
-    },
-    "networkName": "",
-    "osUpdateDate": "12-12-2003",
-    "osVersion": "0.123v",
-    "simCards": Array [
-      Object {
-        "active": true,
-        "carrier": "AAAAAAAAAAAA",
-        "iccid": 1234,
-        "imei": 5678,
-        "meid": 8765,
-        "network": "Y-Mobile",
-        "networkLevel": 0.5,
-        "number": 12345678,
-        "seid": "1234",
-        "slot": 1,
+    Object {
+      "basicInfo": Object {
+        "batteryLevel": 9001,
+        "lastBackup": "20-01-30T07:35:01.562Z20",
+        "memorySpace": Object {
+          "free": 99999999999999,
+          "full": 9001,
+        },
+        "osUpdateDate": "12-12-2003",
+        "osVersion": "0.123v",
+        "simCards": Array [
+          Object {
+            "active": true,
+            "carrier": "AAAAAAAAAAAA",
+            "iccid": 1234,
+            "imei": 5678,
+            "meid": 8765,
+            "network": "Y-Mobile",
+            "networkLevel": 0.5,
+            "number": 12345678,
+            "seid": "1234",
+            "slot": 1,
+          },
+          Object {
+            "active": false,
+            "carrier": "BBBBBBBBBBBB",
+            "iccid": 412,
+            "imei": 42,
+            "meid": 1410,
+            "network": "X-Mobile",
+            "networkLevel": 0.69,
+            "number": 7001234523,
+            "seid": "x123",
+            "slot": 2,
+          },
+        ],
       },
-      Object {
-        "active": false,
-        "carrier": "BBBBBBBBBBBB",
-        "iccid": 412,
-        "imei": 42,
-        "meid": 1410,
-        "network": "X-Mobile",
-        "networkLevel": 0.69,
-        "number": 7001234523,
-        "seid": "x123",
-        "slot": 2,
-      },
-    ],
-  },
-}
-`)
+    }
+  `)
 })
 
 test("disconnect returns true and updates state", async () => {
@@ -158,22 +144,12 @@ test("disconnect returns true and updates state", async () => {
   const state = store.getState()
 
   expect(state).toMatchInlineSnapshot(`
-Object {
-  "basicInfo": Object {
-    "batteryLevel": 0,
-    "disconnectedDevice": true,
-    "lastBackup": "10.11.2019",
-    "memorySpace": Object {
-      "free": 0,
-      "full": 16000000000,
-    },
-    "networkName": "",
-    "osUpdateDate": "",
-    "osVersion": "1.0",
-    "simCards": Array [],
-  },
-}
-`)
+    Object {
+      "basicInfo": Object {
+        "disconnectedDevice": true,
+      },
+    }
+  `)
 })
 
 test("change sim switches active property on sim cards", async () => {
@@ -257,45 +233,43 @@ test("change sim switches active property on sim cards", async () => {
   await store.dispatch.basicInfo.changeSim(simCard)
 
   expect(store.getState()).toMatchInlineSnapshot(`
-Object {
-  "basicInfo": Object {
-    "batteryLevel": 9001,
-    "disconnectedDevice": false,
-    "lastBackup": "20-01-30T07:35:01.562Z20",
-    "memorySpace": Object {
-      "free": 99999999999999,
-      "full": 9001,
-    },
-    "networkName": "",
-    "osUpdateDate": "12-12-2003",
-    "osVersion": "0.123v",
-    "simCards": Array [
-      Object {
-        "active": false,
-        "carrier": "AAAAAAAAAAAA",
-        "iccid": 1234,
-        "imei": 5678,
-        "meid": 8765,
-        "network": "Y-Mobile",
-        "networkLevel": 0.5,
-        "number": 12345678,
-        "seid": "1234",
-        "slot": 1,
+    Object {
+      "basicInfo": Object {
+        "batteryLevel": 9001,
+        "lastBackup": "20-01-30T07:35:01.562Z20",
+        "memorySpace": Object {
+          "free": 99999999999999,
+          "full": 9001,
+        },
+        "osUpdateDate": "12-12-2003",
+        "osVersion": "0.123v",
+        "simCards": Array [
+          Object {
+            "active": false,
+            "carrier": "AAAAAAAAAAAA",
+            "iccid": 1234,
+            "imei": 5678,
+            "meid": 8765,
+            "network": "Y-Mobile",
+            "networkLevel": 0.5,
+            "number": 12345678,
+            "seid": "1234",
+            "slot": 1,
+          },
+          Object {
+            "active": true,
+            "carrier": "BBBBBBBBBBBB",
+            "iccid": 412,
+            "imei": 42,
+            "meid": 1410,
+            "network": "X-Mobile",
+            "networkLevel": 0.69,
+            "number": 7001234523,
+            "seid": "x123",
+            "slot": 2,
+          },
+        ],
       },
-      Object {
-        "active": true,
-        "carrier": "BBBBBBBBBBBB",
-        "iccid": 412,
-        "imei": 42,
-        "meid": 1410,
-        "network": "X-Mobile",
-        "networkLevel": 0.69,
-        "number": 7001234523,
-        "seid": "x123",
-        "slot": 2,
-      },
-    ],
-  },
-}
-`)
+    }
+  `)
 })
