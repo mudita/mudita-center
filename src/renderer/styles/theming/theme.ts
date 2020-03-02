755bed63d13bc32bd3c30025845729db5df6d02b
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-03-02 19:43:16 +0100

    Improved modals styling

--- src/renderer/styles/theming/theme.ts
+++ src/renderer/styles/theming/theme.ts
@@ -130,6 +130,12 @@ const theme = {
     viewWidth: columnWidth * 12 + gutterWidth * 13,
     menuWidth: columnWidth * 3 + gutterWidth * 3,
   },
+  zIndex: {
+    content: 1,
+    dropdown: 2,
+    modalBackdrop: 3,
+    modal: 4,
+  },
 }
 
 export type Theme = typeof theme
