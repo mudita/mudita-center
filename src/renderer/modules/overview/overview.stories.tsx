66dc62163c2de82e5bf0a0c545f11fdf49810b0b
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-03-02 19:42:46 +0100

    Added overview's modals stories

--- src/renderer/modules/overview/overview.stories.tsx
+++ src/renderer/modules/overview/overview.stories.tsx
@@ -1,13 +1,96 @@
 import { storiesOf } from "@storybook/react"
 import React from "react"
-import Overview from "Renderer/modules/overview/overview.container"
 import { Provider } from "react-redux"
 import store from "Renderer/store"
+import OverviewUI from "Renderer/modules/overview/overview-ui.component"
+import {
+  CheckingUpdatesModal,
+  DownloadingUpdateCancelledModal,
+  DownloadingUpdateFinishedModal,
+  DownloadingUpdateInterruptedModal,
+  DownloadingUpdateModal,
+  UpdateAvailable,
+  UpdateNotAvailable,
+  UpdateServerError,
+} from "Renderer/modules/overview/overview.modals"
+import {
+  ModalBackdrop,
+  ModalWrapper,
+} from "Renderer/components/core/modal/modal.styled.elements"
+import FunctionComponent from "Renderer/types/function-component.interface"
 
+/**
+ * Please do not review.
+ * This is going to be merged with https://github.com/Appnroll/pure-desktop-app/pull/75
+ */
 storiesOf("Views|Overview", module).add("Overview", () => (
   <div style={{ maxWidth: "63rem" }}>
     <Provider store={store}>
-      <Overview />
+      <OverviewUI />
     </Provider>
   </div>
 ))
+
+/**
+ * This can be reviewed.
+ */
+const ModalStory: FunctionComponent = ({ children }) => (
+  <div style={{ maxWidth: "63rem" }}>
+    <Provider store={store}>
+      <OverviewUI />
+    </Provider>
+    <ModalWrapper>{children}</ModalWrapper>
+    <ModalBackdrop />
+  </div>
+)
+storiesOf("Views|Overview/Modals", module)
+  .add("Checking for updates", () => (
+    <ModalStory>
+      <CheckingUpdatesModal />
+    </ModalStory>
+  ))
+  .add("Update is available", () => (
+    <ModalStory>
+      <UpdateAvailable />
+    </ModalStory>
+  ))
+  .add("Update is not available", () => (
+    <ModalStory>
+      <UpdateNotAvailable />
+    </ModalStory>
+  ))
+  .add("Update error", () => (
+    <ModalStory>
+      <UpdateServerError />
+    </ModalStory>
+  ))
+  .add("Starting download", () => (
+    <ModalStory>
+      <DownloadingUpdateModal />
+    </ModalStory>
+  ))
+  .add("Downloading", () => (
+    <ModalStory>
+      <DownloadingUpdateModal timeLeft={30} speed={5678} />
+    </ModalStory>
+  ))
+  .add("Finishing download", () => (
+    <ModalStory>
+      <DownloadingUpdateModal timeLeft={0} speed={5678} />
+    </ModalStory>
+  ))
+  .add("Downloading finished", () => (
+    <ModalStory>
+      <DownloadingUpdateFinishedModal />
+    </ModalStory>
+  ))
+  .add("Downloading cancelled", () => (
+    <ModalStory>
+      <DownloadingUpdateCancelledModal />
+    </ModalStory>
+  ))
+  .add("Downloading interrupted", () => (
+    <ModalStory>
+      <DownloadingUpdateInterruptedModal />
+    </ModalStory>
+  ))
