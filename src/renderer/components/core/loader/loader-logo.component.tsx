5263472b828c19e9776862fa037b44d01e5cb5d1
staszewski <kamilstaszewski95@gmail.com>
2020-03-05 10:31:52 +0100

    PDA-104 Add transparent loader-logo, remove old one

--- src/renderer/components/core/loader/loader-logo.component.tsx
+++ src/renderer/components/core/loader/loader-logo.component.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
 import { ImgHTMLAttributes } from "react"
 import Image from "Renderer/components/core/image/image.component"
-import Gif from "Renderer/images/loader.gif"
+import Gif from "Renderer/images/loader-transparent.gif"
 
 const LoaderLogo: FunctionComponent<ImgHTMLAttributes<HTMLImageElement>> = ({
   ...rest
