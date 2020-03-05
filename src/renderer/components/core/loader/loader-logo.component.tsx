f1439cdd21678f608026a2ed47930b252fb94fe6
staszewski <kamilstaszewski95@gmail.com>
2020-03-05 09:20:34 +0100

    PDA-104 Add predefined values of height and width to loader-logo

--- src/renderer/components/core/loader/loader-logo.component.tsx
+++ src/renderer/components/core/loader/loader-logo.component.tsx
@@ -5,7 +5,17 @@ import Image from "Renderer/components/core/image/image.component"
 import Gif from "Renderer/images/loader.gif"
 
 const LoaderLogo: FunctionComponent<ImgHTMLAttributes<HTMLImageElement>> = ({
+  height = 300,
+  width = 300,
   ...rest
-}) => <Image data-testid="loader-logo" src={Gif} {...rest} />
+}) => (
+  <Image
+    data-testid="loader-logo"
+    src={Gif}
+    height={height}
+    width={width}
+    {...rest}
+  />
+)
 
 export default LoaderLogo
