cc9531aa3989e59d654aded1e949dcbdf199f903
staszewski <kamilstaszewski95@gmail.com>
2020-03-09 13:05:47 +0100

    PDA-114 Add select to story

--- src/renderer/components/core/icon/interactive-icon.stories.tsx
+++ src/renderer/components/core/icon/interactive-icon.stories.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 import InteractiveIcon from "Renderer/components/core/icon/interactive-icon.component"
 import styled from "styled-components"
 import { InteractiveIconType } from "Renderer/components/core/icon/interactive-icon.config"
+import { select, withKnobs } from "@storybook/addon-knobs"
 
 const Container = styled.div`
   height: 100vh;
@@ -11,13 +12,28 @@ const Container = styled.div`
   align-items: center;
 `
 
-storiesOf("Components|Interactive Icon", module).add("Range", () => {
-  return (
-    <Container>
-      <InteractiveIcon
-        iconState={1}
-        interactiveIconType={InteractiveIconType.Range}
-      />
-    </Container>
-  )
-})
+storiesOf("Components|Interactive Icon", module)
+  .addDecorator(withKnobs)
+  .add("Range", () => {
+    const label = "Interactive Range"
+    const options = {
+      noRange: 0,
+      veryLowRange: 1,
+      lowRange: 10,
+      mediumRange: 40,
+      highRange: 60,
+      veryHighRange: 90,
+    }
+
+    const defaultValue = 0
+
+    const value = select(label, options, defaultValue)
+    return (
+      <Container>
+        <InteractiveIcon
+          iconState={value}
+          interactiveIconType={InteractiveIconType.Range}
+        />
+      </Container>
+    )
+  })
