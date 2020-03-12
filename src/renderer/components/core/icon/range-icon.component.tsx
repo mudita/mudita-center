c5a0961b5a7deba60416bb4b5285dfe08baecdcc
staszewski <kamilstaszewski95@gmail.com>
2020-03-12 09:25:02 +0100

    PDA-114 Remove config. Keep config inside component

--- src/renderer/components/core/icon/range-icon.component.tsx
+++ src/renderer/components/core/icon/range-icon.component.tsx
@@ -1,10 +1,35 @@
 import * as React from "react"
-import { Props as IconProps } from "Renderer/components/core/icon/icon.component"
+import Icon, {
+  Props as IconProps,
+} from "Renderer/components/core/icon/icon.component"
 import FunctionComponent from "Renderer/types/function-component.interface"
-import {
-  getInteractiveRangeIcon,
-  InteractiveIconProps,
-} from "Renderer/components/core/icon/range-icon.config"
+import { Type } from "Renderer/components/core/icon/icon.config"
+
+interface InteractiveIconProps {
+  strenght: number
+}
+
+const getInteractiveRangeIcon = (
+  strength: number,
+  rest: Partial<IconProps>
+) => {
+  switch (true) {
+    case strength === 0:
+      return <Icon type={Type.NoRange} {...rest} />
+    case strength > 0 && strength <= 20:
+      return <Icon type={Type.VeryLowRange} {...rest} />
+    case strength > 20 && strength <= 40:
+      return <Icon type={Type.LowRange} {...rest} />
+    case strength > 40 && strength <= 60:
+      return <Icon type={Type.MediumRange} {...rest} />
+    case strength > 60 && strength <= 80:
+      return <Icon type={Type.HighRange} {...rest} />
+    case strength > 80 && strength <= 100:
+      return <Icon type={Type.VeryHighRange} {...rest} />
+    default:
+      return
+  }
+}
 
 const RangeIcon: FunctionComponent<InteractiveIconProps & IconProps> = ({
   strenght,
