c1085ae77e29515d8d5aebce350fe89539cd7997
staszewski <kamilstaszewski95@gmail.com>
2020-03-12 09:41:58 +0100

    PDA-114 Add default case. Revert order of statement in switch

--- src/renderer/components/core/icon/range-icon.component.tsx
+++ src/renderer/components/core/icon/range-icon.component.tsx
@@ -14,20 +14,20 @@ const getInteractiveRangeIcon = (
   rest: Partial<IconProps>
 ) => {
   switch (true) {
+    case strength > 80:
+      return <Icon type={Type.VeryHighRange} {...rest} />
+    case strength > 60:
+      return <Icon type={Type.HighRange} {...rest} />
+    case strength > 40:
+      return <Icon type={Type.MediumRange} {...rest} />
+    case strength > 20:
+      return <Icon type={Type.LowRange} {...rest} />
+    case strength > 0:
+      return <Icon type={Type.VeryLowRange} {...rest} />
     case strength === 0:
       return <Icon type={Type.NoRange} {...rest} />
-    case strength > 0 && strength <= 20:
-      return <Icon type={Type.VeryLowRange} {...rest} />
-    case strength > 20 && strength <= 40:
-      return <Icon type={Type.LowRange} {...rest} />
-    case strength > 40 && strength <= 60:
-      return <Icon type={Type.MediumRange} {...rest} />
-    case strength > 60 && strength <= 80:
-      return <Icon type={Type.HighRange} {...rest} />
-    case strength > 80 && strength <= 100:
-      return <Icon type={Type.VeryHighRange} {...rest} />
     default:
-      return
+      return <Icon type={Type.NoRange} {...rest} />
   }
 }
 
