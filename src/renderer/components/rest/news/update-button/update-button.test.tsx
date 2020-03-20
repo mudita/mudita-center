cdada838edf8771b9fa41ac7524e4644208b2927
staszewski <kamilstaszewski95@gmail.com>
2020-03-20 15:01:21 +0100

    PDA-146 Remove wait from test

--- src/renderer/components/rest/news/update-button/update-button.test.tsx
+++ src/renderer/components/rest/news/update-button/update-button.test.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom"
 import React from "react"
 import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-intl"
 import UpdateButtonComponent from "Renderer/components/rest/news/update-button/update-button.component"
-import { fireEvent, wait } from "@testing-library/react"
+import { fireEvent } from "@testing-library/react"
 
 test("calls function passed through props", async () => {
   const onUpdating = jest.fn()
@@ -15,8 +15,6 @@ test("calls function passed through props", async () => {
 
   fireEvent.click(button)
 
-  await wait(() => {
-    expect(onUpdating).toHaveBeenCalled()
-    expect(onUpdating).toHaveBeenCalledTimes(1)
-  })
+  expect(onUpdating).toHaveBeenCalled()
+  expect(onUpdating).toHaveBeenCalledTimes(1)
 })
