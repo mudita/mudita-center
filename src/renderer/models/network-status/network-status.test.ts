04cee65b90560bb08a18aa91c8ead83ac4c83c87
staszewski <kamilstaszewski95@gmail.com>
2020-03-27 09:34:20 +0100

    PDA-152 Mock window.navigator.onLine

--- src/renderer/models/network-status/network-status.test.ts
+++ src/renderer/models/network-status/network-status.test.ts
@@ -1,7 +1,27 @@
 import networkStatus from "Renderer/models/network-status/network-status"
 import { init } from "@rematch/core"
 
-test("effect is dispatched and store is updated", async () => {
+test("effect is dispatched and store is updated - false return variant", async () => {
+  const onLine = jest.spyOn(window.navigator, "onLine", "get")
+  onLine.mockReturnValue(false)
+
+  const store = init({
+    models: { networkStatus },
+  })
+
+  await store.dispatch.networkStatus.updateOnlineStatus()
+
+  expect(store.getState()).toEqual({
+    networkStatus: {
+      online: false,
+    },
+  })
+})
+
+test("effect is dispatched and store is updated - true return variant", async () => {
+  const onLine = jest.spyOn(window.navigator, "onLine", "get")
+  onLine.mockReturnValue(true)
+
   const store = init({
     models: { networkStatus },
   })
