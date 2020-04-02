14ddce721aba45bf14090af6d1bb1f4538b9a4d0
staszewski <kamilstaszewski95@gmail.com>
2020-04-02 11:38:37 +0200

    PDA-149 Remove create-response

--- src/renderer/utils/create-response.ts
+++
@@ -1,9 +0,0 @@
-const createResponse = (statusCode: number, statusMessage: string) => ({
-  body: JSON.stringify({
-    message: statusMessage,
-    status: statusCode,
-  }),
-  statusCode,
-})
-
-export default createResponse
