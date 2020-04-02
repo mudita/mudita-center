7907f795b8bafb192f705106d54b33ebe33f4a81
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-04-02 15:00:18 +0200

    Contact details fix

--- src/renderer/modules/phone/components/contact-details.component.tsx
+++ src/renderer/modules/phone/components/contact-details.component.tsx
@@ -102,7 +102,6 @@ const AdditionalInfoItem = styled.div`
 const Input = styled(InputComponent).attrs(() => ({
   disabled: true,
 }))<InputComponentProps>`
-  width: 100%;
   background-color: transparent;
   padding: 2.4rem 0 1.6rem 0;
 `
