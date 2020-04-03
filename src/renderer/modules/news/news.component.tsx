610cb7dd616536287b9dfa1f466f7b7b4ebf7881
staszewski <kamilstaszewski95@gmail.com>
2020-04-03 13:13:58 +0200

    PDA-149 UChange files names

--- src/renderer/modules/news/news.component.tsx
+++ src/renderer/modules/news/news.component.tsx
@@ -1,33 +1,18 @@
-import React from "react"
-import FunctionComponent from "Renderer/types/function-component.interface"
-import {
-  IdItem,
-  NewsEntry,
-} from "Renderer/models/mudita-news/mudita-news.interface"
-import { noop } from "Renderer/utils/noop"
-import Cards from "Renderer/components/rest/news/cards/cards.component"
+import { connect } from "react-redux"
+import { RootModel } from "Renderer/models/models"
+import { select } from "Renderer/store"
+import News from "Renderer/modules/news/news-ui.component"
 
-interface Props {
-  newsItems: Record<string, NewsEntry>
-  sortedIds: IdItem[]
-  commentsCount: Record<string, number>
-  loadData?: () => void
+const selection = select((models: any) => ({
+  sortedIds: models.muditaNews.sortedIds,
+}))
+
+const mapStateToProps = (state: RootModel) => {
+  return { ...state.muditaNews, ...selection(state, null) }
 }
 
-const News: FunctionComponent<Props> = ({
-  newsItems,
-  commentsCount,
-  loadData = noop,
-  sortedIds,
-}) => (
-  <div>
-    <Cards
-      newsItems={newsItems}
-      commentsCount={commentsCount}
-      loadData={loadData}
-      sortedIds={sortedIds}
-    />
-  </div>
-)
+const mapDispatchToProps = (dispatch: any) => ({
+  loadData: () => dispatch.muditaNews.loadData(),
+})
 
-export default News
+export default connect(mapStateToProps, mapDispatchToProps)(News)
