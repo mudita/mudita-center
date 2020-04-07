5f1962b5a0787324afe2fbee818328db2723e7d7
staszewski <kamilstaszewski95@gmail.com>
2020-04-07 17:23:04 +0200

    PDA-150 Correct ccalling and converting to base64

--- src/renderer/models/mudita-news/get-base-64.ts
+++ src/renderer/models/mudita-news/get-base-64.ts
@@ -3,14 +3,14 @@ import axios from "axios"
 export const getBase64 = async (url?: string) => {
   try {
     return await axios
-      .get(url, {
+      .get("https:" + url, {
         responseType: "arraybuffer",
-        proxy: {
-          host: "localhost",
-          port: 2003,
-        },
       })
-      .then(response => Buffer.from(response.data, "binary").toString("base64"))
+      .then(
+        response =>
+          "data:image;base64," +
+          Buffer.from(response.data, "binary").toString("base64")
+      )
   } catch (e) {
     console.log(e)
   }
--- src/renderer/models/mudita-news/mudita-news.ts
+++ src/renderer/models/mudita-news/mudita-news.ts
@@ -36,36 +36,6 @@ export default {
     async loadData() {
       const data = await initNews()
       dispatch.muditaNews.update(data)
-      const {
-        data: { items, includes },
-      } = await axios.get(
-        `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries/?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=newsItem`
-      )
-      const news = items.map(({ fields, sys }: Entry<NewsEntry>) => {
-        return {
-          ...fields,
-          newsId: sys.id,
-          updatedAt: sys.updatedAt,
-          createdAt: sys.createdAt,
-          imageId: fields?.image?.sys?.id,
-        }
-      })
-      for (const item of news) {
-        const {
-          fields: {
-            title,
-            file: { url },
-          },
-        } = includes.Asset.find((asset: Asset) => {
-          return item?.image?.sys?.id === asset.sys.id
-        })
-        item.imageSource = await getBase64(url)
-        item.imageAlt = title
-      }
-      // const imageUrls = news.map(item => item.imageSource)
-      // const imageCalls = imageUrls.map(url => getBase64(url))
-      // const base64Urls = await Promise.all(imageCalls)
-      console.log(news)
     },
     async loadOfflineData() {
       const defaultNews: DefaultNewsItems = await getNews()
