8158d50916f2f768c73fe092f822d74de8fe9411
staszewski <kamilstaszewski95@gmail.com>
2020-04-20 11:36:57 +0200

    PDA-160 Extract UI to stateless component

---
+++ src/renderer/modules/settings/settings-ui.component.tsx
@@ -0,0 +1,128 @@
+import React from "react"
+import FunctionComponent from "Renderer/types/function-component.interface"
+import {
+  ActionsWrapper,
+  Name,
+  TableRow,
+} from "Renderer/components/rest/messages/topics-table.component"
+import Text, {
+  TextDisplayStyle,
+} from "Renderer/components/core/text/text.component"
+import styled from "styled-components"
+import ButtonToggler, {
+  ButtonTogglerItem,
+} from "Renderer/components/core/button-toggler/button-toggler.component"
+import { FormattedMessage } from "react-intl"
+import { borderColor } from "Renderer/styles/theming/theme-getters"
+import { ToggleState } from "Renderer/modules/settings/settings.component"
+
+const SettingsTableRow = styled(TableRow)`
+  grid-template-areas: "Checkbox Checkbox Actions Actions";
+  grid-template-columns: 11rem 1fr 15rem;
+  border-bottom: solid 0.2rem ${borderColor("listItem")};
+`
+
+const Data = styled.div`
+  grid-area: Checkbox;
+  align-self: center;
+`
+
+const SettingsLabel = styled(Name)`
+  margin-left: 4rem;
+`
+
+const SettingsToggler = styled(ButtonToggler)`
+  margin-right: 4rem;
+`
+
+const SettingsTogglerItem = styled(ButtonTogglerItem)`
+  padding: 0 3.6rem;
+  width: 75%;
+`
+
+const SettingsDescriptionWrapper = styled.div`
+  border-bottom: solid 0.2rem ${borderColor("listItem")};
+`
+
+const SettingsDescription = styled(Text)`
+  margin-left: 4rem;
+  margin-bottom: 3.2rem;
+`
+
+const SettingsWrapper = styled.section`
+  padding-top: 3.2rem;
+`
+
+interface Props {
+  autostartStatus?: string
+  tetheringStatus?: string
+  setAutostartStatus: (label: string) => void
+  setTetheringStatus: (label: string) => void
+  togglerState: ToggleState[]
+}
+
+const SettingsUI: FunctionComponent<Props> = ({
+  autostartStatus,
+  tetheringStatus,
+  setAutostartStatus,
+  setTetheringStatus,
+  togglerState,
+}) => {
+  return (
+    <SettingsWrapper>
+      <SettingsDescriptionWrapper>
+        <SettingsDescription
+          displayStyle={TextDisplayStyle.MediumFadedLightText}
+        >
+          <FormattedMessage id="view.name.settings.backup.description" />
+        </SettingsDescription>
+      </SettingsDescriptionWrapper>
+      <SettingsTableRow checkMode={false}>
+        <Data>
+          <SettingsLabel displayStyle={TextDisplayStyle.LargeText}>
+            <FormattedMessage id="view.name.settings.backup.autostartLabel" />
+          </SettingsLabel>
+        </Data>
+        <ActionsWrapper>
+          <SettingsToggler filled>
+            {togglerState.map((label, index) => {
+              const changeStatus = () => setAutostartStatus(label)
+              return (
+                <SettingsTogglerItem
+                  key={index}
+                  label={label}
+                  onClick={changeStatus}
+                  active={autostartStatus === label}
+                />
+              )
+            })}
+          </SettingsToggler>
+        </ActionsWrapper>
+      </SettingsTableRow>
+      <SettingsTableRow checkMode={false}>
+        <Data>
+          <SettingsLabel displayStyle={TextDisplayStyle.LargeText}>
+            <FormattedMessage id="view.name.settings.backup.tetheringLabel" />
+          </SettingsLabel>
+        </Data>
+        <ActionsWrapper>
+          <SettingsToggler filled>
+            {togglerState.map((label, index) => {
+              const changeStatus = () => setTetheringStatus(label)
+              return (
+                <SettingsTogglerItem
+                  key={index}
+                  label={label}
+                  onClick={changeStatus}
+                  active={tetheringStatus === label}
+                />
+              )
+            })}
+          </SettingsToggler>
+        </ActionsWrapper>
+      </SettingsTableRow>
+    </SettingsWrapper>
+  )
+}
+
+export default SettingsUI
--- src/renderer/modules/settings/settings.component.tsx
+++ src/renderer/modules/settings/settings.component.tsx
@@ -1,117 +1,29 @@
 import React, { useState } from "react"
-
 import FunctionComponent from "Renderer/types/function-component.interface"
-import {
-  ActionsWrapper,
-  Name,
-  TableRow,
-} from "Renderer/components/rest/messages/topics-table.component"
-import Text, {
-  TextDisplayStyle,
-} from "Renderer/components/core/text/text.component"
-import styled from "styled-components"
-import ButtonToggler, {
-  ButtonTogglerItem,
-} from "Renderer/components/core/button-toggler/button-toggler.component"
-import { FormattedMessage } from "react-intl"
-import { borderColor } from "Renderer/styles/theming/theme-getters"
+import SettingsUI from "Renderer/modules/settings/settings-ui.component"
 
-const SettingsTableRow = styled(TableRow)`
-  grid-template-areas: "Checkbox Checkbox Actions Actions";
-  grid-template-columns: 11rem 1fr 15rem;
-  border-bottom: solid 0.2rem ${borderColor("listItem")};
-`
+export enum ToggleState {
+  On = "On",
+  Off = "Off",
+}
 
-const Data = styled.div`
-  grid-area: Checkbox;
-  align-self: center;
-`
-
-const SettingsLabel = styled(Name)`
-  margin-left: 4rem;
-`
-
-const SettingsToggler = styled(ButtonToggler)`
-  margin-right: 4rem;
-`
-
-const SettingsTogglerItem = styled(ButtonTogglerItem)`
-  padding: 0 3.6rem;
-  width: 75%;
-`
-
-const SettingsDescriptionWrapper = styled.div`
-  border-bottom: solid 0.2rem ${borderColor("listItem")};
-`
-
-const SettingsDescription = styled(Text)`
-  margin-left: 4rem;
-  margin-bottom: 3.2rem;
-`
-
-const SettingsWrapper = styled.section`
-  padding-top: 3.2rem;
-`
-
-const twoStateToggler = ["Off", "On"]
+const twoStateToggler = [ToggleState.Off, ToggleState.On]
 
 const Settings: FunctionComponent = () => {
-  const [autostartStatus, setAutostartStatus] = useState(twoStateToggler[0])
-  const [tetheringStatus, setTetheringStatus] = useState(twoStateToggler[0])
+  const [autostartStatus, setAutostartStatus] = useState<string>(
+    twoStateToggler[0]
+  )
+  const [tetheringStatus, setTetheringStatus] = useState<string>(
+    twoStateToggler[0]
+  )
   return (
-    <SettingsWrapper>
-      <SettingsDescriptionWrapper>
-        <SettingsDescription
-          displayStyle={TextDisplayStyle.MediumFadedLightText}
-        >
-          <FormattedMessage id="view.name.settings.backup.description" />
-        </SettingsDescription>
-      </SettingsDescriptionWrapper>
-      <SettingsTableRow checkMode={false}>
-        <Data>
-          <SettingsLabel displayStyle={TextDisplayStyle.LargeText}>
-            <FormattedMessage id="view.name.settings.backup.autostartLabel" />
-          </SettingsLabel>
-        </Data>
-        <ActionsWrapper>
-          <SettingsToggler filled>
-            {twoStateToggler.map((label, index) => {
-              const changeStatus = () => setAutostartStatus(label)
-              return (
-                <SettingsTogglerItem
-                  key={index}
-                  label={label}
-                  onClick={changeStatus}
-                  active={autostartStatus === label}
-                />
-              )
-            })}
-          </SettingsToggler>
-        </ActionsWrapper>
-      </SettingsTableRow>
-      <SettingsTableRow checkMode={false}>
-        <Data>
-          <SettingsLabel displayStyle={TextDisplayStyle.LargeText}>
-            <FormattedMessage id="view.name.settings.backup.tetheringLabel" />
-          </SettingsLabel>
-        </Data>
-        <ActionsWrapper>
-          <SettingsToggler filled>
-            {twoStateToggler.map((label, index) => {
-              const changeStatus = () => setTetheringStatus(label)
-              return (
-                <SettingsTogglerItem
-                  key={index}
-                  label={label}
-                  onClick={changeStatus}
-                  active={tetheringStatus === label}
-                />
-              )
-            })}
-          </SettingsToggler>
-        </ActionsWrapper>
-      </SettingsTableRow>
-    </SettingsWrapper>
+    <SettingsUI
+      autostartStatus={autostartStatus}
+      tetheringStatus={tetheringStatus}
+      setAutostartStatus={setAutostartStatus}
+      setTetheringStatus={setTetheringStatus}
+      togglerState={twoStateToggler}
+    />
   )
 }
 
