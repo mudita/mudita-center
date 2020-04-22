db320928d28fd9c7caaaf7ffe09255c0519b0a55
staszewski <kamilstaszewski95@gmail.com>
2020-04-22 10:06:06 +0200

    PDA-166 Component composition

--- src/renderer/modules/settings/tabs/backup.component.tsx
+++ src/renderer/modules/settings/tabs/backup.component.tsx
@@ -14,90 +14,102 @@ import Location from "Renderer/components/core/location/location.component"
 import ButtonComponent from "Renderer/components/core/button/button.component"
 import { DisplayStyle } from "Renderer/components/core/button/button.config"
 import { LocationPath } from "Renderer/components/core/location/location.enum"
+import styled from "styled-components"
+import {
+  ActionsWrapper,
+  DataWrapper,
+  Message,
+  Name,
+  TableRow,
+} from "Renderer/components/rest/messages/topics-table.component"
+import { borderColor } from "Renderer/styles/theming/theme-getters"
+import ButtonToggler, {
+  ButtonTogglerItem,
+} from "Renderer/components/core/button-toggler/button-toggler.component"
+import Text, {
+  TextDisplayStyle,
+} from "Renderer/components/core/text/text.component"
+import { FormattedMessage } from "react-intl"
 
+const BackupTableRow = styled(TableRow)`
+  grid-template-areas: "Checkbox Actions";
+  grid-template-columns: 1fr 15rem;
+  border-bottom: solid 0.2rem ${borderColor("listItem")};
+`
+
+const BackupDescriptionWrapper = styled.div`
+  border-bottom: solid 0.2rem ${borderColor("listItem")};
+`
+
+const BackupDescription = styled(Text)`
+  margin-left: 4rem;
+  margin-bottom: 3.2rem;
+`
+
+const BackupWrapper = styled.section`
+  padding-top: 3.2rem;
+`
+
+const BackupDataWrapper = styled(DataWrapper)`
+  margin-left: 4rem;
+`
 // DO NOT REVIEW THIS CODE, IT'S FOR TESTING PURPOSES ONLY
 const Backup: FunctionComponent = () => {
-  const [settings, setSettings] = useState<AppSettings>()
-  const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
-
-  useEffect(() => {
-    ;(async () => {
-      setSettings(await getAppSettings())
-    })()
-  }, [ignored])
-
-  const toggleAutostart = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (settings) {
-      setSettings({ ...settings, appAutostart: event.target.checked })
-    }
-  }
-
-  const changeBackupLocation = (event: ChangeEvent<HTMLInputElement>) => {
-    if (settings) {
-      setSettings({ ...settings, pureOsBackupLocation: event.target.value })
-    }
-  }
-
-  const changeDownloadLocation = (event: ChangeEvent<HTMLInputElement>) => {
-    if (settings) {
-      setSettings({ ...settings, pureOsDownloadLocation: event.target.value })
-    }
-  }
-
-  const reset = async () => {
-    await resetAppSettings()
-    forceUpdate()
-  }
+  // const [settings, setSettings] = useState<AppSettings>()
+  // const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
+  //
+  // useEffect(() => {
+  //   ;(async () => {
+  //     setSettings(await getAppSettings())
+  //   })()
+  // }, [ignored])
+  //
+  // const toggleAutostart = async (event: ChangeEvent<HTMLInputElement>) => {
+  //   if (settings) {
+  //     setSettings({ ...settings, appAutostart: event.target.checked })
+  //   }
+  // }
+  //
+  // const changeBackupLocation = (event: ChangeEvent<HTMLInputElement>) => {
+  //   if (settings) {
+  //     setSettings({ ...settings, pureOsBackupLocation: event.target.value })
+  //   }
+  // }
+  //
+  // const changeDownloadLocation = (event: ChangeEvent<HTMLInputElement>) => {
+  //   if (settings) {
+  //     setSettings({ ...settings, pureOsDownloadLocation: event.target.value })
+  //   }
+  // }
+  //
+  // const reset = async () => {
+  //   await resetAppSettings()
+  //   forceUpdate()
+  // }
 
   return (
-    <div>
-      Settings
-      <br />
-      <Link to={URL_ONBOARDING.root}>Go to onboarding</Link>
-      <br />
-      <hr />
-      <br />
-      {settings && (
-        <div>
-          <InputCheckbox
-            label={"Autostart"}
-            checked={settings?.appAutostart}
-            onChange={toggleAutostart}
-          />
-          <br />
-          <div>
-            <InputText
-              placeholder={"Pure OS backup location"}
-              size={120}
-              type={"text"}
-              value={settings?.pureOsBackupLocation}
-              onChange={changeBackupLocation}
-            />
-            <br />
-            <InputText
-              placeholder={"Pure OS download location"}
-              size={120}
-              type={"text"}
-              value={settings?.pureOsDownloadLocation}
-              onChange={changeDownloadLocation}
-            />
-            <br />
-            <Location locationToUpdate={LocationPath.PureOsBackup}>
-              <ButtonComponent label={"Change Backup Location"} />
-            </Location>
-            <Location locationToUpdate={LocationPath.PureOsDownload}>
-              <ButtonComponent label={"Change OSdownload location"} />
-            </Location>
-            <br />
-            <ButtonComponent
-              label={"Reset"}
-              displayStyle={DisplayStyle.Secondary}
-              onClick={reset}
-            />
-          </div>
-        </div>
-      )}
-    </div>
+    <BackupWrapper>
+      <BackupDescriptionWrapper>
+        <BackupDescription displayStyle={TextDisplayStyle.MediumFadedLightText}>
+          <FormattedMessage id="view.name.settings.backup.description" />
+        </BackupDescription>
+      </BackupDescriptionWrapper>
+      <BackupTableRow checkMode={false}>
+        <BackupDataWrapper>
+          <Name displayStyle={TextDisplayStyle.LargeText}>
+            <FormattedMessage id="view.name.settings.backup.autostartLabel" />
+          </Name>
+          <Message displayStyle={TextDisplayStyle.MediumFadedLightText}>
+            lala
+          </Message>
+        </BackupDataWrapper>
+        <ActionsWrapper>
+          <Location locationToUpdate={LocationPath.PureOsDownload}>
+            <ButtonComponent label={"Change OSdownload location"} />
+          </Location>
+        </ActionsWrapper>
+      </BackupTableRow>
+    </BackupWrapper>
   )
 }
 
