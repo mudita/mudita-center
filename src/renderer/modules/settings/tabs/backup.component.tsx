4a163b2441d2f82b5f4906600c19c435e5754889
staszewski <kamilstaszewski95@gmail.com>
2020-04-22 10:31:11 +0200

    PDA-166 Pass backup location to UI

--- src/renderer/modules/settings/tabs/backup.component.tsx
+++ src/renderer/modules/settings/tabs/backup.component.tsx
@@ -1,18 +1,9 @@
-import React, { ChangeEvent, useEffect, useReducer, useState } from "react"
-
+import React, { useEffect, useState } from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
-import { Link } from "react-router-dom"
-import { URL_ONBOARDING } from "Renderer/constants/urls"
-import {
-  getAppSettings,
-  resetAppSettings,
-} from "Renderer/requests/app-settings.request"
-import InputCheckbox from "Renderer/components/core/input-checkbox/input-checkbox.component"
+import { getAppSettings } from "Renderer/requests/app-settings.request"
 import { AppSettings } from "App/main/default-app-settings"
-import { InputText } from "Renderer/components/core/input-text/input-text.elements"
 import Location from "Renderer/components/core/location/location.component"
 import ButtonComponent from "Renderer/components/core/button/button.component"
-import { DisplayStyle } from "Renderer/components/core/button/button.config"
 import { LocationPath } from "Renderer/components/core/location/location.enum"
 import styled from "styled-components"
 import {
@@ -23,9 +14,6 @@ import {
   TableRow,
 } from "Renderer/components/rest/messages/topics-table.component"
 import { borderColor } from "Renderer/styles/theming/theme-getters"
-import ButtonToggler, {
-  ButtonTogglerItem,
-} from "Renderer/components/core/button-toggler/button-toggler.component"
 import Text, {
   TextDisplayStyle,
 } from "Renderer/components/core/text/text.component"
@@ -55,38 +43,12 @@ const BackupDataWrapper = styled(DataWrapper)`
 `
 // DO NOT REVIEW THIS CODE, IT'S FOR TESTING PURPOSES ONLY
 const Backup: FunctionComponent = () => {
-  // const [settings, setSettings] = useState<AppSettings>()
-  // const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
-  //
-  // useEffect(() => {
-  //   ;(async () => {
-  //     setSettings(await getAppSettings())
-  //   })()
-  // }, [ignored])
-  //
-  // const toggleAutostart = async (event: ChangeEvent<HTMLInputElement>) => {
-  //   if (settings) {
-  //     setSettings({ ...settings, appAutostart: event.target.checked })
-  //   }
-  // }
-  //
-  // const changeBackupLocation = (event: ChangeEvent<HTMLInputElement>) => {
-  //   if (settings) {
-  //     setSettings({ ...settings, pureOsBackupLocation: event.target.value })
-  //   }
-  // }
-  //
-  // const changeDownloadLocation = (event: ChangeEvent<HTMLInputElement>) => {
-  //   if (settings) {
-  //     setSettings({ ...settings, pureOsDownloadLocation: event.target.value })
-  //   }
-  // }
-  //
-  // const reset = async () => {
-  //   await resetAppSettings()
-  //   forceUpdate()
-  // }
-
+  const [settings, setSettings] = useState<AppSettings>()
+  useEffect(() => {
+    ;(async () => {
+      setSettings(await getAppSettings())
+    })()
+  }, [])
   return (
     <BackupWrapper>
       <BackupDescriptionWrapper>
@@ -100,7 +62,7 @@ const Backup: FunctionComponent = () => {
             <FormattedMessage id="view.name.settings.backup.label" />
           </Name>
           <Message displayStyle={TextDisplayStyle.MediumFadedLightText}>
-            lala
+            {settings?.pureOsBackupLocation}
           </Message>
         </BackupDataWrapper>
         <ActionsWrapper>
