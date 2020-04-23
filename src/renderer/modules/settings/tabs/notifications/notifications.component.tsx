1d74db634838bc8b280972c0b95e901b322c0e4c
staszewski <kamilstaszewski95@gmail.com>
2020-04-23 09:41:14 +0200

    PDA-160 Adapt notification to SettingsToggler

--- src/renderer/modules/settings/tabs/notifications/notifications.component.tsx
+++ src/renderer/modules/settings/tabs/notifications/notifications.component.tsx
@@ -1,38 +1,10 @@
-import React, { useState } from "react"
+import React from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
-import {
-  ToggleState,
-  twoStateToggler,
-} from "Renderer/modules/settings/settings.component"
-import NotificationsUI from "Renderer/modules/settings/tabs/notifications/notifications-ui.component"
+import { twoStateToggler } from "Renderer/modules/settings/settings.enum"
+import NotificationsUI from "Renderer/components/rest/settings/notifications-ui.component"
 
 const Notifications: FunctionComponent = () => {
-  const [incomingCallsNotifications, setIncomingCallsNotifications] = useState<
-    ToggleState
-  >(ToggleState.Off)
-  const [
-    incomingMessagesNotifications,
-    setIncomingMessagesNotifications,
-  ] = useState<ToggleState>(ToggleState.Off)
-  const [lowBatteryNotifications, setLowBatteryNotifications] = useState<
-    ToggleState
-  >(ToggleState.Off)
-  const [pureOsUpdatesNotifications, setPureOsUpdatesNotifications] = useState<
-    ToggleState
-  >(ToggleState.Off)
-  return (
-    <NotificationsUI
-      incomingCallsNotifications={incomingCallsNotifications}
-      incomingMessagesNotifications={incomingMessagesNotifications}
-      lowBatteryNotifications={lowBatteryNotifications}
-      pureOsUpdatesNotifications={pureOsUpdatesNotifications}
-      setIncomingCallsNotifications={setIncomingCallsNotifications}
-      setIncomingMessagesNotifications={setIncomingMessagesNotifications}
-      setLowBatteryNotifications={setLowBatteryNotifications}
-      setPureOsUpdatesNotifications={setPureOsUpdatesNotifications}
-      togglerState={twoStateToggler}
-    />
-  )
+  return <NotificationsUI togglerState={twoStateToggler} />
 }
 
 export default Notifications
