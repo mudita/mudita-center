1b2365adf4f2b0e07232e1efc84aa0d14e0a02ca
staszewski <kamilstaszewski95@gmail.com>
2020-04-27 16:02:47 +0200

    PDA-160 Remove changeToggleValue

--- src/renderer/components/rest/settings/settings-toggler.component.tsx
+++ src/renderer/components/rest/settings/settings-toggler.component.tsx
@@ -7,7 +7,6 @@ import ButtonToggler, {
 } from "Renderer/components/core/button-toggler/button-toggler.component"
 import { twoStateToggler } from "Renderer/modules/settings/settings-toggler-state"
 import { ToggleState } from "Renderer/modules/settings/settings-toggle-state.enum"
-import { noop } from "Renderer/utils/noop"
 
 const Toggler = styled(ButtonToggler)`
   margin-right: 4rem;
@@ -20,20 +19,17 @@ const TogglerItem = styled(ButtonTogglerItem)`
 
 interface Props {
   toggleValue?: string
-  changeToggleValue: (label: ToggleState) => void
-  onToggle?: (label: ToggleState) => void
+  onToggle: (label: ToggleState) => void
 }
 
 const SettingsToggler: FunctionComponent<Props> = ({
   toggleValue,
-  changeToggleValue,
-  onToggle = noop,
+  onToggle,
 }) => {
   return (
     <Toggler filled>
       {twoStateToggler.map(label => {
         const changeStatus = () => {
-          changeToggleValue(label)
           onToggle(label)
         }
         return (
