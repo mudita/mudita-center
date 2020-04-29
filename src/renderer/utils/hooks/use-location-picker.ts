3fc11f363cb77ea7b019d8f2b9103538630f78fa
staszewski <kamilstaszewski95@gmail.com>
2020-04-29 08:09:55 +0200

    PDA-166 Remove state in useLocationPicker

--- src/renderer/utils/hooks/use-location-picker.ts
+++ src/renderer/utils/hooks/use-location-picker.ts
@@ -1,21 +1,15 @@
 import { updateLocationSettings } from "Renderer/requests/app-settings.request"
-import { useEffect, useState } from "react"
 import { LocationPath } from "Renderer/components/core/location/location.enum"
 
 const useLocationPicker = (
   locationToUpdate: LocationPath,
   onSuccessCallback?: () => void
 ) => {
-  const [successState, setSuccessState] = useState()
-  const openDialog = async () => {
-    setSuccessState(await updateLocationSettings(locationToUpdate))
-  }
-  useEffect(() => {
-    if (successState) {
+  return async () => {
+    if (await updateLocationSettings(locationToUpdate)) {
       onSuccessCallback?.()
     }
-  }, [successState])
-  return openDialog
+  }
 }
 
 export default useLocationPicker
