41d42a4c2fb39fc23e24efe7d3d812a331c0300f
staszewski <kamilstaszewski95@gmail.com>
2020-05-09 11:05:18 +0200

    PDA-210 IThis interface is redundant. Use direcly in store

--- src/renderer/models/settings/settings.ts
+++ src/renderer/models/settings/settings.ts
@@ -1,13 +1,13 @@
 import { Dispatch } from "Renderer/store"
 import { getAppSettings } from "Renderer/requests/app-settings.request"
-import { Store } from "Renderer/models/settings/settings.interface"
 import updateSettingsRequest from "Renderer/requests/update-settings.request"
 import { Option } from "Renderer/components/rest/settings/settings-toggler.component"
+import { AppSettings as StoreValues } from "App/main/default-app-settings"
 
 export default {
   state: {},
   reducers: {
-    update(state: Store, payload: { [key in Option]?: boolean }) {
+    update(state: StoreValues, payload: { [key in Option]?: boolean }) {
       return { ...state, ...payload }
     },
   },
