172417afc56c1a7dd7a89e5067ff48749ba3ba2c
staszewski <kamilstaszewski95@gmail.com>
2020-05-11 15:44:31 +0200

    PDA-211 Pass a props just a state.settings. Spread effects

--- src/renderer/modules/settings/tabs/audio-conversion/audio-conversion.container.tsx
+++ src/renderer/modules/settings/tabs/audio-conversion/audio-conversion.container.tsx
@@ -1,24 +1,13 @@
 import { connect } from "react-redux"
 import { RootModel } from "Renderer/models/models"
 import AudioConversion from "Renderer/modules/settings/tabs/audio-conversion/audio-conversion.component"
-import {
-  ConversionFormat,
-  Convert,
-} from "Renderer/components/rest/settings/audio-conversion-radio-group.enum"
 
 const mapStateToProps = (state: RootModel) => {
-  return {
-    ...state.settings,
-  }
+  return state.settings
 }
 
 const mapDispatchToProps = (dispatch: any) => ({
-  loadSettings: () => dispatch.settings.loadSettings(),
-  setNonStandardAudioFilesConversion: (option: boolean) =>
-    dispatch.settings.setNonStandardAudioFilesConversion(option),
-  setConvert: (option: Convert) => dispatch.settings.setConvert(option),
-  setConversionFormat: (option: ConversionFormat) =>
-    dispatch.settings.setConversionFormat(option),
+  ...dispatch.settings,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AudioConversion)
