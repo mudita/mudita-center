8689ea0761eeb8dd78e0ab49afad0bb17958f27f
staszewski <kamilstaszewski95@gmail.com>
2020-05-19 12:20:47 +0200

    PDA-174 Add icons to sidebar

--- src/renderer/components/rest/messages/message-details.component.tsx
+++ src/renderer/components/rest/messages/message-details.component.tsx
@@ -1,9 +1,30 @@
 import React from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
-import { Sidebar } from "Renderer/components/core/table/table.component"
+import {
+  Sidebar,
+  SidebarHeaderIcon,
+} from "Renderer/components/core/table/table.component"
+import { Type } from "Renderer/components/core/icon/icon.config"
+import { noop } from "Renderer/utils/noop"
 
-const MessageDetails: FunctionComponent<{}> = () => {
-  return <Sidebar show>lala</Sidebar>
+interface Props {
+  details: any
+}
+
+const MessageDetails: FunctionComponent<Props> = ({ details }) => {
+  const icons = (
+    <>
+      <SidebarHeaderIcon Icon={Type.Calls} onClick={noop} />
+      <SidebarHeaderIcon Icon={Type.Contacts} onClick={noop} />
+      <SidebarHeaderIcon Icon={Type.BorderCheckIcon} onClick={noop} />
+      <SidebarHeaderIcon Icon={Type.Delete} onClick={noop} />
+    </>
+  )
+  return (
+    <Sidebar show headerRight={icons}>
+      {details.caller.firstName}
+    </Sidebar>
+  )
 }
 
 export default MessageDetails
