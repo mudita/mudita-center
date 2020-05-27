0c9ff55c0ba1b830da414cf45e60482f2f01556c
staszewski <kamilstaszewski95@gmail.com>
2020-05-27 08:29:01 +0200

    PDA-174 Update tests

--- src/renderer/modules/messages/messages.test.tsx
+++ src/renderer/modules/messages/messages.test.tsx
@@ -5,28 +5,34 @@ import Messages from "./messages.component"
 import { mockAllIsIntersecting } from "react-intersection-observer/test-utils"
 import { fireEvent } from "@testing-library/dom"
 
+const caller = {
+  id: "59198364-5ba4-4e4b-8802-330db16911b5",
+  firstName: "Lance",
+  lastName: "Goyette",
+  phoneNumber: "+93 832 460 374",
+}
+
 const list = [
   {
     id: "4e429cff-c6ed-4f46-92e6-e8ff446071bc",
-    caller: {
-      id: "59198364-5ba4-4e4b-8802-330db16911b5",
-      firstName: "Lance",
-      lastName: "Goyette",
-      phoneNumber: "+93 832 460 374",
-    },
+    caller,
     unread: true,
     messages: [
       {
+        author: caller,
         id: "27a7108d-d5b8-4bb5-87bc-2cfebcecd571",
         date: "2019-10-18T11:27:15.256Z",
         content:
           "Adipisicing non qui Lorem aliqua officia laboris ad reprehenderit dolor mollit.",
+        interlocutor: true,
       },
       {
+        author: caller,
         id: "70cdc31d-ca8e-4d0c-8751-897ae2f3fb7d",
         date: "2019-10-18T11:45:35.112Z",
         content:
           "Dolore esse occaecat ipsum officia ad laborum excepteur quis. Dolore esse occaecat ipsum officia ad laborum excepteur quis. Dolore esse occaecat ipsum officia ad laborum excepteur quis.",
+        interlocutor: true,
       },
     ],
   },
