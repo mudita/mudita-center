900b2be9326f5aa101449e7f1503e336d21e709b
staszewski <kamilstaszewski95@gmail.com>
2020-05-28 13:24:14 +0200

    PDA-174 Correct tests

--- src/renderer/models/messages/utils/topics-utils.test.ts
+++ src/renderer/models/messages/utils/topics-utils.test.ts
@@ -1,22 +1,30 @@
 import { searchTopics } from "Renderer/models/messages/utils/topics-utils"
 import { mockedList } from "Renderer/components/rest/messages/__mocks__/caller-data"
 
-test("finds search value - first name", () => {
+test("searches value - first name", () => {
   const searchValue = mockedList[0].caller.firstName
-  expect(searchTopics(mockedList, searchValue)).toBeTruthy()
+  expect(searchTopics(mockedList, searchValue)).toHaveLength(mockedList.length)
+  const wrongSearchValue = "asdadaa"
+  expect(searchTopics(mockedList, wrongSearchValue)).toHaveLength(0)
 })
 
-test("finds search value - last name", () => {
+test("searches value - last name", () => {
   const searchValue = mockedList[0].caller.lastName
-  expect(searchTopics(mockedList, searchValue)).toBeTruthy()
+  expect(searchTopics(mockedList, searchValue)).toHaveLength(mockedList.length)
+  const wrongSearchValue = "ladakslodasiopd"
+  expect(searchTopics(mockedList, wrongSearchValue)).toHaveLength(0)
 })
 
-test("finds search value - phone number", () => {
+test("searches value - phone number", () => {
   const searchValue = mockedList[0].caller.phoneNumber
-  expect(searchTopics(mockedList, searchValue)).toBeTruthy()
+  expect(searchTopics(mockedList, searchValue)).toHaveLength(mockedList.length)
+  const wrongSearchValue = "+123 456 789"
+  expect(searchTopics(mockedList, wrongSearchValue)).toHaveLength(0)
 })
 
-test("finds search value - messages", () => {
+test("searches value - messages", () => {
   const searchValue = mockedList[0].messages[0].content[0]
-  expect(searchTopics(mockedList, searchValue)).toBeTruthy()
+  expect(searchTopics(mockedList, searchValue)).toHaveLength(mockedList.length)
+  const wrongSearchValue = "lalal sadrsa"
+  expect(searchTopics(mockedList, wrongSearchValue)).toHaveLength(0)
 })
