4286e51696053afceac9b67232a8266181e5e5a3
staszewski <kamilstaszewski95@gmail.com>
2020-06-01 07:44:00 +0200

    PDA-225 Update storybook like it is done with other modals (Add view as bg)

--- src/renderer/modules/messages/messages.stories.tsx
+++ src/renderer/modules/messages/messages.stories.tsx
@@ -4,15 +4,11 @@ import Messages from "./messages.component"
 import { rowsMessages } from "Renderer/components/core/table/table.fake-data"
 import TemplateModal from "Renderer/modules/messages/template-modal.component"
 import styled from "styled-components"
-import { ModalWrapper } from "Renderer/components/core/modal/modal.styled.elements"
-
-const TemplateWrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  background-color: gray;
-`
+import {
+  ModalBackdrop,
+  ModalWrapper,
+} from "Renderer/components/core/modal/modal.styled.elements"
+import { mockedList } from "Renderer/components/rest/messages/__mocks__/caller-data"
 
 const TemplateModalWrapper = styled(ModalWrapper)`
   padding: 4rem 3.2rem;
@@ -25,15 +21,17 @@ storiesOf("Views|Messages", module).add("Messages", () => (
 ))
 
 storiesOf("Views|Messages/Modals", module).add("Template modal", () => (
-  <TemplateWrapper>
+  <div style={{ maxWidth: "97.5rem" }}>
+    <Messages searchValue="" list={mockedList} />
     <TemplateModalWrapper>
       <TemplateModal
         templates={[
-          "1Lorem ipsum dolor sit amet.",
+          "1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet. 1Lorem ipsum dolor sit amet.",
           "2Lorem ipsum dolor sit amet.",
           "3Lorem ipsum dolor sit amet.",
         ]}
       />
     </TemplateModalWrapper>
-  </TemplateWrapper>
+    <ModalBackdrop />
+  </div>
 ))
