1cd5ae50fcd15a7a0dce96ca47d62ba598daff3b
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-06-03 16:57:12 +0200

    Text editor component integration

--- src/renderer/modules/messages/tabs/templates.component.tsx
+++ src/renderer/modules/messages/tabs/templates.component.tsx
@@ -4,7 +4,26 @@ import { notes } from "Renderer/components/core/table/table.fake-data"
 import TemplatesPanel from "Renderer/components/rest/messages/templates/templates-panel.component"
 import useTableSelect from "Renderer/utils/hooks/useTableSelect"
 import TemplatesList from "Renderer/components/rest/messages/templates/templates-list.component"
-import { TableWithSidebarWrapper } from "Renderer/components/core/table/table.component"
+import {
+  Sidebar,
+  TableWithSidebarWrapper,
+} from "Renderer/components/core/table/table.component"
+import useTableSidebar from "Renderer/utils/hooks/useTableSidebar"
+import styled from "styled-components"
+import { useTextEditor } from "Renderer/components/core/text-editor/text-editor.hook"
+import TextEditor from "Renderer/components/core/text-editor/text-editor.component"
+import { asyncNoop } from "Renderer/utils/noop"
+import { defineMessages } from "react-intl"
+import { intl } from "Renderer/utils/intl"
+
+const messages = defineMessages({
+  charactersNumber: { id: "view.name.messages.templates.charactersNumber" },
+})
+
+const TemplatesSidebar = styled(Sidebar)`
+  --header-height: 5.6rem;
+  border-top: none;
+`
 
 export interface Template {
   id: string
@@ -26,6 +45,14 @@ const Templates: FunctionComponent<TemplatesProps> = ({
     Template
   >(notes)
 
+  const sidebarHook = useTableSidebar<Template>()
+  const { closeSidebar, activeRow } = sidebarHook
+
+  const textEditorHook = useTextEditor(activeRow, asyncNoop)
+  const {
+    temporaryText: { length: textLength },
+  } = textEditorHook
+
   return (
     <>
       <TemplatesPanel
@@ -37,7 +64,18 @@ const Templates: FunctionComponent<TemplatesProps> = ({
         toggleAll={toggleAll}
       />
       <TableWithSidebarWrapper>
-        <TemplatesList templates={notes} {...rest} />
+        <TemplatesList templates={notes} {...sidebarHook} {...rest} />
+        <TemplatesSidebar show={Boolean(activeRow)} onClose={closeSidebar}>
+          {activeRow && (
+            <TextEditor
+              {...textEditorHook}
+              statsInfo={intl.formatMessage(messages.charactersNumber, {
+                charactersCount: textLength,
+                smsCount: Math.ceil(textLength / 160),
+              })}
+            />
+          )}
+        </TemplatesSidebar>
       </TableWithSidebarWrapper>
     </>
   )
