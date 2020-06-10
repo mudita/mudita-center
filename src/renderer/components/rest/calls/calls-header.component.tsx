a607f285946672b8ca53c5a4d3e5ff03658dd89c
staszewski <kamilstaszewski95@gmail.com>
2020-06-10 11:35:57 +0200

    PDA-235 Pass Visibility filter to header

--- src/renderer/components/rest/calls/calls-header.component.tsx
+++ src/renderer/components/rest/calls/calls-header.component.tsx
@@ -13,15 +13,24 @@ import styled from "styled-components"
 import { VisibilityFilter } from "App/renderer/models/calls/calls.interface"
 
 const toggleState = [
-  intl.formatMessage({
-    id: "view.name.phone.calls.allCallTypes",
-  }),
-  intl.formatMessage({
-    id: "view.name.phone.calls.receivedCalls",
-  }),
-  intl.formatMessage({
-    id: "view.name.phone.calls.missedCalls",
-  }),
+  {
+    label: intl.formatMessage({
+      id: "view.name.phone.calls.allCallTypes",
+    }),
+    visibilityFilter: VisibilityFilter.All,
+  },
+  {
+    label: intl.formatMessage({
+      id: "view.name.phone.calls.receivedCalls",
+    }),
+    visibilityFilter: VisibilityFilter.Received,
+  },
+  {
+    label: intl.formatMessage({
+      id: "view.name.phone.calls.missedCalls",
+    }),
+    visibilityFilter: VisibilityFilter.Missed,
+  },
 ] as const
 
 const CallsButtonTogglerItem = styled(ButtonTogglerItem)`
@@ -35,19 +44,35 @@ interface Props {
 const CallsHeader: FunctionComponent<Props> = ({
   changeVisibilityFilter = noop,
 }) => {
-  const [activeLabel, setActiveLabel] = useState(toggleState[0])
+  const [activeLabel, setActiveLabel] = useState(toggleState[0].label)
+  const showAllCalls = () => changeVisibilityFilter(VisibilityFilter.All)
+  const showReceivedCalls = () =>
+    changeVisibilityFilter(VisibilityFilter.Received)
+  const showMissedCalls = () => changeVisibilityFilter(VisibilityFilter.Missed)
   return (
     <FiltersWrapper checkMode>
       <UnreadFilters>
         <ButtonToggler>
-          {toggleState.map((label, i) => {
-            const onClick = () => noop()
+          {toggleState.map(({ label, visibilityFilter }, i) => {
+            const getFilterByLabel = () => {
+              switch (visibilityFilter) {
+                case VisibilityFilter.All:
+                  showAllCalls()
+                  setActiveLabel(label)
+                case VisibilityFilter.Received:
+                  showReceivedCalls()
+                  setActiveLabel(label)
+                case VisibilityFilter.Missed:
+                  showMissedCalls()
+                  setActiveLabel(label)
+              }
+            }
             return (
               <CallsButtonTogglerItem
                 key={i}
                 label={label}
-                onClick={onClick}
-                active={false}
+                onClick={getFilterByLabel}
+                active={activeLabel === label}
               />
             )
           })}
