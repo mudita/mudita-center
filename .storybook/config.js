e5dfca907710f3184fde8d73dcf0230d031a820b
staszewski <kamilstaszewski95@gmail.com>
2020-06-15 14:48:50 +0200

    PDA-240 Remove unused import. Correct import for devMode model

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,17 +2,14 @@ import React from "react"
 import { ThemeProvider } from "styled-components"
 import { Normalize } from "styled-normalize"
 import { configure, addDecorator } from "@storybook/react"
-import { action } from "@storybook/addon-actions"
-
 import GlobalStyle from "Renderer/styles/global-style.component"
 import theme from "Renderer/styles/theming/theme"
 import "Renderer/fonts/fonts.css"
-
 import localeEn from "Renderer/locales/main/en-US.json"
 import { IntlProvider } from "react-intl"
 import { LANGUAGE } from "Renderer/constants/languages"
 import { init } from "@rematch/core"
-import devMode from "../src/renderer/models/dev-mode/dev-mode"
+import devMode from "Renderer/models/dev-mode/dev-mode"
 import selectPlugin from "@rematch/select"
 import { Provider } from "react-redux"
 
