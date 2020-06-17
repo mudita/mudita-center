29b0f82806850ae6189db1261e15e86b3aa334f0
staszewski <kamilstaszewski95@gmail.com>
2020-06-17 11:21:21 +0200

    PDA-269 No need to export here

--- src/renderer/components/rest/calls/calls-table.component.tsx
+++ src/renderer/components/rest/calls/calls-table.component.tsx
@@ -65,7 +65,7 @@ interface Props {
   calls: Call[]
 }
 
-export const today = (date: Date) => moment(date).isSame(Date.now(), "days")
+const today = (date: Date) => moment(date).isSame(Date.now(), "days")
 
 const CallsTable: FunctionComponent<Props> = ({ calls }) => {
   const { getRowStatus, toggleRow } = useTableSelect(basicRows)
