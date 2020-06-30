58eba76d08baadfb8aa4bf5077d8cb903121ad80
staszewski <kamilstaszewski95@gmail.com>
2020-06-30 07:34:03 +0200

    PDA-274 Extract replacing newlines with empty string to a dedicated function

--- src/renderer/models/templates/filter-templates.ts
+++ src/renderer/models/templates/filter-templates.ts
@@ -1,12 +1,12 @@
 import { Template } from "Renderer/modules/messages/tabs/templates-ui.component"
+import { removeNewLinesFromString } from "Renderer/utils/remove-new-lines-from-string"
 
 export const filterTemplates = (templates: Template[], searchValue: string) => {
   if (searchValue.length > 0) {
     return templates.filter(template => {
-      return template.text
-        .replace(/\s/g, "")
+      return removeNewLinesFromString(template.text)
         .toLocaleLowerCase()
-        .includes(searchValue.toLocaleLowerCase().replace(/\s/g, ""))
+        .includes(removeNewLinesFromString(searchValue).toLocaleLowerCase())
     })
   }
   return templates
