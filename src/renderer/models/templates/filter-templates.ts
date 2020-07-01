77bb930153e33c663a61f109cf8dfc8406aca591
staszewski <kamilstaszewski95@gmail.com>
2020-07-01 08:26:15 +0200

    PDA-274 Add string transform function

--- src/renderer/models/templates/filter-templates.ts
+++ src/renderer/models/templates/filter-templates.ts
@@ -1,12 +1,16 @@
 import { Template } from "Renderer/modules/messages/tabs/templates-ui.component"
 import { removeNewLinesFromString } from "Renderer/utils/remove-new-lines-from-string"
 
+const prepareStringForSearch = (text: string) => {
+  return removeNewLinesFromString(text).toLocaleLowerCase()
+}
+
 export const filterTemplates = (templates: Template[], searchValue: string) => {
   if (searchValue.length > 0) {
     return templates.filter(template => {
-      return removeNewLinesFromString(template.text)
-        .toLocaleLowerCase()
-        .includes(removeNewLinesFromString(searchValue).toLocaleLowerCase())
+      return prepareStringForSearch(template.text).includes(
+        prepareStringForSearch(searchValue)
+      )
     })
   }
   return templates
