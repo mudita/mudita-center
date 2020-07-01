317e2fcbf80b644afcb7d0632250432dd4dbc49e
staszewski <kamilstaszewski95@gmail.com>
2020-07-01 09:17:06 +0200

    PDA-277 Add renderer function in tests

--- src/renderer/modules/messages/messages-panel.test.tsx
+++ src/renderer/modules/messages/messages-panel.test.tsx
@@ -4,38 +4,39 @@ import MessagesPanel from "Renderer/modules/messages/messages-panel.component"
 import { fireEvent } from "@testing-library/dom"
 import { MessagePanelTestIds } from "Renderer/modules/messages/messages-panel.interface"
 
+const defaultProps = {
+  selectedItemsCount: 0,
+  searchValue: "",
+}
+
+const renderer = (extraProps?: {}) => {
+  const props = {
+    ...defaultProps,
+    ...extraProps,
+  }
+  return renderWithThemeAndIntl(<MessagesPanel {...props} />)
+}
+
 test("filter buttons are rendered when there are no selected checkboxes", () => {
-  const { getByTestId } = renderWithThemeAndIntl(
-    <MessagesPanel selectedItemsCount={0} searchValue={""} />
-  )
+  const { getByTestId } = renderer()
   expect(getByTestId(MessagePanelTestIds.FilterButtons)).toBeInTheDocument()
 })
 
 test("filter buttons are not rendered when there are selected checkboxes", () => {
-  const { queryByTestId } = renderWithThemeAndIntl(
-    <MessagesPanel selectedItemsCount={1} searchValue={""} />
-  )
+  const { queryByTestId } = renderer({ selectedItemsCount: 1 })
   expect(
     queryByTestId(MessagePanelTestIds.FilterButtons)
   ).not.toBeInTheDocument()
 })
 
 test("when there are selected checkboxes, selection manager is rendered", () => {
-  const { getByTestId } = renderWithThemeAndIntl(
-    <MessagesPanel selectedItemsCount={1} searchValue={""} />
-  )
+  const { getByTestId } = renderer({ selectedItemsCount: 1 })
   expect(getByTestId(MessagePanelTestIds.SelectionManager)).toBeInTheDocument()
 })
 
 test("search works", () => {
   const changeSearchValue = jest.fn()
-  const { getByRole } = renderWithThemeAndIntl(
-    <MessagesPanel
-      selectedItemsCount={0}
-      searchValue={""}
-      changeSearchValue={changeSearchValue}
-    />
-  )
+  const { getByRole } = renderer({ changeSearchValue })
   const searchInput = getByRole("searchbox")
   fireEvent.change(searchInput, { target: { value: "G" } })
   expect(changeSearchValue).toBeCalled()
