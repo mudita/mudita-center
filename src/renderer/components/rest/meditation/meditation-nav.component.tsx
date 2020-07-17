e6808b04fdbc4ce97aae794bda7ba963fac6f9d8
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-07-17 09:39:37 +0200

    Styling

--- src/renderer/components/rest/meditation/meditation-nav.component.tsx
+++ src/renderer/components/rest/meditation/meditation-nav.component.tsx
@@ -1,13 +1,16 @@
 import React from "react"
-import styled from "styled-components"
 
 import FunctionComponent from "Renderer/types/function-component.interface"
-import Text, {
-  TextDisplayStyle,
-} from "Renderer/components/core/text/text.component"
 import ArrowLeft from "Renderer/svg/arrow-long-left.svg"
 import ArrowRight from "Renderer/svg/arrow-long-right.svg"
 import { noop } from "Renderer/utils/noop"
+import {
+  Button,
+  DateRange,
+  GotoButton,
+  WeekIndicator,
+  Wrapper,
+} from "Renderer/components/rest/meditation/meditation-nav.styled"
 
 type MediationDate = string | Date
 
@@ -24,45 +27,6 @@ const formatDate = (input: MediationDate): string => {
   return ""
 }
 
-const DateRange = styled(Text).attrs(() => ({
-  displayStyle: TextDisplayStyle.LargeBoldText,
-}))`
-  margin-bottom: 0.4rem;
-  text-align: center;
-  min-width: 17rem;
-`
-
-const WeekIndicator = styled(Text).attrs(() => ({
-  displayStyle: TextDisplayStyle.SmallFadedText,
-}))``
-
-const Button = styled.button`
-  background: none;
-  border: 0;
-  outline: 0;
-  cursor: pointer;
-  position: relative;
-  top: -0.2rem;
-`
-
-const Wrapper = styled.div`
-  display: flex;
-`
-
-const GotoButton = styled(Text).attrs(() => ({
-  displayStyle: TextDisplayStyle.SmallSupplementaryText,
-  element: "button",
-}))`
-  line-height: 1.7rem;
-  cursor: pointer;
-  background: none;
-  border: 0;
-  padding: 0;
-  position: relative;
-  top: -0.2rem;
-  margin-right: 1.65rem;
-`
-
 const MeditationNav: FunctionComponent<MeditationNavProps> = ({
   startDate,
   endDate,
@@ -71,17 +35,21 @@ const MeditationNav: FunctionComponent<MeditationNavProps> = ({
     <>
       <Wrapper>
         <GotoButton>Go to today</GotoButton>
-        <Button onClick={noop}>
-          <ArrowLeft />
-        </Button>
-        <DateRange>
-          {formatDate(startDate)} - {formatDate(endDate)}
-        </DateRange>
-        <Button onClick={noop}>
-          <ArrowRight />
-        </Button>
+        <div>
+          <Wrapper as="nav">
+            <Button onClick={noop}>
+              <ArrowLeft />
+            </Button>
+            <DateRange>
+              {formatDate(startDate)} - {formatDate(endDate)}
+            </DateRange>
+            <Button onClick={noop}>
+              <ArrowRight />
+            </Button>
+          </Wrapper>
+          <WeekIndicator>This week</WeekIndicator>
+        </div>
       </Wrapper>
-      <WeekIndicator>This week</WeekIndicator>
     </>
   )
 }
---
+++ src/renderer/components/rest/meditation/meditation-nav.styled.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+import Text, {
+  TextDisplayStyle,
+} from "Renderer/components/core/text/text.component"
+
+export const DateRange = styled(Text).attrs(() => ({
+  displayStyle: TextDisplayStyle.LargeBoldText,
+}))`
+  margin-bottom: 0.4rem;
+  text-align: center;
+  min-width: 17rem;
+`
+
+export const WeekIndicator = styled(Text).attrs(() => ({
+  displayStyle: TextDisplayStyle.SmallFadedText,
+}))`
+  width: 100%;
+  text-align: center;
+`
+
+export const Button = styled.button`
+  background: none;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
+  position: relative;
+  top: -0.2rem;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`
+
+export const GotoButton = styled(Text).attrs(() => ({
+  displayStyle: TextDisplayStyle.SmallSupplementaryText,
+  element: "button",
+}))`
+  line-height: 1.7rem;
+  cursor: pointer;
+  background: none;
+  border: 0;
+  padding: 0;
+  align-self: flex-start;
+  margin-right: 1.65rem;
+`
