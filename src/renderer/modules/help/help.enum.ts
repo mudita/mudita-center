d9d41a27397ff266cb690100a76b40ad31a7f6f6
staszewski <kamilstaszewski95@gmail.com>
2020-07-27 13:02:25 +0200

    PDA_332 Update enum string

--- src/renderer/modules/help/help.enum.ts
+++ src/renderer/modules/help/help.enum.ts
@@ -1,5 +1,5 @@
 export enum HelpComponentTestIds {
   Wrapper = "help-component-wrapper",
   ToggleButton = "help-component-toggle-button",
-  Title = "view.name.help.title",
+  Title = "help-component-title",
 }
