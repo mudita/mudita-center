cc4ed12f3b323e266f81d4a8cbc629c6992d3703
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-07-27 18:13:25 +0200

    Updated pure backup adapter to use electron-store

--- src/backend/adapters/pure-phone-backups/pure-phone-backups.adapter.ts
+++ src/backend/adapters/pure-phone-backups/pure-phone-backups.adapter.ts
@@ -1,21 +1,17 @@
 import PurePhoneBackupAdapter from "Backend/adapters/pure-phone-backups/pure-phone-backups-adapter.class"
 import BackupItemInfo from "Common/interfaces/backup-item-info.interface"
-import { app } from "electron"
-import { name } from "../../../../package.json"
 import fs from "fs-extra"
 import path from "path"
 import moment from "moment"
+import settingsStore from "App/main/store/settings"
 
 class PurePhoneBackups extends PurePhoneBackupAdapter {
   public async getBackups(): Promise<BackupItemInfo[]> {
-    const settingsFilePath = `${app.getPath("appData")}/${name}/settings.json`
-
-    const { pureOsBackupLocation } = await fs.readJson(settingsFilePath)
+    const pureOsBackupLocation = settingsStore.get("pureOsBackupLocation")
     const backups: BackupItemInfo[] = []
-
     const regex = /^pure_backup_(\d{12})\.zip$/m
 
-    if (await fs.pathExists(pureOsBackupLocation)) {
+    if (pureOsBackupLocation && (await fs.pathExists(pureOsBackupLocation))) {
       for (const fileName of await fs.readdir(pureOsBackupLocation)) {
         if (regex.test(fileName)) {
           const { size } = await fs.stat(
