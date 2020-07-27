fe59f9814c483c3ea97b90ca77c3eb53eeff5657
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-07-27 20:31:17 +0200

    Setting component update

--- src/renderer/modules/settings/settings.component.tsx
+++ src/renderer/modules/settings/settings.component.tsx
@@ -7,9 +7,8 @@ import { noop } from "Renderer/utils/noop"
 export interface SettingsProps {
   appAutostart: boolean
   appTethering: boolean
-  setAutostart?: (option: boolean) => void
-  setTethering?: (option: boolean) => void
-  updateSettings: (settings: Partial<AppSettings>) => void
+  setAutostart?: (option: AppSettings["appAutostart"]) => void
+  setTethering?: (option: AppSettings["appTethering"]) => void
 }
 
 const Settings: FunctionComponent<SettingsProps> = ({
@@ -17,15 +16,13 @@ const Settings: FunctionComponent<SettingsProps> = ({
   appTethering,
   setAutostart = noop,
   setTethering = noop,
-}) => {
-  return (
-    <SettingsUI
-      appAutostart={appAutostart}
-      appTethering={appTethering}
-      setAutostart={setAutostart}
-      setTethering={setTethering}
-    />
-  )
-}
+}) => (
+  <SettingsUI
+    appAutostart={appAutostart}
+    appTethering={appTethering}
+    setAutostart={setAutostart}
+    setTethering={setTethering}
+  />
+)
 
 export default Settings
