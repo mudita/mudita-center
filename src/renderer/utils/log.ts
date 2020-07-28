9c601850ddd5f2b1a1d88ec467ddc150ecb86b26
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-07-28 12:28:52 +0200

    Added log util

---
+++ src/renderer/utils/log.ts
@@ -0,0 +1,13 @@
+let log: Console
+
+if (window.require) {
+  log = window.require("electron-log")
+  // TODO: add a remote url to send logs to the specified the server
+  // See also src/main/main.ts
+  // log.transports.remote.level = "warn"
+  // log.transports.remote.url = "http://localhost:3000/log"
+} else {
+  log = console
+}
+
+export default log
