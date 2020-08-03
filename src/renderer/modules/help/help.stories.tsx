0ec552a22f960c02223152eacaf77e1fa19fb1ee
staszewski <kamilstaszewski95@gmail.com>
2020-08-03 09:41:38 +0200

    PDA-332 Add story

---
+++ src/renderer/modules/help/help.stories.tsx
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+import { helpSeed } from "App/seeds/help"
+import Help from "Renderer/modules/help/help.component"
+import history from "Renderer/routes/history"
+import { Router } from "react-router"
+
+storiesOf("Views|Help", module).add("Help", () => (
+  <div style={{ maxWidth: "97.5rem" }}>
+    <Router history={history}>
+      <Help list={helpSeed.list} />
+    </Router>
+  </div>
+))
