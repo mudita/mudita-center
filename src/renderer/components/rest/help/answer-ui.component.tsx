e7343bda511626b7473cddced0f8f708f3325de4
staszewski <kamilstaszewski95@gmail.com>
2020-08-03 12:54:59 +0200

    PDA-333 Start UI + downloading from CF

--- src/renderer/components/rest/help/answer-ui.component.tsx
+++ src/renderer/components/rest/help/answer-ui.component.tsx
@@ -1,7 +1,25 @@
 import React from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
 import { RouteComponentProps } from "react-router"
+import Button from "Renderer/components/core/button/button.component"
+import { Type } from "Renderer/components/core/icon/icon.config"
+import { DisplayStyle } from "Renderer/components/core/button/button.config"
+import { intl } from "Renderer/utils/intl"
+import styled from "styled-components"
+import { URL_MAIN } from "Renderer/constants/urls"
 import { QuestionAndAnswer } from "Renderer/modules/help/help.component"
+
+const AnswerContainer = styled.div`
+  padding: 0 10.5rem;
+`
+
+const AnswerHeader = styled.div`
+  margin: 4.5rem 0 5.3rem 0;
+`
+
+const BackLink = styled(Button)`
+  width: max-content;
+`
 
 interface Props extends RouteComponentProps<{ questionId: string }> {
   list: QuestionAndAnswer
@@ -9,7 +27,21 @@ interface Props extends RouteComponentProps<{ questionId: string }> {
 
 const AnswerUI: FunctionComponent<Props> = ({ match, list }) => {
   const { items } = list
-  return <div>{items[match.params.questionId].answer}</div>
+  return (
+    <AnswerContainer>
+      <AnswerHeader>
+        <BackLink
+          Icon={Type.ArrowLongLeft}
+          displayStyle={DisplayStyle.Link1}
+          label={intl.formatMessage({
+            id: "view.name.help.backLinkText",
+          })}
+          to={URL_MAIN.help}
+        />
+      </AnswerHeader>
+      <p>{items[match.params.questionId].answer}</p>
+    </AnswerContainer>
+  )
 }
 
 export default AnswerUI
--- src/renderer/wrappers/help-app.component.tsx
+++ src/renderer/wrappers/help-app.component.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentType } from "react"
+import React, { ComponentType, useEffect, useState } from "react"
 import FunctionComponent from "Renderer/types/function-component.interface"
 import { Route, RouteComponentProps, Router, Switch } from "react-router"
 import { URL_MAIN } from "Renderer/constants/urls"
@@ -6,12 +6,27 @@ import { History } from "history"
 import AnswerUI from "Renderer/components/rest/help/answer-ui.component"
 import { helpSeed } from "App/seeds/help"
 import Help from "Renderer/modules/help/help.component"
+import axios from "axios"
 
 interface Props {
   history: History
 }
 
 const HelpApp: FunctionComponent<Props> = ({ history }) => {
+  const [data, setData] = useState()
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: response } = await axios.get(
+        `https://preview.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/staging/entries/?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=pdaHelp`
+      )
+
+      setData(response)
+    }
+
+    fetchData()
+  }, [])
+
+  console.log(data)
   const renderRoute = (Component: ComponentType<any>) => (
     props: RouteComponentProps
   ) => {
