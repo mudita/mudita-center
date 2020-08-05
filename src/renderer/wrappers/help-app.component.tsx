9ab3577c511bc96c585ad93cc6018e0dd325b5fb
staszewski <kamilstaszewski95@gmail.com>
2020-08-05 12:05:42 +0200

    PDA-333 Add 2 more envs that can be changed in contentful link

--- src/renderer/wrappers/help-app.component.tsx
+++ src/renderer/wrappers/help-app.component.tsx
@@ -22,7 +22,7 @@ const HelpApp: FunctionComponent<Props> = ({ history }) => {
   useEffect(() => {
     const fetchData = async () => {
       const { data: response } = await axios.get(
-        `https://preview.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/staging/entries/?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=pdaHelp`
+        `https://${process.env.CONTENTFUL_ENV}.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_SPACE}/entries/?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=pdaHelp`
       ) // this link has to be replaced with correct one before release
       const normalizeData = normalizeHelpData(response)
       setData(normalizeData)
