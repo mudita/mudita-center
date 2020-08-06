b6793ff18a9dee5fa9b2c0fcceb34628146a4f4d
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-08-06 17:31:14 +0200

    Contact support data sending implementation

--- src/renderer/modules/onboarding/troubleshooting.component.tsx
+++ src/renderer/modules/onboarding/troubleshooting.component.tsx
@@ -3,8 +3,14 @@ import { useHistory } from "react-router"
 import { URL_ONBOARDING } from "Renderer/constants/urls"
 import OnboardingTroubleshooting from "Renderer/components/rest/onboarding/onboarding-troubleshooting.component"
 import modalService from "Renderer/components/core/modal/modal.service"
-import ContactModal from "Renderer/components/rest/contact-modal/contact-modal.component"
+import ContactModal, {
+  SupportFormData,
+} from "Renderer/components/rest/contact-modal/contact-modal.component"
 import { getFullAppLogs } from "Renderer/requests/app-logs.request"
+import axios from "axios"
+import logger from "Renderer/utils/log"
+import { ContactSupportSuccess } from "Renderer/components/rest/contact-modal/contact-modal-success.component"
+import { ContactSupportFailed } from "Renderer/components/rest/contact-modal/contact-modal-failed.component"
 
 const Troubleshooting = () => {
   const history = useHistory()
@@ -13,9 +19,52 @@ const Troubleshooting = () => {
     // TODO: do some logic to retry connection
     history.push(URL_ONBOARDING.connecting)
   }
+
   const contactSupport = async () => {
-    const log = await getFullAppLogs()
-    modalService.openModal(<ContactModal log={log} />)
+    const appLog = await getFullAppLogs()
+
+    const sendForm = async (formData: SupportFormData) => {
+      modalService.rerenderModal(
+        <ContactModal log={appLog} onSend={sendForm} sending />
+      )
+      const attachments = []
+
+      for (const attachment of formData.attachments || []) {
+        attachments.push({
+          filename: attachment.name,
+          path: attachment.path,
+        })
+      }
+
+      try {
+        await axios.post(
+          // TODO: Replace with real address after merging https://github.com/Appnroll/mudita-website/pull/934
+          "http://localhost:9010/.netlify/functions/contact-support",
+          {
+            ...formData,
+            attachments,
+            log: appLog,
+          }
+        )
+        modalService.openModal(
+          <ContactSupportSuccess withoutEmail={!formData.email} />,
+          true
+        )
+      } catch (error) {
+        const { log, ...errorBody } = JSON.parse(error.config.data)
+        const simpleError = { ...error }
+        simpleError.config.data = {
+          ...errorBody,
+          log: "Log is omitted due to its size",
+        }
+
+        logger.log(`Contact support error: ${JSON.stringify(simpleError)}`)
+
+        modalService.openModal(<ContactSupportFailed />, true)
+      }
+    }
+
+    modalService.openModal(<ContactModal log={appLog} onSend={sendForm} />)
   }
   return (
     <OnboardingTroubleshooting onRetry={onRetry} onContact={contactSupport} />
