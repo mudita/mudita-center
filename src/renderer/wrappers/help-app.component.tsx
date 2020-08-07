8ce41c3ab2eb8d68cadc2d480e56a4a1f47171a8
staszewski <kamilstaszewski95@gmail.com>
2020-08-07 13:04:30 +0200

    PDA-333 Add ID to environemnt env

--- src/renderer/wrappers/help-app.component.tsx
+++ src/renderer/wrappers/help-app.component.tsx
@@ -22,7 +22,7 @@ const HelpApp: FunctionComponent<Props> = ({ history }) => {
   useEffect(() => {
     const fetchData = async () => {
       const { data: response } = await axios.get(
-        `https://${process.env.PDA_CONTENTFUL_HOST}/spaces/${process.env.PDA_CONTENTFUL_SPACE_ID}/environments/${process.env.PDA_CONTENTFUL_ENVIRONMENT}/entries/?access_token=${process.env.PDA_CONTENTFUL_ACCESS_TOKEN}&content_type=pdaHelp`
+        `https://${process.env.PDA_CONTENTFUL_HOST}/spaces/${process.env.PDA_CONTENTFUL_SPACE_ID}/environments/${process.env.PDA_CONTENTFUL_ENVIRONMENT_ID}/entries/?access_token=${process.env.PDA_CONTENTFUL_ACCESS_TOKEN}&content_type=pdaHelp`
       ) // this link has to be replaced with correct one before release
       const normalizeData = normalizeHelpData(response)
       setData(normalizeData)
