2b83e7025168b8a46c5524e75dacc64accac9c4a
staszewski <kamilstaszewski95@gmail.com>
2020-08-10 17:22:02 +0200

    PDA-352 Remove enum

--- src/main/functions/register-help-set-store-listener.ts
+++ src/main/functions/register-help-set-store-listener.ts
@@ -2,10 +2,6 @@ import { ipcMain } from "electron-better-ipc"
 import { HelpActions } from "Common/enums/help-actions.enum"
 import helpStore from "App/main/store/help"
 
-export enum AppLogsEvents {
-  Get = "get-app-logs",
-}
-
 const registerHelpSetStoreListener = () => {
   ipcMain.handle(HelpActions.SetStoreValue, (event, response) => {
     return helpStore.set("data", response)
