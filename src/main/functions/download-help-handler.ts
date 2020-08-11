e837ce9ed8a8ba554274d9c789b1dd3a9225f817
Kamil Staszewski <kamilstaszewski95@gmail.com>
2020-08-11 12:09:40 +0200

    Update src/main/functions/download-help-handler.ts
    Co-authored-by: Mateusz Kocz <mateusz@appnroll.com>

--- src/main/functions/download-help-handler.ts
+++ src/main/functions/download-help-handler.ts
@@ -5,14 +5,14 @@ import { createClient } from "contentful"
 export const registerDownloadHelpHandler = () =>
   ipcMain.handle(
     HelpActions.DownloadContentfulData,
-    async (event, response) => {
+   (event, response) => {
       const client = createClient({
         accessToken: process.env.PDA_CONTENTFUL_ACCESS_TOKEN as string,
         space: process.env.PDA_CONTENTFUL_SPACE_ID as string,
         environment: process.env.PDA_CONTENTFUL_ENVIRONMENT_ID,
         host: process.env.PDA_CONTENTFUL_HOST,
       })
-      return await client.getEntries({ content_type: "helpItem" })
+      return client.getEntries({ content_type: "helpItem" })
     }
   )
 
