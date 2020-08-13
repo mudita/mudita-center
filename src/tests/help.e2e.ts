9a5ff33ad0251ddc5d644d7bd53135fa6fed9752
staszewski <kamilstaszewski95@gmail.com>
2020-08-13 12:54:01 +0200

    Remove top lvl describe

--- src/tests/help.e2e.ts
+++ src/tests/help.e2e.ts
@@ -1,26 +1,24 @@
 import { startApp, stopApp } from "App/tests/hooks"
 import { MenuGropTestIds } from "Renderer/components/rest/menu/menu-grop-test-ids.enum"
 
-describe("Help", () => {
-  let app: any
+let app: any
 
-  beforeEach(async () => {
-    app = await startApp()
-  })
+beforeEach(async () => {
+  app = await startApp()
+})
 
-  afterEach(async () => {
-    await stopApp(app)
-  })
+afterEach(async () => {
+  await stopApp(app)
+})
 
-  test("help link opens new window", async () => {
-    const initialWindowCount = await app.client
-      .waitUntilWindowLoaded()
-      .getWindowCount()
-    expect(initialWindowCount).toEqual(1)
-    await app.client.$(`*[data-testid=${MenuGropTestIds.Help}]`).click()
-    const windowCountAfterHelpClick = await app.client
-      .waitUntilWindowLoaded()
-      .getWindowCount()
-    expect(windowCountAfterHelpClick).toEqual(2)
-  })
+test("help link opens new window", async () => {
+  const initialWindowCount = await app.client
+    .waitUntilWindowLoaded()
+    .getWindowCount()
+  expect(initialWindowCount).toEqual(1)
+  await app.client.$(`*[data-testid=${MenuGropTestIds.Help}]`).click()
+  const windowCountAfterHelpClick = await app.client
+    .waitUntilWindowLoaded()
+    .getWindowCount()
+  expect(windowCountAfterHelpClick).toEqual(2)
 })
