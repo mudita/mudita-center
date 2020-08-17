dda0aeeebc2ea4562c7a19c525a6c63f029aeea9
staszewski <kamilstaszewski95@gmail.com>
2020-08-17 10:12:39 +0200

    PDA-354 Add test that is checking hooks result structure

--- src/renderer/utils/hooks/use-help-search/use-help-search.test.ts
+++ src/renderer/utils/hooks/use-help-search/use-help-search.test.ts
@@ -2,7 +2,12 @@ import { act, renderHook } from "@testing-library/react-hooks"
 import { useHelpSearch } from "Renderer/utils/hooks/use-help-search/use-help-search"
 import { ipcRenderer } from "electron-better-ipc"
 import { HelpActions } from "Common/enums/help-actions.enum"
-import { contentfulSeed, exampleQuestion } from "App/seeds/help"
+import {
+  contentfulSeed,
+  seedAnswers,
+  seedCollectionIds,
+  seedQuestions,
+} from "App/seeds/help"
 import { ChangeEvent } from "react"
 
 const mockIpc = () =>
@@ -36,13 +41,33 @@ test("callback works", async () => {
 test("search works", async () => {
   const { result, waitForNextUpdate } = renderHook(() => useHelpSearch())
   await waitForNextUpdate()
-  const event = { target: { value: exampleQuestion } }
+  const event = { target: { value: seedQuestions[0] } }
   act(() => {
     result.current.searchQuestion(
       (event as unknown) as ChangeEvent<HTMLInputElement>
     )
   })
 
-  expect(result.current.searchValue).toEqual(exampleQuestion)
+  expect(result.current.searchValue).toEqual(seedQuestions[0])
   expect(result.current.data.collection).toHaveLength(1)
+})
+
+test("collection contains desired ids", async () => {
+  const { result, waitForNextUpdate } = renderHook(() => useHelpSearch())
+  await waitForNextUpdate()
+  expect(result.current.data.collection).toEqual(seedCollectionIds)
+})
+
+test("result has desired structure", async () => {
+  const { result, waitForNextUpdate } = renderHook(() => useHelpSearch())
+  await waitForNextUpdate()
+  seedCollectionIds.forEach((id, index) => {
+    expect(result.current.data.items[id]).toEqual(
+      expect.objectContaining({
+        id,
+        question: seedQuestions[index],
+        answer: seedAnswers[index],
+      })
+    )
+  })
 })
--- src/seeds/help.ts
+++ src/seeds/help.ts
@@ -51,7 +51,49 @@ const answer: Document = {
   ],
 }
 
-export const exampleQuestion = "aaaasdsadia11"
+export const seedQuestions = ["aaaasdsadia11", "Loremasdjadj1lalaa"]
+export const seedCollectionIds = [
+  "24YEjwJx8jAuedvWDz8rvU",
+  "1NESOKKWZCTjV8rlSE4JbH",
+]
+export const seedAnswers = [
+  {
+    nodeType: "document",
+    data: {},
+    content: [
+      {
+        nodeType: "heading-1",
+        content: [
+          {
+            nodeType: "text",
+            value: "lala",
+            marks: [],
+            data: {},
+          },
+        ],
+        data: {},
+      },
+    ],
+  },
+  {
+    nodeType: "document",
+    data: {},
+    content: [
+      {
+        nodeType: "heading-2",
+        content: [
+          {
+            nodeType: "text",
+            value: "1231231lala",
+            marks: [],
+            data: {},
+          },
+        ],
+        data: {},
+      },
+    ],
+  },
+]
 
 export const contentfulSeed = {
   sys: {
@@ -71,7 +113,7 @@ export const contentfulSeed = {
           },
         },
         type: "Entry",
-        id: "24YEjwJx8jAuedvWDz8rvU",
+        id: seedCollectionIds[0],
         contentType: {
           sys: {
             type: "Link",
@@ -92,37 +134,8 @@ export const contentfulSeed = {
         locale: "en-US",
       },
       fields: {
-        question: exampleQuestion,
-        answer: {
-          nodeType: "document",
-          data: {},
-          content: [
-            {
-              nodeType: "heading-1",
-              content: [
-                {
-                  nodeType: "text",
-                  value: "lala",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "paragraph",
-              content: [
-                {
-                  nodeType: "text",
-                  value: "",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-          ],
-        },
+        question: seedQuestions[0],
+        answer: seedAnswers[0],
         slug: "example-question-2",
       },
     },
@@ -136,7 +149,7 @@ export const contentfulSeed = {
           },
         },
         type: "Entry",
-        id: "1NESOKKWZCTjV8rlSE4JbH",
+        id: seedCollectionIds[1],
         contentType: {
           sys: {
             type: "Link",
@@ -157,90 +170,8 @@ export const contentfulSeed = {
         locale: "en-US",
       },
       fields: {
-        question: "asdjadj1lalaa",
-        answer: {
-          nodeType: "document",
-          data: {},
-          content: [
-            {
-              nodeType: "heading-2",
-              content: [
-                {
-                  nodeType: "text",
-                  value:
-                    "Consectetur adipiscing elit. Fusce imperdiet nisi odio, et iaculis justo sagittis non.",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "paragraph",
-              content: [
-                {
-                  nodeType: "text",
-                  value:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus diam neque, varius ac fermentum sit amet, interdum in metus. Vivamus eleifend turpis nec accumsan mollis.",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "paragraph",
-              content: [
-                {
-                  nodeType: "text",
-                  value:
-                    "Sed nunc erat, tempor vel risus nec, consectetur lobortis lectus. Maecenas ultricies ex mi, quis consequat est cursus ut. Phasellus ut ante quis metus lacinia lacinia a non ante. Etiam ut libero sit amet sem rutrum mollis quis sed sapien. Donec vitae lacus vitae odio auctor rhoncus et sed ipsum. Pellentesque ac viverra turpis. Aliquam posuere lorem non orci placerat venenatis. Mauris posuere consectetur orci sed sodales.",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "heading-3",
-              content: [
-                {
-                  nodeType: "text",
-                  value:
-                    "Maecenas ultricies ex mi, quis consequat est cursus ut.",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "paragraph",
-              content: [
-                {
-                  nodeType: "text",
-                  value:
-                    "Sed nunc erat, tempor vel risus nec, consectetur lobortis lectus. Maecenas ultricies ex mi, quis consequat est cursus ut. Phasellus ut ante quis metus lacinia lacinia a non ante. Etiam ut libero sit amet sem rutrum mollis quis sed sapien. Donec vitae lacus vitae odio auctor rhoncus et sed ipsum.",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-            {
-              nodeType: "paragraph",
-              content: [
-                {
-                  nodeType: "text",
-                  value: "",
-                  marks: [],
-                  data: {},
-                },
-              ],
-              data: {},
-            },
-          ],
-        },
+        question: seedQuestions[1],
+        answer: seedAnswers[1],
         slug: "example-question-lala",
       },
     },
