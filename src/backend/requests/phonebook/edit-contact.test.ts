6bc1742de118c55092ef63e250a431131636392d
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-08-17 14:11:58 +0200

    Tests

--- src/backend/requests/phonebook/edit-contact.test.ts
+++ src/backend/requests/phonebook/edit-contact.test.ts
@@ -3,9 +3,9 @@ import { ipcMain } from "electron-better-ipc"
 import { IpcRequest } from "Common/requests/ipc-request.enum"
 import { DeviceResponseStatus } from "Backend/adapters/device-response.interface"
 import registerEditContactRequest from "Backend/requests/phonebook/edit-contact.request"
-import { generateFakeContact } from "Renderer/models/phone/phone.utils"
+import { phoneSeed } from "App/seeds/phone"
 
-const fakeContact = generateFakeContact()
+const fakeContact = phoneSeed.db[0]
 
 test("edit contact properly", () => {
   registerEditContactRequest(getFakeAdapters())
