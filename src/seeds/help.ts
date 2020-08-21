90a060198cbda917bd20e1b0bc11be13fbb082b2
staszewski <kamilstaszewski95@gmail.com>
2020-08-21 12:59:33 +0200

    PDA-350 Fix ts errors in help seed

--- src/seeds/help.ts
+++ src/seeds/help.ts
@@ -193,49 +193,75 @@ export const data: QuestionAndAnswer = {
   items: {
     "e66895c6-3d65-4f96-8c81-e58c43ec6aee": {
       id: "e66895c6-3d65-4f96-8c81-e58c43ec6aee",
-      question:
-        "Fuga tenetur necessitatibus perferendis aliquid officia recusandae et.",
-      answer,
+      question: {
+        ["en-US"]:
+          "Fuga tenetur necessitatibus perferendis aliquid officia recusandae et.",
+      },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "73f44c12-5c00-433f-a9cf-e7e503fa2594": {
       id: "73f44c12-5c00-433f-a9cf-e7e503fa2594",
-      question: "Delectus ea distinctio dolore ea.",
-      answer,
+      question: { ["en-US"]: "Delectus ea distinctio dolore ea." },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "a042a5a8-6478-4e2b-8040-36b97497740f": {
       id: "a042a5a8-6478-4e2b-8040-36b97497740f",
-      question: "Odio id eligendi aliquid.",
-      answer,
+      question: { ["en-US"]: "Odio id eligendi aliquid." },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "832b39ed-140b-476c-b1d8-6de8e441e3c2": {
       id: "832b39ed-140b-476c-b1d8-6de8e441e3c2",
-      question: "Odio id eligendi aliquid.",
-      answer,
+      question: { ["en-US"]: "Odio id eligendi aliquid." },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "6d959bb2-489e-4419-a107-60abaa745d5c1": {
       id: "6d959bb2-489e-4419-a107-60abaa745d5c1",
-      question: "Autem porro consectetur voluptatem.",
-      answer,
+      question: { ["en-US"]: "Autem porro consectetur voluptatem." },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "6d959bb2-489e-4419-a107-60abaa745d5c2": {
       id: "6d959bb2-489e-4419-a107-60abaa745d5c2",
-      question: "Autem porro consectetur voluptatem.",
-      answer,
+      question: { ["en-US"]: "Autem porro consectetur voluptatem." },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "3c0c7f13-5b84-4da8-a7ea-e66a654b7c7f": {
       id: "3c0c7f13-5b84-4da8-a7ea-e66a654b7c7f",
-      question: "Laboriosam suscipit ut aperiam molestiae veritatis.",
-      answer,
+      question: {
+        ["en-US"]: "Laboriosam suscipit ut aperiam molestiae veritatis.",
+      },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "4c0c7f13-5b84-4da8-a7ea-e66a654b7c7f": {
       id: "4c0c7f13-5b84-4da8-a7ea-e66a654b7c7f",
-      question: "Laboriosam suscipit ut aperiam molestiae veritatis.",
-      answer,
+      question: {
+        ["en-US"]: "Laboriosam suscipit ut aperiam molestiae veritatis.",
+      },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
     "116d959bb2-489e-4419-a107-60abaa745d5c2": {
       id: "116d959bb2-489e-4419-a107-60abaa745d5c2",
-      question: "Saepe non quasi at ipsa autem molestias et consequuntur.",
-      answer,
+      question: {
+        ["en-US"]: "Saepe non quasi at ipsa autem molestias et consequuntur.",
+      },
+      answer: {
+        ["en-US"]: answer,
+      },
     },
   },
 }
