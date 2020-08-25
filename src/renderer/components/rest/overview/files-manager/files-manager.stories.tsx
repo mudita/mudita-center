4f1853ca52f4c5118608db83694476fd2e97e726
staszewski <kamilstaszewski95@gmail.com>
2020-08-25 11:03:38 +0200

    PDA-343 Fix broken story

--- src/renderer/components/rest/overview/files-manager/files-manager.stories.tsx
+++ src/renderer/components/rest/overview/files-manager/files-manager.stories.tsx
@@ -1,4 +1,3 @@
-import { storiesOf } from "@storybook/react"
 import React from "react"
 import FilesManager from "Renderer/components/rest/overview/files-manager/files-manager.component"
 import styled from "styled-components"
@@ -6,6 +5,8 @@ import Text, {
   TextDisplayStyle,
 } from "Renderer/components/core/text/text.component"
 import { action } from "@storybook/addon-actions"
+import { Story, Meta } from "@storybook/react/types-6-0"
+import { MemoryRouter } from "react-router"
 
 const Part = styled.div`
   padding: 2rem;
@@ -14,38 +15,44 @@ const Part = styled.div`
   }
 `
 
-storiesOf("Overview|FilesManager", module).add("FilesManager", () => {
-  return (
-    <div style={{ maxWidth: "63rem" }}>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>No space used</Text>
-        <FilesManager
-          usedSpace={0}
-          onFilesOpen={action("open files manager")}
-        />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>Custom parameters</Text>
-        <FilesManager
-          usedSpace={29}
-          maxSpace={512}
-          onFilesOpen={action("open files manager")}
-        />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>Half space used</Text>
-        <FilesManager
-          usedSpace={7.99}
-          onFilesOpen={action("open files manager")}
-        />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>Full space used</Text>
-        <FilesManager
-          usedSpace={16}
-          onFilesOpen={action("open files manager")}
-        />
-      </Part>
-    </div>
-  )
-})
+export const DifferentSpaceUsed = () => (
+  <>
+    <Part>
+      <Text displayStyle={TextDisplayStyle.SmallText}>No space used</Text>
+      <FilesManager usedSpace={0} onFilesOpen={action("open files manager")} />
+    </Part>
+    <Part>
+      <Text displayStyle={TextDisplayStyle.SmallText}>Custom parameters</Text>
+      <FilesManager
+        usedSpace={29}
+        maxSpace={512}
+        onFilesOpen={action("open files manager")}
+      />
+    </Part>
+    <Part>
+      <Text displayStyle={TextDisplayStyle.SmallText}>Half space used</Text>
+      <FilesManager
+        usedSpace={7.99}
+        onFilesOpen={action("open files manager")}
+      />
+    </Part>
+    <Part>
+      <Text displayStyle={TextDisplayStyle.SmallText}>Full space used</Text>
+      <FilesManager usedSpace={16} onFilesOpen={action("open files manager")} />
+    </Part>
+  </>
+)
+
+export default {
+  title: "Overview/FilesManager",
+  component: DifferentSpaceUsed,
+  decorators: [
+    (Story: Story) => (
+      <div style={{ maxWidth: "63rem" }}>
+        <MemoryRouter initialEntries={["/phone"]}>
+          <Story />
+        </MemoryRouter>
+      </div>
+    ),
+  ],
+} as Meta
