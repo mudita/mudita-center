8fd547aeae7a130fb27478ebacbccb6e4e1ad703
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-09-03 11:13:46 +0200

    Changed TODO comment

--- src/renderer/utils/log.ts
+++ src/renderer/utils/log.ts
@@ -4,10 +4,7 @@ let log: Console | ElectronLog
 
 if (window.require) {
   log = window.require("electron-log")
-  // TODO: add a remote url to send logs to the specified the server or use Rollbar
-  // See also src/main/main.ts
-  // log.transports.remote.level = "warn"
-  // log.transports.remote.url = "http://localhost:3000/log"
+  // TODO: add a Rollbar
 } else {
   log = console
 }
