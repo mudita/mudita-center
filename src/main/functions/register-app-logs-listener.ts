dfcd8ea7075bba397a8dca325f558b6aa4c8fd65
Michał Kurczewski <michalkurczewski94@gmail.com>
2020-09-04 17:35:56 +0200

    Wrote new logs request and registrar functions

--- src/main/functions/register-app-logs-listener.ts
+++ src/main/functions/register-app-logs-listener.ts
@@ -1,18 +1,33 @@
 import fs from "fs-extra"
-import log from "electron-log"
 import { ipcMain } from "electron-better-ipc"
+import { name } from "../../../package.json"
+import { app } from "electron"
+import logger from "App/main/utils/logger"
+import path from "path"
 
 export enum AppLogsEvents {
   Get = "get-app-logs",
 }
 
 const registerAppLogsListeners = () => {
-  ipcMain.answerRenderer(
-    AppLogsEvents.Get,
-    (path: string = log.transports.file.getFile().path) => {
-      return `${fs.readFileSync(path)}`
+  ipcMain.answerRenderer(AppLogsEvents.Get, async () => {
+    const logs: string[] = []
+    try {
+      const logsPath = `${app.getPath("appData")}/${name}/logs`
+      const files = await fs.readdir(logsPath)
+
+      for (const fileName of files) {
+        if (/^pda-.*\.log$/.test(fileName)) {
+          const log = await fs.readFile(path.join(logsPath, fileName), "utf-8")
+          logs.push(`========== ${fileName} ==========`)
+          logs.push(log + "\n")
+        }
+      }
+    } catch (error) {
+      logger.error(error)
     }
-  )
+    return logs.join("\n")
+  })
 }
 
 export default registerAppLogsListeners
--- src/renderer/requests/app-logs.request.ts
+++ src/renderer/requests/app-logs.request.ts
@@ -1,41 +1,28 @@
-import { ipcRenderer } from "electron-better-ipc"
-import { AppLogsEvents } from "App/main/functions/register-app-logs-listener"
 import { defineMessages } from "react-intl"
 import { intl } from "Renderer/utils/intl"
 import { version } from "../../../package.json"
 import os from "os"
-import log from "Renderer/utils/log"
-import { ElectronLog } from "electron-log"
+import { ipcRenderer } from "electron-better-ipc"
+import { AppLogsEvents } from "App/main/functions/register-app-logs-listener"
 
-export const getMainAppLogs = (): Promise<string> => {
-  return ipcRenderer.callMain(AppLogsEvents.Get)
-}
+const messages = defineMessages({
+  appVersion: {
+    id: "component.modal.support.details.appVersion",
+  },
+  os: {
+    id: "component.modal.support.details.os",
+  },
+})
 
-export const getRendererAppLogs = (): Promise<string> => {
-  const path = (log as ElectronLog).transports.file.getFile().path
-  return ipcRenderer.callMain(AppLogsEvents.Get, path)
-}
-
-export const getFullAppLogs = async (): Promise<string> => {
-  const messages = defineMessages({
-    appVersion: {
-      id: "component.modal.support.details.appVersion",
-    },
-    os: {
-      id: "component.modal.support.details.os",
-    },
-  })
-
-  const mainAppLogs = await getMainAppLogs()
-
-  const rendererAppLogs = await getRendererAppLogs()
+export const getAppLogs = async (): Promise<string> => {
+  const appLogs = await ipcRenderer.callMain(AppLogsEvents.Get)
 
   const logParts = [
     `${intl.formatMessage(messages.appVersion)}: ${version}`,
     `${intl.formatMessage(
       messages.os
     )}: ${os.platform()} ${os.arch()}, ${os.release()}`,
-    `Main logs: \n${mainAppLogs}\n\nRenderer logs:\n${rendererAppLogs}`,
+    appLogs,
   ]
 
   return logParts.join("\n")
