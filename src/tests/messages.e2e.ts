20471d95004d5b74706149330352e9688dc08868
staszewski <kamilstaszewski95@gmail.com>
2020-09-07 09:17:33 +0200

    One more expect for checking if the value that we are looking for renders in single row that we have on screen

--- src/tests/messages.e2e.ts
+++ src/tests/messages.e2e.ts
@@ -11,7 +11,7 @@ afterEach(async () => {
   await stopApp(app)
 })
 
-test("cos", async () => {
+test("searching by phone number renders one result", async () => {
   await app.client.$(`*[data-testid=${MenuGropTestIds.Messages}]`).click()
   const phoneNumber = await app.client
     .$(`*[data-testid='message-row']`)
@@ -20,4 +20,9 @@ test("cos", async () => {
   await app.client.$("input").setValue(phoneNumber)
   const rows = await app.client.$$(`*[data-testid='message-row']`)
   expect(rows).toHaveLength(1)
+  const resultsText = await app.client
+    .$(`*[data-testid='message-row']`)
+    .$("p")
+    .getText()
+  expect(resultsText).toBe(await app.client.$("input").getValue())
 })
