10caf72aa2386e67e0178618cc35fc346a409780
staszewski <kamilstaszewski95@gmail.com>
2020-09-08 13:14:01 +0200

    Use enum in message-row

--- src/tests/messages.e2e.ts
+++ src/tests/messages.e2e.ts
@@ -1,5 +1,6 @@
 import { startApp, stopApp } from "App/tests/hooks"
 import { MenuGroupTestIds } from "Renderer/components/rest/menu/menu-group-test-ids.enum"
+import { MessagesListTestIds } from "Renderer/modules/messages/messages-list-test-ids.enum"
 
 let app: any
 
@@ -18,7 +19,7 @@ test("searching by phone number renders one result", async () => {
     .$("p")
     .getText()
   await app.client.$("input").setValue(phoneNumber)
-  const rows = await app.client.$$(`*[data-testid='message-row']`)
+  const rows = await app.client.$$(`*[data-testid=${MessagesListTestIds.Row}]`)
   expect(rows).toHaveLength(1)
   const resultsText = await app.client
     .$(`*[data-testid='message-row']`)
