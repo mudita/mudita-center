d2cd2f7d506a773d1cf56c293eeaabf31b2ac7bb
staszewski <kamilstaszewski95@gmail.com>
2020-09-09 09:06:09 +0200

    PDA-340 Use phone seed in contact-panel test

--- src/renderer/components/rest/phone/contact-panel.test.tsx
+++ src/renderer/components/rest/phone/contact-panel.test.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import ContactPanel from "Renderer/components/rest/phone/contact-panel.component"
 import { fireEvent } from "@testing-library/dom"
 import { ContactPanelTestIdsEnum } from "Renderer/components/rest/phone/contact-panel-test-ids.enum"
+import { phoneSeedInput } from "App/seeds/phone"
 
 const defaultProps = {
   onSearchTermChange: jest.fn(),
@@ -27,18 +28,7 @@ test("search works", () => {
 
 test("selection manager is displayed when there is at least one contact selected", () => {
   const { getByTestId } = renderer({
-    selectedContacts: [
-      {
-        id: "f5b8b756-62fb-4580-a101-2c4b1aa27c0b",
-        firstName: "Anya",
-        lastName: "VonRueden",
-        secondaryPhoneNumber: "+83489610269",
-        note: "fugiat qui",
-        ice: true,
-        favourite: true,
-        speedDial: 3,
-      },
-    ],
+    selectedContacts: [phoneSeedInput[0]],
   })
   expect(
     getByTestId(ContactPanelTestIdsEnum.SelectionManager)
