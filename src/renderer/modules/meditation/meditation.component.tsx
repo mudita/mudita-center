22a82168b57265b1ccb21827b406b0a9a0e1262e
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-09-11 11:44:28 +0200

    meditation ui

--- src/renderer/modules/meditation/meditation.component.tsx
+++ src/renderer/modules/meditation/meditation.component.tsx
@@ -1,16 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { defineMessages } from "react-intl"
+import { intl } from "Renderer/utils/intl"
+
 import { FunctionComponent } from "Renderer/types/function-component.interface"
-import DataBoxes from "Renderer/components/rest/meditation/data-boxes.component"
-import styled from "styled-components"
+import DataBoxes from "Renderer/components/rest/meditation/data-box/data-boxes.component"
+import MeditationNav from "Renderer/components/rest/meditation/nav/meditation-nav.component"
+import MeditationStats from "Renderer/components/rest/meditation/stats/meditation-stats.component"
+import ButtonToggler, {
+  ButtonTogglerItem,
+} from "Renderer/components/core/button-toggler/button-toggler.component"
+import { ChartType } from "Renderer/components/rest/meditation/stats/meditation-stats.enum"
+import { generateMeditationData } from "App/__mocks__/meditation-stats.mock"
+import {
+  MeditationStatsWrapper,
+  MeditationWrapper,
+  NavigationWrapper,
+} from "Renderer/modules/meditation/meditation.styled"
 
-const MeditationWrapper = styled.div`
-  padding: 0 3rem 0 4rem;
-`
+const messages = defineMessages({
+  weekly: {
+    id: "view.generic.weekly",
+  },
+  monthly: {
+    id: "view.generic.monthly",
+  },
+  yearly: {
+    id: "view.generic.yearly",
+  },
+})
 
-const Meditation: FunctionComponent = () => (
-  <MeditationWrapper>
-    <DataBoxes />
-  </MeditationWrapper>
-)
+const Meditation: FunctionComponent = () => {
+  const [chartType, setChartType] = useState<ChartType>(ChartType.Weekly)
+
+  const selectChartType = (type: ChartType) => {
+    return () => {
+      setChartType(type)
+    }
+  }
+
+  return (
+    <MeditationWrapper>
+      <NavigationWrapper>
+        <ButtonToggler>
+          <ButtonTogglerItem
+            active={chartType === ChartType.Weekly}
+            label={intl.formatMessage(messages.weekly)}
+            onClick={selectChartType(ChartType.Weekly)}
+          />
+          <ButtonTogglerItem
+            active={chartType === ChartType.Monthly}
+            label={intl.formatMessage(messages.monthly)}
+            onClick={selectChartType(ChartType.Monthly)}
+          />
+          <ButtonTogglerItem
+            active={chartType === ChartType.Yearly}
+            label={intl.formatMessage(messages.yearly)}
+            onClick={selectChartType(ChartType.Yearly)}
+          />
+        </ButtonToggler>
+        <MeditationNav startDate={"2020-09-11"} />
+      </NavigationWrapper>
+      <MeditationStatsWrapper>
+        <MeditationStats
+          chartType={chartType}
+          statsData={generateMeditationData(chartType)}
+        />
+      </MeditationStatsWrapper>
+      <DataBoxes />
+    </MeditationWrapper>
+  )
+}
 
 export default Meditation
