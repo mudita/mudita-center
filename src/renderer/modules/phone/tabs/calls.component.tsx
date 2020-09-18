1e895431ea11dc4209912e1bab30fb9a7ba60f6f
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-09-18 10:26:17 +0200

    modal added

--- src/renderer/modules/phone/tabs/calls.component.tsx
+++ src/renderer/modules/phone/tabs/calls.component.tsx
@@ -6,6 +6,17 @@ import { VisibilityFilter } from "App/renderer/models/calls/calls.interface"
 import CallsHeader from "Renderer/components/rest/calls/calls-header.component"
 import CallsTable from "Renderer/components/rest/calls/calls-table.component"
 import useTableSelect from "Renderer/utils/hooks/useTableSelect"
+import modalService from "Renderer/components/core/modal/modal.service"
+import { defineMessages } from "react-intl"
+import { intl, textFormatters } from "Renderer/utils/intl"
+
+import DeleteModal from "Renderer/components/core/modal/delete-modal.component"
+import { createFullName } from "Renderer/models/phone/phone.helpers"
+
+const messages = defineMessages({
+  title: { id: "view.name.calls.deleteTitle" },
+  body: { id: "view.name.calls.deleteBody" },
+})
 
 interface Props {
   changeVisibilityFilter?: (filter: VisibilityFilter) => void
@@ -27,6 +38,31 @@ const Calls: FunctionComponent<Props> = ({
     allRowsSelected,
     resetRows,
   } = useTableSelect<Details>(calls)
+
+  const openDeleteModal = (details: Details) => {
+    const callerName = createFullName(details.caller)
+    const callsCount = details.timesMissed || 1
+    const modalConfig = {
+      title: intl.formatMessage({ ...messages.title }),
+      message: {
+        ...messages.body,
+        values: { ...textFormatters, num: callsCount, name: callerName },
+      },
+    }
+
+    const handleDelete = async () => {
+      modalService.rerenderModal(<DeleteModal {...modalConfig} deleting />)
+
+      deleteCall([details.id])
+
+      await modalService.closeModal()
+    }
+
+    modalService.openModal(
+      <DeleteModal {...modalConfig} onDelete={handleDelete} />
+    )
+  }
+
   return (
     <>
       <CallsHeader
@@ -38,7 +74,7 @@ const Calls: FunctionComponent<Props> = ({
         resetRows={resetRows}
       />
       <CallsTable
-        deleteCall={deleteCall}
+        deleteCall={openDeleteModal}
         calls={calls}
         getRowStatus={getRowStatus}
         toggleRow={toggleRow}
