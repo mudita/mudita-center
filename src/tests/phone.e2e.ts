998b75975aa5567b0a75a8917cae129c3a97fd42
staszewski <kamilstaszewski95@gmail.com>
2020-09-28 13:21:32 +0200

    Use getEnum helper in tests

--- src/tests/phone.e2e.ts
+++ src/tests/phone.e2e.ts
@@ -2,6 +2,7 @@ import { startApp, stopApp } from "App/tests/hooks"
 import { MenuGroupTestIds } from "Renderer/components/rest/menu/menu-group-test-ids.enum"
 import localeEn from "Renderer/locales/default/en-US.json"
 import { VisibilityFilter } from "Renderer/models/calls/calls.interface"
+import { Type, getEnumName } from "Renderer/components/core/icon/icon.config"
 
 let app: any
 
@@ -24,13 +25,19 @@ test("receiving calls filter works correctly", async () => {
   await app.client.$(`*[data-testid=${MenuGroupTestIds.Phone}]`).click()
   await app.client.$(`*[data-testid=${VisibilityFilter.Received}]`).click()
   expect(
-    await app.client.isExisting("*[data-testid='icon-IncomingCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.IncomingCall)}']`
+    )
   ).toBeTruthy()
   expect(
-    await app.client.isExisting("*[data-testid='icon-MissedCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.MissedCall)}']`
+    )
   ).toBeFalsy()
   expect(
-    await app.client.isExisting("*[data-testid='icon-ConferenceCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.ConferenceCall)}']`
+    )
   ).toBeFalsy()
 })
 
@@ -38,12 +45,18 @@ test("missed calls filter works correctly", async () => {
   await app.client.$(`*[data-testid=${MenuGroupTestIds.Phone}]`).click()
   await app.client.$(`*[data-testid=${VisibilityFilter.Missed}]`).click()
   expect(
-    await app.client.isExisting("*[data-testid='icon-MissedCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.MissedCall)}']`
+    )
   ).toBeTruthy()
   expect(
-    await app.client.isExisting("*[data-testid='icon-IncomingCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.IncomingCall)}']`
+    )
   ).toBeFalsy()
   expect(
-    await app.client.isExisting("*[data-testid='icon-ConferenceCall']")
+    await app.client.isExisting(
+      `*[data-testid='icon-${getEnumName(Type.ConferenceCall)}']`
+    )
   ).toBeFalsy()
 })
