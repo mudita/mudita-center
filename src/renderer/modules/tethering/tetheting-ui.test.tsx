7ee6b927bff572db4064db6cb61b3b54cd1a902f
Kamil Staszewski <kamilstaszewski95@gmail.com>
2020-09-29 11:25:31 +0200

    Update src/renderer/modules/tethering/tetheting-ui.test.tsx
    Co-authored-by: Michał Kurczewski <m.kurczewski@appnroll.com>

--- src/renderer/modules/tethering/tetheting-ui.test.tsx
+++ src/renderer/modules/tethering/tetheting-ui.test.tsx
@@ -44,7 +44,7 @@ describe("Pure disconnected screen tests", () => {
     ).toBeInTheDocument()
   })
 
-  test("third notification is visible", () => {
+  test("modem notification is visible", () => {
     const { queryByTestId } = renderer(props)
     expect(
       queryByTestId(TetheringTestIds.ModemNotification)
