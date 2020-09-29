74364e2c5ccc3bc3abc58626433a01ab810edfbf
Daniel Karski <daniel.karski5q@gmail.com>
2020-09-29 11:20:03 +0200

    Changed Checkbox to VisibleCheckbox for ContactList

--- src/renderer/components/rest/phone/contact-list.component.tsx
+++ src/renderer/components/rest/phone/contact-list.component.tsx
@@ -12,9 +12,9 @@ import Table, {
   TextPlaceholder,
 } from "Renderer/components/core/table/table.component"
 import { UseTableSelect } from "Renderer/utils/hooks/useTableSelect"
-import InputCheckbox, {
-  Size,
-} from "Renderer/components/core/input-checkbox/input-checkbox.component"
+import { VisibleCheckbox } from "Renderer/components/rest/visible-checkbox/visible-checkbox"
+import { animatedOpacityActiveStyles } from "Renderer/components/rest/animated-opacity/animated-opacity"
+import { Size } from "Renderer/components/core/input-checkbox/input-checkbox.component"
 import Avatar, {
   AvatarSize,
   basicAvatarStyles,
@@ -23,8 +23,6 @@ import {
   backgroundColor,
   borderRadius,
   textColor,
-  transitionTime,
-  transitionTimingFunction,
 } from "Renderer/styles/theming/theme-getters"
 import Text, {
   TextDisplayStyle,
@@ -45,22 +43,6 @@ import {
   NewContact,
   ResultsState,
 } from "Renderer/models/phone/phone.typings"
-
-const visibleCheckboxStyles = css`
-  opacity: 1;
-  visibility: visible;
-`
-
-const Checkbox = styled(InputCheckbox)<{ visible?: boolean }>`
-  opacity: 0;
-  visibility: hidden;
-  transition: opacity ${transitionTime("faster")}
-      ${transitionTimingFunction("smooth")},
-    visibility ${transitionTime("faster")} ${transitionTimingFunction("smooth")};
-  margin: 0 auto;
-
-  ${({ visible }) => visible && visibleCheckboxStyles};
-`
 
 export const lightAvatarStyles = css`
   background-color: ${backgroundColor("row")};
@@ -121,8 +103,8 @@ const SelectableContacts = styled(Table)<{ mouseLock?: boolean }>`
 
   ${Row} {
     :hover {
-      ${Checkbox} {
-        ${visibleCheckboxStyles};
+      ${VisibleCheckbox} {
+        ${animatedOpacityActiveStyles};
       }
       ${InitialsAvatar} {
         ${lightAvatarStyles};
@@ -233,7 +215,7 @@ const ContactList: FunctionComponent<ContactListProps> = ({
                     ref={ref}
                   >
                     <Col>
-                      <Checkbox
+                      <VisibleCheckbox
                         checked={selected}
                         onChange={onChange}
                         size={Size.Small}
