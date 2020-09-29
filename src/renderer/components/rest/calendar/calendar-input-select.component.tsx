9e3d2abc81c2cb8bc5d9927056ca934aad5d0f32
Daniel Karski <daniel.karski5q@gmail.com>
2020-09-29 08:43:59 +0200

    Created CalendarInputSelect basing on InputSelect

---
+++ src/renderer/components/rest/calendar/calendar-input-select.component.tsx
@@ -0,0 +1,100 @@
+import React from "react"
+import { FunctionComponent } from "Renderer/types/function-component.interface"
+import styled, { css } from "styled-components"
+import { defineMessages, FormattedDate } from "react-intl"
+import { intl } from "Renderer/utils/intl"
+import InputSelect, {
+  RenderListItemProps,
+  renderSearchableText,
+  SelectInputItem,
+} from "Renderer/components/core/input-select/input-select.component"
+import { CalendarEvent } from "Renderer/modules/calendar/calendar.interface"
+import { TimeWindow } from "Renderer/components/rest/calendar/time-window.component"
+import { textColor } from "Renderer/styles/theming/theme-getters"
+
+const messages = defineMessages({
+  searchPlaceholder: { id: "view.name.calendar.panel.searchPlaceholder" },
+})
+
+const ListItem = styled(SelectInputItem)`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ListItemDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 12rem;
+  text-align: right;
+  color: ${textColor("secondary")};
+
+  span:first-of-type {
+    margin-bottom: 0.8rem;
+  }
+`
+
+const renderListItem = ({
+  item: { name, date },
+  searchString,
+  props,
+}: RenderListItemProps<CalendarEvent>) => {
+  const [startDate] = date
+  return (
+    <ListItem {...props}>
+      <span>{renderSearchableText(name, searchString)}</span>
+      <ListItemDate>
+        <span>
+          <FormattedDate
+            value={startDate}
+            year="numeric"
+            month="short"
+            day="2-digit"
+            weekday="short"
+          />
+        </span>
+        <span>
+          <TimeWindow date={date} />
+        </span>
+      </ListItemDate>
+    </ListItem>
+  )
+}
+
+const renderCustomValue = (item: CalendarEvent) => item.name
+
+const filteringFunction = (item: CalendarEvent, search: string) => {
+  return item.name.toLowerCase().includes(search.toLowerCase())
+}
+
+interface Props {
+  events: CalendarEvent[]
+  eventSelected?: CalendarEvent
+  onEventSelect: (option: CalendarEvent) => void
+}
+
+const CalendarInputSelect: FunctionComponent<Props> = ({
+  events,
+  eventSelected,
+  onEventSelect,
+  ...props
+}) => {
+  return (
+    <InputSelect
+      searchable
+      outlined
+      options={events}
+      value={eventSelected}
+      onSelect={onEventSelect}
+      label={intl.formatMessage(messages.searchPlaceholder)}
+      renderValue={renderCustomValue}
+      filteringFunction={filteringFunction}
+      renderListItem={renderListItem}
+      listStyles={css`
+        max-height: 19rem;
+      `}
+      {...props}
+    />
+  )
+}
+
+export default CalendarInputSelect
--- src/renderer/components/rest/calendar/calendar.stories.tsx
+++ src/renderer/components/rest/calendar/calendar.stories.tsx
@@ -1,5 +1,7 @@
 import { storiesOf } from "@storybook/react"
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
+import { calendarSeed } from "App/seeds/calendar"
 import {
   SyncCalendarModal,
   SynchronizingFailedModal,
@@ -8,6 +10,8 @@ import {
 } from "Renderer/components/rest/calendar/calendar.modals"
 import StoryContainer from "Renderer/components/storybook/story-container.component"
 import Story from "Renderer/components/storybook/story.component"
+import CalendarInputSelect from "Renderer/components/rest/calendar/calendar-input-select.component"
+import { CalendarEvent } from "Renderer/modules/calendar/calendar.interface"
 
 storiesOf("Views/Calendar/Modals", module).add("All", () => {
   return (
@@ -25,5 +29,22 @@ storiesOf("Views/Calendar/Modals", module).add("All", () => {
         <SyncCalendarModal />
       </Story>
     </StoryContainer>
+  )
+})
+
+const InputSelect = styled(CalendarInputSelect)`
+  min-width: 38rem;
+`
+
+storiesOf("Views/Calendar", module).add("Input", () => {
+  const [eventSelected, setEventSelected] = useState<CalendarEvent>()
+  return (
+    <Story title="Input Select">
+      <InputSelect
+        events={calendarSeed}
+        eventSelected={eventSelected}
+        onEventSelect={setEventSelected}
+      />
+    </Story>
   )
 })
