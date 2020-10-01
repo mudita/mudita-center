8c8e8997d4f59537158374731c66d3b64311f1b8
staszewski <kamilstaszewski95@gmail.com>
2020-10-01 10:43:45 +0200

    PDA-387 Remove sort example with hook in stories. Fix errors in notes story

--- src/renderer/modules/tools/tabs/notes.stories.tsx
+++ src/renderer/modules/tools/tabs/notes.stories.tsx
@@ -3,15 +3,21 @@ import { storiesOf } from "@storybook/react"
 import { notesSeed } from "App/seeds/notes"
 import Notes from "Renderer/modules/tools/tabs/notes.component"
 import AppViewStory from "Renderer/components/rest/storybook/app-view.component"
+import { action } from "@storybook/addon-actions"
+
+const commonProps = {
+  sortDescending: true,
+  toggleSortOrder: action("toggle sort order"),
+}
 
 storiesOf("Views|Tools/Notes", module)
   .add("Default", () => (
     <AppViewStory>
-      <Notes notesList={notesSeed.notesList} />
+      <Notes notesList={notesSeed.notesList} {...commonProps} />
     </AppViewStory>
   ))
   .add("Empty", () => (
     <AppViewStory>
-      <Notes notesList={[]} />
+      <Notes notesList={[]} {...commonProps} />
     </AppViewStory>
   ))
