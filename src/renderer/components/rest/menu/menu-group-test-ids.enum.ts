20f10a07805e34cf27de5e79f8a1dd4aa718b21c
staszewski <kamilstaszewski95@gmail.com>
2020-10-02 14:07:54 +0200

    Add tests for screen that are not developed yet, just to check out if navigation works correctly

--- src/renderer/components/rest/menu/menu-group-test-ids.enum.ts
+++ src/renderer/components/rest/menu/menu-group-test-ids.enum.ts
@@ -2,4 +2,6 @@ export enum MenuGroupTestIds {
   Help = "help-menu-button",
   Messages = "overview-menu-link",
   Phone = "phone-menu-link",
+  FilesManager = "files-manager-link",
+  Music = "music-link",
 }
