e54e86d93228b6f0a0e5cb8613375ffba26b695f
staszewski <kamilstaszewski95@gmail.com>
2020-10-07 09:12:37 +0200

    PDA-339 Finish option box

--- src/renderer/components/rest/recovery-mode/recovery-mode.styled.tsx
+++ src/renderer/components/rest/recovery-mode/recovery-mode.styled.tsx
@@ -1,11 +1,32 @@
 import styled from "styled-components"
 import { backgroundColor } from "Renderer/styles/theming/theme-getters"
+import Icon from "Renderer/components/core/icon/icon.component"
+import Text from "Renderer/components/core/text/text.component"
 
 export const OptionBox = styled.div`
+  display: flex;
+  box-sizing: border-box;
   height: 12.6rem;
   width: 38rem;
+  padding: 1.6rem 1.6rem 2.4rem 1.6rem;
   background-color: ${backgroundColor("minor")};
   &:hover {
     background-color: ${backgroundColor("icon")};
   }
+`
+
+export const OptionBoxIcon = styled(Icon)`
+  min-width: 3.2rem;
+`
+
+export const TextWrapper = styled.div`
+  margin-left: 1.6rem;
+`
+
+export const BoldOptionText = styled(Text)`
+  margin-top: 0.6rem;
+`
+
+export const OptionText = styled(Text)`
+  margin-top: 0.8rem;
 `
--- src/renderer/modules/recovery-mode/recovery-mode.component.tsx
+++ src/renderer/modules/recovery-mode/recovery-mode.component.tsx
@@ -1,13 +1,34 @@
 import React from "react"
 import { FunctionComponent } from "Renderer/types/function-component.interface"
-import { OptionBox } from "Renderer/components/rest/recovery-mode/recovery-mode.styled"
+import {
+  BoldOptionText,
+  OptionBox,
+  OptionBoxIcon,
+  OptionText,
+  TextWrapper,
+} from "Renderer/components/rest/recovery-mode/recovery-mode.styled"
 import { Link } from "react-router-dom"
 import { URL_MAIN } from "Renderer/constants/urls"
+import { IconSize } from "Renderer/components/core/icon/icon.component"
+import { Type } from "Renderer/components/core/icon/icon.config"
+import { TextDisplayStyle } from "Renderer/components/core/text/text.component"
 
 const RecoveryMode: FunctionComponent<{}> = () => (
   <div>
     <Link to={URL_MAIN.messages}>
-      <OptionBox />
+      <OptionBox>
+        <OptionBoxIcon type={Type.Delete} size={IconSize.Bigger} />
+        <TextWrapper>
+          <BoldOptionText displayStyle={TextDisplayStyle.TertiaryHeading}>
+            Backup
+          </BoldOptionText>
+          <OptionText displayStyle={TextDisplayStyle.MediumFadedLightText}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet,
+            deleniti enim esse fuga ipsum non quae unde ut! Atque dolor ipsum
+            laboriosam molestias voluptatem voluptatum.
+          </OptionText>
+        </TextWrapper>
+      </OptionBox>
     </Link>
   </div>
 )
