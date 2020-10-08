ed2d21fcecd0cb462abf2e3b8716f192955b4d31
staszewski <kamilstaszewski95@gmail.com>
2020-10-08 18:54:47 +0200

    PDA-339 Place contact support in external file to reuse it in onboarding and in recovery mode view

--- src/renderer/modules/recovery-mode/recovery-mode.component.tsx
+++ src/renderer/modules/recovery-mode/recovery-mode.component.tsx
@@ -2,15 +2,18 @@ import { noop } from "Renderer/utils/noop"
 import RecoveryModeUI from "Renderer/modules/recovery-mode/recovery-mode-ui.component"
 import { FunctionComponent } from "Renderer/types/function-component.interface"
 import React from "react"
+import { contactSupport } from "Renderer/utils/contact-support"
 
-const RecoveryMode: FunctionComponent<{}> = () => (
-  <RecoveryModeUI
-    onBackupClick={noop}
-    onRebootOsClick={noop}
-    onRestoreClick={noop}
-    onFactoryResetClick={noop}
-    onSupportButtonClick={noop}
-  />
-)
+const RecoveryMode: FunctionComponent<{}> = () => {
+  return (
+    <RecoveryModeUI
+      onBackupClick={noop}
+      onRebootOsClick={noop}
+      onRestoreClick={noop}
+      onFactoryResetClick={noop}
+      onSupportButtonClick={contactSupport}
+    />
+  )
+}
 
 export default RecoveryMode
