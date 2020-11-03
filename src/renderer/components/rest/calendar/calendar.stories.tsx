da4e6f7fad44d979ca437b2e6519810d4896021d
mkurczewski <michalkurczewski94@gmail.com>
2020-11-03 13:18:35 +0100

    Calendar stories cleanup

--- src/renderer/components/rest/calendar/calendar.stories.tsx
+++ src/renderer/components/rest/calendar/calendar.stories.tsx
@@ -1,7 +1,5 @@
 import { storiesOf } from "@storybook/react"
-import React, { useState } from "react"
-import styled from "styled-components"
-import { calendarSeed } from "App/seeds/calendar"
+import React from "react"
 import {
   SyncCalendarModal,
   SynchronizingFailedModal,
@@ -10,9 +8,6 @@ import {
 } from "Renderer/components/rest/calendar/calendar.modals"
 import StoryContainer from "Renderer/components/storybook/story-container.component"
 import Story from "Renderer/components/storybook/story.component"
-import CalendarInputSearch from "Renderer/components/rest/calendar/calendar-input-search.component"
-import { CalendarEvent } from "Renderer/modules/calendar/calendar.interface"
-import { noop } from "Renderer/utils/noop"
 
 storiesOf("Views/Calendar/Modals", module).add("All", () => {
   return (
@@ -30,23 +25,5 @@ storiesOf("Views/Calendar/Modals", module).add("All", () => {
         <SyncCalendarModal />
       </Story>
     </StoryContainer>
-  )
-})
-
-const InputSearch = styled(CalendarInputSearch)`
-  min-width: 38rem;
-`
-
-storiesOf("Views/Calendar", module).add("Input", () => {
-  const [selectedEvent, setSelectedEvent] = useState<CalendarEvent>()
-  return (
-    <Story title="Input Search">
-      <InputSearch
-        events={calendarSeed}
-        selectedEvent={selectedEvent}
-        onEventSelect={setSelectedEvent}
-        onEventValueChange={noop}
-      />
-    </Story>
   )
 })
