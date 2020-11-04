574fe25f86960ca298d45cf7a0746aafaf85097c
staszewski <kamilstaszewski95@gmail.com>
2020-11-04 12:21:22 +0100

    Add test regarding filtering in meditation view

--- src/tests/meditation.e2e.ts
+++ src/tests/meditation.e2e.ts
@@ -1,6 +1,7 @@
 import { startApp, stopApp } from "App/tests/hooks"
 import { MenuGroupTestIds } from "Renderer/components/rest/menu/menu-group-test-ids.enum"
 import { URL_MAIN } from "Renderer/constants/urls"
+import { ChartType } from "Renderer/components/rest/meditation/stats/meditation-stats.enum"
 
 let app: any
 
@@ -16,4 +17,17 @@ test("menu button takes user to correct page", async () => {
   await app.client.$(`*[data-testid=${MenuGroupTestIds.Meditation}]`).click()
   const hash = await app.client.execute(() => window.location.hash)
   expect(hash.value).toEqual(`#${URL_MAIN.meditation}`)
+})
+
+test("initial filter is set to weekly", async () => {
+  await app.client.$(`*[data-testid=${MenuGroupTestIds.Meditation}]`).click()
+  expect(
+    await app.client.isExisting(`*[data-testid=${ChartType.Weekly}]`)
+  ).toBeTruthy()
+  expect(
+    await app.client.isExisting(`*[data-testid=${ChartType.Monthly}]`)
+  ).toBeFalsy()
+  expect(
+    await app.client.isExisting(`*[data-testid=${ChartType.Yearly}]`)
+  ).toBeFalsy()
 })
