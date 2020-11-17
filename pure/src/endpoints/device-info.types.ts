47b1ce1ef0f4fd4be49122ef7504cd1b234e771a
Daniel Karski <daniel.karski5q@gmail.com>
2020-11-17 09:10:32 +0100

    Refactor device-info enums

--- pure/src/endpoints/device-info.types.ts
+++ pure/src/endpoints/device-info.types.ts
@@ -6,23 +6,23 @@ enum BatteryState {
 enum SIM {
   SIM1,
   SIM2,
-  SIM_FAIL,
-  SIM_UNKNOWN,
-  NONE,
+  SIM_Fail,
+  SIM_Unknown,
+  none,
 }
 
 enum SignalStrength {
-  zero = 0,
-  one = 1,
-  two = 2,
-  three = 3,
-  four = 4,
-  five = 5,
+  Zero,
+  One,
+  Two,
+  Three,
+  Four,
+  Five,
 }
 
 enum Tray {
-  OUT = 0,
-  IN = 1,
+  Out ,
+  In ,
 }
 
 enum AccessTechnology {
