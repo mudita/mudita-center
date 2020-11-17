2d51fb74054775137405d471c7656a4b5a2c862e
staszewski <kamilstaszewski95@gmail.com>
2020-11-17 07:18:25 +0100

    Revert "PDA-525 Remove redundant metadata fields"
    This reverts commit a9ef9c0b5b89fead8cdf18200fe5716a42873525.

--- src/__mocks__/google-contacts.ts
+++ src/__mocks__/google-contacts.ts
@@ -5,28 +5,62 @@ export const mockedGoogleContacts = {
       etag: "%EgsBAj0JPgs/EEA3LhoEAQIFByIMU2NRbGlzSWF5S0U9",
       names: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "4b3a68250d7520cd",
+            },
+          },
           displayNameLastFirst: "Bombolada, Bombolo",
         },
       ],
       addresses: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "4b3a68250d7520cd",
+            },
+          },
           streetAddress: "bomboladzka 1",
           postalCode: "00-123",
-          city: "Warsaw"
         },
       ],
       emailAddresses: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "4b3a68250d7520cd",
+            },
+          },
           value: "bombol@bombol.com",
         },
       ],
       phoneNumbers: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "4b3a68250d7520cd",
+            },
+          },
           value: "11111111111111",
         },
       ],
       biographies: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "53a3da810b4cdd4f",
+            },
+          },
           value: "notatka",
           contentType: "TEXT_PLAIN",
         },
@@ -37,23 +71,50 @@ export const mockedGoogleContacts = {
       etag: "%EgsBAj0JPgs/EEA3LhoEAQIFByIMU2dYZ2QxeTFjalE9",
       names: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "53a3da810b4cdd4f",
+            },
+          },
           displayNameLastFirst: "Bombolada, Kolejny bombolek",
         },
       ],
       addresses: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "53a3da810b4cdd4f",
+            },
+          },
           streetAddress: "lolo 123",
           postalCode: "123-123",
-          city: "Warsaw"
         },
       ],
       emailAddresses: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "53a3da810b4cdd4f",
+            },
+          },
           value: "alolo@bombol.pl",
         },
       ],
       phoneNumbers: [
         {
+          metadata: {
+            primary: true,
+            source: {
+              type: "CONTACT",
+              id: "53a3da810b4cdd4f",
+            },
+          },
           value: "12341234234",
         },
       ],
