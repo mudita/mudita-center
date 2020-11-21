5d13a13c6a492a18e4d2ee925bdf52858e0a904f
Pawel <pawel@appnrolls-MacBook-Pro.local>
2020-11-21 14:55:56 +0100

    moved repeating pattern to beforeAll

--- src/tests/contact-form.e2e.ts
+++ src/tests/contact-form.e2e.ts
@@ -9,6 +9,10 @@ const email = "essa@gmail.com"
 
 beforeEach(async () => {
   app = await startApp()
+  await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
+  const hash = await app.client.execute(() => window.location.hash)
+  expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
+  await app.client.$(`//*[text()="Contact support"]`).click()
 })
 
 afterEach(async () => {
@@ -16,28 +20,17 @@ afterEach(async () => {
 })
 
 test("menu takes user to correct page", async () => {
-  await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
-  const hash = await app.client.execute(() => window.location.hash)
-  expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
-  await app.client.$(`//*[text()="Contact support"]`).click()
+
   expect(ModalTestIds.Header).toBeVisible
 })
 
 test("contact form informs a user that email is required", async () => {
-  await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
-  const hash = await app.client.execute(() => window.location.hash)
-  expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
-  await app.client.$(`//*[text()='Contact support']`).click()
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`)
   const errorMessage = await app.client.$(`//*[text()="Email is required"]`)
   expect(errorMessage).toBeVisible
 })
 
 test("contact form informs a user that only valid email can be used", async () => {
-  await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
-  const hash = await app.client.execute(() => window.location.hash)
-  expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
-  await app.client.$(`//*[text()='Contact support']`).click()
   await app.client
     .$(`*[data-testid=${ModalTestIds.Email}]`)
     .setValue(incorrectEmail)
@@ -47,10 +40,6 @@ test("contact form informs a user that only valid email can be used", async () =
 })
 
 test("user can send a message to the support", async () => {
-  await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
-  const hash = await app.client.execute(() => window.location.hash)
-  expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
-  await app.client.$(`//*[text()='Contact support']`).click()
   await app.client.$(`*[data-testid=${ModalTestIds.Email}]`).setValue(email)
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`)
   const messageSent = await app.client.$(`//*[text()="Message sent"]`)
