160a6127e1c6816a3df886d3edc159fb6ac42d07
Pawel <pawel@appnrolls-MacBook-Pro.local>
2020-11-23 15:27:08 +0100

    fixed function

--- src/tests/contact-form.e2e.ts
+++ src/tests/contact-form.e2e.ts
@@ -20,13 +20,13 @@ afterEach(async () => {
 })
 
 test("user can navigate to the contact form through Backup screen", async () => {
-  expect(ModalTestIds.Header).toBeVisible
+  expect(ModalTestIds.Header).toBeVisible()
 })
 
 test("contact form informs a user that email is required", async () => {
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`)
   const errorMessage = await app.client.$(`//*[text()="Email is required"]`)
-  expect(errorMessage).toBeVisible
+  expect(errorMessage).toBeVisible()
 })
 
 test("contact form informs a user that only valid email can be used", async () => {
@@ -35,12 +35,12 @@ test("contact form informs a user that only valid email can be used", async () =
     .setValue(incorrectEmail)
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`)
   const errorMessage = await app.client.$(`//*[text()="Email is invalid"]`)
-  expect(errorMessage).toBeVisible
+  expect(errorMessage).toBeVisible()
 })
 
 test("user can send a message to the support", async () => {
   await app.client.$(`*[data-testid=${ModalTestIds.Email}]`).setValue(email)
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`)
   const messageSent = await app.client.$(`//*[text()="Message sent"]`)
-  expect(messageSent).toBeVisible
+  expect(messageSent).toBeVisible()
 })
