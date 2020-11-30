01460f3c0039c23e9f339d784c55caa541d0a4a6
mkurczewski <michalkurczewski94@gmail.com>
2020-11-30 18:35:45 +0100

    Added vcf parser

---
+++ src/renderer/modules/phone/helpers/parseVcf/parse-vcf.ts
@@ -0,0 +1,92 @@
+import vCard from "vcf"
+import { BaseContactModel } from "Renderer/models/phone/phone.typings"
+
+interface vCardContact {
+  [key: string]: vCard.Property | vCard.Property[]
+}
+
+export const readFile = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onload = (event) => resolve(event.target?.result as string)
+    reader.onerror = (event) => reject(event.target?.error)
+    reader.readAsText(file)
+  })
+}
+
+const parseContact = (contact: vCardContact): BaseContactModel => {
+  let [lastName, firstName] = (contact.n?.valueOf() as string).split(";")
+  let primaryPhoneNumber = ""
+  let secondaryPhoneNumber = ""
+  let firstAddressLine = ""
+  let secondAddressLine = ""
+  let note = ""
+
+  if (!firstName && !lastName) {
+    firstName = contact.fv?.valueOf() as string
+  }
+
+  if (contact.tel) {
+    if (Array.isArray(contact.tel)) {
+      primaryPhoneNumber = contact.tel[0].valueOf() as string
+      secondaryPhoneNumber = contact.tel[1].valueOf() as string
+    } else {
+      primaryPhoneNumber = contact.tel.valueOf() as string
+    }
+  }
+
+  if (contact.adr) {
+    let address: string
+
+    if (Array.isArray(contact.adr)) {
+      address = contact.adr[0].valueOf() as string
+    } else {
+      address = contact.adr.valueOf() as string
+    }
+
+    address.split(";").forEach((chunk) => {
+      const trimmedChunk = chunk.trim()
+      const { length } = trimmedChunk
+
+      if (length) {
+        if (firstAddressLine.length + length <= 30) {
+          firstAddressLine += `${trimmedChunk}, `
+        } else if (secondAddressLine.length + length <= 30) {
+          secondAddressLine += `${trimmedChunk}, `
+        }
+      }
+    })
+
+    firstAddressLine = firstAddressLine.trim().slice(0, -1)
+    secondAddressLine = secondAddressLine.trim().slice(0, -1)
+  }
+
+  if (contact.note) {
+    note = (contact.note.valueOf() as string).substr(0, 30)
+  }
+
+  return {
+    firstName,
+    lastName,
+    email: contact.email?.valueOf() as string,
+    primaryPhoneNumber,
+    secondaryPhoneNumber,
+    firstAddressLine,
+    secondAddressLine,
+    note,
+  }
+}
+
+const parseVcf = async (files: File[]): Promise<BaseContactModel[]> => {
+  const parsedContacts: BaseContactModel[] = []
+
+  for (const file of files) {
+    const contacts = vCard.parse(await readFile(file))
+    contacts.forEach(({ data }) => parsedContacts.push(parseContact(data)))
+  }
+
+  return parsedContacts
+}
+
+export default parseVcf
