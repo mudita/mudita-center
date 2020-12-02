f27b0c060b662280490e27016a56af714b05d45c
Daniel Karski <daniel.karski5q@gmail.com>
2020-12-02 07:42:22 +0100

    Refactored stories

--- src/renderer/components/rest/overview/network/network.stories.tsx
+++ src/renderer/components/rest/overview/network/network.stories.tsx
@@ -1,12 +1,14 @@
 import { storiesOf } from "@storybook/react"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Network from "Renderer/components/rest/overview/network/network.component"
-import getFakeAdapters from "App/tests/get-fake-adapters"
 import Text, {
   TextDisplayStyle,
 } from "Renderer/components/core/text/text.component"
 import { action } from "@storybook/addon-actions"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import SimInfo from "Common/interfaces/sim-info.interface"
+import getFakeAdapters from "App/tests/get-fake-adapters"
+import StoryContainer from "Renderer/components/storybook/story-container.component"
 
 const Part = styled.div`
   padding: 2rem;
@@ -14,41 +16,46 @@ const Part = styled.div`
     margin-bottom: 2rem;
   }
 `
+const storyContainerStyle = css`
+  display: block;
+  max-width: 63rem;
+`
 
-getFakeAdapters()
-  .pureNetwork.getSimCards()
-  .then(({ data }) => {
-    storiesOf("Overview/Network", module).add("Network", () => {
-      const fakeSimCards = data ?? []
-      return (
-        <div style={{ maxWidth: "63rem" }}>
-          <Part>
-            <Text displayStyle={TextDisplayStyle.SmallText}>No SIM card</Text>
-            <Network />
-          </Part>
-          <Part>
-            <Text displayStyle={TextDisplayStyle.SmallText}>
-              One SIM card (active)
-            </Text>
-            <Network simCards={fakeSimCards.slice(0, 1)} />
-          </Part>
-          <Part>
-            <Text displayStyle={TextDisplayStyle.SmallText}>
-              One SIM card (inactive)
-            </Text>
-            <Network
-              simCards={fakeSimCards.slice(1, 2)}
-              onSimChange={action("SIM changed")}
-            />
-          </Part>
-          <Part>
-            <Text displayStyle={TextDisplayStyle.SmallText}>Two SIM cards</Text>
-            <Network
-              simCards={fakeSimCards}
-              onSimChange={action("SIM changed")}
-            />
-          </Part>
-        </div>
-      )
-    })
-  })
+storiesOf("Views|Overview/Network ", module).add("Network", () => {
+  const [fakeSimCards, setFakeSimCards] = useState<SimInfo[]>([])
+  useEffect(() => {
+    const fetch = async () => {
+      const { data = [] } = await getFakeAdapters().pureNetwork.getSimCards()
+      setFakeSimCards(data)
+    }
+    fetch()
+  },[])
+
+  return (
+    <StoryContainer customStyle={storyContainerStyle}>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>No SIM card</Text>
+        <Network />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          One SIM card (active)
+        </Text>
+        <Network simCards={fakeSimCards.slice(0, 1)} />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          One SIM card (inactive)
+        </Text>
+        <Network
+          simCards={fakeSimCards.slice(1, 2)}
+          onSimChange={action("SIM changed")}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>Two SIM cards</Text>
+        <Network simCards={fakeSimCards} onSimChange={action("SIM changed")} />
+      </Part>
+    </StoryContainer>
+  )
+})
--- src/renderer/components/rest/overview/system/system.stories.tsx
+++ src/renderer/components/rest/overview/system/system.stories.tsx
@@ -1,12 +1,13 @@
 import { storiesOf } from "@storybook/react"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import System from "Renderer/components/rest/overview/system/system.component"
 import Text, {
   TextDisplayStyle,
 } from "Renderer/components/core/text/text.component"
 import getFakeAdapters from "App/tests/get-fake-adapters"
 import { action } from "@storybook/addon-actions"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import StoryContainer from "Renderer/components/storybook/story-container.component"
 
 const fakeSystemInfo = getFakeAdapters().purePhone
 const updated = new Date(fakeSystemInfo.getOsUpdateDate()).toLocaleDateString(
@@ -20,63 +21,73 @@ const Part = styled.div`
   }
 `
 
-fakeSystemInfo.getOsVersion().then(({ data }) => {
-  const osVersion = data ?? ""
+const storyContainerStyle = css`
+  display: block;
+  max-width: 59rem;
+`
 
-  storiesOf("Overview/System", module).add("System", () => {
-    return (
-      <div style={{ maxWidth: "59rem" }}>
-        <Part>
-          <Text displayStyle={TextDisplayStyle.SmallText}>Not updated yet</Text>
-          <System
-            osVersion={osVersion}
-            onUpdateCheck={action("checked for update")}
-            lastUpdate={"2020-01-14T11:31:08.244Z"}
-          />
-        </Part>
-        <Part>
-          <Text displayStyle={TextDisplayStyle.SmallText}>
-            Updated some time ago
-          </Text>
-          <System
-            osVersion={osVersion}
-            lastUpdate={updated}
-            onUpdateCheck={action("checked for update")}
-          />
-        </Part>
-        <Part>
-          <Text displayStyle={TextDisplayStyle.SmallText}>
-            New update available
-          </Text>
-          <System
-            osVersion={osVersion}
-            lastUpdate={updated}
-            onUpdate={action("update requested")}
-            updateAvailable
-          />
-        </Part>
-        <Part>
-          <Text displayStyle={TextDisplayStyle.SmallText}>
-            New (first) update available
-          </Text>
-          <System
-            osVersion={osVersion}
-            onUpdate={action("update requested")}
-            updateAvailable
-            lastUpdate={"2020-01-14T11:31:08.244Z"}
-          />
-        </Part>
-        <Part>
-          <Text displayStyle={TextDisplayStyle.SmallText}>
-            Updated just now (custom text)
-          </Text>
-          <System
-            osVersion={osVersion}
-            lastUpdate={"just now"}
-            onUpdateCheck={action("checked for update")}
-          />
-        </Part>
-      </div>
-    )
-  })
+storiesOf("Views|Overview/System", module).add("System", () => {
+  const [osVersion, setOsVersion] = useState("")
+  useEffect(() => {
+    const fetch = async () => {
+      const { data = "" } = await fakeSystemInfo.getOsVersion()
+      setOsVersion(data)
+    }
+    fetch()
+  }, [])
+
+  return (
+    <StoryContainer customStyle={storyContainerStyle}>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>Not updated yet</Text>
+        <System
+          osVersion={osVersion}
+          onUpdateCheck={action("checked for update")}
+          lastUpdate={"2020-01-14T11:31:08.244Z"}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          Updated some time ago
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={updated}
+          onUpdateCheck={action("checked for update")}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          New update available
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={updated}
+          onUpdate={action("update requested")}
+          updateAvailable
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          New (first) update available
+        </Text>
+        <System
+          osVersion={osVersion}
+          onUpdate={action("update requested")}
+          updateAvailable
+          lastUpdate={"2020-01-14T11:31:08.244Z"}
+        />
+      </Part>
+      <Part>
+        <Text displayStyle={TextDisplayStyle.SmallText}>
+          Updated just now (custom text)
+        </Text>
+        <System
+          osVersion={osVersion}
+          lastUpdate={"just now"}
+          onUpdateCheck={action("checked for update")}
+        />
+      </Part>
+    </StoryContainer>
+  )
 })
