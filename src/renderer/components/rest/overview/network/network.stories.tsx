fe75767bf5a9f50c2916e067ed9a12e8bec3a3e5
Daniel Karski <daniel.karski5q@gmail.com>
2020-12-03 08:47:42 +0100

    Refactor stories

--- src/renderer/components/rest/overview/network/network.stories.tsx
+++ src/renderer/components/rest/overview/network/network.stories.tsx
@@ -1,24 +1,17 @@
 import { storiesOf } from "@storybook/react"
 import React, { useEffect, useState } from "react"
 import Network from "Renderer/components/rest/overview/network/network.component"
-import Text, {
-  TextDisplayStyle,
-} from "Renderer/components/core/text/text.component"
 import { action } from "@storybook/addon-actions"
-import styled, { css } from "styled-components"
+import { css } from "styled-components"
 import { SimCard } from "Renderer/models/basic-info/basic-info.typings"
 import getFakeAdapters from "App/tests/get-fake-adapters"
 import StoryContainer from "Renderer/components/storybook/story-container.component"
+import Story from "Renderer/components/storybook/story.component"
 
-const Part = styled.div`
-  padding: 2rem;
-  > p {
-    margin-bottom: 2rem;
+const storyStyle = css`
+  > * {
+    width: 63rem;
   }
-`
-const storyContainerStyle = css`
-  display: block;
-  max-width: 63rem;
 `
 
 storiesOf("Views|Overview/Network ", module).add("Network", () => {
@@ -32,30 +25,22 @@ storiesOf("Views|Overview/Network ", module).add("Network", () => {
   },[])
 
   return (
-    <StoryContainer customStyle={storyContainerStyle}>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>No SIM card</Text>
+    <StoryContainer column>
+      <Story title="No SIM card" customStyle={storyStyle}>
         <Network />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          One SIM card (active)
-        </Text>
+      </Story>
+      <Story title="One SIM card (active)" customStyle={storyStyle}>
         <Network simCards={fakeSimCards.slice(0, 1)} />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          One SIM card (inactive)
-        </Text>
+      </Story>
+      <Story title="One SIM card (inactive)" customStyle={storyStyle}>
         <Network
           simCards={fakeSimCards.slice(1, 2)}
           onSimChange={action("SIM changed")}
         />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>Two SIM cards</Text>
+      </Story>
+      <Story title="Two SIM cards" customStyle={storyStyle}>
         <Network simCards={fakeSimCards} onSimChange={action("SIM changed")} />
-      </Part>
+      </Story>
     </StoryContainer>
   )
 })
--- src/renderer/components/rest/overview/system/system.stories.tsx
+++ src/renderer/components/rest/overview/system/system.stories.tsx
@@ -1,29 +1,21 @@
 import { storiesOf } from "@storybook/react"
 import React, { useEffect, useState } from "react"
 import System from "Renderer/components/rest/overview/system/system.component"
-import Text, {
-  TextDisplayStyle,
-} from "Renderer/components/core/text/text.component"
 import getFakeAdapters from "App/tests/get-fake-adapters"
 import { action } from "@storybook/addon-actions"
-import styled, { css } from "styled-components"
+import { css } from "styled-components"
 import StoryContainer from "Renderer/components/storybook/story-container.component"
+import Story from "Renderer/components/storybook/story.component"
 
 const fakeSystemInfo = getFakeAdapters().purePhone
 const updated = new Date(fakeSystemInfo.getOsUpdateDate()).toLocaleDateString(
   "en-US"
 )
 
-const Part = styled.div`
-  padding: 2rem;
-  > p {
-    margin-bottom: 2rem;
+const storyStyle = css`
+  > * {
+    width: 59rem;
   }
-`
-
-const storyContainerStyle = css`
-  display: block;
-  max-width: 59rem;
 `
 
 storiesOf("Views|Overview/System", module).add("System", () => {
@@ -37,57 +29,44 @@ storiesOf("Views|Overview/System", module).add("System", () => {
   }, [])
 
   return (
-    <StoryContainer customStyle={storyContainerStyle}>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>Not updated yet</Text>
+    <StoryContainer column>
+      <Story title="Not updated yet" customStyle={storyStyle}>
         <System
           osVersion={osVersion}
           onUpdateCheck={action("checked for update")}
           lastUpdate={"2020-01-14T11:31:08.244Z"}
         />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          Updated some time ago
-        </Text>
+      </Story>
+      <Story title="Updated some time ago" customStyle={storyStyle}>
         <System
           osVersion={osVersion}
           lastUpdate={updated}
           onUpdateCheck={action("checked for update")}
         />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          New update available
-        </Text>
+      </Story>
+      <Story title="New update available" customStyle={storyStyle}>
         <System
           osVersion={osVersion}
           lastUpdate={updated}
           onUpdate={action("update requested")}
           updateAvailable
         />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          New (first) update available
-        </Text>
+      </Story>
+      <Story title="New (first) update available" customStyle={storyStyle}>
         <System
           osVersion={osVersion}
           onUpdate={action("update requested")}
           updateAvailable
           lastUpdate={"2020-01-14T11:31:08.244Z"}
         />
-      </Part>
-      <Part>
-        <Text displayStyle={TextDisplayStyle.SmallText}>
-          Updated just now (custom text)
-        </Text>
+      </Story>
+      <Story title="Updated just now (custom text)" customStyle={storyStyle}>
         <System
           osVersion={osVersion}
           lastUpdate={"just now"}
           onUpdateCheck={action("checked for update")}
         />
-      </Part>
+      </Story>
     </StoryContainer>
   )
 })
