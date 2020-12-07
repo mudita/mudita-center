8ba8a6376ddf86041edf08a18e590998617d2ba7
staszewski <kamilstaszewski95@gmail.com>
2020-12-07 11:14:18 +0100

    PDA-484 Use data that is inside test, not a seed

--- src/renderer/components/rest/phone/contact-import-modal.test.tsx
+++ src/renderer/components/rest/phone/contact-import-modal.test.tsx
@@ -1,15 +1,31 @@
 import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-intl"
 import React from "react"
 import ContactImportModal from "Renderer/components/rest/phone/contact-import-modal.component"
-import { phoneSeedInput } from "App/seeds/phone"
 import { noop } from "Renderer/utils/noop"
 import { ModalTestIds } from "Renderer/components/core/modal/modal-test-ids.enum"
 import { ContactImportModalTestIds } from "Renderer/components/rest/phone/contact-import-modal-test-ids.enum"
 
+const contacts = [
+  {
+    id: "0",
+    firstName: "John",
+    lastName: "Doe",
+    primaryPhoneNumber: "+71 195 069 214",
+    secondaryPhoneNumber: "",
+    email: "johndoe@mudita.com",
+    note: "sapiente rem dignissimos sunt",
+    ice: false,
+    favourite: false,
+    blocked: false,
+    firstAddressLine: "Baker street 221, London",
+    secondAddressLine: "",
+  },
+]
+
 const renderer = (extraProps?: {}) => {
   return renderWithThemeAndIntl(
     <ContactImportModal
-      contacts={phoneSeedInput}
+      contacts={contacts}
       onActionButtonClick={noop}
       {...extraProps}
     />
@@ -27,25 +43,29 @@ test("action button is called with correct payload", () => {
   const onActionButtonClick = jest.fn()
   const { getByTestId } = renderer({ onActionButtonClick })
   getByTestId(ModalTestIds.ModalActionButton).click()
-  expect(onActionButtonClick).toBeCalledWith(phoneSeedInput)
+  expect(onActionButtonClick).toBeCalledWith(contacts)
 })
 
 test("correct amount of rows is rendered", () => {
   const { getAllByTestId } = renderer()
-  expect(getAllByTestId(ContactImportModalTestIds.ContactRow)).toHaveLength(phoneSeedInput.length)
+  expect(getAllByTestId(ContactImportModalTestIds.ContactRow)).toHaveLength(
+    contacts.length
+  )
 })
 
 test("all checkboxes are initially checked", () => {
   const { container } = renderer()
   const checkboxes = container.querySelectorAll('[type="checkbox"]')
-  checkboxes.forEach(checkbox => expect(checkbox).toBeChecked())
+  checkboxes.forEach((checkbox) => expect(checkbox).toBeChecked())
 })
 
 test("toggle all checkbox can uncheck remaining checkboxes", () => {
   const { getByTestId, container } = renderer()
-  const toggleAllCheckbox = getByTestId(ContactImportModalTestIds.ToggleAllCheckbox)
+  const toggleAllCheckbox = getByTestId(
+    ContactImportModalTestIds.ToggleAllCheckbox
+  )
   expect(toggleAllCheckbox).toBeChecked()
   toggleAllCheckbox.click()
   const checkboxes = container.querySelectorAll('[type="checkbox"]')
-  checkboxes.forEach(checkbox => expect(checkbox).not.toBeChecked())
+  checkboxes.forEach((checkbox) => expect(checkbox).not.toBeChecked())
 })
