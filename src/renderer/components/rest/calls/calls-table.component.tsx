bf20139331fe74c26f813bd272a607e6a292c041
staszewski <kamilstaszewski95@gmail.com>
2021-01-08 14:43:12 +0100

    Revert "PDA-449 Calls with react-virtualized"
    This reverts commit f7d6a477c2a687510632be266f22587e7b391404.

--- src/renderer/components/rest/calls/calls-table.component.tsx
+++ src/renderer/components/rest/calls/calls-table.component.tsx
@@ -12,7 +12,6 @@ import { FunctionComponent } from "Renderer/types/function-component.interface"
 import { UseTableSelect } from "Renderer/utils/hooks/useTableSelect"
 import { intl } from "Renderer/utils/intl"
 import { defineMessages } from "react-intl"
-import { AutoSizer, List } from "react-virtualized"
 
 const messages = defineMessages({
   name: { id: "view.name.phone.calls.name" },
@@ -49,20 +48,6 @@ const CallsTable: FunctionComponent<Props> = ({
   isContactCreated,
   onDeleteClick,
 }) => {
-  const renderRow = ({ index, style }: any) => (
-    <CallRow
-      key={index}
-      onRowClick={onRowClick}
-      getRowStatus={getRowStatus}
-      toggleRow={toggleRow}
-      callData={calls[index]}
-      noneRowsSelected={noneRowsSelected}
-      sidebarOpened={sidebarOpened}
-      activeRow={activeRow}
-      onDeleteClick={onDeleteClick}
-      style={style}
-    />
-  )
   return (
     <TableWithSidebarWrapper>
       <SelectableCalls
@@ -76,18 +61,19 @@ const CallsTable: FunctionComponent<Props> = ({
           <Col>{intl.formatMessage(messages.duration)}</Col>
           <Col>{intl.formatMessage(messages.date)}</Col>
         </Labels>
-        <AutoSizer disableHeight>
-          {({ width }) => (
-            <List
-              height={595}
-              width={width}
-              overscanRowCount={10}
-              rowRenderer={renderRow}
-              rowCount={calls.length}
-              rowHeight={64}
-            />
-          )}
-        </AutoSizer>
+        {calls.map((row, i) => (
+          <CallRow
+            key={i}
+            onRowClick={onRowClick}
+            getRowStatus={getRowStatus}
+            toggleRow={toggleRow}
+            callData={row}
+            noneRowsSelected={noneRowsSelected}
+            sidebarOpened={sidebarOpened}
+            activeRow={activeRow}
+            onDeleteClick={onDeleteClick}
+          />
+        ))}
       </SelectableCalls>
       {sidebarOpened && (
         <CallDetails
