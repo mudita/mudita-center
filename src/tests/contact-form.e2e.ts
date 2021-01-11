cbb91e5642cf82ad4256e7059b1676751a2771b8
Pawel <pawel@appnrolls-MacBook-Pro.local>
2021-01-11 08:45:13 +0100

    fixwd tests after debug mode changes

--- src/tests/contact-form.e2e.ts
+++ src/tests/contact-form.e2e.ts
@@ -10,7 +10,7 @@ const email = "e2e@test.com"
 beforeEach(async () => {
   app = await startApp()
   await app.client.$(`*[data-testid="icon-Close"]`).click()
-  await app.client.$(`*[data-testid="icon-MuditaLogoWithText"]`).click()
+  await app.client.pause(1000)
   await app.client.$(`*[data-testid=${MenuGroupTestIds.Backup}]`).click()
   const hash = await app.client.execute(() => window.location.hash)
   expect(hash.value).toEqual(`#${URL_RECOVERY_MODE.root}`)
@@ -23,19 +23,15 @@ afterEach(async () => {
 
 test("user can navigate to the contact form through Backup screen", async () => {
   expect(
-    await app.client.isExisting(
-      `//*[text()="Mudita Center Support"]`
-    )
+    await app.client.isExisting(`//*[text()="Mudita Center Support"]`)
   ).toBe(true)
 })
 
 test("contact form informs a user that email is required", async () => {
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`).click()
-  expect(
-    await app.client.isExisting(
-      `//*[text()="Email is required"]`
-    )
-  ).toBe(true)
+  expect(await app.client.isExisting(`//*[text()="Email is required"]`)).toBe(
+    true
+  )
 })
 
 test("contact form informs a user that only valid email can be used", async () => {
@@ -43,19 +39,13 @@ test("contact form informs a user that only valid email can be used", async () =
     .$(`*[data-testid=${ModalTestIds.Email}]`)
     .setValue(incorrectEmail)
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`).click()
-  expect(
-    await app.client.isExisting(
-      `//*[text()="Email is invalid"]`
-    )
-  ).toBe(true)
+  expect(await app.client.isExisting(`//*[text()="Email is invalid"]`)).toBe(
+    true
+  )
 })
 
 test("user can send a message to the support", async () => {
   await app.client.$(`*[data-testid=${ModalTestIds.Email}]`).setValue(email)
   await app.client.$(`*[data-testid=${ModalTestIds.ModalActionButton}]`).click()
-  expect(
-    await app.client.isExisting(
-      `//*[text()="Sending"]`
-    )
-  ).toBe(true)
+  expect(await app.client.isExisting(`//*[text()="Sending"]`)).toBe(true)
 })
