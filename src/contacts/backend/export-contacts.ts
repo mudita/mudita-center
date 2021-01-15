66cc4012dcca663df72555292574c8fd98537a8a
mkurczewski <michalkurczewski94@gmail.com>
2021-01-15 10:45:19 +0100

    Improved filename creation

--- src/contacts/backend/export-contacts.ts
+++ src/contacts/backend/export-contacts.ts
@@ -2,28 +2,41 @@ import fs from "fs-extra"
 import { ipcMain } from "electron-better-ipc"
 import { IpcRequest } from "Common/requests/ipc-request.enum"
 import { Contact } from "App/contacts/store/contacts.type"
-import { app, dialog } from "electron"
+import { app, dialog, shell } from "electron"
 import convertContacts from "App/contacts/helpers/convert-contacts/convert-contacts"
 import { intl } from "Renderer/utils/intl"
 import path from "path"
+import { defineMessages } from "react-intl"
+import { createFullName } from "App/contacts/store/contacts.helpers"
+
+const messages = defineMessages({
+  dialogTitle: { id: "view.name.phone.contacts.export.saveDialogTitle" },
+  defaultFilename: { id: "view.name.phone.contacts.export.defaultFilename" },
+})
 
 const registerContactsExportListener = () => {
   ipcMain.answerRenderer(
     IpcRequest.ExportContacts,
     async (contacts: Contact[]) => {
       const { canceled, filePath } = await dialog.showSaveDialog({
-        title: intl.formatMessage(
-          { id: "view.name.phone.contacts.export.saveDialogTitle" },
-          { count: contacts.length }
+        title: intl.formatMessage(messages.dialogTitle, {
+          count: contacts.length,
+        }),
+        defaultPath: path.join(
+          app.getPath("documents"),
+          intl.formatMessage(messages.defaultFilename, {
+            name: createFullName(contacts[0]),
+            contactsLeft: contacts.length - 1,
+          })
         ),
-        defaultPath: path.join(app.getPath("documents"), "contacts"),
         buttonLabel: "Save",
         properties: ["createDirectory", "showOverwriteConfirmation"],
         filters: [{ name: "vcf", extensions: ["vcf"] }],
       })
 
       if (!canceled && filePath) {
-        fs.writeFileSync(filePath, convertContacts(contacts), "utf-8")
+        await fs.writeFile(filePath, convertContacts(contacts), "utf-8")
+        shell.showItemInFolder(filePath)
       }
     }
   )
