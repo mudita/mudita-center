2f53a5fed7d2526b6e7e08e341459b470b690a87
staszewski <kamilstaszewski95@gmail.com>
2021-01-18 14:34:34 +0100

    PDA-630 Add mudita eslint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  processors: ["stylelint-processor-styled-components"],
-  extends: [
-    "stylelint-config-recommended",
-    "stylelint-config-styled-components",
-  ],
-  rules: {
-    "unit-whitelist": [
-      ["%", "em", "rem", "vw", "vh", "s", "ms", "deg", "vmin", "vmax", "fr"],
-      {
-        ignoreProperties: { px: ["media-feature-name"] },
-      },
-    ],
-  },
+  // processors: ["stylelint-processor-styled-components"],
+  // extends: [
+  //   "stylelint-config-recommended",
+  //   "stylelint-config-styled-components",
+  // ],
+  // rules: {
+  //   "unit-whitelist": [
+  //     ["%", "em", "rem", "vw", "vh", "s", "ms", "deg", "vmin", "vmax", "fr"],
+  //     {
+  //       ignoreProperties: { px: ["media-feature-name"] },
+  //     },
+  //   ],
+  // },
+  extends: "@mudita/stylelint-config",
 }
