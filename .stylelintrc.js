1fcd97fa95dbfbd5d90c5f539ae3df90bbb2c8f5
staszewski <kamilstaszewski95@gmail.com>
2021-01-19 07:52:52 +0100

    PDA-630 Remove commented code

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  // processors: ["stylelint-processor-styled-components"],
-  // extends: [
-  //   "stylelint-config-recommended",
-  //   "stylelint-config-styled-components",
-  // ],
-  // rules: {
-  //   "unit-whitelist": [
-  //     ["%", "em", "rem", "vw", "vh", "s", "ms", "deg", "vmin", "vmax", "fr"],
-  //     {
-  //       ignoreProperties: { px: ["media-feature-name"] },
-  //     },
-  //   ],
-  // },
   extends: "@mudita/stylelint-config",
 }
