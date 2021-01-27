3cc8f29c7581b4f3cb89ed533e87c694a5951820
staszewski <kamilstaszewski95@gmail.com>
2021-01-27 08:36:40 +0100

    Trying to access getPath - succesS

--- src/tests/overview.e2e.ts
+++ src/tests/overview.e2e.ts
@@ -1,7 +1,7 @@
 import { enablePhoneSimulation, startApp, stopApp } from "App/tests/hooks"
-import { PhoneTestIds } from "Renderer/components/rest/overview/phone/phone-test-ids.enum"
-import { ButtonTogglerTestIds } from "Renderer/components/core/button-toggler/button-toggler-test-ids.enum"
-import { URL_MAIN } from "Renderer/constants/urls"
+// import { PhoneTestIds } from "Renderer/components/rest/overview/phone/phone-test-ids.enum"
+// import { ButtonTogglerTestIds } from "Renderer/components/core/button-toggler/button-toggler-test-ids.enum"
+// import { URL_MAIN } from "Renderer/constants/urls"
 import { MenuGroupTestIds } from "Renderer/components/rest/menu/menu-group-test-ids.enum"
 
 let app: any
@@ -18,31 +18,36 @@ afterEach(async () => {
   await stopApp(app)
 })
 
-test("app starts on the correct page", async () => {
-  const hash = await app.client.execute(() => window.location.hash)
-  expect(hash.value).toEqual(`#${URL_MAIN.overview}`)
-})
+// test("app starts on the correct page", async () => {
+//   const hash = await app.client.execute(() => window.location.hash)
+//   expect(hash.value).toEqual(`#${URL_MAIN.overview}`)
+// })
+//
+// test("after clicking disconnect button, part of menu is not displayed", async () => {
+//   expect(
+//     await app.client.isExisting("*[data-testid='menu.header.yourPure']")
+//   ).toBeTruthy()
+//   await app.client.$(`*[data-testid=${PhoneTestIds.DisconnectButton}]`).click()
+//   expect(
+//     await app.client.isExisting("*[data-testid='menu.header.yourPure']")
+//   ).toBeFalsy()
+// })
+//
+// test("button toggler changes sim correctly", async () => {
+//   await app.client.isExisting("*[data-testid='location']")
+//   const previouslyInactiveSim = await app.client
+//     .$(`*[data-state=${ButtonTogglerTestIds.InactiveState}]`)
+//     .getText()
+//   await app.client
+//     .$(`*[data-state=${ButtonTogglerTestIds.InactiveState}]`)
+//     .click()
+//   const currentlyActiveSim = await app.client
+//     .$(`*[data-state=${ButtonTogglerTestIds.ActiveState}]`)
+//     .getText()
+//   expect(currentlyActiveSim).toEqual(previouslyInactiveSim)
+// })
 
-test("after clicking disconnect button, part of menu is not displayed", async () => {
-  expect(
-    await app.client.isExisting("*[data-testid='menu.header.yourPure']")
-  ).toBeTruthy()
-  await app.client.$(`*[data-testid=${PhoneTestIds.DisconnectButton}]`).click()
-  expect(
-    await app.client.isExisting("*[data-testid='menu.header.yourPure']")
-  ).toBeFalsy()
-})
-
-test("button toggler changes sim correctly", async () => {
-  await app.client.isExisting("*[data-testid='location']")
-  const previouslyInactiveSim = await app.client
-    .$(`*[data-state=${ButtonTogglerTestIds.InactiveState}]`)
-    .getText()
-  await app.client
-    .$(`*[data-state=${ButtonTogglerTestIds.InactiveState}]`)
-    .click()
-  const currentlyActiveSim = await app.client
-    .$(`*[data-state=${ButtonTogglerTestIds.ActiveState}]`)
-    .getText()
-  expect(currentlyActiveSim).toEqual(previouslyInactiveSim)
+test("should ", async () => {
+  const a = await app.electron.remote.app.getPath("appData")
+  console.log(a)
 })
