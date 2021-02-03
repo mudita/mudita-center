8ad7322ba605fad163a63f352d7adcbbad1bf24f
Bartosz Rak <bartosz.rak.official@gmail.com>
2021-02-03 06:24:12 +0100

    fix(shared): resolve aliases issue

--- shared/build.sh
+++ shared/build.sh
@@ -1,13 +1,15 @@
 echo '\033[0;32m \n### SHARED ###'
-echo "\n## 0. Remove old /shared-dist"
+echo "\n## 0. Remove old /shared/dist"
 rm -rv shared/dist
 echo "---> Done"
 
-echo "\n## 1. Building"
+echo "\n## 1. Building and re-writing paths"
 npm run build:shared
 echo "---> Done"
 
-echo "\n## 2. Move a proper tsconfig"
-cp shared/tsconfig.dist.json shared/dist/tsconfig.json
+echo "\n## 2. Copying assets"
+cp -rv src/renderer/images  shared/dist/src/renderer
+cp -rv src/renderer/svg  shared/dist/src/renderer
 echo "---> Done"
+
 echo "\033[0m"
