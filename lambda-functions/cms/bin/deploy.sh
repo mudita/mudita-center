17796fbc1da9b735e7ff3cfd840509718f872fff
staszewski <kamilstaszewski95@gmail.com>
2021-02-08 14:38:36 +0100

    PDA-658 Add script / update readme

--- lambda-functions/cms/bin/deploy.sh
+++ lambda-functions/cms/bin/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+export STACK_NAME=$(grep STACK_NAME .env | xargs)
+export S3_BUCKET=$(grep S3_BUCKET .env | xargs)
+export AWS_REGION=$(grep AWS_REGION .env | xargs)
 set -e
 npm i
 npm prune --production
-sam deploy
+sam deploy --region $AWS_REGION --s3-bucket $S3_BUCKET --stack-name $STACK_NAME
