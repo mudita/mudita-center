b63e0462b536304bdca54955a61b4c6fbb960661
mkurczewski <michalkurczewski94@gmail.com>
2021-02-09 12:42:20 +0100

    Added script for downloading fonts from github repo

---
+++ scripts/downloadFonts.js
@@ -0,0 +1,45 @@
+const axios = require("axios")
+const fs = require("fs-extra")
+const path = require("path")
+
+require("dotenv").config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? path.join(__dirname, "../.env.production")
+      : path.join(__dirname, "../.env.development"),
+})
+
+/**
+ * Function that downloads "GT Pressura" font which is unavailable publicly.
+ * It needs an access to proper repo defined in FONTS_DIRECTORY_URL env
+ * and GitHub access token provided in GITHUB_ACCESS_TOKEN env.
+ */
+;(async () => {
+  console.log("Downloading fonts...")
+  const requiredFiles = [
+    "GT-Pressura-Bold.otf",
+    "GT-Pressura-Light.otf",
+    "GT-Pressura-Regular.otf",
+    "style.css",
+  ]
+
+  try {
+    for (const fileName of requiredFiles) {
+      const url = `${process.env.FONTS_DIRECTORY_URL}/${fileName}`
+      const { data } = await axios.get(url, {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
+        },
+      })
+      await fs.writeFile(
+        path.join(__dirname, "../src/renderer/fonts/main/", fileName),
+        data
+      )
+      console.log(`Downloaded ${fileName}`)
+    }
+
+    console.log("Fonts downloading finished.")
+  } catch (error) {
+    console.log(error)
+  }
+})()
