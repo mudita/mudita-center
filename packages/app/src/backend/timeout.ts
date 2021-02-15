aff7cfa6a5c9bb554977c963431e44159bcf5152
Daniel Karski <daniel.karski5q@gmail.com>
2021-02-15 08:21:38 +0100

    Added documentation to timeout helper

--- packages/app/src/backend/timeout.ts
+++ packages/app/src/backend/timeout.ts
@@ -1,3 +1,25 @@
+/**
+ *
+ * The function set a timeout. Returns a promise of the timeout and a function to cancel a set timeout.
+ *
+ * Parameter Value
+ * `ms` - Optional. The number of milliseconds to wait before executing the code. If omitted, the value 0 is used
+ * Tip: 1000 ms = 1 second.
+ *
+ * Return Array Values
+ * `promise` - first element is a promise of the timeout
+ * `cancel` - second element is a function to cancel a set timeout
+ *
+ * Example
+ * ```ts
+ *  const [promise, cancel] = timeout(1000)
+ *
+ *  promise.then(() => console.log("The promise isn't resolved"))
+ *  cancel()
+ *  console.log("Log is visible before the timeout")
+ * ```
+ */
+
 const timeout = (ms: number): [Promise<void>, () => void] => {
   let timeoutId: NodeJS.Timeout
 
