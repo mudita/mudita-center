cdb0c908563e415110b795d985a162c9bcf2fdfa
staszewski <kamilstaszewski95@gmail.com>
2021-02-16 11:04:59 +0100

    PDA-694 Add licence

--- packages/app/src/calendar/helpers/convert-events/convert-events.test.ts
+++ packages/app/src/calendar/helpers/convert-events/convert-events.test.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/LICENSE.md
+ */
+
 import { convertEventsToICal } from "App/calendar/helpers/convert-events/convert-events-to-ical"
 import { eventsData } from "App/seeds/calendar"
 
