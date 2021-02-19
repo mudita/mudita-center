15c952ba6674134db7e827a80d4bfb7f5c7fe1cd
staszewski <kamilstaszewski95@gmail.com>
2021-02-19 11:36:47 +0100

    PDA-730 Add licences

--- packages/app/src/calendar/components/export-error-modal/export-error-modal.test.tsx
+++ packages/app/src/calendar/components/export-error-modal/export-error-modal.test.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/LICENSE.md
+ */
+
 import { renderWithThemeAndIntl } from "Renderer/utils/render-with-theme-and-intl"
 import ExportErrorModal from "App/calendar/components/export-error-modal/export-error-modal.component"
 import React from "react"
