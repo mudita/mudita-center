17069b81effa117226535d7966273bff3d922e57
staszewski <kamilstaszewski95@gmail.com>
2021-03-02 15:06:02 +0100

    PDA-726 Add event code

--- packages/app/src/renderer/modules/calendar/calendar-ui.component.tsx
+++ packages/app/src/renderer/modules/calendar/calendar-ui.component.tsx
@@ -3,10 +3,9 @@
  * For licensing, see https://github.com/mudita/mudita-center/LICENSE.md
  */
 
-import React from "react"
+import React, { useState } from "react"
 import { FunctionComponent } from "Renderer/types/function-component.interface"
 import CalendarPanel from "Renderer/components/rest/calendar/calendar-panel.component"
-import EventsList from "Renderer/components/rest/calendar/events-list.component"
 import { EmptyState } from "Renderer/components/core/table/table.component"
 import { CalendarTestIds } from "Renderer/modules/calendar/calendar-test-ids.enum"
 import { defineMessages } from "react-intl"
@@ -15,7 +14,9 @@ import { UseTableSelect } from "Renderer/utils/hooks/useTableSelect"
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction"
 import styled from "styled-components"
+import Faker from "faker"
 
 const messages = defineMessages({
   emptyStateTitle: { id: "view.name.calendar.noEvents" },
@@ -40,9 +41,44 @@ const CalendarUI: FunctionComponent<Props> = ({
   events,
   openSelectVendorModal,
   tableSelectHook,
-  selectedEventIndex,
+  // selectedEventIndex,
   onEventSelect,
 }) => {
+  const [calendarEvents, setCalendarEvents] = useState()
+  console.log({ calendarEvents })
+  const todayStr = new Date().toISOString().replace(/T.*$/, "") // YYYY-MM-DD of today
+
+  const INITIAL_EVENTS = [
+    {
+      id: 1,
+      title: "All-day event",
+      start: todayStr,
+    },
+    {
+      id: 2,
+      title: "Timed event",
+      start: todayStr + "T12:00:00",
+    },
+  ]
+  const handleDateSelect = (selectInfo: any) => {
+    console.log("select")
+    console.log({ selectInfo })
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    calendarApi.addEvent({
+      id: Faker.random.uuid(),
+      title: Faker.random.word(),
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay,
+    })
+  }
+
+  const handleEvents = (events: any) => {
+    setCalendarEvents(events)
+  }
   return (
     <>
       <CalendarPanel
@@ -57,13 +93,18 @@ const CalendarUI: FunctionComponent<Props> = ({
       {events.length > 0 ? (
         <Container>
           <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin]}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             headerToolbar={{
               left: "prev,next today",
               center: "title",
               right: "dayGridMonth,timeGridWeek,timeGridDay",
             }}
             initialView="timeGridWeek"
+            initialEvents={INITIAL_EVENTS as any}
+            editable
+            selectable
+            select={handleDateSelect}
+            eventsSet={handleEvents}
           />
         </Container>
       ) : (
