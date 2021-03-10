7b336b741cae308bca26d152c1fc6c49e2cbe4ca
Kamil <kamil@e9aruMBP16Mudita.local>
2021-03-10 16:29:47 +0100

    PDA-736 Uncomment stats

--- packages/app/src/renderer/components/rest/overview/phone/phone.styled.tsx
+++ packages/app/src/renderer/components/rest/overview/phone/phone.styled.tsx
@@ -40,7 +40,6 @@ export const BatteryStats = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  visibility: hidden; /* TODO: Remove when feature becomes available */
 
   h2 {
     margin-top: 0.8rem;
@@ -51,10 +50,9 @@ export const BatteryStats = styled.div`
 export const SignalStats = styled.div`
   margin-top: 2.4rem;
   margin-bottom: 6rem;
-  /* display: flex; TODO: Uncomment when feature becomes available*/
+  display: flex;
   flex-direction: column;
   align-items: center;
-  display: none; /* TODO: Remove when feature becomes available */
 
   p {
     margin-top: 0.8rem;
