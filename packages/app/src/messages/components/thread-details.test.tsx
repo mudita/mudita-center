2a094cc367f777c64a32ebe1d321f63110389014
staszewski <kamilstaszewski95@gmail.com>
2021-03-12 08:36:27 +0100

    PDA-617 Update test

--- packages/app/src/messages/components/thread-details.test.tsx
+++ packages/app/src/messages/components/thread-details.test.tsx
@@ -207,8 +207,11 @@ test("retry button tries to load thread again after initial call", () => {
 
 test("error modal renders when error is thrown", () => {
   const getMessagesResultMapStateByThreadId = jest.fn(() => ResultState.Error)
-  const { getByLabelText } = renderer({
+  const { getByLabelText, getByText, queryByLabelText } = renderer({
     getMessagesResultMapStateByThreadId,
   })
   expect(getByLabelText("modal1")).toBeInTheDocument()
+  getByText("open modal 2").click()
+  expect(queryByLabelText("modal1")).not.toBeInTheDocument()
+  expect(getByLabelText("modal2")).toBeInTheDocument()
 })
