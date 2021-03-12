06ce339ac961974998576ab82896b45e3e944054
staszewski <kamilstaszewski95@gmail.com>
2021-03-12 17:31:17 +0100

    Add deviceInfo GET test

--- packages/contract-test/__tests__/contract-test.test.js
+++ packages/contract-test/__tests__/contract-test.test.js
@@ -3,36 +3,69 @@ const DeviceManager = require("@mudita/pure").default
 describe("contract-test", () => {
   let phone
 
-  beforeAll(() => {
-    return DeviceManager.getDevices().then(devices => {
-      phone = devices[0]
-      if (!phone) {
-        throw new Error("Your phone is not connected or was not recognised.")
-      }
+  beforeAll(async () => {
+    const [device] = await DeviceManager.getDevices()
+    phone = device
+    if (!phone) {
+      throw new Error("Your phone is not connected or was not recognised.")
+    }
+  })
+
+  describe("device connection", () => {
+    test("device connects", async () => {
+      const { status } = await phone.connect()
+      expect(status).toEqual(200)
     })
   })
 
   describe("Device Info", () => {
-    test("GET", () => {
-      return phone.request({
+    let response
+    beforeAll(async () => {
+      response = await phone.request({
         endpoint: 1,
-        method: 1
-      }).then(response => {
-        expect(response).toMatchInlineSnapshot({
-          uuid: expect.any(Number)
-        },`
-        Object {
-          "status": 200,
-          "uuid": 1,
-          "method": 1,
-          "body": Object {
-            "networkStatus": "1",
-            "selectedSim": "0",
-            "signalStrength": "5",
-          }
-        }
-      `)
+        method: 1,
       })
+    })
+    test("GET", () => {
+      expect(response).toMatchInlineSnapshot(
+        {
+          uuid: expect.any(String),
+          body: {
+            batteryLevel: expect.any(String),
+            currentRTCTime: expect.any(String),
+          },
+        },
+        `
+      Object {
+        "body": Object {
+          "accessTechnology": "255",
+          "batteryLevel": Any<String>,
+          "batteryState": "1",
+          "currentRTCTime": Any<String>,
+          "fsFree": "13879",
+          "fsFreePercent": "100",
+          "fsTotal": "13913",
+          "gitBranch": "master",
+          "gitRevision": "888744e",
+          "gitTag": "release-0.52.1-35-g888744ee",
+          "networkStatus": "0",
+          "selectedSim": "4",
+          "signalStrength": "3",
+          "trayState": "1",
+          "updateHistory": null,
+        },
+        "endpoint": 1,
+        "status": 200,
+        "uuid": Any<String>,
+      }
+    `
+      )
+    })
+    test("battery level", () => {
+      const level = Number(response.body.batteryLevel)
+      expect(level).toEqual(expect.any(Number))
+      expect(level).toBeGreaterThanOrEqual(0)
+      expect(level).toBeLessThanOrEqual(100)
     })
   })
 })
