7903ffbf46381b95ee210084656f72d7ad96d791
Daniel Karski <daniel.karski5q@gmail.com>
2021-03-13 09:10:11 +0100

    Refactored notarize script

--- packages/app/scripts/notarize.js
+++ packages/app/scripts/notarize.js
@@ -1,41 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const electronNotarize = require('electron-notarize');
+const fs = require("fs")
+const path = require("path")
+const { build } = require("../package.json")
+const { notarize } = require("electron-notarize")
 
 require("dotenv").config({
   path: path.join(__dirname, "../.electron-builder.env"),
 })
 
 module.exports = async function (context) {
-  const { electronPlatformName, appOutDir } = context;
+  const { electronPlatformName, appOutDir } = context
 
-  if(electronPlatformName !== 'darwin'){
-    return;
+  if (electronPlatformName !== "darwin") {
+    return
   }
 
-  console.log("after sign hook triggered", context, context.packager.appInfo)
+  const appBundleId = build.appId
+  const appPath = path.join(
+    appOutDir,
+    `${context.packager.appInfo.productFilename}.app`
+  )
 
-  const appBundleId = "com.mudita.center"
-  const appPath = path.join(appOutDir, `${context.packager.appInfo.productFilename}.app`)
-
-  if(!fs.existsSync(appPath)){
-    console.log("skip")
-    return;
+  if (!fs.existsSync(appPath)) {
+    console.log(`Notarizing ${appBundleId} not found at ${appPath}`)
+    return
   }
 
-  console.log(`Notarizing ${appBundleId} found at ${appPath}`);
+  console.log(`Notarizing ${appBundleId} found at ${appPath}`)
 
   try {
-    await electronNotarize.notarize({
+    await notarize({
       appBundleId,
       appPath,
       appleId: process.env.APPLE_ID,
       appleIdPassword: process.env.APPLE_ID_PASSWORD,
-    });
+    })
   } catch (error) {
     console.error(error)
   }
 
   console.log(`Done notarizing ${appBundleId}`)
 }
-
