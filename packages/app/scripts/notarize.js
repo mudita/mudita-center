3a46868fdc36ca8c826433beb801f466a40914e8
Daniel Karski <daniel.karski5q@gmail.com>
2021-03-13 08:59:14 +0100

    Moved notarize to scripts

--- packages/app/scripts/notarize.js
+++ packages/app/scripts/notarize.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const electronNotarize = require('electron-notarize');
-console.log("path.join: ", path.join(__dirname, "../.electron-builder.env"))
 
 require("dotenv").config({
   path: path.join(__dirname, "../.electron-builder.env"),
 })
 
-module.exports = async function (params) {
-  if(process.platform != "darwin"){
-    return
+module.exports = async function (context) {
+  const { electronPlatformName, appOutDir } = context;
+
+  if(electronPlatformName !== 'darwin'){
+    return;
   }
 
-  console.log("after sign hook triggered", params, params.packager.appInfo)
+  console.log("after sign hook triggered", context, context.packager.appInfo)
 
   const appBundleId = "com.mudita.center"
-  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
+  const appPath = path.join(appOutDir, `${context.packager.appInfo.productFilename}.app`)
 
   if(!fs.existsSync(appPath)){
     console.log("skip")
