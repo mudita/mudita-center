307d2ca4d1717da81c98d3c8f6b72125a5fac2d2
Kamil Poprawa <kamilxunai@yahoo.com>
2021-03-16 09:04:41 +0100

    [PDA-770] add requestTokens test

--- packages/app/src/renderer/models/external-providers/outlook/outlook.helpers.test.ts
+++ packages/app/src/renderer/models/external-providers/outlook/outlook.helpers.test.ts
@@ -3,10 +3,30 @@
  * For licensing, see https://github.com/mudita/mudita-center/LICENSE.md
  */
 
-import { handleScope } from "Renderer/models/external-providers/outlook/outlook.helpers"
+import {
+  handleScope,
+  requestTokens,
+} from "Renderer/models/external-providers/outlook/outlook.helpers"
 import { OutLookScope } from "Renderer/models/external-providers/outlook/outlook.interface"
+import MockAdapter from "axios-mock-adapter"
+import axios from "axios"
 
 const scope = "offline_access, https://graph.microsoft.com/contacts.readwrite"
+
+let axiosMock = new MockAdapter(axios)
+
+beforeEach(() => {
+  axiosMock = new MockAdapter(axios)
+})
+
+test("requestTokens returns token", async () => {
+  axiosMock.onPost().reply(200, {
+    access_token: "token-123",
+    refresh_token: "refresh_token-1234",
+  })
+  const tokens = await requestTokens("M.R3_BAY.123", scope)
+  expect(tokens.access_token).toBe("token-123")
+})
 
 test("handleScope returns proper value", () => {
   expect(handleScope(OutLookScope.Contacts)).toBe(scope)
