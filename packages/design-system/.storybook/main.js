1c6deae18fbb5d6a132bfd4f4c8d89aba5081d4e
mkurczewski <michalkurczewski94@gmail.com>
2021-03-22 16:44:08 +0100

    basic setup

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,10 +1,14 @@
+const path = require("path")
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.resolve.alias["App"] = path.resolve(__dirname, "../src/")
+    config.resolve.alias["Components"] = path.resolve(
+      __dirname,
+      "../src/components"
+    )
+    return config
+  },
 }
--- packages/design-system/src/stories/Button.tsx
+++ packages/design-system/src/stories/Button.tsx
@@ -5,7 +5,13 @@ import "./button.css"
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+export const Button = ({
+  primary,
+  backgroundColor,
+  size,
+  label,
+  ...props
+}: any) => {
   const mode = primary
     ? "storybook-button--primary"
     : "storybook-button--secondary"
