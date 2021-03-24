223b0f42bce13e3f8689e99ec59cccaa15a1c0fd
staszewski <kamilstaszewski95@gmail.com>
2021-03-24 14:40:50 +0100

    PDA-786 Removing logic regarding changing id to string

--- packages/pure/src/formatter/pure-v1.formatter.test.ts
+++ packages/pure/src/formatter/pure-v1.formatter.test.ts
@@ -6,33 +6,6 @@
 import { PureV1Formatter } from "./pure-v1.formatter"
 import { Endpoint, Method, RequestConfig, ResponseStatus } from "../device"
 
-const contact = {
-  address: "6 Czeczota St.02600 Warsaw",
-  altName: "Boligłowa",
-  blocked: false,
-  favourite: true,
-  id: 19,
-  numbers: ["123123213123"],
-  priName: "Alek",
-}
-
-const getResponse = {
-  status: ResponseStatus.Ok,
-  body: {
-    entries: [contact, contact],
-    totalCount: 2,
-  },
-  endpoint: Endpoint.Contacts,
-  uuid: 3227,
-}
-
-const postOrPutResponse = {
-  status: ResponseStatus.Ok,
-  body: contact,
-  endpoint: Endpoint.Contacts,
-  uuid: 3227,
-}
-
 const updateErrorResponse = {
   status: ResponseStatus.BadRequest,
   endpoint: Endpoint.Update,
@@ -45,7 +18,7 @@ const updateErrorResponse = {
 
 const requestConfig: RequestConfig = {
   body: {
-    id: "123",
+    id: 12,
   },
   endpoint: Endpoint.Contacts,
   filePath: "/example/filepath",
@@ -57,55 +30,21 @@ let formatter: PureV1Formatter
 beforeEach(() => (formatter = new PureV1Formatter()))
 
 test("body id is returned as number when method is set to POST", () => {
-  const { body } = formatter.formatRequestConfig(requestConfig)
-  expect(typeof body.id).toBe("number")
+  const result = formatter.formatRequestConfig(requestConfig)
+  expect(typeof result.body.id).toBe("number")
 })
 
 test("body id is returned as number when method is set to DELETE", () => {
-  const { body } = formatter.formatRequestConfig({
+  const result = formatter.formatRequestConfig({
     ...requestConfig,
     method: Method.Delete,
   })
-  expect(typeof body.id).toBe("number")
-})
-
-test("body id is not formatted when method is set to GET", () => {
-  const { body } = formatter.formatRequestConfig({
-    ...requestConfig,
-    method: Method.Get,
-  })
-  expect(typeof body.id).toBe("string")
-})
-
-test("formatResponse handles response correctly", () => {
-  const result = formatter.formatResponse(Method.Get, getResponse)
-  expect(typeof result.body.entries[0].id).toBe("string")
-})
-
-test("body's id is returned as a string in PUT method in format response", () => {
-  const { body } = formatter.formatResponse(Method.Put, postOrPutResponse)
-  expect(typeof body.id).toBe("string")
+  expect(typeof result.body.id).toBe("number")
 })
 
 test("correct error is returned when formatting", () => {
   const result = formatter.formatResponse(Method.Post, updateErrorResponse)
   expect(result.error?.code).toEqual(1012)
-})
-
-test("entries id in get response is returned as a string", () => {
-  const result = formatter.handleContactEndpointResponse(
-    Method.Get,
-    getResponse
-  )
-  expect(typeof result.body.entries[0].id).toBe("string")
-})
-
-test("body's id is returned as a string in PUT method ", () => {
-  const { body } = formatter.handleContactEndpointResponse(
-    Method.Put,
-    postOrPutResponse
-  )
-  expect(typeof body.id).toBe("string")
 })
 
 test("correct error is returned when input is directly passed to the handler", () => {
--- packages/pure/src/formatter/pure-v1.formatter.ts
+++ packages/pure/src/formatter/pure-v1.formatter.ts
@@ -10,11 +10,7 @@ import {
   Response,
 } from "../device/device.types"
 import { Formatter } from "./formatter"
-import {
-  Contact,
-  DeviceUpdateError,
-  deviceUpdateErrorCodeMap,
-} from "../endpoints"
+import { DeviceUpdateError, deviceUpdateErrorCodeMap } from "../endpoints"
 
 export class PureV1Formatter extends Formatter {
   formatRequestConfig(config: RequestConfig): RequestConfig {
@@ -33,34 +29,10 @@ export class PureV1Formatter extends Formatter {
   formatResponse(method: Method, response: Response<any>): Response<any> {
     const { endpoint } = response
     switch (endpoint) {
-      case Endpoint.Contacts:
-        return this.handleContactEndpointResponse(method, response)
       case Endpoint.Update:
         return this.handleUpdateEndpointResponse(method, response)
       default:
         return response
-    }
-  }
-
-  handleContactEndpointResponse(
-    method: Method,
-    response: Response<any>
-  ): Response<any> {
-    const { body } = response
-    if (method === Method.Put) {
-      return { ...response, body: { ...body, id: String(body.id) } }
-    } else if (method === Method.Get) {
-      return {
-        ...response,
-        body: {
-          entries: body.entries.map((entry: Contact) => ({
-            ...entry,
-            id: String(entry.id),
-          })),
-        },
-      }
-    } else {
-      return response
     }
   }
 
