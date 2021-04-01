a8c86e865fb30027e30cbd75e244b5ed209f2acc
staszewski <kamilstaszewski95@gmail.com>
2021-04-01 16:35:00 +0200

    PDA-782 More tests for modal

--- packages/app/src/renderer/components/core/modal-dialog/modal-dialog.test.tsx
+++ packages/app/src/renderer/components/core/modal-dialog/modal-dialog.test.tsx
@@ -7,13 +7,45 @@ const renderer = (props = {}) =>
   renderWithThemeAndIntl(<ModalDialog isOpen={true} {...props} />)
 
 test("close button is hidden by default", () => {
-  const {queryByTestId} = renderer()
+  const { queryByTestId } = renderer()
   expect(queryByTestId(ModalTestIds.CloseButton)).not.toBeInTheDocument()
 })
 
-test("close button performs close modal action", () => {
+test("close button closes modal", () => {
   const closeModal = jest.fn()
-  const {getByTestId} = renderer({closeModal})
+  const { getByTestId } = renderer({ closeModal })
   getByTestId(ModalTestIds.CloseButton).click()
   expect(closeModal).toBeCalled()
+})
+
+test("close action button renders by default", () => {
+  const { getByTestId } = renderer()
+  expect(getByTestId(ModalTestIds.CloseBottomButton)).toBeInTheDocument()
+})
+
+test("close action button closes modal", () => {
+  const closeModal = jest.fn()
+  const { getByTestId } = renderer({ closeModal })
+  getByTestId(ModalTestIds.CloseBottomButton).click()
+  expect(closeModal).toBeCalled()
+})
+
+test("close action button doesn't render", () => {
+  const { queryByTestId } = renderer({ closeButton: false })
+  expect(queryByTestId(ModalTestIds.CloseBottomButton)).not.toBeInTheDocument()
+})
+
+test("action button doesn't render by default", () => {
+  const { queryByTestId } = renderer()
+  expect(queryByTestId(ModalTestIds.ModalActionButton)).not.toBeInTheDocument()
+})
+
+test("action button perform its action", () => {
+  const onActionButtonClick = jest.fn()
+  const { getByTestId } = renderer({
+    actionButtonLabel: "somelabel",
+    onActionButtonClick,
+  })
+  getByTestId(ModalTestIds.ModalActionButton).click()
+  expect(onActionButtonClick).toBeCalled()
 })
