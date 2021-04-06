f4756cacf617100e7b5d6bee703d247e2f175348
staszewski <kamilstaszewski95@gmail.com>
2021-04-06 08:40:27 +0200

    PDA-782 Correct new story format

--- packages/app/src/renderer/components/core/modal-dialog/modal-dialog.stories.tsx
+++ packages/app/src/renderer/components/core/modal-dialog/modal-dialog.stories.tsx
@@ -3,7 +3,7 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import { storiesOf } from "@storybook/react"
+import { Meta } from "@storybook/react"
 import Story from "Renderer/components/storybook/story.component"
 import { StoryModalWrapper } from "Renderer/components/core/modal/modal.styled.elements"
 import Text, {
@@ -16,40 +16,21 @@ import {
   TitleOrder,
 } from "Renderer/components/core/modal/modal.interface"
 import { noop } from "Renderer/utils/noop"
+import { Story as StoryInterface } from "@storybook/react"
 
-storiesOf("Components|Core/ModalDialog", module)
-  .add("Large With title", () => {
-    return (
-      <Story title="With title" transparentMode>
-        <StoryModalWrapper>
-          <ModalDialog title={"Title"} isOpen>
-            <Text displayStyle={TextDisplayStyle.LargeText}>
-              Lorem ipsum dolor sit amet
-            </Text>
-          </ModalDialog>
-        </StoryModalWrapper>
-      </Story>
-    )
-  })
-  .add("Large With title and subtitle", () => (
-    <Story title="With title and subtitle" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog isOpen title={"Title"} subtitle={"Subtitle"}>
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Large With titles reversed", () => (
-    <Story title="With titles reversed" transparentMode>
+const Template: StoryInterface<
+  React.ComponentProps<typeof ModalDialog> & { storyTitle: string }
+> = (args) => {
+  return (
+    <Story title={args.storyTitle} transparentMode>
       <StoryModalWrapper>
         <ModalDialog
+          title={args.title}
+          subtitle={args.subtitle}
+          titleOrder={args.titleOrder}
+          actionButtonLabel={args.actionButtonLabel}
+          onActionButtonClick={args.onActionButtonClick}
           isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          titleOrder={TitleOrder.SubtitleFirst}
         >
           <Text displayStyle={TextDisplayStyle.LargeText}>
             Lorem ipsum dolor sit amet
@@ -57,213 +38,126 @@ storiesOf("Components|Core/ModalDialog", module)
         </ModalDialog>
       </StoryModalWrapper>
     </Story>
-  ))
-  .add("Large With action button", () => (
-    <Story title="With action button" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          actionButtonLabel={"Done"}
-          onActionButtonClick={noop}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Medium With title", () => {
-    return (
-      <Story title="With title" transparentMode>
-        <StoryModalWrapper>
-          <ModalDialog title={"Title"} isOpen size={ModalSize.Medium}>
-            <Text displayStyle={TextDisplayStyle.LargeText}>
-              Lorem ipsum dolor sit amet
-            </Text>
-          </ModalDialog>
-        </StoryModalWrapper>
-      </Story>
-    )
-  })
-  .add("Medium With title and subtitle", () => (
-    <Story title="With title and subtitle" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          size={ModalSize.Medium}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Medium With titles reversed", () => (
-    <Story title="With titles reversed" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          titleOrder={TitleOrder.SubtitleFirst}
-          size={ModalSize.Medium}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Medium With action button", () => (
-    <Story title="With action button" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          actionButtonLabel={"Done"}
-          onActionButtonClick={noop}
-          size={ModalSize.Medium}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Small With title", () => {
-    return (
-      <Story title="With title" transparentMode>
-        <StoryModalWrapper>
-          <ModalDialog title={"Title"} isOpen size={ModalSize.Small}>
-            <Text displayStyle={TextDisplayStyle.LargeText}>
-              Lorem ipsum dolor sit amet
-            </Text>
-          </ModalDialog>
-        </StoryModalWrapper>
-      </Story>
-    )
-  })
-  .add("Small With title and subtitle", () => (
-    <Story title="With title and subtitle" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          size={ModalSize.Small}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Small With titles reversed", () => (
-    <Story title="With titles reversed" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          titleOrder={TitleOrder.SubtitleFirst}
-          size={ModalSize.Small}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Small With action button", () => (
-    <Story title="With action button" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          actionButtonLabel={"Done"}
-          onActionButtonClick={noop}
-          size={ModalSize.Small}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Very small With title", () => {
-    return (
-      <Story title="With title" transparentMode>
-        <StoryModalWrapper>
-          <ModalDialog title={"Title"} isOpen size={ModalSize.VerySmall}>
-            <Text displayStyle={TextDisplayStyle.LargeText}>
-              Lorem ipsum dolor sit amet
-            </Text>
-          </ModalDialog>
-        </StoryModalWrapper>
-      </Story>
-    )
-  })
-  .add("Very small With title and subtitle", () => (
-    <Story title="With title and subtitle" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          size={ModalSize.VerySmall}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Very small With titles reversed", () => (
-    <Story title="With titles reversed" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          titleOrder={TitleOrder.SubtitleFirst}
-          size={ModalSize.VerySmall}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
-  .add("Very small With action button", () => (
-    <Story title="With action button" transparentMode>
-      <StoryModalWrapper>
-        <ModalDialog
-          isOpen
-          title={"Title"}
-          subtitle={"Subtitle"}
-          actionButtonLabel={"Done"}
-          onActionButtonClick={noop}
-          size={ModalSize.VerySmall}
-        >
-          <Text displayStyle={TextDisplayStyle.LargeText}>
-            Lorem ipsum dolor sit amet
-          </Text>
-        </ModalDialog>
-      </StoryModalWrapper>
-    </Story>
-  ))
+  )
+}
+
+export default {
+  title: "Components|Core/ModalDialog",
+  component: ModalDialog,
+} as Meta
+
+export const LargeWithTitle = Template.bind({})
+LargeWithTitle.args = {
+  title: "Title",
+}
+
+export const LargeWithTitleAndSubtitle = Template.bind({})
+LargeWithTitleAndSubtitle.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+}
+
+export const LargeWithTitleReversed = Template.bind({})
+LargeWithTitleReversed.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  titleOrder: TitleOrder.SubtitleFirst,
+}
+
+export const LargeWithActionButton = Template.bind({})
+LargeWithActionButton.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  actionButtonLabel: "Done",
+  onActionButtonClick: noop,
+}
+
+export const MediumWithTitle = Template.bind({})
+MediumWithTitle.args = {
+  title: "Title",
+  size: ModalSize.Medium,
+}
+
+export const MediumWithTitleAndSubtitle = Template.bind({})
+MediumWithTitleAndSubtitle.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  size: ModalSize.Medium,
+}
+
+export const MediumWithTitleReversed = Template.bind({})
+MediumWithTitleReversed.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  titleOrder: TitleOrder.SubtitleFirst,
+  size: ModalSize.Medium,
+}
+
+export const MediumWithActionButton = Template.bind({})
+MediumWithActionButton.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  actionButtonLabel: "Done",
+  onActionButtonClick: noop,
+  size: ModalSize.Medium,
+}
+
+export const SmallWithTitle = Template.bind({})
+SmallWithTitle.args = {
+  title: "Title",
+  size: ModalSize.Small,
+}
+
+export const SmallWithTitleAndSubtitle = Template.bind({})
+SmallWithTitleAndSubtitle.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  size: ModalSize.Small,
+}
+
+export const SmallWithTitleReversed = Template.bind({})
+SmallWithTitleReversed.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  titleOrder: TitleOrder.SubtitleFirst,
+  size: ModalSize.Small,
+}
+
+export const SmallWithActionButton = Template.bind({})
+SmallWithActionButton.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  actionButtonLabel: "Done",
+  onActionButtonClick: noop,
+  size: ModalSize.Small,
+}
+
+export const VerySmallWithTitle = Template.bind({})
+VerySmallWithTitle.args = {
+  title: "Title",
+  size: ModalSize.VerySmall,
+}
+
+export const VerySmallWithTitleAndSubtitle = Template.bind({})
+VerySmallWithTitleAndSubtitle.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  size: ModalSize.VerySmall,
+}
+
+export const VerySmallWithTitleReversed = Template.bind({})
+VerySmallWithTitleReversed.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  titleOrder: TitleOrder.SubtitleFirst,
+  size: ModalSize.VerySmall,
+}
+
+export const VerySmallWithActionButton = Template.bind({})
+VerySmallWithActionButton.args = {
+  title: "Title",
+  subtitle: "Subtitle",
+  actionButtonLabel: "Done",
+  onActionButtonClick: noop,
+  size: ModalSize.VerySmall,
+}
