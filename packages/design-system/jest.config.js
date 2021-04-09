bd412c9b925f1436ae3a809bf1f912d28fa5ce9c
mkurczewski <michalkurczewski94@gmail.com>
2021-04-09 12:40:07 +0200

    Fixed jest settings to support SVG properly

--- packages/design-system/jest.config.js
+++ packages/design-system/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/config/jest/mocks/file-mock.js",
+    ".+\\.(svg)$": "<rootDir>/config/jest/mocks/svg-mock.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverageFrom: [
@@ -12,4 +13,5 @@ module.exports = {
   ],
   coverageReporters: ["lcov", "text-summary", "json-summary"],
   coverageDirectory: "coverage",
+  preset: "ts-jest",
 }
