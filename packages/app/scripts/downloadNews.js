355a9efd6f3b7d21a12520e96618a124c61a27c9
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-05-06 21:05:37 +0200

    CP-23 download default news from contentful

--- packages/app/scripts/downloadNews.js
+++ packages/app/scripts/downloadNews.js
@@ -1,14 +1,20 @@
+const axios = require("axios")
 const path = require("path")
 const fs = require("fs-extra")
 
 ;(async () => {
+  MUDITA_CENTER_SERVER_URL = "https://api.center.mudita.com/.netlify/functions"
   try {
     await fs.ensureDir(path.resolve(path.join("src", "main")))
     const jsonPath = path.join("src", "main", "default-news.json")
-    const data = { key: "value" }
+
+    const url = `${MUDITA_CENTER_SERVER_URL}/news`
+    const { data } = await axios.get(url, {
+      params: { limit: 6 },
+    })
     await fs.writeJson(path.resolve(jsonPath), data)
-    console.log("news file")
+    console.log("News downloading finished.")
   } catch (error) {
-    console.log("Error while downloading news.")
+    console.log("Error while downloading news.", error)
   }
 })()
