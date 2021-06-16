aea336774ddd80460974905fe7af3b013f1d1864
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-06-16 10:48:54 +0200

    CP-178 add loader to update OS modal

--- packages/app/src/renderer/modules/overview/overview.stories.tsx
+++ packages/app/src/renderer/modules/overview/overview.stories.tsx
@@ -147,7 +147,7 @@ storiesOf("Views|Overview/Modals", module)
   ))
   .add("Updating progress", () => (
     <ModalStory>
-      <UpdatingProgressModal progressValue={80} />
+      <UpdatingProgressModal />
     </ModalStory>
   ))
   .add("Updating success", () => (
