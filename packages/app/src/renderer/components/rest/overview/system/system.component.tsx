ee4396da4542632c0c298fea814ef716ec192c8c
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-06-23 15:03:11 +0200

    CP-69 add checking if you are up to date with os version

--- packages/app/src/renderer/components/rest/overview/system/system.component.tsx
+++ packages/app/src/renderer/components/rest/overview/system/system.component.tsx
@@ -10,6 +10,7 @@ import Card, {
   CardAction,
   CardActionButton,
   CardText,
+  CardContent,
 } from "Renderer/components/rest/overview/card.elements"
 import styled from "styled-components"
 import Text, {
@@ -20,115 +21,112 @@ import { intl } from "Renderer/utils/intl"
 import {
   fontWeight,
   letterSpacing,
-  textColor,
+  backgroundColor,
 } from "Renderer/styles/theming/theme-getters"
 import { noop } from "Renderer/utils/noop"
-import { Type } from "Renderer/components/core/icon/icon.config"
 import { SystemTestIds } from "Renderer/components/rest/overview/system/system-test-ids"
 import { OverviewTestIds } from "Renderer/modules/overview/overview-test-ids.enum"
 
 const TextInfo = styled(CardText)``
 
 const Version = styled.div`
-  display: flex;
-  align-items: baseline;
-
   span {
     margin-left: 0.8rem;
     font-weight: ${fontWeight("light")};
     text-transform: none;
   }
-`
-
-const LastUpdate = styled(Text)`
-  margin-top: 1.2rem;
-  letter-spacing: ${letterSpacing("small")}rem;
-  color: ${textColor("secondary")};
+  p {
+    margin-bottom: 0.8rem;
+  }
 `
 
 const AvailableUpdate = styled(Text)`
   margin-top: 1.6rem;
   text-transform: none;
+  display: inline-box;
+  padding: 0.3rem 0.5rem;
+  border-radius: 4px;
   letter-spacing: ${letterSpacing("small")}rem;
+  background-color: ${backgroundColor("minor")};
 `
 
 const System: FunctionComponent<SystemProps> = ({
   className,
   osVersion,
-  lastUpdate,
   updateAvailable,
   updateDownloaded,
   onUpdateCheck = noop,
   onUpdate = noop,
   onDownload = noop,
 }) => {
+  const releaseNumber = osVersion.slice(8)
+
   return (
     <Card className={className}>
-      <TextInfo>
-        <Version>
-          <Text
-            displayStyle={TextDisplayStyle.SecondaryBoldHeading}
-            data-testid={OverviewTestIds.OsVersion}
-          >
-            {osVersion}
-          </Text>
-          <Text element={"span"} displayStyle={TextDisplayStyle.SmallText}>
-            <FormattedMessage id="module.overview.systemVersion" />
-          </Text>
-        </Version>
-        {Boolean(lastUpdate) && (
-          <LastUpdate displayStyle={TextDisplayStyle.SmallFadedText}>
-            <FormattedMessage
-              id="module.overview.systemLastUpdate"
-              values={{
-                date: new Date(lastUpdate).toLocaleDateString("en-US"),
-              }}
-            />
-          </LastUpdate>
-        )}
-        {updateAvailable && (
-          <AvailableUpdate displayStyle={TextDisplayStyle.SmallText}>
-            {updateDownloaded ? (
-              <FormattedMessage id="module.overview.systemUpdateDownloaded" />
+      <div>
+        <Text displayStyle={TextDisplayStyle.SecondaryBoldHeading}>
+          <FormattedMessage id="module.overview.muditaOsUpdateTitle" />
+        </Text>
+      </div>
+      <CardContent>
+        <TextInfo>
+          <Version>
+            <Text displayStyle={TextDisplayStyle.MediumFadedLightText}>
+              Current version:
+            </Text>
+            <Text
+              displayStyle={TextDisplayStyle.LargeText}
+              data-testid={OverviewTestIds.OsVersion}
+            >
+              <FormattedMessage id="module.overview.muditaOsUpdateTitle" />
+              {" " + releaseNumber}
+            </Text>
+          </Version>
+          {updateAvailable ? (
+            <AvailableUpdate displayStyle={TextDisplayStyle.SmallFadedText}>
+              {updateDownloaded ? (
+                <FormattedMessage id="module.overview.systemUpdateDownloaded" />
+              ) : (
+                <FormattedMessage id="module.overview.systemUpdateAvailable" />
+              )}
+            </AvailableUpdate>
+          ) : (
+            <AvailableUpdate displayStyle={TextDisplayStyle.SmallFadedText}>
+              <FormattedMessage id="module.overview.systemUpdateUpToDate" />
+            </AvailableUpdate>
+          )}
+        </TextInfo>
+        <CardAction filled>
+          {updateAvailable ? (
+            updateDownloaded ? (
+              <CardActionButton
+                active
+                label={intl.formatMessage({
+                  id: "module.overview.systemUpdateAction",
+                })}
+                onClick={onUpdate}
+              />
             ) : (
-              <FormattedMessage id="module.overview.systemUpdateAvailable" />
-            )}
-          </AvailableUpdate>
-        )}
-      </TextInfo>
-      <CardAction filled>
-        {updateAvailable ? (
-          updateDownloaded ? (
-            <CardActionButton
-              active
-              label={intl.formatMessage({
-                id: "module.overview.systemUpdateAction",
-              })}
-              Icon={Type.Reload}
-              onClick={onUpdate}
-            />
+              <CardActionButton
+                active
+                label={intl.formatMessage({
+                  id: "module.overview.systemDownloadAction",
+                })}
+                onClick={onDownload}
+                data-testid={SystemTestIds.DownloadButton}
+              />
+            )
           ) : (
             <CardActionButton
               active
               label={intl.formatMessage({
-                id: "module.overview.systemDownloadAction",
+                id: "module.overview.systemCheckForUpdates",
               })}
-              Icon={Type.DownloadWhite}
-              onClick={onDownload}
-              data-testid={SystemTestIds.DownloadButton}
+              onClick={onUpdateCheck}
             />
-          )
-        ) : (
-          <CardActionButton
-            active
-            label={intl.formatMessage({
-              id: "module.overview.systemCheckForUpdates",
-            })}
-            Icon={Type.Reload}
-            onClick={onUpdateCheck}
-          />
-        )}
-      </CardAction>
+          )}
+        </CardAction>
+      </CardContent>
     </Card>
   )
 }
