66eba887a6fff4dd7e399e24a34568e0642b658c
Daniel Karski <daniel.karski5q@gmail.com>
2021-07-08 11:15:12 +0200

    Added fetching app configuration in build process

---
+++ packages/app/scripts/downloadAppConfiguration.ts
@@ -0,0 +1,26 @@
+import axios from "axios"
+const path = require("path")
+const fs = require("fs-extra")
+
+require("dotenv").config({
+  path: path.join(__dirname, "../../../.env"),
+})
+
+let defaultData = { osVersion: "0.0.0" }
+
+;(async () => {
+  const jsonPath = path.join("src", "main", "app-configuration.json")
+  try {
+    await fs.ensureDir(path.resolve(path.join("src", "main")))
+
+    const url = `${process.env.MUDITA_CENTER_SERVER_URL}/configuration-os-version`
+    const { status, data } = await axios.get<{ osVersion: string }>(url)
+    if (status === 200 && data !== undefined) {
+      await fs.writeJson(path.resolve(jsonPath), data)
+    } else {
+      await fs.writeJson(path.resolve(jsonPath), defaultData)
+    }
+  } catch (error) {
+    await fs.writeJson(path.resolve(jsonPath), defaultData)
+  }
+})()
