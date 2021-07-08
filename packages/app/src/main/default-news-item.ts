7bff01caf1d673b8434f7a0a65e72bdb921aedad
Daniel Karski <daniel.karski5q@gmail.com>
2021-07-08 11:15:28 +0200

    Refactored fetching default news

--- packages/app/src/main/default-news-item.ts
+++ packages/app/src/main/default-news-item.ts
@@ -3,24 +3,20 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-const fs = require("fs-extra")
 import { NewsEntry } from "Renderer/models/mudita-news/mudita-news.interface"
 
 export interface DefaultNewsItems {
   newsItems: NewsEntry[]
 }
 
-const getDefaultNewsItems = async (): Promise<DefaultNewsItems> => {
-  let data
-  if (fs.access("./default-news.json")) {
-    data = require("./default-news.json")
-  } else {
-    data = { newsItems: [] }
-  }
-  const { newsItems } = data
-  return {
-    newsItems,
-  }
+let data: DefaultNewsItems = { newsItems: [] }
+
+try {
+  data = require("./default-news.json") as DefaultNewsItems
+} catch {}
+
+const getDefaultNewsItems = (): DefaultNewsItems => {
+  return data
 }
 
 export default getDefaultNewsItems
