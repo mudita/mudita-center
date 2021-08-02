088a684e0746067fa4aadc7a90b077fbd5f31f9f
Daniel Karski <daniel.karski5q@gmail.com>
2021-08-02 10:02:50 +0200

    Moved DevicePortInfo global properties as class static

--- packages/pure/src/device-port-info.ts
+++ packages/pure/src/device-port-info.ts
@@ -5,25 +5,31 @@
 
 import { PortInfo } from "serialport"
 
-export const productId = "0100"
-export const vendorId = "3310"
-export const manufacturer = "Mudita"
-
-// TODO: Remove tmp values when pure will be released, https://appnroll.atlassian.net/browse/CP-457
-const tmpProductId = "0622"
-const tmpVendorId = "045e"
-
 class DevicePortInfo {
+  static productId = "0100"
+  static vendorId = "3310"
+  static manufacturer = "Mudita"
+
+  // TODO: Remove tmp values when pure will be released, https://appnroll.atlassian.net/browse/CP-457
+  static tmpProductId = "0622"
+  static tmpVendorId = "045e"
+
   static isVendorId(portInfo: Partial<PortInfo>): boolean {
     // toLowerCase() is needed tu unify the codes as different platforms
     // shows them in different casing (eg. 045E vs 045e)
     const id = portInfo.vendorId?.toLowerCase()
-    return id === vendorId.toLowerCase() || id === tmpVendorId.toLowerCase()
+    return (
+      id === DevicePortInfo.vendorId.toLowerCase() ||
+      id === DevicePortInfo.tmpVendorId.toLowerCase()
+    )
   }
 
   static isProductId(portInfo: Partial<PortInfo>): boolean {
     const id = portInfo.productId?.toLowerCase()
-    return id === productId.toLowerCase() || id === tmpProductId.toLowerCase()
+    return (
+      id === DevicePortInfo.productId.toLowerCase() ||
+      id === DevicePortInfo.tmpProductId.toLowerCase()
+    )
   }
 
   static isPortInfoMatch(portInfo: Partial<PortInfo>): boolean {
