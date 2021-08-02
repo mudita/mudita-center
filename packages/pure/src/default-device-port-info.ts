3ad7ea2afe2391b1751fdc23e22f09a3e2d3faaf
Daniel Karski <daniel.karski5q@gmail.com>
2021-08-02 09:11:15 +0200

    Handle original and temporary port info values

--- packages/pure/src/default-device-port-info.ts
+++ packages/pure/src/default-device-port-info.ts
@@ -1,18 +1,29 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
 import { PortInfo } from "serialport"
 
-export const productId = "0622"
-export const vendorId = "045e"
+export const productId = "0100"
+export const vendorId = "3310"
 export const manufacturer = "Mudita"
+
+// TODO: Remove tmp values when pure will be released, https://appnroll.atlassian.net/browse/CP-457
+const tmpProductId = "0622"
+const tmpVendorId = "045e"
 
 class DevicePortInfo {
   static isVendorId(portInfo: Partial<PortInfo>): boolean {
     // toLowerCase() is needed tu unify the codes as different platforms
     // shows them in different casing (eg. 045E vs 045e)
-    return portInfo.vendorId?.toLowerCase() === vendorId.toLowerCase()
+    const id = portInfo.vendorId?.toLowerCase()
+    return id === vendorId.toLowerCase() || id === tmpVendorId.toLowerCase()
   }
 
   static isProductId(portInfo: Partial<PortInfo>): boolean {
-    return portInfo.productId?.toLowerCase() === productId.toLowerCase()
+    const id = portInfo.productId?.toLowerCase()
+    return id === productId.toLowerCase() || id === tmpProductId.toLowerCase()
   }
 
   static isPortInfoMatch(portInfo: Partial<PortInfo>): boolean {
