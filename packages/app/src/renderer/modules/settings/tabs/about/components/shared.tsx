7b43dca3aeae1970f3ef0d7519487f1890e33e55
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-08-05 10:26:13 +0200

    CP-385 add mudita license to shared.tsx

--- packages/app/src/renderer/modules/settings/tabs/about/components/shared.tsx
+++ packages/app/src/renderer/modules/settings/tabs/about/components/shared.tsx
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
 
 import styled from "styled-components"
 import Text from "Renderer/components/core/text/text.component"
