d69ea276ffb622e2b04fdb38eb0c802cd6d5cd83
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-08-06 09:43:08 +0200

    CP-185 format file

--- packages/app/src/contacts/components/contact-input-search/contact-input-search.test.tsx
+++ packages/app/src/contacts/components/contact-input-search/contact-input-search.test.tsx
@@ -3,41 +3,40 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import {isItemMatching} from "App/contacts/components/contact-input-search/contact-input-search.component"
+import { isItemMatching } from "App/contacts/components/contact-input-search/contact-input-search.component"
 import { Contact } from "App/contacts/store/contacts.type"
 
 const defaultProps = {
-    contact: 
-    {
-        id: "0",
-        firstName: "Sławomir",
-        lastName: "Borewicz",
-        primaryPhoneNumber: "+71 195 069 214",
-        secondaryPhoneNumber: "",
-        email: "example@mudita.com",
-        note: "sapiente rem dignissimos sunt",
-        ice: false,
-        favourite: false,
-        blocked: false,
-        firstAddressLine: "Malczewskiego 3, Warszawa",
-        secondAddressLine: "",
-    } as Contact,
+  contact: {
+    id: "0",
+    firstName: "Sławomir",
+    lastName: "Borewicz",
+    primaryPhoneNumber: "+71 195 069 214",
+    secondaryPhoneNumber: "",
+    email: "example@mudita.com",
+    note: "sapiente rem dignissimos sunt",
+    ice: false,
+    favourite: false,
+    blocked: false,
+    firstAddressLine: "Malczewskiego 3, Warszawa",
+    secondAddressLine: "",
+  } as Contact,
 }
 
 test("isItemMatching returns true when search string in email", () => {
-    const searchString = "example"
-    const result = isItemMatching(defaultProps.contact, searchString)
-    expect(result).toBe(true)
+  const searchString = "example"
+  const result = isItemMatching(defaultProps.contact, searchString)
+  expect(result).toBe(true)
 })
 
 test("isItemMatching returns true when search string in primaryPhoneNumber", () => {
-    const searchString = "069"
-    const result = isItemMatching(defaultProps.contact, searchString)
-    expect(result).toBe(true)
+  const searchString = "069"
+  const result = isItemMatching(defaultProps.contact, searchString)
+  expect(result).toBe(true)
 })
 
 test("isItemMatching returns false when no match ", () => {
-    const searchString = "000"
-    const result = isItemMatching(defaultProps.contact, searchString)
-    expect(result).toBe(false)
+  const searchString = "000"
+  const result = isItemMatching(defaultProps.contact, searchString)
+  expect(result).toBe(false)
 })
