28ca4250463c4718dc2cb9421d13770af0791b3c
Daniel Karski <daniel.karski5q@gmail.com>
2021-08-14 08:38:03 +0200

    Refactored parse-vcf tests

--- packages/app/src/contacts/helpers/parse-vcf/parse-vcf.test.ts
+++ packages/app/src/contacts/helpers/parse-vcf/parse-vcf.test.ts
@@ -4,79 +4,59 @@
  */
 
 import path from "path"
-import createFile from "Renderer/utils/create-file"
+import createFile from "Renderer/utils/create-file/create-file"
 import parseVcf from "./parse-vcf"
 
-test("parsing vCard contact works properly", async () => {
-  expect(
-    await parseVcf([createFile(path.join(__dirname, "./single-contact.vcf"))])
-  ).toStrictEqual([
-    {
-      firstName: "John",
-      lastName: "Doe",
-      email: "example@mudita.com",
-      primaryPhoneNumber: "123 456 789",
-      secondaryPhoneNumber: "32 123 44 55",
-      firstAddressLine: "4929 Pine Garden Lane, Atlanta",
-      secondAddressLine: "GA 30339, USA",
-      note: "Vestibulum ac diam sit amet qu",
-    },
-  ])
-})
+describe("Parse VCF helper", () => {
+  const singleContactFile = createFile(
+    path.join(__dirname, "./single-contact.vcf")
+  )
+  const multipleContactsFile = createFile(
+    path.join(__dirname, "./multiple-contacts.vcf")
+  )
+  const encodedContactFile = createFile(
+    path.join(__dirname, "./encoded-contact.vcf")
+  )
 
-test("parsing multiple vCard contacts works properly", async () => {
-  expect(
-    await parseVcf([
-      createFile(path.join(__dirname, "./multiple-contacts.vcf")),
+  test("should return list with one contact when is just single record", async () => {
+    const contacts = await parseVcf([singleContactFile])
+    expect(contacts).toStrictEqual([
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "example@mudita.com",
+        primaryPhoneNumber: "123 456 789",
+        secondaryPhoneNumber: "32 123 44 55",
+        firstAddressLine: "4929 Pine Garden Lane, Atlanta",
+        secondAddressLine: "GA 30339, USA",
+        note: "Vestibulum ac diam sit amet qu",
+      },
     ])
-  ).toStrictEqual([
-    {
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "example@mudita.com",
-      primaryPhoneNumber: "987 654 321",
-      secondaryPhoneNumber: "",
-      firstAddressLine: "",
-      secondAddressLine: "",
-      note: "Nulla quis lorem ut libero mal",
-    },
-    {
-      firstName: "John",
-      lastName: "Doe",
-      email: "example@mudita.com",
-      primaryPhoneNumber: "123 456 789",
-      secondaryPhoneNumber: "32 123 44 55",
-      firstAddressLine: "4929 Pine Garden Lane, Atlanta",
-      secondAddressLine: "GA 30339, USA",
-      note: "Vestibulum ac diam sit amet qu",
-    },
-  ])
-})
+  })
 
-test("parsing encoded vCard works properly", async () => {
-  expect(
-    await parseVcf([
-      createFile(path.join(__dirname, "./single-encoded-contact.vcf")),
-    ])
-  ).toStrictEqual([
-    {
-      firstName: "是",
-      lastName: "Jürgen",
-      email: "example@mudita.com",
-      primaryPhoneNumber: "123 456 789",
-      secondaryPhoneNumber: "32 123 44 55",
-      firstAddressLine: "Saudi Arabia, 11564, Arabia",
-      secondAddressLine: "",
-      note: "اكتشف",
-    },
-  ])
-})
+  test("should return contact list when in a file is multiple records", async () => {
+    const contacts = await parseVcf([multipleContactsFile])
+    expect(contacts).toHaveLength(2)
+  })
 
-test("parsing multiple vcf files works properly", async () => {
-  expect(
-    await parseVcf([
-      createFile(path.join(__dirname, "./single-contact.vcf")),
-      createFile(path.join(__dirname, "./multiple-contacts.vcf")),
+  test("should handle list of files", async () => {
+    const contacts = await parseVcf([singleContactFile, multipleContactsFile])
+    expect(contacts).toHaveLength(3)
+  })
+
+  test("should decode records file when are encoded", async () => {
+    const contacts = await parseVcf([encodedContactFile])
+    expect(contacts).toStrictEqual([
+      {
+        firstName: "是",
+        lastName: "Jürgen",
+        email: "example@mudita.com",
+        primaryPhoneNumber: "123 456 789",
+        secondaryPhoneNumber: "32 123 44 55",
+        firstAddressLine: "Saudi Arabia, 11564, Arabia",
+        secondAddressLine: "",
+        note: "اكتشف",
+      },
     ])
-  ).toHaveLength(3)
+  })
 })
