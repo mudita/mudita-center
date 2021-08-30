9294ba9107a87873d2321fae75e6007fbd630128
Daniel Karski <daniel.karski5q@gmail.com>
2021-08-30 12:49:09 +0200

    Fix lint

--- packages/app/src/main/utils/is-release.ts
+++ packages/app/src/main/utils/is-release.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Mudita sp. z o.o. All rights reserved.
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
+
 import isVersionMatch from "App/overview/helpers/is-version-match"
 
 export const isRelease = (version: string): boolean => {
