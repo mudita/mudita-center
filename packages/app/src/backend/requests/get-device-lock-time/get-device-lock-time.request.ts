cd1f0158a0ecc53361b6abad5b79040ca66a21c9
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-09-06 15:16:42 +0200

    CP-133 fix typo

--- packages/app/src/backend/requests/get-device-lock-time/get-device-lock-time.request.ts
+++ packages/app/src/backend/requests/get-device-lock-time/get-device-lock-time.request.ts
@@ -7,7 +7,7 @@ import Adapters from "Backend/adapters/adapters.interface"
 import createEndpoint from "Backend/endpoints/create-endpoint"
 import { IpcRequest } from "Common/requests/ipc-request.enum"
 import DeviceResponse from "Backend/adapters/device-response.interface"
-import { GetPhoneLockTimeResponseBody } from "@mudita/Pure"
+import { GetPhoneLockTimeResponseBody } from "@mudita/pure"
 
 const handleGetDeviceLockTime = ({
   purePhone,
