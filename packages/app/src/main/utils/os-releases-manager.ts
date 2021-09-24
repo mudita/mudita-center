b03222b71f1c08eabb40cce7e3ad8df242ce6710
Daniel Karski <daniel.karski5q@gmail.com>
2021-09-24 09:43:33 +0200

    Fixed lint

--- packages/app/src/main/utils/os-releases-manager.ts
+++ packages/app/src/main/utils/os-releases-manager.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Mudita sp. z o.o. All rights reserved.
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
+
 import { Feature, flags } from "App/feature-flags"
 
 class OsReleasesManager {
