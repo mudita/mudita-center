7e0ae0fc7d27949a3b4c4abd307692e69391050e
igorbezsmertnyii <mr.igor96@outlook.com>
2021-10-06 15:31:05 +0200

    [CP-603] Implement crash dump info modal

---
+++ packages/app/src/crash-dump/components/crash-dump-modal/crash-dump-modal.component.tsx
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import React from "react"
+import { defineMessages } from "react-intl"
+import { intl } from "Renderer/utils/intl"
+import { FunctionComponent } from "Renderer/types/function-component.interface"
+import ModalDialog from "Renderer/components/core/modal-dialog/modal-dialog.component"
+import { ModalSize } from "Renderer/components/core/modal/modal.interface"
+import Icon from "Renderer/components/core/icon/icon.component"
+import { Type } from "Renderer/components/core/icon/icon.config"
+import { ModalContent } from "Renderer/components/core/modal-dialog/modal-dialog-shared"
+import Text, {
+  TextDisplayStyle,
+} from "Renderer/components/core/text/text.component"
+import { IconWrapper } from "App/crash-dump/components/crash-dump-modal/crash-dump-modal.styled"
+import { CrashDumpModalTestingIds } from "App/crash-dump/components/crash-dump-modal/crash-dump-modal-testing-ids.enum"
+
+export interface CrashDumpProps {
+  open: boolean
+  onClose: () => void
+  onAccept: () => void
+}
+
+const messages = defineMessages({
+  title: { id: "component.crashDumpModal.title" },
+  label: { id: "component.crashDumpModal.label" },
+  text: { id: "component.crashDumpModal.text" },
+})
+
+export const CrashDumpModal: FunctionComponent<CrashDumpProps> = ({ open, onClose, onAccept }) => {
+  return (
+    <ModalDialog
+      title={intl.formatMessage(messages.title)}
+      size={ModalSize.Small}
+      open={open}
+      closeModal={onClose}
+      closeButtonLabel="Cancel"
+      onActionButtonClick={onAccept}
+      actionButtonLabel="Accept"
+    >
+      <ModalContent data-testid={CrashDumpModalTestingIds.Content}>
+        <IconWrapper>
+          <Icon type={Type.ThinFail} width={3} height={3} />
+        </IconWrapper>
+        <Text data-testid={CrashDumpModalTestingIds.Label} displayStyle={TextDisplayStyle.LargeBoldText} message={messages.label} />
+        <Text data-testid={CrashDumpModalTestingIds.Text} displayStyle={TextDisplayStyle.MediumFadedLightText} message={messages.text} />
+      </ModalContent>
+    </ModalDialog>
+  )
+}
