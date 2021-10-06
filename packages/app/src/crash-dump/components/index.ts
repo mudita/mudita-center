89babe99819e41e92301e88d1a41f6aa59f75624
igorbezsmertnyii <mr.igor96@outlook.com>
2021-10-06 16:59:18 +0200

    [CP-603] Implement crash dump sending modal

--- packages/app/src/crash-dump/components/index.ts
+++ packages/app/src/crash-dump/components/index.ts
@@ -3,4 +3,8 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
+<<<<<<< HEAD
 export * from "./crash-dump-modal"
+=======
+export * from "./crash-dump-sending-modal"
+>>>>>>> [CP-603] Implement crash dump sending modal
