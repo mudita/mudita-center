891f375019332b08b504fb4ec38230af4c227672
Weronika Bolesta <weronika.bolesta@mudita.com>
2021-11-02 09:47:38 +0100

    CP-633 check validation for another fnumber changes

--- packages/app/src/contacts/components/contact-edit/contact-edit.component.tsx
+++ packages/app/src/contacts/components/contact-edit/contact-edit.component.tsx
@@ -107,6 +107,7 @@ const ContactEdit: FunctionComponent<ContactEditProps> = ({
     formState: { errors },
     setValue,
     setError,
+    trigger,
   } = useForm({
     defaultValues: contact,
     mode: "onChange",
@@ -145,6 +146,15 @@ const ContactEdit: FunctionComponent<ContactEditProps> = ({
       })
     }
   }, [validationError])
+
+  useEffect(() => {
+    if (
+      fields.primaryPhoneNumber !== "" &&
+      fields.secondaryPhoneNumber !== ""
+    ) {
+      trigger()
+    }
+  }, [fields.primaryPhoneNumber, fields.secondaryPhoneNumber])
 
   const savingPossible =
     fields.firstName?.trim() ||
