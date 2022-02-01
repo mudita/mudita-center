d21084d1e3d1d0426ab4e63a0ffaa823ace34abe
Daniel Karski <daniel.karski5q@gmail.com>
2022-02-01 08:02:18 +0100

    Update to v14

---
+++ packages/app/src/renderer/register-app-context-menu.ts
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { app } from "@electron/remote"
+import store from "Renderer/store"
+import { AppHotkeys } from "App/hotkeys/hotkeys.types"
+import {
+  togglePureSimulation,
+  toggleHarmonySimulation,
+} from "App/dev-mode/store/dev-mode.helpers"
+import ContextMenu from "App/context-menu/context-menu"
+import contactsContextMenu from "App/contacts/helpers/context-menu/context-menu"
+import importDeviceLogFiles from "Renderer/requests/import-device-log-files.request"
+import { name } from "../../package.json"
+import importDeviceCrashDumpFiles from "Renderer/requests/import-device-crash-dumps-files.request"
+import { loadThreads } from "App/messages/actions"
+import { clearAllThreads } from "App/messages/actions/base.action"
+
+const filePath = `${app.getPath("appData")}/${name}/pure-logs`
+
+const registerAppContextMenu = (menu: ContextMenu) => {
+  menu.registerItems("Device", [
+    {
+      labelCreator: () => {
+        return store.getState().devMode.pureSimulation
+          ? "Stop simulating Pure connection"
+          : "Simulate Pure connection"
+      },
+      click: togglePureSimulation,
+      accelerator: AppHotkeys.pureSimulation,
+    },
+    {
+      labelCreator: () => {
+        return store.getState().devMode.harmonySimulation
+          ? "Stop simulating Harmony connection"
+          : "Simulate Harmony connection"
+      },
+      click: toggleHarmonySimulation,
+      accelerator: AppHotkeys.HarmonySimulation,
+    },
+    {
+      label: "Download logs file",
+      click: () => importDeviceLogFiles(filePath),
+    },
+    {
+      label: "Download crash dumps file",
+      click: () => importDeviceCrashDumpFiles(filePath),
+    },
+  ])
+
+  menu.registerItems("Messages", [
+    {
+      label: "Load default threads",
+      click: () => store.dispatch(loadThreads({ limit: 5, offset: 0 })),
+    },
+    {
+      label: "Clear all threads",
+      click: () => store.dispatch(clearAllThreads()),
+    },
+  ])
+
+  menu.registerItems("Contacts", contactsContextMenu)
+
+  menu.registerItems("Calendar", [
+    {
+      label: "Load default events",
+      click: () => store.dispatch.calendar._devLoadDefaultEvents(),
+    },
+    {
+      label: "Clear all events",
+      click: () => store.dispatch.calendar._devClearAllEvents(),
+    },
+  ])
+}
+
+export default registerAppContextMenu
