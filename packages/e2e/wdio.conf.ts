626f0ca906ee3383d1172d11c162a1451f5613b9
igorbezsmertnyii <mr.igor96@outlook.com>
2022-02-02 11:22:30 +0100

    [CP-938] Implementing JSON reporter and fix latest release check

--- packages/e2e/wdio.conf.ts
+++ packages/e2e/wdio.conf.ts
@@ -181,7 +181,15 @@ export const config: WebdriverIO.Config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: [
+    "spec",
+    [
+      "json",
+      {
+        outputDir: "./results",
+      },
+    ],
+  ],
 
   //
   // Options to be passed to Mocha.
