5bcbf950329c833b01388e1cf4061c25cd4fbaba
Daniel Karski <daniel.karski5q@gmail.com>
2022-04-08 09:32:11 +0200

    Fail sync status during Harmony connection fixed

--- packages/app/src/data-sync/observers/device-connection.observer.test.ts
+++ packages/app/src/data-sync/observers/device-connection.observer.test.ts
@@ -10,6 +10,8 @@ import DeviceService, { DeviceServiceEventName } from "Backend/device-service"
 import { MetadataStore } from "App/metadata/services"
 import { DataSyncService } from "App/data-sync/services/data-sync.service"
 import { getDeviceInfoRequest } from "Backend/adapters/device-base-info/device-base-info.adapter"
+import { DeviceType, MuditaDevice } from "@mudita/pure"
+import { IpcEvent } from "App/data-sync/constants"
 
 jest.mock("Backend/adapters/device-base-info/device-base-info.adapter")
 
@@ -21,12 +23,14 @@ const deviceService = {
   },
 } as unknown as DeviceService
 const keyStorage = new MetadataStore()
-const indexStorageService = {
-  indexAll: jest.fn().mockReturnValue(true),
-} as unknown as DataSyncService
+let indexStorageService: DataSyncService
 
 describe("Method: observe", () => {
   beforeEach(() => {
+    jest.resetAllMocks()
+    indexStorageService = {
+      indexAll: jest.fn().mockReturnValue(Promise.resolve(true)),
+    } as unknown as DataSyncService
     subject = new DeviceConnectionObserver(
       deviceService,
       keyStorage,
@@ -36,23 +40,51 @@ describe("Method: observe", () => {
     )
   })
 
-  test("calls the `DeviceService.request` and `DataSyncService.indexAll` methods when `DeviceServiceEventName.DeviceUnlocked` has been emitted", async () => {
-    ;(getDeviceInfoRequest as unknown as jest.Mock).mockReturnValue({
-      data: {
-        deviceToken: "1234567890",
-        serialNumber: "0000000000",
-      },
+  describe("when the `DeviceUnlocked` event is emit with `MuditaPure` device type", () => {
+    test("calls the `DeviceService.request` and `DataSyncService.indexAll` methods when `DeviceServiceEventName.DeviceUnlocked` has been emitted", async () => {
+      ;(getDeviceInfoRequest as unknown as jest.Mock).mockReturnValue({
+        data: {
+          deviceToken: "1234567890",
+          serialNumber: "0000000000",
+        },
+      })
+      expect(indexStorageService.indexAll).toHaveBeenCalledTimes(0)
+      expect(getDeviceInfoRequest).toHaveBeenCalledTimes(0)
+      expect((ipcMain as any).sendToRenderers).toHaveBeenCalledTimes(0)
+
+      subject.observe()
+
+      eventEmitterMock.emit(DeviceServiceEventName.DeviceUnlocked, {
+        deviceType: DeviceType.MuditaPure,
+      } as MuditaDevice)
+      await Promise.resolve()
+      await Promise.resolve()
+      await Promise.resolve()
+
+      expect((ipcMain as any).sendToRenderers).toHaveBeenCalledWith(
+        IpcEvent.DataLoaded
+      )
+      expect(indexStorageService.indexAll).toHaveBeenCalledTimes(1)
+      expect(getDeviceInfoRequest).toHaveBeenCalledTimes(1)
     })
-    expect(indexStorageService.indexAll).toHaveBeenCalledTimes(0)
-    expect(getDeviceInfoRequest).toHaveBeenCalledTimes(0)
+  })
 
-    subject.observe()
+  describe("when the `DeviceUnlocked` event is emit with `MuditaHarmony` device type", () => {
+    test("no run `indexAll` process and emit `DataLoaded` event", async () => {
+      expect(indexStorageService.indexAll).toHaveBeenCalledTimes(0)
+      expect(getDeviceInfoRequest).toHaveBeenCalledTimes(0)
+      expect((ipcMain as any).sendToRenderers).toHaveBeenCalledTimes(0)
+      subject.observe()
 
-    eventEmitterMock.emit(DeviceServiceEventName.DeviceUnlocked)
-    await Promise.resolve()
-    await Promise.resolve()
+      eventEmitterMock.emit(DeviceServiceEventName.DeviceUnlocked, {
+        deviceType: DeviceType.MuditaHarmony,
+      } as MuditaDevice)
 
-    expect(indexStorageService.indexAll).toHaveBeenCalledTimes(1)
-    expect(getDeviceInfoRequest).toHaveBeenCalledTimes(1)
+      expect((ipcMain as any).sendToRenderers).toHaveBeenCalledWith(
+        IpcEvent.DataLoaded
+      )
+      expect(indexStorageService.indexAll).not.toHaveBeenCalledTimes(1)
+      expect(getDeviceInfoRequest).not.toHaveBeenCalledTimes(1)
+    })
   })
 })
