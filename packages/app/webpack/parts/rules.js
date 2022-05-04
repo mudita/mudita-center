09e9b0c35883535f4454a4ba749de2618d9a7570
Daniel Karski <daniel.karski5q@gmail.com>
2022-05-04 14:33:30 +0200

    `@babel/plugin-proposal-nullish-coalescing-operator` plugin to `tsxMain`

--- packages/app/webpack/parts/rules.js
+++ packages/app/webpack/parts/rules.js
@@ -17,6 +17,7 @@ const tsxMain = (production) => ({
       ["@babel/plugin-proposal-class-properties", { loose: true }],
       ["@babel/plugin-proposal-private-methods", { loose: true }],
       ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+      ["@babel/plugin-proposal-nullish-coalescing-operator"],
     ],
   },
 })
