bf5f5e57738bea9d8dbe9aa02a9db127f001b3f8
Weronika Bolesta <weronika.bolesta@mudita.com>
2022-05-19 00:48:06 +0200

    CP-1139 read and unread state on

--- packages/app/src/messages/presenters/thread.presenter.ts
+++ packages/app/src/messages/presenters/thread.presenter.ts
@@ -5,7 +5,6 @@
 
 import { Thread as PureThread } from "@mudita/pure"
 import { Thread } from "App/messages/reducers"
-import { Feature, flags } from "App/feature-flags"
 
 export class ThreadPresenter {
   static mapToThread(pureThread: PureThread): Thread {
@@ -18,8 +17,7 @@ export class ThreadPresenter {
     } = pureThread
     return {
       messageSnippet,
-      // TODO: turn on in https://appnroll.atlassian.net/browse/PDA-802
-      unread: flags.get(Feature.ProductionAndAlpha) ? false : isUnread,
+      unread: isUnread,
       id: String(threadID),
       phoneNumber: String(number),
       lastUpdatedAt: new Date(lastUpdatedAt * 1000),
