4c8452769ab8a93aed81e615a8acab5bfb7db547
rajmundex <lukasz.jarkowski@mudita.com>
2022-06-02 15:01:08 +0200

    change test file location, minor fixes based on review comments

--- packages/e2e/src/page-objects/settings.page.ts
+++ packages/e2e/src/page-objects/settings.page.ts
@@ -2,95 +2,87 @@ import { ChainablePromiseElement } from "webdriverio"
 import Page from "./page"
 
 class SettingsPage extends Page {
-
-
-    public get generalTab(): ChainablePromiseElement<
+  public get generalTab(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
+  > {
     return $('[data-testid="icon-Connection"]')
-    }
+  }
 
-    public get backupTab(): ChainablePromiseElement<
+  public get backupTab(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="icon-BackupFolder"]')
-    }
+  }
 
-    public get aboutTab(): ChainablePromiseElement<
-    Promise<WebdriverIO.Element>
-    > {
+  public get aboutTab(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
     return $('[data-testid="icon-MuditaLogo"]')
-    }
+  }
 
-    public get generalSendLogsTextLabel(): ChainablePromiseElement<
+  public get generalSendLogsTextLabel(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
-        return $("p*=Send Mudita Center logs to Mudita")
-    }
+  > {
+    return $("p*=Send Mudita Center logs to Mudita")
+  }
 
-    public get backupLocationPathTextLabel(): ChainablePromiseElement<
+  public get backupLocationPathTextLabel(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
-        return $('[data-testid="backup-location"]')
-    }    
+  > {
+    return $('[data-testid="backup-location"]')
+  }
 
-    public get aboutInstalledVersionTextLabel(): ChainablePromiseElement<
+  public get aboutInstalledVersionTextLabel(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
-        return $("p*=Mudita Center - installed version:")
-    }
-    
-    public get aboutTermsOfServiceTextLabel(): ChainablePromiseElement<
-    Promise<WebdriverIO.Element>
-    > {
-        return $("p*=Terms of service")
-    }
-    
-    public get aboutPrivacyPolicyTextLabel(): ChainablePromiseElement<
-    Promise<WebdriverIO.Element>
-    > {
-        return $("p*=Privacy Policy")
-    }
+  > {
+    return $("p*=Mudita Center - installed version:")
+  }
 
-    public get aboutLicenseTextLabel(): ChainablePromiseElement<
+  public get aboutTermsOfServiceTextLabel(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
-        return $("p*=License")
-    }
+  > {
+    return $("p*=Terms of service")
+  }
 
-    public get closeButton(): ChainablePromiseElement<
+  public get aboutPrivacyPolicyTextLabel(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
+  > {
+    return $("p*=Privacy Policy")
+  }
+
+  public get aboutLicenseTextLabel(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $("p*=License")
+  }
+
+  public get closeButton(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
     return $('[data-testid="icon-Close"]')
-    }
+  }
 
-    public get addToFavouritessCheckbox(): ChainablePromiseElement<
+  public get addToFavouritessCheckbox(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
+  > {
     return $('[name="favourite"]')
-    }
+  }
 
-    public get cancelButton(): ChainablePromiseElement<
+  public get cancelButton(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
-    return $("p=Cancel")
-    }
+  > {
+    return $("p*=Cancel")
+  }
 
-    public get saveButton(): ChainablePromiseElement<
+  public get saveButton(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
+  > {
     return $('[data-testid="save-button"]')
-    }
+  }
 
-
-    public get noContactsText(): ChainablePromiseElement<
+  public get noContactsText(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
-    > {
+  > {
     return $('[data-testid="contact-list-no-result]')
-    }
-
+  }
 }
-
-
 
 export default new SettingsPage()
---
+++ packages/e2e/src/specs/settings/mc_version_check.e2e.ts
@@ -0,0 +1,37 @@
+import NavigationTabs from "../../page-objects/tabs.page"
+import SettingsPage from "../../page-objects/settings.page"
+import { version } from "../../../../app/package.json"
+
+describe("Checking Mudita Center version", () => {
+  before(async () => {
+    // Waiting for device connected through USB
+    await browser.executeAsync((done) => {
+      setTimeout(done, 10000)
+    })
+  })
+
+  it("Application should have the same version as in package.json", async () => {
+    const appVersionFromPackageJson = version
+
+    const newsTab = await NavigationTabs.settingsTab
+    await newsTab.waitForDisplayed()
+    await newsTab.click()
+    await browser.executeAsync((done) => {
+      setTimeout(done, 2000)
+    })
+
+    const tabAbout = await SettingsPage.aboutTab
+    await tabAbout.waitForDisplayed()
+    await tabAbout.click()
+    await browser.executeAsync((done) => {
+      setTimeout(done, 2000)
+    })
+    const appVersionFromAppField =
+      await SettingsPage.aboutInstalledVersionTextLabel
+    const appVersionFromAppTextValue = await appVersionFromAppField.getText()
+    const appVersionFromApp = appVersionFromAppTextValue.split(": ")[1]
+    console.log("appVersionFromJson")
+    console.log("appVersionFromApp")
+    await expect(appVersionFromApp).toEqual(appVersionFromPackageJson)
+  })
+})
