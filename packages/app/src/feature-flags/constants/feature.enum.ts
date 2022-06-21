d6142bebf3eec932da6149953ce6bf2f0ba2b34c
Daniel Karski <daniel.karski5q@gmail.com>
2022-06-21 10:21:30 +0200

    Update packages/app/src/feature-flags/constants/feature.enum.ts
    Co-authored-by: Weronika Bolesta <weronika.bolesta@mudita.com>

--- packages/app/src/feature-flags/constants/feature.enum.ts
+++ packages/app/src/feature-flags/constants/feature.enum.ts
@@ -16,5 +16,5 @@ export enum Feature {
   ProductionReleaseOnly = "production-release-only",
   TestProductionReleaseOnly = "test-production-release-only",
   AllReleaseListAvailable = "all-release-list-available",
-  ReadAndUnreadMessages = "mark-and-unread-messages",
+  ReadAndUnreadMessages = "read-and-unread-messages",
 }
