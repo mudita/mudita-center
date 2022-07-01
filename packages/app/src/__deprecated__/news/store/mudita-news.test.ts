bd6ef60ceb417c81dbd621843ae7cd1b4b5e019e
Michał Wójcik <wojcik92michal@gmail.com>
2022-07-01 15:51:53 +0200

    [CP-1281] improve mudita news tests

--- packages/app/src/__deprecated__/news/store/mudita-news.test.ts
+++ packages/app/src/__deprecated__/news/store/mudita-news.test.ts
@@ -10,12 +10,19 @@ import { ipcRenderer } from "electron-better-ipc"
 import { NewsEvents } from "App/__deprecated__/main/functions/register-news-listener/register-news-listener"
 import { newsItems } from "App/__deprecated__/news/components/cards/cards-mock-data"
 
+const offlineDataResponse = {
+  newsItems: [newsItems[0]],
+  lastUpdate: "2020-04-28T11:23:22.696Z",
+}
+
+const onlineDataResponse = {
+  newsItems: [...newsItems],
+  lastUpdate: "2021-05-29T13:42:59.777Z",
+}
+
 afterEach(() => {
   ;(ipcRenderer as any).__rendererCalls = {}
 })
-
-const offlineData = [newsItems[0]]
-const onlineData = [...newsItems]
 
 test("call is performed after dispatching effect and gets initial state", () => {
   const store = init({
@@ -40,24 +47,51 @@ describe("when network is connected", () => {
       },
     })
     ;(ipcRenderer as any).__rendererCalls = {
-      [NewsEvents.GetCachedNews]: Promise.resolve({
-        newsItems: offlineData,
-        lastUpdate: "2020-04-28T11:23:22.696Z",
-      }),
-      [NewsEvents.GetUpdatedNews]: Promise.resolve({
-        newsItems: onlineData,
-        lastUpdate: "2020-04-29T11:23:22.696Z",
-      }),
+      [NewsEvents.GetCachedNews]: Promise.resolve(offlineDataResponse),
+      [NewsEvents.GetUpdatedNews]: Promise.resolve(onlineDataResponse),
     }
 
     await store.dispatch.networkStatus.updateOnlineStatus()
     await store.dispatch.muditaNews.loadData("", store.getState())
     expect(store.getState()).toMatchSnapshot()
   })
+
+  test("updating store data is called twice - for offline data and for online data", async () => {
+    const dispatchMock = {
+      muditaNews: {
+        update: jest.fn(),
+      },
+    }
+
+    const rootMock = {
+      networkStatus: {
+        online: true,
+      },
+    }
+
+    ;(ipcRenderer as any).__rendererCalls = {
+      [NewsEvents.GetCachedNews]: Promise.resolve(offlineDataResponse),
+      [NewsEvents.GetUpdatedNews]: Promise.resolve(onlineDataResponse),
+    }
+
+    await (muditaNews as any)
+      .effects(dispatchMock)
+      .loadData(undefined, rootMock)
+
+    expect(dispatchMock.muditaNews.update).toHaveBeenCalledTimes(2)
+    expect(dispatchMock.muditaNews.update).toHaveBeenNthCalledWith(
+      1,
+      offlineDataResponse
+    )
+    expect(dispatchMock.muditaNews.update).toHaveBeenNthCalledWith(
+      2,
+      onlineDataResponse
+    )
+  })
 })
 
 describe("when network is not connected", () => {
-  test("offline data and returned", async () => {
+  test("offline data is returned", async () => {
     const store = init({
       models: {
         muditaNews,
@@ -65,17 +99,39 @@ describe("when network is not connected", () => {
       },
     })
     ;(ipcRenderer as any).__rendererCalls = {
-      [NewsEvents.GetCachedNews]: Promise.resolve({
-        newsItems: offlineData,
-        lastUpdate: "2020-04-28T11:23:22.696Z",
-      }),
-      [NewsEvents.GetUpdatedNews]: Promise.resolve({
-        newsItems: onlineData,
-        lastUpdate: "2020-04-29T11:23:22.696Z",
-      }),
+      [NewsEvents.GetCachedNews]: Promise.resolve(offlineDataResponse),
     }
 
     await store.dispatch.muditaNews.loadData("", store.getState())
     expect(store.getState()).toMatchSnapshot()
+  })
+
+  test("updating store data is called once", async () => {
+    const dispatchMock = {
+      muditaNews: {
+        update: jest.fn(),
+      },
+    }
+
+    const rootMock = {
+      networkStatus: {
+        online: false,
+      },
+    }
+
+    ;(ipcRenderer as any).__rendererCalls = {
+      [NewsEvents.GetCachedNews]: Promise.resolve(offlineDataResponse),
+      [NewsEvents.GetUpdatedNews]: Promise.resolve(onlineDataResponse),
+    }
+
+    await (muditaNews as any)
+      .effects(dispatchMock)
+      .loadData(undefined, rootMock)
+
+    expect(dispatchMock.muditaNews.update).toHaveBeenCalledTimes(1)
+    expect(dispatchMock.muditaNews.update).toHaveBeenNthCalledWith(
+      1,
+      offlineDataResponse
+    )
   })
 })
