f2f60716f31f037d9c3671fdaa955fe819341a73
Daniel Karski <daniel.karski5q@gmail.com>
2022-07-19 08:19:59 +0200

    Parsing in data-sync module fixed

--- packages/app/src/data-sync/presenters/thread/thread.presenter.test.ts
+++ packages/app/src/data-sync/presenters/thread/thread.presenter.test.ts
@@ -58,4 +58,46 @@ describe("`ThreadPresenter`", () => {
       ]
     `)
   })
+
+  test("when sms table is empty serialize record properly", async () => {
+    const threadInput: ThreadInput = {
+      threads: {
+        columns: [
+          "_id",
+          "contact_id",
+          "date",
+          "last_dir",
+          "msg_count",
+          "number_id",
+          "read",
+          "snippet",
+        ],
+        values: [["1", "4", "391", "2", "2", "5", "1", "Test"]],
+      },
+      contact_number: {
+        columns: ["_id", "contact_id", "number_user", "number_e164", "type"],
+        values: [["5", "4", "+91898402777", "", "0"]],
+      },
+      sms: {
+        columns: [],
+        values: [
+        ],
+      },
+    }
+
+    const presenter = new ThreadPresenter()
+    const threadObjects = presenter.serializeToObject(threadInput)
+    expect(threadObjects).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "lastUpdatedAt": 1970-01-01T00:06:31.000Z,
+          "messageSnippet": "Test",
+          "messageType": "OUTBOX",
+          "phoneNumber": "+91898402777",
+          "unread": true,
+        },
+      ]
+    `)
+  })
 })
