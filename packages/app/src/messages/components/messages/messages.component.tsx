1cc5490e7536e057f87735bee93e23572c83eae7
igorbezsmertnyii <mr.igor96@outlook.com>
2022-07-21 11:59:38 +0200

    [CP-1455] Fix new conversation draft functionality

--- packages/app/src/messages/components/messages/messages.component.tsx
+++ packages/app/src/messages/components/messages/messages.component.tsx
@@ -217,7 +217,10 @@ const Messages: FunctionComponent<MessagesProps> = ({
         setContent("")
       }
     } else {
-      if (debouncedContent) {
+      if (
+        debouncedContent &&
+        activeThread?.phoneNumber !== mockThread?.phoneNumber
+      ) {
         handleAddNewMessage(MessageType.DRAFT)
         updateFieldState("draftDeleting", false)
       }
