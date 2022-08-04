d45f7ad849a3c13f6ed852b320bc75176bb25510
Weronika Bolesta <weronika.bolesta@mudita.com>
2022-08-04 09:27:49 +0200

    CP-1403 add emailRegexp variable

--- packages/app/src/__deprecated__/renderer/utils/form-validators.ts
+++ packages/app/src/__deprecated__/renderer/utils/form-validators.ts
@@ -11,6 +11,9 @@ export const phoneNumberRegexp = /^[ \\.+\-#()\d]*$/im
 export const passwordRegexp =
   /^(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
 
+export const emailRegexp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 export const primaryPhoneNumberValidator = (
   // AUTO DISABLED - fix me if you like :)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -89,8 +92,7 @@ export const secondaryPhoneNumberValidator = (
 
 export const emailValidator: RegisterOptions = {
   pattern: {
-    value:
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    value: emailRegexp,
     message: intl.formatMessage({ id: "component.formErrorInvalidEmail" }),
   },
   required: intl.formatMessage({ id: "component.formErrorRequiredEmail" }),
@@ -98,8 +100,7 @@ export const emailValidator: RegisterOptions = {
 
 export const emailValidatorNotRequired: RegisterOptions = {
   pattern: {
-    value:
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    value: emailRegexp,
     message: intl.formatMessage({ id: "component.formErrorInvalidEmail" }),
   },
 }
