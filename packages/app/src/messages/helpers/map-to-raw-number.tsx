2c02a3f5243977afe53db4d20d29cfdf9709923c
Daniel Karski <daniel.karski5q@gmail.com>
2022-08-04 08:07:14 +0200

    Lint fixed

--- packages/app/src/messages/helpers/map-to-raw-number.tsx
+++ packages/app/src/messages/helpers/map-to-raw-number.tsx
@@ -4,7 +4,7 @@
  */
 
 export const mapToRawNumber = (phoneNumber: string): string => {
-  if (/^[+]*[(]?[0-9]{1,3}[)]?[-\s\./0-9]*$/g.test(phoneNumber)) {
+  if (/^[+]*[(]?[0-9]{1,3}[)]?[-\s./0-9]*$/g.test(phoneNumber)) {
     return phoneNumber.replace(/\s/g, "")
   } else {
     return phoneNumber
