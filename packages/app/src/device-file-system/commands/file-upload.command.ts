a84f4f68d9d2264090b8b1463e925114ea1074eb
igorbezsmertnyii <mr.igor96@outlook.com>
2022-08-10 11:51:05 +0200

    [CP-498] Separate functionality by dedicated commands for retrieve and upload files

---
+++ packages/app/src/device-file-system/commands/file-upload.command.ts
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { Endpoint, Method } from "@mudita/pure"
+import DeviceService from "App/__deprecated__/backend/device-service"
+import { FileSystemService } from "App/file-system/services/file-system.service.refactored"
+import { AppError } from "App/core/errors"
+import { Result, ResultObject } from "App/core/builder"
+import { BaseCommand } from "App/device-file-system/commands/base.command"
+import { RequestResponseStatus } from "App/core/types/request-response.interface"
+import { DeviceFileSystemError } from "App/device-file-system/constants"
+
+export class FileUploadCommand extends BaseCommand {
+  constructor(
+    public deviceService: DeviceService,
+    public fileSystemService: FileSystemService
+  ) {
+    super(deviceService)
+  }
+
+  public async exec(
+    directory: string,
+    path: string
+  ): Promise<ResultObject<undefined>> {
+    let data: Buffer | Uint8Array
+
+    try {
+      data = await this.fileSystemService.readFile(path)
+    } catch (error) {
+      return Result.failed(
+        new AppError(
+          DeviceFileSystemError.FileUploadUnreadable,
+          `Uploading file: file ${path} can't be opened`
+        )
+      )
+    }
+
+    const fileSize = Buffer.byteLength(data)
+    const fileCrc32 = this.countCRC32(data)
+    const fileName = path.split("/").reverse()[0]
+
+    const response = await this.deviceService.request({
+      endpoint: Endpoint.FileSystem,
+      method: Method.Put,
+      body: {
+        fileSize,
+        fileCrc32,
+        fileName: [directory, fileName].join("/"),
+      },
+    })
+
+    if (
+      response.status !== RequestResponseStatus.Ok ||
+      response.data === undefined
+    ) {
+      return Result.failed(
+        new AppError(
+          DeviceFileSystemError.FileUploadRequest,
+          "Uploading file: Something went wrong in init sending request"
+        )
+      )
+    }
+
+    const { txID, chunkSize } = response.data
+    return this.sendFileRequest(data, txID, chunkSize)
+  }
+
+  private async sendFileRequest(
+    buffer: Buffer | Uint8Array,
+    txID: string,
+    chunkSize: number,
+    chunkNo = 1
+  ): Promise<ResultObject<undefined>> {
+    try {
+      const sliceStart = (chunkNo - 1) * chunkSize
+      const sliceEnd = sliceStart + chunkSize
+      const chunkedBuffer = buffer.slice(sliceStart, sliceEnd)
+      const chunkedBufferSize = Buffer.byteLength(chunkedBuffer)
+      const lastChunk = chunkedBufferSize < chunkSize
+
+      const response = await this.deviceService.request({
+        endpoint: Endpoint.FileSystem,
+        method: Method.Put,
+        body: {
+          txID,
+          chunkNo,
+          data: chunkedBuffer.toString("base64"),
+        },
+      })
+
+      if (response.status !== RequestResponseStatus.Ok) {
+        return Result.failed(
+          new AppError(
+            DeviceFileSystemError.FileUploadChunk,
+            "Uploading file: Something went wrong in sent chunk fie."
+          )
+        )
+      } else {
+        if (lastChunk) {
+          return Result.success(undefined)
+        }
+        return this.sendFileRequest(buffer, txID, chunkSize, chunkNo + 1)
+      }
+    } catch (error) {
+      return Result.failed(
+        new AppError(
+          DeviceFileSystemError.FileUploadChunk,
+          error
+            ? (error as Error).message
+            : "Uploading file: Something went wrong in sent chunk fie."
+        )
+      )
+    }
+  }
+}
