e27a1acabbfc4ba94e00aeaaf1341925995e7279
igorbezsmertnyii <mr.igor96@outlook.com>
2022-08-24 13:22:12 +0200

    [CP-1551] Improve `SearchResult` interface

--- packages/app/src/search/dto/search-result.object.ts
+++ packages/app/src/search/dto/search-result.object.ts
@@ -3,16 +3,14 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
+import { Contact } from "App/contacts/dto"
+import { Message, Thread } from "App/messages/dto"
+import { Template } from "App/templates/dto"
 import { DataIndex } from "App/index-storage/constants"
 
-type ScopeKeys =
-  | DataIndex.Contact
-  | DataIndex.Message
-  | DataIndex.Template
-  | DataIndex.Thread
-
-export type SearchResult = {
-  // AUTO DISABLED - fix me if you like :)
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key in ScopeKeys]?: any[] | undefined
+export interface SearchResult {
+  [DataIndex.Contact]?: Contact[] | undefined
+  [DataIndex.Message]?: Message[] | undefined
+  [DataIndex.Thread]?: Thread[] | undefined
+  [DataIndex.Template]?: Template[] | undefined
 }
