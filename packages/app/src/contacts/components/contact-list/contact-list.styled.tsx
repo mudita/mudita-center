fa520cf8e754354034631eadb157f0e4a6a9b345
Michał Wójcik <wojcik92michal@gmail.com>
2022-09-02 10:31:58 +0200

    [CP-1524] show too long list contacts shortened

--- packages/app/src/contacts/components/contact-list/contact-list.styled.tsx
+++ packages/app/src/contacts/components/contact-list/contact-list.styled.tsx
@@ -70,7 +70,16 @@ export const BlockedIcon = styled(Icon).attrs(() => ({
   margin-left: 1.6rem;
 `
 
-export const NameSpan = styled(Text)``
+/* stylelint-disable property-no-vendor-prefix */
+/* stylelint-disable value-no-vendor-prefix */
+export const NameSpan = styled(Text)`
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-break: break-all;
+  overflow: hidden;
+`
 
 export const ClickableCol = styled(Col)<{ disabled?: boolean }>`
   height: 100%;
