1122782bb4905ac31d58cca52d6218142bbcc961
Daniel Karski <daniel.karski5q@gmail.com>
2022-12-22 09:56:01 +0100

    force update flag disabled until handle CP-1708

--- packages/app/src/feature-flags/features/index.ts
+++ packages/app/src/feature-flags/features/index.ts
@@ -204,7 +204,7 @@ export const features: EnvironmentConfig = {
   },
   [Feature.ForceUpdate]: {
     [Environment.Development]: false,
-    [Environment.Production]: true,
+    [Environment.Production]: false,
     [Environment.AlphaProduction]: false,
   },
 }
