4e4eb6e024647c74cf124bd1678ff7a70ee0400b
Daniel Karski <daniel.karski5q@gmail.com>
2023-02-28 08:46:05 +0100

    Delete no used imports for `OnboardingNotCompleteModalStory`

--- packages/app/src/overview/components/onboarding-not-complete-modal/onboarding-not-complete-modal.stories.tsx
+++ packages/app/src/overview/components/onboarding-not-complete-modal/onboarding-not-complete-modal.stories.tsx
@@ -7,7 +7,6 @@ import React, { FunctionComponent } from "react"
 import { Meta } from "@storybook/react"
 import Story from "App/__deprecated__/renderer/components/storybook/story.component"
 import { action } from "@storybook/addon-actions"
-import { NotEnoughSpaceModal } from "App/overview/components/update-os-modals/not-enough-space-modal/not-enough-space-modal.component"
 import { OnboardingNotCompleteModal } from "App/overview/components/onboarding-not-complete-modal/onboarding-not-complete-modal.component"
 
 export const OnboardingNotCompleteModalStory: FunctionComponent = () => {
