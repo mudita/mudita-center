ad1e4d87613d53129966599c5ba14875ecfa2444
rajmundex <lukasz.jarkowski@mudita.com>
2023-03-03 11:10:26 +0100

    added new delete scenarios

--- packages/e2e/src/page-objects/messages.page.ts
+++ packages/e2e/src/page-objects/messages.page.ts
@@ -7,34 +7,224 @@ import { ChainablePromiseElement } from "webdriverio"
 import Page from "./page"
 
 class MessagesPage extends Page {
-  public get newMessageButton(): ChainablePromiseElement<
+  public get buttonNewMessage(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="message-panel-new-message-button"]')
   }
 
-  public get searchContactsInput(): ChainablePromiseElement<
+  public get inputSearchContacts(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="receiver-input-select-input"]')
   }
 
-  public get messageInput(): ChainablePromiseElement<
+  public get inputMessage(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="thread-details-text-area-input"]')
   }
 
-  public get notSendMessageIcon(): ChainablePromiseElement<
+  public get buttonSendMessage(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="thread-details-text-area-send-button"]')
+  }
+
+  public get iconNotSendMessage(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="message-bubble-not-send-icon"]')
   }
 
-  public get notSendThreadIcon(): ChainablePromiseElement<
+  public get iconNotSendThread(): ChainablePromiseElement<
     Promise<WebdriverIO.Element>
   > {
     return $('[data-testid="thread-not-send-message-icon"]')
+  }
+
+  public get buttonDeleteOnThreadDetailScreen(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-Delete"]')
+  }
+
+  public get buttonAddContactThreadDetailScreen(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-NewContact"]')
+  }
+
+  public get buttonCloseThreadDetailScreen(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-Close"]')
+  }
+
+  public get buttonMarkUnreadThreadDetailScreen(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-Close"]')
+  }
+
+  public get sendingStatus(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="dot"]')
+  }
+
+  public get containerMessage(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="message-bubble-container"]')
+  }
+
+  public get containerMessageContent(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="message-bubble-message-content"]')
+  }
+
+  async hoverOverMessage() {
+    await this.containerMessageContent.moveTo()
+  }
+
+  public get dropdownMessageAction(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="message-bubble-dropdown-action-button"]')
+  }
+
+  //
+  public get buttonSingleMessageOptions(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return this.containerMessage.$('[data-testid="icon-More"]')
+  }
+
+  async clickMessageOptionsButton() {
+    await this.buttonSingleMessageOptions.waitForClickable()
+    await this.buttonSingleMessageOptions.click()
+  }
+
+  public get listEmptyThread(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="messages-empty-thread-list-state"]')
+  }
+
+  public get dropdownListMessage(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="dropdown"]')
+  }
+
+  public get optionDeleteMessageDropdown(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return this.dropdownListMessage.$(
+      '[data-testid="message-bubble-delete-message-button"]'
+    )
+  }
+
+  async clickDeleteMessage() {
+    await this.optionDeleteMessageDropdown.waitForClickable()
+    await this.optionDeleteMessageDropdown.click()
+  }
+
+  public get buttonOptionsThreadListScreen(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-More"]')
+  }
+
+  public get optionDeleteDropdown(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="dropdown-delete"]')
+  }
+
+  async clickNewMessageButton() {
+    await this.buttonNewMessage.waitForClickable({ timeout: 7000 })
+    await this.buttonNewMessage.click()
+  }
+
+  async insertTextToSearchContactInput(phoneNumber: string) {
+    await this.inputSearchContacts.waitForDisplayed()
+    await this.inputSearchContacts.setValue(phoneNumber)
+  }
+
+  async insertTextToMessageInput(messageText: string) {
+    await this.inputMessage.waitForDisplayed()
+    await this.inputMessage.setValue(messageText)
+  }
+
+  async sendMessage(phoneNumber: string, messageText: string) {
+    // click NEW MESSAGE button
+    await this.clickNewMessageButton()
+    //input phone number into contact search field
+    await this.insertTextToSearchContactInput(phoneNumber)
+    //input message text into message input
+    await this.insertTextToMessageInput(messageText)
+    //send by pressing Send button
+
+    await this.buttonSendMessage.waitForClickable()
+    await this.buttonSendMessage.click()
+  }
+
+  async clickCloseThreadDetailsScreen() {
+    await this.buttonCloseThreadDetailScreen.waitForClickable({ timeout: 7000 })
+    await this.buttonCloseThreadDetailScreen.click()
+  }
+
+  async clickThreadOptionsButton() {
+    await this.buttonOptionsThreadListScreen.waitForClickable({ timeout: 7000 })
+    await this.buttonOptionsThreadListScreen.click()
+  }
+
+  async clickDropdownThreadOptionDelete() {
+    await this.optionDeleteDropdown.waitForClickable({ timeout: 7000 })
+    await this.optionDeleteDropdown.click()
+  }
+
+  async clickDeleteButtonOnConversationDetailsScreen() {
+    await this.buttonDeleteOnThreadDetailScreen.waitForClickable({
+      timeout: 7000,
+    })
+    await this.buttonDeleteOnThreadDetailScreen.click()
+  }
+
+  public get singleThreadRow(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="message-row"]')
+  }
+
+  async hoverOverSingleThreadRow() {
+    await this.singleThreadRow.waitForDisplayed()
+    await this.singleThreadRow.moveTo()
+  }
+
+  public get checkboxThread(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="checkbox"]')
+  }
+
+  async clickThreadCheckbox() {
+    await this.checkboxThread.waitForClickable()
+    await this.checkboxThread.click()
+  }
+
+  public get buttonDeleteSelectionManager(): ChainablePromiseElement<
+    Promise<WebdriverIO.Element>
+  > {
+    return $('[data-testid="icon-Delete"]')
+  }
+
+  async clickDeleteButtonSelectionMaanager() {
+    await this.buttonDeleteSelectionManager.waitForClickable()
+    await this.buttonDeleteSelectionManager.click()
   }
 }
 
--- packages/e2e/src/specs/messages/messages-send.e2e.ts
+++ packages/e2e/src/specs/messages/messages-send.e2e.ts
@@ -5,36 +5,146 @@
 
 import NavigationTabs from "../../page-objects/tabs.page"
 import MessagesPage from "../../page-objects/messages.page"
+import modalMessagesPage from "../../page-objects/modal-messages.page"
+import ModalGeneralPage from "../../page-objects/modal-general.page"
+import messagesPage from "../../page-objects/messages.page"
 
-describe("Messages send", () => {
+describe("Messages send and delete", () => {
   before(async () => {
     // Waiting for device connected through USB
     await browser.executeAsync((done) => {
       setTimeout(done, 10000)
     })
+
+    //ModalGeneralPage.closeModalButtonClick()
+    NavigationTabs.messagesTabClick()
+    await browser.pause(2000)
   })
-  it("Should display fail status if message not send", async () => {
-    const messagesTab = await NavigationTabs.messagesTab
-    await messagesTab.waitForDisplayed()
-    await messagesTab.click()
-    await browser.executeAsync((done) => {
-      setTimeout(done, 2000)
-    })
+  it("Send message to incorrect number using send button", async () => {
     //Send new message
-    const newMessageBtn = await MessagesPage.newMessageButton
-    await newMessageBtn.click()
-    const searchInput = await MessagesPage.searchContactsInput
-    searchInput.setValue("12345")
-    const msgInput = await MessagesPage.messageInput
-    msgInput.setValue("test SMS")
-    browser.keys("\uE007")
-    //wait for sending
-    await browser.executeAsync((done) => {
-      setTimeout(done, 4000)
-    })
-    const notSendMessageIcon = await MessagesPage.notSendMessageIcon
-    const notSendThreadIcon = await MessagesPage.notSendThreadIcon
+    await messagesPage.sendMessage(
+      "12345",
+      "SMS to incorrect number (Send button)"
+    )
+
+    //wait for sending and for failed send icons to appear
+    //browser.pause(4000)
+    const notSendMessageIcon = await MessagesPage.iconNotSendMessage
+    await notSendMessageIcon.waitForDisplayed({ timeout: 7000 })
+    const notSendThreadIcon = await MessagesPage.iconNotSendThread
+    await notSendThreadIcon.waitForDisplayed({ timeout: 7000 })
     expect(notSendMessageIcon).toBeDisabled()
     expect(notSendThreadIcon).toBeDisabled()
+  })
+  it("Should delete the thread using delete thread button on open thread screen", async () => {
+    //press delete button in the thread
+
+    await MessagesPage.clickDeleteButtonOnConversationDetailsScreen()
+
+    await modalMessagesPage.buttonConfirmDeleteClick()
+
+    const emptyThreadList = MessagesPage.listEmptyThread
+    await emptyThreadList.waitForDisplayed({ timeout: 7000 })
+    await expect(emptyThreadList).toBeDisplayed()
+
+    //delay for status popup to disappear
+    await browser.pause(5000)
+  })
+
+  it("Send message to incorrect number using Enter key", async () => {
+    // click New Message Button
+    await MessagesPage.clickNewMessageButton()
+    await MessagesPage.insertTextToSearchContactInput("54321")
+    await MessagesPage.insertTextToMessageInput(
+      "SMS to incorrect number (Enter key send)"
+    )
+    // Enter key press
+    await browser.keys("\uE007")
+
+    const notSendMessageIcon = await MessagesPage.iconNotSendMessage
+    //extended time to  wait for delete status popup to dissappear
+    await notSendMessageIcon.waitForDisplayed({ timeout: 10000 })
+    const notSendThreadIcon = await MessagesPage.iconNotSendThread
+    await notSendThreadIcon.waitForDisplayed({ timeout: 10000 })
+    expect(notSendMessageIcon).toBeDisabled()
+    expect(notSendThreadIcon).toBeDisabled()
+  })
+
+  it("Should close the thread and delete it from the thread list screen", async () => {
+    // press X to close the thread details screen
+    await MessagesPage.clickCloseThreadDetailsScreen()
+    // press thread options button (...)
+    await MessagesPage.clickThreadOptionsButton()
+    //click delete from dropdown
+    await MessagesPage.clickDropdownThreadOptionDelete()
+    //confirm delete operation
+    await modalMessagesPage.buttonConfirmDeleteClick()
+
+    const emptyThreadList = MessagesPage.listEmptyThread
+    await emptyThreadList.waitForDisplayed({ timeout: 8000 })
+    await expect(emptyThreadList).toBeDisplayed()
+  })
+
+  it("Send message to incorrect number using send button", async () => {
+    //Send new message
+    await messagesPage.sendMessage(
+      "02468",
+      "SMS to incorrect number (Send button)"
+    )
+
+    //wait for sending and for failed send icons to appear
+    //browser.pause(4000)
+    const notSendMessageIcon = await MessagesPage.iconNotSendMessage
+    await notSendMessageIcon.waitForDisplayed({ timeout: 7000 })
+    const notSendThreadIcon = await MessagesPage.iconNotSendThread
+    await notSendThreadIcon.waitForDisplayed({ timeout: 7000 })
+    expect(notSendMessageIcon).toBeDisabled()
+    expect(notSendThreadIcon).toBeDisabled()
+  })
+  it("Click message options button and delete it", async () => {
+    //Send new message
+    await browser.pause(9000)
+    await MessagesPage.hoverOverMessage()
+
+    await MessagesPage.clickMessageOptionsButton()
+    await browser.saveScreenshot("./Screen.png")
+    await MessagesPage.clickDeleteMessage()
+
+    await modalMessagesPage.buttonConfirmDeleteClick()
+
+    const emptyThreadList = MessagesPage.listEmptyThread
+    await emptyThreadList.waitForDisplayed({ timeout: 7000 })
+    await expect(emptyThreadList).toBeDisplayed()
+  })
+
+  it("Send message to incorrect number using send button", async () => {
+    //Send new message
+    await messagesPage.sendMessage(
+      "13579",
+      "SMS to incorrect number (Send button)"
+    )
+    const notSendMessageIcon = await MessagesPage.iconNotSendMessage
+    await notSendMessageIcon.waitForDisplayed({ timeout: 7000 })
+    const notSendThreadIcon = await MessagesPage.iconNotSendThread
+    await notSendThreadIcon.waitForDisplayed({ timeout: 7000 })
+    expect(notSendMessageIcon).toBeDisabled()
+    expect(notSendThreadIcon).toBeDisabled()
+  })
+  it("Should close the thread and delete thread using selection manager", async () => {
+    // press X to close the thread details screen
+    await MessagesPage.clickCloseThreadDetailsScreen()
+
+    await MessagesPage.hoverOverSingleThreadRow()
+
+    await MessagesPage.clickThreadCheckbox()
+
+    await MessagesPage.clickDeleteButtonSelectionMaanager()
+
+    await modalMessagesPage.buttonConfirmDeleteClick()
+
+    await browser.saveScreenshot("./Screenshot1.png")
+    const emptyThreadList = MessagesPage.listEmptyThread
+    await emptyThreadList.waitForDisplayed({ timeout: 8000 })
+    await expect(emptyThreadList).toBeDisplayed()
   })
 })
