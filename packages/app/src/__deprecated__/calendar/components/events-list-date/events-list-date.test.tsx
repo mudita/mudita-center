10bb741dc967e2ac0c88f7d37a549298efc8e0d8
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-03-09 16:49:53 +0100

    Reversing event-list-date.test.tsx, wrong node

--- packages/app/src/__deprecated__/calendar/components/events-list-date/events-list-date.test.tsx
+++ packages/app/src/__deprecated__/calendar/components/events-list-date/events-list-date.test.tsx
@@ -20,7 +20,7 @@ const multiDaysEvent = {
   endDate: "2020-02-14T11:00:00.000Z",
 }
 const oneDayResult = `Wednesday, February 12, 2020, 10:00 AM - 11:00 AM`
-const multiDaysResult = `February 12, 2020 at 10:00 AM - February 14, 2020 at 11:00 AM`
+const multiDaysResult = `February 12, 2020, 10:00 AM - February 14, 2020, 11:00 AM`
 
 test("show one day event date", () => {
   const { container } = renderWithThemeAndIntl(
