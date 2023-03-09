c30b7f50a1f9127651fc08e73f265ac75bffd0ba
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-03-09 16:30:15 +0100

    Fixes for tests for changes in news sorting

--- packages/app/src/news/helpers/sort-by-creation-date-in-descending-order/sort-by-creation-date-in-descending-order.helper.test.ts
+++ packages/app/src/news/helpers/sort-by-creation-date-in-descending-order/sort-by-creation-date-in-descending-order.helper.test.ts
@@ -9,6 +9,25 @@ import { sortByDateInDescendingOrder } from "./sort-by-creation-date-in-descendi
 const newsEntityMock: NewsEntry[] = [
   {
     category: "Blog",
+    title: "Remote work",
+    content:
+      "What started as a way to keep employees safe, morphed into the world’s largest work-from-home experiment.",
+    image: {
+      sys: undefined,
+    },
+    communityLink: "https://forum.mudita.com/t/the-future-of-work/2632",
+    link: "https://mudita.com/community/blog/is-remote-work-here-to-stay/",
+    discussionId: "2632",
+    commentsCount: 1,
+    date: "2021-06-02T13:16+00:00",
+    newsId: "3oanllPiOTmu6GD72CTZFD",
+    updatedAt: "2021-07-27T13:07:31.243Z",
+    createdAt: "2021-07-27T13:07:15.756Z",
+    imageAlt: "Remote work",
+    imageSource: "",
+  },
+  {
+    category: "Blog",
     title: "Testing Mudita Pure",
     content:
       "Recently, our Team had the exciting opportunity to test Mudita Pure for an entire week.",
@@ -25,25 +44,6 @@ const newsEntityMock: NewsEntry[] = [
     updatedAt: "2021-07-27T12:58:23.083Z",
     createdAt: "2021-07-27T12:58:23.083Z",
     imageAlt: "Pure testing videos",
-    imageSource: "",
-  },
-  {
-    category: "Blog",
-    title: "Remote work",
-    content:
-      "What started as a way to keep employees safe, morphed into the world’s largest work-from-home experiment.",
-    image: {
-      sys: undefined,
-    },
-    communityLink: "https://forum.mudita.com/t/the-future-of-work/2632",
-    link: "https://mudita.com/community/blog/is-remote-work-here-to-stay/",
-    discussionId: "2632",
-    commentsCount: 1,
-    date: "2021-06-02T13:16+00:00",
-    newsId: "3oanllPiOTmu6GD72CTZFD",
-    updatedAt: "2021-07-27T13:07:31.243Z",
-    createdAt: "2021-07-27T13:07:15.756Z",
-    imageAlt: "Remote work",
     imageSource: "",
   },
 ]
