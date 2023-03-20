7154c66066a01bc4ecbf79c07e03e2b7cad6be82
rajmundex <lukasz.jarkowski@mudita.com>
2023-03-20 17:28:43 +0100

    tests update

--- packages/e2e/src/specs/messages/messages-thread-creation.e2e.ts
+++ packages/e2e/src/specs/messages/messages-thread-creation.e2e.ts
@@ -7,13 +7,13 @@ import NavigationTabs from "../../page-objects/tabs.page"
 import MessagesPage from "../../page-objects/messages.page"
 import ModalGeneralPage from "../../page-objects/modal-general.page"
 import MessagesConversationPage from "../../page-objects/messages-conversation.page"
+import BrowseContactsModal from "../../page-objects/messages-browse-contacts-modal.page"
 
+import { addNewContact } from "../../helpers/contacts.helper"
 import {
   deleteConversationOnThreadList,
   sendMessage,
 } from "../../helpers/messages.helper"
-
-import { addNewContact } from "../../helpers/contacts.helper"
 
 describe("New thread creation scenarios - no contacts & no threads", () => {
   before(async () => {
@@ -25,66 +25,105 @@ describe("New thread creation scenarios - no contacts & no threads", () => {
     await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
     await NavigationTabs.clickMessagesTab()
   })
-  it("press phone book button (empty phone book) - UI check", async () => {})
+  it("Press browse contacts button and check no contacts is displayed", async () => {
+    await MessagesPage.clickNewMessageButton()
+    await MessagesConversationPage.clickBrowseContactsButton()
 
-  it("input message first then recipient number ", async () => {})
+    const searchInput = await BrowseContactsModal.searchInput
+    await searchInput.waitForDisplayed()
+
+    const contactList = await BrowseContactsModal.emptyContactList
+    await expect(contactList).toBeDisplayed()
+  })
+
+  it("Checks modal title is Browse Contacts and close it", async () => {
+    const modalTitle = await BrowseContactsModal.modalHeader
+    expect(modalTitle).toHaveText("Browse Contacts")
+
+    await BrowseContactsModal.closeModalButtonClick()
+  })
+
+  it("Input message first then recipient number and check message Send button is clickable", async () => {
+    await MessagesConversationPage.insertTextToMessageInput(
+      "First input message text"
+    )
+    await MessagesConversationPage.insertTextToSearchContactInput("11111")
+
+    const sendButton = await MessagesConversationPage.sendMessageButton
+    await expect(sendButton).toBeClickable()
+  })
+
+  it("Change receipient number", async () => {
+    const testNumber = "22222"
+    console.log("**************************")
+
+    await MessagesConversationPage.insertTextToSearchContactInput(testNumber)
+
+    await browser.saveScreenshot("./recipientChange.png")
+    const recipientInput = MessagesConversationPage.searchContactsInput
+    await expect(recipientInput).toHaveValue(testNumber)
+  })
 
   after(async () => {
-    await deleteConversationOnThreadList()
+    //await deleteConversationOnThreadList()
   })
 })
 
-describe("New thread creation scenarios - no contacts & thread exists", () => {
-  before(async () => {
-    // Waiting for device connected through USB
-    await browser.executeAsync((done) => {
-      setTimeout(done, 10000)
-    })
+// describe("New thread creation scenarios - no contacts & thread exists", () => {
+//   before(async () => {
+//     // Waiting for device connected through USB
+//     await browser.executeAsync((done) => {
+//       setTimeout(done, 10000)
+//     })
 
-    await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
-    await NavigationTabs.clickMessagesTab()
-  })
-  it("input recipient number of existing thread and press enter ", async () => {})
+//     await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
+//     await NavigationTabs.clickMessagesTab()
+//   })
+//   it("input recipient number of existing thread and press enter ", async () => {})
 
-  after(async () => {
-    await deleteConversationOnThreadList()
-  })
-})
+//   after(async () => {
+//     await deleteConversationOnThreadList()
+//   })
+// })
 
-describe("New thread creation scenarios - contact exists & no threads", () => {
-  before(async () => {
-    // Waiting for device connected through USB
-    await browser.executeAsync((done) => {
-      setTimeout(done, 10000)
-    })
+// describe("New thread creation scenarios - contact exists & no threads", () => {
+//   before(async () => {
+//     // Waiting for device connected through USB
+//     await browser.executeAsync((done) => {
+//       setTimeout(done, 10000)
+//     })
 
-    await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
-    await NavigationTabs.clickContactsTab()
-    await addNewContact("Henryk", "Kwiatkowski", "+48664364535")
+//     await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
+//     await NavigationTabs.clickContactsTab()
+//     await addNewContact("Henryk", "Kwiatkowski", "+48664364535")
 
-    await NavigationTabs.clickMessagesTab()
-  })
-  it("press phone book button (contact exist)", async () => {})
-  it("insert part of existing contact name into search contact input", async () => {})
-  it("insert part of existing contact number into search contact input", async () => {})
-  after(async () => {
-    await deleteConversationOnThreadList()
-  })
-})
+//     await NavigationTabs.clickMessagesTab()
+//   })
+//   xit("press phone book button (contact exist)", async () => {})
+//   xit("insert part of existing contact name into search contact input", async () => {})
+//   xit("insert part of existing contact number into search contact input", async () => {})
+//   after(async () => {
+//     await deleteConversationOnThreadList()
+//   })
+// })
 
-describe("New thread creation scenarios - contact exists & thread exists", () => {
-  before(async () => {
-    // Waiting for device connected through USB
-    await browser.executeAsync((done) => {
-      setTimeout(done, 10000)
-    })
+// describe("New thread creation scenarios - contact exists & thread exists", () => {
+//   before(async () => {
+//     // Waiting for device connected through USB
+//     await browser.executeAsync((done) => {
+//       setTimeout(done, 10000)
+//     })
 
-    await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
-    await NavigationTabs.clickMessagesTab()
-  })
-  it("input recipient number of existing thread and press enter ", async () => {})
+//     await ModalGeneralPage.clickUpdateAvailableModalCloseButton()
+//     await NavigationTabs.clickMessagesTab()
+//   })
+//   xit("input recipient number of existing thread and press enter ", async () => {})
 
-  after(async () => {
-    await deleteConversationOnThreadList()
-  })
-})
+//   after(async () => {
+//     try {
+//       await deleteConversationOnThreadList()
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   })
+// })
