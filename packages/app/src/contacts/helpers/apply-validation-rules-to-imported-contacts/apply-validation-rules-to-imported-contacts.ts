8cf0fcf64c8e23bce76277913ba29bd368b04284
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-04-12 16:40:12 +0200

    Don't import contacts with empty phone number

--- packages/app/src/contacts/helpers/apply-validation-rules-to-imported-contacts/apply-validation-rules-to-imported-contacts.ts
+++ packages/app/src/contacts/helpers/apply-validation-rules-to-imported-contacts/apply-validation-rules-to-imported-contacts.ts
@@ -9,9 +9,14 @@ import { NewContact } from "App/contacts/reducers"
 export const applyValidationRulesToImportedContacts = (
   importedContacts: NewContact[]
 ): NewContact[] => {
-  return importedContacts.map((contact) => ({
-    ...contact,
-    firstName: contact.firstName?.substring(0, MaxNameLength),
-    lastName: contact.lastName?.substring(0, MaxNameLength),
-  }))
+  return importedContacts
+    .map((contact) => ({
+      ...contact,
+      firstName: contact.firstName?.substring(0, MaxNameLength),
+      lastName: contact.lastName?.substring(0, MaxNameLength),
+    }))
+    .filter(
+      ({ primaryPhoneNumber }) =>
+        primaryPhoneNumber !== undefined && primaryPhoneNumber !== ""
+    )
 }
