9d8ba03187cea8ea45056bf64410f05a7e11b04e
Lukasz Jarkowski <lukasz.jarkowski@mudita.com>
2023-04-20 12:40:47 +0200

    test data

---
+++ packages/e2e/src/test-data/contacts-only-test-data.ts
@@ -0,0 +1,64 @@
+export const contacts: Array<{
+  firstName: string
+  lastName: string
+  primaryNumber: string
+  secondaryNumber: string
+  addressFirstLine: string
+  addressSecondLine: string
+  addToFavourites: boolean
+}> = [
+  {
+    firstName: "Henryk",
+    lastName: "Zaskroniec",
+    primaryNumber: "33441",
+    secondaryNumber: "",
+    addressFirstLine: "Domaniewska 30",
+    addressSecondLine: "00-675 Warszawa",
+    addToFavourites: true,
+  },
+  {
+    firstName: "Zbyszek",
+    lastName: "Gamon",
+    primaryNumber: "44115",
+    secondaryNumber: "12344",
+    addressFirstLine: "",
+    addressSecondLine: "",
+    addToFavourites: false,
+  },
+  {
+    firstName: "Krzysztof",
+    lastName: "Padaka",
+    primaryNumber: "22443",
+    secondaryNumber: "",
+    addressFirstLine: "Podwawelska 2",
+    addressSecondLine: "22-111 Zakopane",
+    addToFavourites: true,
+  },
+  {
+    firstName: "Lucyna",
+    lastName: "Salceson",
+    primaryNumber: "13579",
+    secondaryNumber: "",
+    addressFirstLine: "",
+    addressSecondLine: "",
+    addToFavourites: false,
+  },
+  {
+    firstName: "Stefan",
+    lastName: "Kajak",
+    primaryNumber: "86420",
+    secondaryNumber: "86421",
+    addressFirstLine: "Nowa 24",
+    addressSecondLine: "Otwock",
+    addToFavourites: true,
+  },
+  {
+    firstName: "Dariusz",
+    lastName: "Niepowie",
+    primaryNumber: "11225",
+    secondaryNumber: "22443",
+    addressFirstLine: "",
+    addressSecondLine: "",
+    addToFavourites: false,
+  },
+]
