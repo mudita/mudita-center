4ff8baa07a8fd63702d0fb77184788409a13a5e3
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-04-28 10:18:57 +0200

    Rename of constant for timeout

--- packages/app/src/update/constants/get-release-timeout.constant.ts
+++ packages/app/src/update/constants/get-release-timeout.constant.ts
@@ -3,5 +3,4 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-export const SECOND_IN_MILIS = 1000
-export const GET_RELEASE_TIMEOUT = 60 * SECOND_IN_MILIS
+export const RELEASE_TIMEOUT = 60 * 1000
