ff5a681f0d74d3a83b8815ccbf96b1c4fef6f19e
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-06-06 08:58:41 +0200

    [CP-1990] Implement the logic for filtering Mudita employees from the collected MuditaOS download events (#1274)

--- packages/app/src/update/services/releases.service.ts
+++ packages/app/src/update/services/releases.service.ts
@@ -50,16 +50,37 @@ export class ReleaseService {
 
   // TODO [mw] workaround change needed for QA team. Should be removed when implementing CP-1743
   public async getTestReleasesForSOSUfeature(
-    product: Product
+    product: Product,
+    deviceSerialNumber?: string
   ): Promise<ResultObject<OsRelease[] | undefined>> {
     try {
       const promises = [
-        this.getRelease(product, OsEnvironment.Production, "latest"),
+        this.getRelease(
+          product,
+          OsEnvironment.Production,
+          "latest",
+          deviceSerialNumber
+        ),
         product === Product.BellHybrid
-          ? this.getRelease(product, OsEnvironment.Production, "1.6.0")
+          ? this.getRelease(
+              product,
+              OsEnvironment.Production,
+              "1.6.0",
+              deviceSerialNumber
+            )
           : undefined,
-        this.getRelease(product, OsEnvironment.TestProduction, "latest"),
-        this.getRelease(product, OsEnvironment.Daily, "latest"),
+        this.getRelease(
+          product,
+          OsEnvironment.TestProduction,
+          "latest",
+          deviceSerialNumber
+        ),
+        this.getRelease(
+          product,
+          OsEnvironment.Daily,
+          "latest",
+          deviceSerialNumber
+        ),
       ].filter((item): item is Promise<ReleaseManifest> => !!item)
 
       const releases = await Promise.all(promises)
@@ -80,11 +101,12 @@ export class ReleaseService {
   public async getReleasesByVersions({
     product,
     versions,
+    deviceSerialNumber,
   }: GetReleasesByVersionsInput): Promise<ResultObject<OsRelease[]>> {
     try {
       const releases = await Promise.all(
         versions.map((version) =>
-          this.getRelease(product, releaseSpace, version)
+          this.getRelease(product, releaseSpace, version, deviceSerialNumber)
         )
       )
 
@@ -102,10 +124,16 @@ export class ReleaseService {
   }
 
   public async getLatestRelease(
-    product: Product
+    product: Product,
+    deviceSerialNumber?: string
   ): Promise<ResultObject<OsRelease | undefined>> {
     try {
-      const release = await this.getRelease(product, releaseSpace, "latest")
+      const release = await this.getRelease(
+        product,
+        releaseSpace,
+        "latest",
+        deviceSerialNumber
+      )
 
       return Result.success(GithubReleasePresenter.toRelease(release))
     } catch (error) {
@@ -121,12 +149,14 @@ export class ReleaseService {
   private async getRelease(
     product: Product,
     releaseSpace: OsEnvironment,
-    version: "latest" | string
+    version: "latest" | string,
+    deviceSerialNumber?: string
   ): Promise<ReleaseManifest> {
     const { data } = await this.client.getLatestRelease({
       product,
       environment: releaseSpace,
       version,
+      deviceSerialNumber,
     })
 
     return data
