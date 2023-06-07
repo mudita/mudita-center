07c313c90020e7e7709cd7641da2525ffbb75a78
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-06-07 12:43:21 +0200

    fix with vendorId

--- packages/app/src/device/constants/product-identifier.constant.ts
+++ packages/app/src/device/constants/product-identifier.constant.ts
@@ -7,14 +7,14 @@ export enum ProductID {
   MuditaPure = "0100",
   MuditaHarmony = "0300",
   //TODO CP-1671 kompakt to string values
-  MuditaKompaktCharge = 0x2006,
-  MuditaKompaktTransfer = 0x200a,
-  MuditaKompaktNoDebug = 0x2012,
+  MuditaKompaktCharge = "2006", //0x2006
+  MuditaKompaktTransfer = "200a", //0x200a
+  MuditaKompaktNoDebug = "2012", //0x2012
 }
 
 export enum VendorID {
   MuditaPure = "3310",
   MuditaHarmony = "3310",
   //TODO CP-1671 kompakt to string values
-  MuditaKompakt = 0x0e8d,
+  MuditaKompakt = "0e8d", //0x0e8d
 }
