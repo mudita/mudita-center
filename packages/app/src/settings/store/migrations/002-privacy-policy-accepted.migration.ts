d6482c26c8eab590b98788049006de83631fed28
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2023-07-06 15:34:23 +0200

    lint fix

--- packages/app/src/settings/store/migrations/002-privacy-policy-accepted.migration.ts
+++ packages/app/src/settings/store/migrations/002-privacy-policy-accepted.migration.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Mudita sp. z o.o. All rights reserved.
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
+
 import Store from "electron-store"
 
 // AUTO DISABLED - fix me if you like :)
