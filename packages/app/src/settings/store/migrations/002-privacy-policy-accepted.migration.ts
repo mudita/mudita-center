36dbdf54421885b34686c9203c2855820e73ff27
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2023-07-06 15:29:22 +0200

    Update 002-privacy-policy-accepted.migration.ts

--- packages/app/src/settings/store/migrations/002-privacy-policy-accepted.migration.ts
+++ packages/app/src/settings/store/migrations/002-privacy-policy-accepted.migration.ts
@@ -2,10 +2,11 @@
  * Copyright (c) Mudita sp. z o.o. All rights reserved.
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
+import Store from "electron-store"
 
 // AUTO DISABLED - fix me if you like :)
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export const privacyPolicyAcceptedMigration = (store: any) => {
+export const privacyPolicyAcceptedMigration = (store: Store) => {
   // AUTO DISABLED - fix me if you like :)
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   store.set("privacyPolicyAccepted", false)
