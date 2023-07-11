4259c01ec75aa7bb3b1961fec3e208d14add4208
Oskar Michalkiewicz <oskar.michalkiewicz@gmail.com>
2023-07-11 12:37:10 +0000

    disabled just ctrl + r

--- packages/app/src/__deprecated__/main/main.ts
+++ packages/app/src/__deprecated__/main/main.ts
@@ -164,8 +164,10 @@ const createWindow = async () => {
     })
   )
 
-  win.webContents.on("before-input-event", (event) => {
-    event.preventDefault()
+  win.webContents.on("before-input-event", (event, input) => {
+    if (input.control && input.key.toLowerCase() === "r") {
+      event.preventDefault()
+    }
   })
 
   win.on("closed", () => {
