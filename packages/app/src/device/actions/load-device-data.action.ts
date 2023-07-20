87d7a964b19377df288f2cd4c9e0e37f425c3a85
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2023-07-20 09:42:23 +0200

    Update load-device-data.action.ts

--- packages/app/src/device/actions/load-device-data.action.ts
+++ packages/app/src/device/actions/load-device-data.action.ts
@@ -21,60 +21,61 @@ import { trackOsVersion } from "App/analytic-data-tracker/helpers"
 import { externalUsageDevice } from "App/device/requests/external-usage-device.request"
 import { setExternalUsageDeviceRequest } from "App/analytic-data-tracker/requests/set-external-usage-device.request"
 
-export const loadDeviceData = createAsyncThunk(
-  DeviceEvent.Loading,
-  async (_, { getState, dispatch, rejectWithValue }) => {
-    const state = getState() as ReduxRootState
+export const loadDeviceData = createAsyncThunk<
+  undefined,
+  void,
+  { state: ReduxRootState }
+>(DeviceEvent.Loading, async (_, { getState, dispatch, rejectWithValue }) => {
+  const state = getState()
 
-    if (state.device.state === ConnectionState.Loaded) {
+  if (state.device.state === ConnectionState.Loaded) {
+    return
+  }
+
+  try {
+    const { ok, data, error } = await getDeviceInfoRequest()
+
+    if (!ok || !data) {
+      if (error?.type === DeviceCommunicationError.DeviceLocked) {
+        void dispatch(lockedDevice())
+      }
+
       return
     }
 
-    try {
-      const { ok, data, error } = await getDeviceInfoRequest()
-
-      if (!ok || !data) {
-        if (error?.type === DeviceCommunicationError.DeviceLocked) {
-          void dispatch(lockedDevice())
-        }
-
-        return
-      }
-
-      if (state.device.deviceType !== null) {
-        void trackOsVersion({
-          serialNumber: data.serialNumber,
-          osVersion: data.osVersion,
-          deviceType: state.device.deviceType,
-        })
-      }
-      void setValue({
-        key: MetadataKey.DeviceOsVersion,
-        value: data.osVersion ?? null,
+    if (state.device.deviceType !== null) {
+      void trackOsVersion({
+        serialNumber: data.serialNumber,
+        osVersion: data.osVersion,
+        deviceType: state.device.deviceType,
       })
-      void setValue({
-        key: MetadataKey.DeviceType,
-        value: state.device.deviceType,
-      })
-
-      if (
-        data.serialNumber !== state.device.data?.serialNumber ||
-        state.device.externalUsageDevice === null
-      ) {
-        const resultExternalUsageDevice = state.settings.privacyPolicyAccepted
-          ? await externalUsageDevice(data.serialNumber)
-          : false
-
-        await setExternalUsageDeviceRequest(resultExternalUsageDevice)
-        if (state.settings.privacyPolicyAccepted !== undefined) {
-          dispatch(setExternalUsageDevice(resultExternalUsageDevice))
-        }
-      }
-      dispatch(setDeviceData(data))
-    } catch (error) {
-      return rejectWithValue(error)
     }
+    void setValue({
+      key: MetadataKey.DeviceOsVersion,
+      value: data.osVersion ?? null,
+    })
+    void setValue({
+      key: MetadataKey.DeviceType,
+      value: state.device.deviceType,
+    })
 
-    return
+    if (
+      data.serialNumber !== state.device.data?.serialNumber ||
+      state.device.externalUsageDevice === null
+    ) {
+      const resultExternalUsageDevice = state.settings.privacyPolicyAccepted
+        ? await externalUsageDevice(data.serialNumber)
+        : false
+
+      await setExternalUsageDeviceRequest(resultExternalUsageDevice)
+      if (state.settings.privacyPolicyAccepted !== undefined) {
+        dispatch(setExternalUsageDevice(resultExternalUsageDevice))
+      }
+    }
+    dispatch(setDeviceData(data))
+  } catch (error) {
+    return rejectWithValue(error)
   }
-)
+
+  return
+})
