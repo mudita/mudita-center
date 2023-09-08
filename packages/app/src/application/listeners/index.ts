860bb10b7d0a412d829c6b75f8f3118bce8a2dbe
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-09-08 08:30:51 +0200

    Removed files

--- packages/app/src/application/listeners/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright (c) Mudita sp. z o.o. All rights reserved.
- * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
- */
-
-export * from "./agreement-status.listener"
