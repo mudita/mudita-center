0b7f9606c73f3deb39fb3d13ff12615eddd62334
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2023-09-08 08:22:13 +0200

    Added changed to root-wrapper

--- packages/app/src/device/listeners/index.ts
+++ packages/app/src/device/listeners/index.ts
@@ -3,8 +3,8 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-//export * from "./register-device-onboarding-status.listener"
 export * from "./register-device-initialization-failed.listener"
 export * from "./register-device-lock-time.listener"
 export * from "./register-device-locked.listener"
 export * from "./register-device-unlocked.listener"
+export * from "./register-device-onboarding-status.listener"
