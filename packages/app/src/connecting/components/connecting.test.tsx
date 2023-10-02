493d098da4425bbcaa2550aa9522a9b6dfb9939d
Oskar Michalkiewicz <32848134+OskarMichalkiewicz@users.noreply.github.com>
2023-10-02 08:07:50 +0000

    Revert "[CP-2193] I want to be informed about problem with connecting… (#1423)

--- packages/app/src/connecting/components/connecting.test.tsx
+++ packages/app/src/connecting/components/connecting.test.tsx
@@ -14,8 +14,6 @@ import { SynchronizationState } from "App/data-sync/reducers"
 import { DeviceType } from "App/device/constants"
 import { RequestResponseStatus } from "App/core/types/request-response.interface"
 import { CriticalBatteryLevelModalTestIds } from "App/connecting/components/critical-battery-level-modal/critical-battery-level-modal-test-ids.enum"
-import { Provider } from "react-redux"
-import store from "App/__deprecated__/renderer/store"
 
 jest.mock("App/connecting/requests/register-first-phone-connection")
 
@@ -48,11 +46,7 @@ const render = (extraProps?: Partial<Props>) => {
     ...defaultProps,
     ...extraProps,
   }
-  const outcome = renderWithThemeAndIntl(
-    <Provider store={store}>
-      <Connecting {...props} />
-    </Provider>
-  )
+  const outcome = renderWithThemeAndIntl(<Connecting {...props} />)
   return {
     ...outcome,
   }
