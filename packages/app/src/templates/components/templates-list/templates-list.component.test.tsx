7763ba18a79e9c6fbbe69486e680de351af33a9d
Daniel Karski <daniel.karski5q@gmail.com>
2023-11-23 13:41:56 +0100

    [CP-2241] Template List - readability boost (#1560)

--- packages/app/src/templates/components/templates-list/templates-list.component.test.tsx
+++ packages/app/src/templates/components/templates-list/templates-list.component.test.tsx
@@ -3,11 +3,25 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import React from "react"
+import React, { ComponentProps } from "react"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import createMockStore from "redux-mock-store"
+import { ReduxRootState } from "App/__deprecated__/renderer/store"
 import { renderWithThemeAndIntl } from "App/__deprecated__/renderer/utils/render-with-theme-and-intl"
 import { TemplatesList } from "App/templates/components/templates-list/templates-list.component"
-import { TemplatesListProps } from "App/templates/components/templates-list/templates-list.interface"
 import { Template } from "App/templates/dto"
+
+type Props = ComponentProps<typeof TemplatesList>
+
+const defaultProps: Props = {
+  deleteTemplates: jest.fn(),
+  updateTemplate: jest.fn(),
+  templateReordered: jest.fn(),
+  activeTemplate: undefined,
+  templateFormOpen: false,
+  selectedTemplateIds: [],
+}
 
 const templateMock: Template = {
   id: "1",
@@ -16,22 +30,27 @@ const templateMock: Template = {
   order: 1,
 }
 
-const render = (props: TemplatesListProps) => {
-  return renderWithThemeAndIntl(<TemplatesList {...props} />)
+const render = (extraProps?: Partial<Props>, state?: ReduxRootState) => {
+  const props: Props = {
+    ...defaultProps,
+    ...extraProps,
+  }
+  const store = createMockStore([thunk])(state)
+
+  return renderWithThemeAndIntl(
+    <Provider store={store}>
+      <TemplatesList {...props} />
+    </Provider>
+  )
 }
 
 describe("`TemplatesList` component", () => {
   test("shows empty state if templates list is empty", () => {
-    const { getByText } = render({
-      templates: [],
-      deleteTemplates: jest.fn(),
-      updateTemplate: jest.fn(),
-      toggleRow: jest.fn(),
-      onDragEnd: jest.fn(),
-      active: undefined,
-      templateFormOpen: false,
-      selectedItems: [],
-    })
+    const { getByText } = render({}, {
+      templates: {
+        data: [],
+      },
+    } as unknown as ReduxRootState)
     expect(
       getByText("[value] module.templates.emptyList.title")
     ).toBeInTheDocument()
@@ -41,30 +60,25 @@ describe("`TemplatesList` component", () => {
   })
 
   test("shows templates list", () => {
-    const { getByText } = render({
-      templates: [templateMock],
-      selectedItems: [],
-      deleteTemplates: jest.fn(),
-      updateTemplate: jest.fn(),
-      toggleRow: jest.fn(),
-      onDragEnd: jest.fn(),
-      active: undefined,
-      templateFormOpen: false,
-    })
+    const { getByText } = render({}, {
+      templates: {
+        data: [templateMock],
+      },
+    } as ReduxRootState)
     expect(getByText(templateMock.text)).toBeInTheDocument()
   })
 
   test("shows checkbox if at least one row is selected", () => {
-    const { getByTestId } = render({
-      templates: [templateMock],
-      selectedItems: [templateMock.id],
-      deleteTemplates: jest.fn(),
-      updateTemplate: jest.fn(),
-      toggleRow: jest.fn(),
-      onDragEnd: jest.fn(),
-      active: undefined,
-      templateFormOpen: false,
-    })
+    const { getByTestId } = render(
+      {
+        selectedTemplateIds: [templateMock.id],
+      },
+      {
+        templates: {
+          data: [templateMock],
+        },
+      } as ReduxRootState
+    )
     expect(getByTestId("template-checkbox")).toBeVisible()
   })
 })
