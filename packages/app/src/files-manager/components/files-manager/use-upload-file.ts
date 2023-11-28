c8c8df3071f4ebc1d40e0f69ff69dc19124dc76e
Daniel Karski <daniel.karski5q@gmail.com>
2023-11-28 10:24:15 +0100

    [CP-2300] Refactoring: Simplifying and Cleaning Up Code in useEffect files-manager.component.tsx

---
+++ packages/app/src/files-manager/components/files-manager/use-upload-file.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { useEffect, useRef, useState } from "react"
+import { useDispatch } from "react-redux"
+import { Dispatch } from "App/__deprecated__/renderer/store"
+import { uploadFile } from "App/files-manager/actions"
+
+const useUploadFile = () => {
+  const dispatch = useDispatch<Dispatch>()
+  const uploadActionRef = useRef<ReturnType<typeof dispatch>>()
+  const [uploadActionTimestamp, setUploadActionTimestamp] = useState<number>()
+
+  const handleUploadFiles = () => {
+    uploadActionRef.current = dispatch(uploadFile())
+    setUploadActionTimestamp(new Date().getTime())
+  }
+
+  useEffect(() => {
+    if (!uploadActionTimestamp) {
+      return
+    }
+
+    return () => {
+      if (uploadActionRef.current?.abort) {
+        uploadActionRef.current.abort()
+      }
+    }
+  }, [uploadActionTimestamp])
+
+  return {
+    handleUploadFiles,
+  }
+}
+
+export default useUploadFile
