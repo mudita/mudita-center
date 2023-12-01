84a21dfc8f526569715ae8a8cc9adc3646cd1245
mkurczewski <michal@kurczewski.dev>
2023-12-01 10:51:28 +0100

    [CP-2299] Added license headers

--- packages/app/src/api-demo/input/input-menu.ts
+++ packages/app/src/api-demo/input/input-menu.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Mudita sp. z o.o. All rights reserved.
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
+
 import { IconType } from "App/__deprecated__/renderer/components/core/icon/icon-type"
 
 interface MenuItemConfig {
