ada20cbb8e7ddc1b69f00a9f86fb696e9a07bbd0
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2024-01-03 11:59:21 +0100

    merge issues

--- packages/app/src/__mocks__/p-queue.ts
+++ packages/app/src/__mocks__/p-queue.ts
@@ -3,18 +3,13 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-<<<<<<<< HEAD:packages/app/src/__mocks__/p-queue.ts
 export default class PQueue {
   constructor() {
-    return this;
+    return this
   }
   add(fn: () => void) {
-    return fn();
+    return fn()
   }
   pause() {}
   clear() {}
-========
-export const v4 = () => {
-  return undefined;
->>>>>>>> develop:packages/app/src/__mocks__/uuid.ts
 }
--- packages/app/src/__mocks__/uuid.ts
+++ packages/app/src/__mocks__/uuid.ts
@@ -4,14 +4,5 @@
  */
 
 export const v4 = () => {
-  return undefined;
-}
-
-/**
- * Copyright (c) Mudita sp. z o.o. All rights reserved.
- * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
- */
-
-export const v4 = () => {
-  return undefined;
+  return undefined
 }
