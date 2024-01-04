ed591403c0bb6367aaca05d4301aa33cb5c4312c
Daniel Karski <daniel.karski5q@gmail.com>
2024-01-04 11:52:45 +0100

    [CP-2341] Fix tests

--- libs/core/__deprecated__/renderer/components/core/icon/badge-with-counter.component.tsx
+++ libs/core/__deprecated__/renderer/components/core/icon/badge-with-counter.component.tsx
@@ -5,16 +5,15 @@
 
 import * as React from "react"
 import styled from "styled-components"
-import { fontWeight } from "Core/core/styles/theming/theme-getters"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
-import { TextDisplayStyle } from "Core/__deprecated__/renderer/components/core/text/text.component"
+import { getTextStyles, TextDisplayStyle } from "Core/__deprecated__/renderer/components/core/text/text.component"
 
 interface Props {
   indicator: number
 }
 
 const BadgeWithCounterContainer = styled.span`
-  ${TextDisplayStyle.Headline5};
+  ${getTextStyles(TextDisplayStyle.Headline5)};
   width: 2rem;
   height: 2rem;
   background-color: black;
@@ -26,7 +25,6 @@ const BadgeWithCounterContainer = styled.span`
   justify-content: center;
   top: 0;
   right: 0;
-  font-weight: ${fontWeight("bold")};
   transform: translate(calc(50% + -0.2rem), calc(-50% + 0.8rem));
 `
 export const BadgeWithCounter: FunctionComponent<Props> = ({ indicator }) => {
