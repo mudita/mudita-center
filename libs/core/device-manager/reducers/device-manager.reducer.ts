b04372d859589230e1b95a7d2c257c929306fb40
Daniel Karski <daniel.karski5q@gmail.com>
2024-01-08 10:12:39 +0100

    [CP-2162] mock devices

--- libs/core/device-manager/reducers/device-manager.reducer.ts
+++ libs/core/device-manager/reducers/device-manager.reducer.ts
@@ -5,16 +5,78 @@
 
 import { createReducer } from "@reduxjs/toolkit"
 import { setDiscoveryStatus } from "Core/discovery-device/actions/base.action"
-import { DeviceManagerState } from "Core/device-manager/reducers/device-manager.interface"
+import {
+  DeviceManagerState,
+  DeviceState,
+} from "Core/device-manager/reducers/device-manager.interface"
 import {
   addDevice,
   removeDevice,
 } from "Core/device-manager/actions/base.action"
 import { mapToDevice } from "Core/device-manager/helpers/map-to-device"
 import { setActiveDevice } from "Core/device-manager/actions/set-active-device.action"
+import { DeviceType } from "Core/device"
 
 export const initialState: DeviceManagerState = {
-  devices: [],
+  devices: [
+    {
+      id: "0422130000909",
+      path: "/dev/tty.usbmodem04221300009092",
+      // serialNumber: "0422130000909",
+      serialNumber: undefined,
+      deviceType: DeviceType.MuditaHarmony,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "0121480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "0121480001897",
+      deviceType: DeviceType.MuditaPure,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "0421480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "0421480001897",
+      deviceType: DeviceType.MuditaKompakt,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "1422130000909",
+      path: "/dev/tty.usbmodem04221300009092",
+      serialNumber: "1422130000909",
+      deviceType: DeviceType.MuditaHarmony,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "2421480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "2421480001897",
+      deviceType: DeviceType.MuditaHarmony,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "0021480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "0021480001897",
+      deviceType: DeviceType.MuditaPure,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "3421480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "3421480001897",
+      deviceType: DeviceType.MuditaHarmony,
+      state: DeviceState.Connected,
+    },
+    {
+      id: "0321480001897",
+      path: "/dev/tty.usbmodem04214800018971",
+      serialNumber: "0321480001897",
+      deviceType: DeviceType.MuditaPure,
+      state: DeviceState.Connected,
+    },
+  ],
   activeDeviceId: undefined,
 }
 
