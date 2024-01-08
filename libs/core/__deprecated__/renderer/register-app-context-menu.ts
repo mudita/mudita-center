257b486e6e325e54201b03e43c6555a1ed592494
Daniel Karski <daniel.karski5q@gmail.com>
2024-01-08 08:17:08 +0100

    [2260] Fix app path construction for compatibility

--- libs/core/__deprecated__/renderer/register-app-context-menu.ts
+++ libs/core/__deprecated__/renderer/register-app-context-menu.ts
@@ -3,16 +3,15 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import { app } from "@electron/remote"
 import store from "Core/__deprecated__/renderer/store"
 import ContextMenu from "Core/__deprecated__/context-menu/context-menu"
 import contactsContextMenu from "Core/contacts/helpers/context-menu/context-menu"
 import importDeviceLogFiles from "Core/__deprecated__/renderer/requests/import-device-log-files.request"
-import packageInfo from "../../../../apps/mudita-center/package.json"
 import importDeviceCrashDumpFiles from "Core/__deprecated__/renderer/requests/import-device-crash-dumps-files.request"
 import { clearAllThreads } from "Core/messages/actions/base.action"
+import getAppPath from "Core/__deprecated__/main/utils/get-app-path"
 
-const cwd = `${app.getPath("appData")}/${packageInfo.name}/pure-logs`
+const cwd = getAppPath("pure-logs")
 
 // AUTO DISABLED - fix me if you like :)
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
