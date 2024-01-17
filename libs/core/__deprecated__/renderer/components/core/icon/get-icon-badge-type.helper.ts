808f0db7f274c0043ab0fe4537da078da7e1eda3
Daniel Karski <daniel.karski5q@gmail.com>
2024-01-17 13:15:39 +0100

    [CP-2162] Handle feedback from discussion #1613: Resolved comment at https://github.com/mudita/mudita-center/pull/1613#discussion_r1444856288

--- libs/core/__deprecated__/renderer/components/core/icon/get-icon-badge-type.helper.ts
+++ libs/core/__deprecated__/renderer/components/core/icon/get-icon-badge-type.helper.ts
@@ -5,14 +5,22 @@
 
 import { IconBadgeType } from "Core/__deprecated__/renderer/components/core/icon/icon-badge-type.constant"
 
+/**
+ * The helper function getIconBadgeType is used to transform the value of the badge prop
+ * into the appropriate IconBadgeType or undefined. The main purpose of this function
+ * is to handle the case when the badge is of type boolean.
+ *
+ * @param badge - The value of the badge prop, which can be undefined, boolean, or IconBadgeType.
+ * @returns IconBadgeType or undefined, depending on the value of badge.
+ */
 export const getIconBadgeType = (
-  badge: undefined | boolean | IconBadgeType,
+  badge: undefined | boolean | IconBadgeType
 ): IconBadgeType | undefined => {
-  if (badge === true || badge === IconBadgeType.Badge) {
+  if (badge === true) {
     return IconBadgeType.Badge
-  } else if (badge === IconBadgeType.BadgeWithCounter) {
-    return IconBadgeType.BadgeWithCounter
-  } else {
+  } else if (badge === false || badge === undefined) {
     return undefined
+  } else {
+    return badge
   }
 }
