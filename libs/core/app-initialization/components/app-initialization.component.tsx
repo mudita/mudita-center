6c5aae7881c8b7daf64ea2f58e4a6dafd39cc63e
patryk-sierzega <126664583+patryk-sierzega@users.noreply.github.com>
2024-02-05 10:28:58 +0100

    Some fixes after merge

--- libs/core/app-initialization/components/app-initialization.component.tsx
+++ libs/core/app-initialization/components/app-initialization.component.tsx
@@ -4,70 +4,28 @@
  */
 
 import React from "react"
+import { useSelector } from "react-redux"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
-import { useGenericStoreDemo } from "generic-view/store"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
-import { startInitializingApp } from "Core/app-initialization/actions/start-initializing-app"
-import { Dispatch } from "Core/__deprecated__/renderer/store"
-import { isAppUpdateProcessPassed } from "Core/app-initialization/selectors/is-app-update-process-passed.selector"
-//import { redirectToDiscovery } from "Core/app-initialization/actions/redirect-to-discovery.action"
-import PrivacyPolicyModal from "Core/settings/components/privacy-policy-modal/privacy-policy-modal.component"
-import { shouldPrivacyPolicyVisible } from "Core/app-initialization/selectors/should-privacy-policy-visible.selector"
-import { shouldAppUpdateFlowVisible } from "Core/app-initialization/selectors/should-app-update-flow-visible.selector"
-import { AppUpdateFlow } from "Core/settings/components/app-update-flow/app-update-flow.component"
 import MuditaCenterOnSudoModeContainer from "Core/settings/components/usb-access/mudita-center-on-sudo-mode.container"
 import USBAccessFlowContainer from "Core/settings/components/usb-access/usb-access-flow.container"
 import { ModalsManagerState } from "Core/modals-manager/reducers/modals-manager.interface"
 import { ReduxRootState } from "Core/__deprecated__/renderer/store"
-import { setAppInitializationStatus } from "Core/app-initialization/actions/base.action"
-import { AppInitializationStatus } from "Core/app-initialization/reducers/app-initialization.interface"
+// import { AppInitializationStatus } from "Core/app-initialization/reducers/app-initialization.interface"
+// import { getAppInitializationStatus } from "Core/app-initialization/selectors/get-app-initialization-status.selector"
+// import AppInitializationFlow from "Core/app-initialization/components/app-initialization-flow.component"
+// import { useInitializingAppEffects } from "Core/app-initialization/hooks/use-initializing-app-effects"
 
 const AppInitialization: FunctionComponent = () => {
-  const history = useHistory()
-  const dispatch = useDispatch<Dispatch>()
-
-  const appUpdateProcessPassed = useSelector(isAppUpdateProcessPassed)
-  const privacyPolicyVisible = useSelector(shouldPrivacyPolicyVisible)
-  const appUpdateFlowVisible = useSelector(shouldAppUpdateFlowVisible)
+  //const appInitializationStatus = useSelector(getAppInitializationStatus)
   const { usbAccessFlowShow, appRunWithSudoShow } = useSelector(
     (state: ReduxRootState): ModalsManagerState => state.modalsManager
   )
 
-  // TODO: Demo purpose only, remove in future
-  useGenericStoreDemo()
+  //useInitializingAppEffects()
 
-  useEffect(() => {
-    dispatch(startInitializingApp())
-  }, [dispatch])
-
-  useEffect(() => {
-    const initializationFinished =
-      appUpdateProcessPassed &&
-      !privacyPolicyVisible &&
-      !appRunWithSudoShow &&
-      !usbAccessFlowShow
-    if (initializationFinished) {
-      dispatch(setAppInitializationStatus(AppInitializationStatus.Initialized))
-      //dispatch(redirectToDiscovery(history))
-    }
-  }, [
-    history,
-    dispatch,
-    appUpdateProcessPassed,
-    privacyPolicyVisible,
-    appRunWithSudoShow,
-    usbAccessFlowShow,
-  ])
-
-  if (privacyPolicyVisible) {
-    return <PrivacyPolicyModal />
-  }
-
-  if (appUpdateFlowVisible) {
-    return <AppUpdateFlow />
-  }
+  // if (appInitializationStatus !== AppInitializationStatus.Initialized) {
+  //   return <AppInitializationFlow />
+  // }
 
   if (appRunWithSudoShow) {
     return <MuditaCenterOnSudoModeContainer />
@@ -75,10 +33,6 @@ const AppInitialization: FunctionComponent = () => {
 
   if (usbAccessFlowShow) {
     return <USBAccessFlowContainer />
-  }
-          
-  if (appInitializationStatus !== AppInitializationStatus.Initialized) {
-    return <AppInitializationFlow />
   }
 
   return <></>
