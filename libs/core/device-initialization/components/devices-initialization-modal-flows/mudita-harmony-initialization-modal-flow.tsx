f6b49db1e7dd8d3bd35912c6800933eeec9c12f4
Daniel Karski <daniel.karski5q@gmail.com>
2024-02-08 10:50:42 +0100

    [CP-2390][Multidevice] Weird Center bahavior - it started automatically shows passcode modal closing it

--- libs/core/device-initialization/components/devices-initialization-modal-flows/mudita-harmony-initialization-modal-flow.tsx
+++ libs/core/device-initialization/components/devices-initialization-modal-flows/mudita-harmony-initialization-modal-flow.tsx
@@ -4,24 +4,22 @@
  */
 
 import React, { useEffect } from "react"
-import { useHistory } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
-import { Dispatch } from "Core/__deprecated__/renderer/store"
 import { deviceStatusSelector } from "Core/device/selectors/device-status.selector"
-import { handleInitializeDevice } from "Core/device-initialization/components/devices-initialization-modal-flows/handle-initialize-device.helper"
+import { useDeviceInitializer } from "Core/device-initialization/components/devices-initialization-modal-flows/handle-initialize-device.helper"
 import { initializeMuditaHarmony } from "Core/device-initialization/actions/initialize-mudita-harmony.action"
 import CriticalBatteryLevelModalContainer from "Core/device-initialization/components/critical-battery-level-modal.container"
 import EULAAgreementContainer from "Core/device-initialization/components/eula-agreement/eula-agreement.container"
 
 export const MuditaHarmonyInitializationModalFlow: FunctionComponent = () => {
-  const history = useHistory()
-  const dispatch = useDispatch<Dispatch>()
   const deviceStatus = useSelector(deviceStatusSelector)
 
+  const initializeDevice = useDeviceInitializer(initializeMuditaHarmony)
+
   useEffect(() => {
-    void handleInitializeDevice(dispatch, initializeMuditaHarmony, history)
-  }, [history, dispatch])
+    void initializeDevice()
+  }, [initializeDevice])
 
   if (deviceStatus?.criticalBatteryLevel) {
     return <CriticalBatteryLevelModalContainer />
