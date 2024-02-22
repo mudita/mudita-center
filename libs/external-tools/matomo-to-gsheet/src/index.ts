12c65705b9c3bfe25756b819587fa7fa729c09eb
Daniel Karski <daniel.karski5q@gmail.com>
2024-02-22 13:07:18 +0100

    [CP-2235] Implement script to migrate matomo date to google sheet

--- libs/external-tools/matomo-to-gsheet/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./lib/external-tools-matomo-to-gsheet"
---
+++ libs/external-tools/matomo-to-gsheet/src/lib/script.ts
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import 'dotenv/config';
+import MatomoService from "./matomo/matomo.service"
+import GoogleSheetFactory from "./google-sheet/google-sheet.factory"
+import StatsSheetService from "./google-sheet/stats-sheet.service"
+import getYesterdayDateString from "./utils/get-yesterday-date"
+
+const matomoService = new MatomoService()
+const statsSheetService = new StatsSheetService(
+  GoogleSheetFactory.makeGoogleSheet()
+)
+
+const endDate = getYesterdayDateString()
+
+async function script() {
+  try {
+    const muditaHarmonySheetLastDate = await statsSheetService.getLastDate(
+      "Mudita Harmony - MD"
+    )
+
+    const muditaHarmonyMatomoData = await matomoService.fetchMatomoData({
+      label: "Harmony Update - download",
+      startDate: muditaHarmonySheetLastDate,
+      endDate,
+    })
+
+    await statsSheetService.updateStats(
+      muditaHarmonyMatomoData,
+      "Mudita Harmony - MD"
+    )
+
+    console.log("Dane zaktualizowane w Google Sheets dla Mudita Harmony")
+
+    const muditaPureSheetLastDate = await statsSheetService.getLastDate(
+      "Mudita Pure - MD"
+    )
+
+    const muditaPureMatomoData = await matomoService.fetchMatomoData({
+      label: "Pure Update - download",
+      startDate: muditaPureSheetLastDate,
+      endDate,
+    })
+
+    await statsSheetService.updateStats(
+      muditaPureMatomoData,
+      "Mudita Pure - MD"
+    )
+
+    console.log("Dane zaktualizowane w Google Sheets dla Mudita Pure")
+
+  } catch (error) {
+    console.error(error)
+    return
+  }
+}
+
+void script()
