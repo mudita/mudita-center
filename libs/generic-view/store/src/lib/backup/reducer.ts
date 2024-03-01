3de1e549883158cf08dac4eab428fb64bc322cf9
Łukasz Kowalczyk <37898730+lkowalczyk87@users.noreply.github.com>
2024-03-01 08:50:55 +0100

    [CP-2590] (#1754)

--- libs/generic-view/store/src/lib/backup/reducer.ts
+++ libs/generic-view/store/src/lib/backup/reducer.ts
@@ -4,7 +4,14 @@
  */
 
 import { createReducer } from "@reduxjs/toolkit"
-import { setBackupFiles } from "./actions"
+import {
+  cleanBackupProcess,
+  setBackupProcess,
+  setBackupFiles,
+  setBackupProcessFileStatus,
+  setBackupProcessStatus,
+} from "./actions"
+import { createBackup } from "./create-backup.action"
 
 export interface Backup {
   fileName: string
@@ -18,8 +25,26 @@ export interface Backup {
   }
 }
 
+export type BackupProcessStatus =
+  | "PRE_BACKUP"
+  | "FILES_TRANSFER"
+  | "SAVE_FILE"
+  | "DONE"
+  | "FAILED"
+
+export type BackupProcessFileStatus = "PENDING" | "IN_PROGRESS" | "DONE"
+
+export interface BackupProcess {
+  status: "PRE_BACKUP" | "FILES_TRANSFER" | "SAVE_FILE" | "DONE" | "FAILED"
+  featureFilesTransfer: Record<
+    string,
+    { transferId?: number; status: BackupProcessFileStatus }
+  >
+}
+
 interface BackupState {
   files: Backup[]
+  backupProcess?: BackupProcess
 }
 
 const initialState: BackupState = {
@@ -42,5 +67,42 @@ const initialState: BackupState = {
 export const genericBackupsReducer = createReducer(initialState, (builder) => {
   builder.addCase(setBackupFiles, (state, action) => {
     state.files = action.payload
+  })
+  builder.addCase(cleanBackupProcess, (state, action) => {
+    delete state.backupProcess
+  })
+  builder.addCase(setBackupProcess, (state, action) => {
+    state.backupProcess = action.payload
+  })
+  builder.addCase(setBackupProcessFileStatus, (state, action) => {
+    if (state.backupProcess) {
+      state.backupProcess.featureFilesTransfer[action.payload.feature].status =
+        action.payload.status
+    }
+  })
+  builder.addCase(setBackupProcessStatus, (state, action) => {
+    if (state.backupProcess) {
+      state.backupProcess.status = action.payload
+    }
+  })
+  builder.addCase(createBackup.rejected, (state, action) => {
+    if (state.backupProcess) {
+      state.backupProcess.status = "FAILED"
+    } else {
+      state.backupProcess = {
+        status: "FAILED",
+        featureFilesTransfer: {},
+      }
+    }
+  })
+  builder.addCase(createBackup.fulfilled, (state, action) => {
+    if (state.backupProcess) {
+      state.backupProcess.status = "DONE"
+    } else {
+      state.backupProcess = {
+        status: "DONE",
+        featureFilesTransfer: {},
+      }
+    }
   })
 })
