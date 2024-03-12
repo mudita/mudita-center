f7ea72cb539a38ec485364220ccbf76d4e447347
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2024-03-12 17:36:06 +0100

    Update file-transfer.service.ts

--- libs/device/feature/src/lib/file-transfer/file-transfer.service.ts
+++ libs/device/feature/src/lib/file-transfer/file-transfer.service.ts
@@ -135,46 +135,6 @@ export class APIFileTransferService {
       file,
       crc32
     )
-
-    // const response = await device.request({
-    //   endpoint: "PRE_FILE_TRANSFER",
-    //   method: "POST",
-    //   body: {
-    //     filePath: targetPath,
-    //     fileSize: file.length,
-    //     crc32,
-    //   },
-    // })
-
-    // if (response.ok) {
-    //   const preTransferResponse = PreTransferSendValidator.safeParse(
-    //     response.data.body
-    //   )
-
-    //   const success = preTransferResponse.success
-
-    //   if (!success) {
-    //     return handleError(response.data.status)
-    //   }
-
-    //   this.transfers[preTransferResponse.data.transferId] = {
-    //     crc32,
-    //     fileSize: file.length,
-    //     filePath,
-    //     chunks:
-    //       file.match(
-    //         new RegExp(`.{1,${preTransferResponse.data.chunkSize}}`, "g")
-    //       ) || [],
-    //   }
-
-    //   return Result.success({
-    //     transferId: preTransferResponse.data.transferId,
-    //     chunksCount:
-    //       this.transfers[preTransferResponse.data.transferId].chunks.length,
-    //   })
-    // }
-
-    // return handleError(response.error.type)
   }
 
   @IpcEvent(ApiFileTransferServiceEvents.Send)
@@ -426,49 +386,6 @@ export class APIFileTransferService {
       file,
       crc32
     )
-
-    // return Result.failed(new AppError(GeneralError.IncorrectResponse, ""))
-    // const { crc32, file } = this.prepareFile(filePath)
-
-    // const response = await device.request({
-    //   endpoint: "PRE_FILE_TRANSFER",
-    //   method: "POST",
-    //   body: {
-    //     filePath: targetPath,
-    //     fileSize: file.length,
-    //     crc32,
-    //   },
-    // })
-
-    // if (response.ok) {
-    //   const preTransferResponse = PreTransferSendValidator.safeParse(
-    //     response.data.body
-    //   )
-
-    //   const success = preTransferResponse.success
-
-    //   if (!success) {
-    //     return handleError(response.data.status)
-    //   }
-
-    //   this.transfers[preTransferResponse.data.transferId] = {
-    //     crc32,
-    //     fileSize: file.length,
-    //     filePath,
-    //     chunks:
-    //       file.match(
-    //         new RegExp(`.{1,${preTransferResponse.data.chunkSize}}`, "g")
-    //       ) || [],
-    //   }
-
-    //   return Result.success({
-    //     transferId: preTransferResponse.data.transferId,
-    //     chunksCount:
-    //       this.transfers[preTransferResponse.data.transferId].chunks.length,
-    //   })
-    // }
-
-    // return handleError(response.error.type)
   }
 }
 
