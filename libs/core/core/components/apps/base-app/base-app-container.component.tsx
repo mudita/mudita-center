80e996b209c30a190e58af8786873f76a16c92b6
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2024-03-19 08:59:13 +0100

    review changes

--- libs/core/core/components/apps/base-app/base-app-container.component.tsx
+++ libs/core/core/components/apps/base-app/base-app-container.component.tsx
@@ -10,6 +10,7 @@ import { FunctionComponent } from "Core/core/types/function-component.interface"
 import BaseRoutes from "Core/core/components/apps/base-app/base-app-routes"
 import BaseApp from "Core/core/components/apps/base-app/base-app.component"
 import { RoutesHistoryProvider } from "shared/utils"
+import { ApiDeviceModals } from "generic-view/feature"
 
 const BaseAppContainer: FunctionComponent = () => {
   return (
@@ -18,6 +19,7 @@ const BaseAppContainer: FunctionComponent = () => {
       <RoutesHistoryProvider>
         <BaseRoutes />
       </RoutesHistoryProvider>
+      <ApiDeviceModals />
     </Router>
   )
 }
--- libs/generic-view/feature/src/lib/api-device-modals.tsx
+++ libs/generic-view/feature/src/lib/api-device-modals.tsx
@@ -5,20 +5,8 @@
 
 import React, { FunctionComponent } from "react"
 import { GenericThemeProvider } from "generic-view/theme"
-import {
-  BackupError,
-  BackupRestoreError,
-  ModalBase,
-  ModalCenteredContent,
-} from "generic-view/ui"
-import { useDispatch, useSelector } from "react-redux"
-import {
-  cleanBackupProcess,
-  cleanRestoreProcess,
-  selectActiveDevice,
-  selectBackupProcessStatus,
-  selectBackupRestoreStatus,
-} from "generic-view/store"
+import BackupErrorModal from "./modals/backup-error-modal"
+import RestoreErrorModal from "./modals/restore-error-modal"
 
 export const ApiDeviceModals: FunctionComponent = () => {
   return (
@@ -26,43 +14,5 @@ export const ApiDeviceModals: FunctionComponent = () => {
       <BackupErrorModal />
       <RestoreErrorModal />
     </GenericThemeProvider>
-  )
-}
-
-const BackupErrorModal: FunctionComponent = () => {
-  const dispatch = useDispatch()
-  const activeDevice = useSelector(selectActiveDevice)
-  const backupStatus = useSelector(selectBackupProcessStatus)
-  const opened = backupStatus === "FAILED" && !activeDevice
-
-  const onClose = () => {
-    dispatch(cleanBackupProcess())
-  }
-  return (
-    <ModalBase opened={opened} variant={"small"}>
-      <ModalCenteredContent>
-        <BackupError closeAction={{ type: "custom", callback: onClose }} />
-      </ModalCenteredContent>
-    </ModalBase>
-  )
-}
-
-const RestoreErrorModal: FunctionComponent = () => {
-  const dispatch = useDispatch()
-  const activeDevice = useSelector(selectActiveDevice)
-  const restoreStatus = useSelector(selectBackupRestoreStatus)
-  const opened = restoreStatus === "FAILED" && !activeDevice
-
-  const onClose = () => {
-    dispatch(cleanRestoreProcess())
-  }
-  return (
-    <ModalBase opened={opened} variant={"small"}>
-      <ModalCenteredContent>
-        <BackupRestoreError
-          closeAction={{ type: "custom", callback: onClose }}
-        />
-      </ModalCenteredContent>
-    </ModalBase>
   )
 }
