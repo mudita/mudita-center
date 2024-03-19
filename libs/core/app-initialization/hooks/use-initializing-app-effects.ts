a0fdc13b4c9f8c5c977309aff4ad39289b267d79
Daniel Karski <daniel.karski5q@gmail.com>
2024-03-19 09:36:51 +0100

    [CP-XXX] `useInitializingAppEffects` refactored

--- libs/core/app-initialization/hooks/use-initializing-app-effects.ts
+++ libs/core/app-initialization/hooks/use-initializing-app-effects.ts
@@ -9,35 +9,21 @@ import { startInitializingApp } from "Core/app-initialization/actions/start-init
 import { Dispatch } from "Core/__deprecated__/renderer/store"
 import { setAppInitializationStatus } from "Core/app-initialization/actions/base.action"
 import { AppInitializationStatus } from "Core/app-initialization/reducers/app-initialization.interface"
-import { isAppUpdateProcessPassed } from "Core/app-initialization/selectors/is-app-update-process-passed.selector"
-import { ReduxRootState } from "Core/__deprecated__/renderer/store"
-import { ModalsManagerState } from "Core/modals-manager/reducers/modals-manager.interface"
-import { settingsStateSelector } from "Core/settings/selectors"
+import { isAppInitializationFinishedSelector } from "Core/app-initialization/selectors/is-app-initialization-finished.selector"
 
 export const useInitializingAppEffects = () => {
   const dispatch = useDispatch<Dispatch>()
-  const appUpdateProcessPassed = useSelector(isAppUpdateProcessPassed)
-  const { usbAccessFlowShow } = useSelector(
-    (state: ReduxRootState): ModalsManagerState => state.modalsManager
+  const appInitializationFinished = useSelector(
+    isAppInitializationFinishedSelector
   )
-  const { userHasSerialPortAccess } = useSelector(settingsStateSelector)
 
   useEffect(() => {
     dispatch(startInitializingApp())
   }, [dispatch])
 
   useEffect(() => {
-    if (
-      appUpdateProcessPassed &&
-      userHasSerialPortAccess &&
-      !usbAccessFlowShow
-    ) {
+    if (appInitializationFinished) {
       dispatch(setAppInitializationStatus(AppInitializationStatus.Initialized))
     }
-  }, [
-    dispatch,
-    appUpdateProcessPassed,
-    usbAccessFlowShow,
-    userHasSerialPortAccess,
-  ])
+  }, [dispatch, appInitializationFinished])
 }
