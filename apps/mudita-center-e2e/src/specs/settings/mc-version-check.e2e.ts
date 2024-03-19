0ef9775d5d33db1d692ff41894f04eba83aa0d5f
Tomasz Malecki <tomasz.malecki@mudita.com>
2024-03-19 11:14:13 +0100

    CP-2659 If statement before before and it methods - This could work

--- apps/mudita-center-e2e/src/specs/settings/mc-version-check.e2e.ts
+++ apps/mudita-center-e2e/src/specs/settings/mc-version-check.e2e.ts
@@ -8,38 +8,48 @@ import NavigationTabs from "../../page-objects/tabs.page"
 
 
 describe("Checking Mudita Center version", () => {
-  before(async () => {
-    console.log("CURRENT PROCESS PLATFORM: " + process.platform)
-    if(process.platform === "linux"){
-      console.log(process.platform + " --------- Test Skipped ")
-      return
-    }
-    // Waiting for device connected through USB
-    await browser.executeAsync((done) => {
-      setTimeout(done, 10000)
-    })
-  })
 
-  it("Application should have the same version as in package.json", async () => {
-    const appVersionFromPackageJson = version
+  console.log("CURRENT PROCESS PLATFORM: " + process.platform)
+  if(process.platform === "linux"){
+    console.log(process.platform + " --------- Test Skipped ")
+    return
+  }
+  else{
 
-    const newsTab = await NavigationTabs.settingsTab
-    await newsTab.waitForDisplayed()
-    await newsTab.click()
-    await browser.executeAsync((done) => {
-      setTimeout(done, 2000)
+    before(async () => {
+
+      // Waiting for device connected through USB
+      await browser.executeAsync((done) => {
+        setTimeout(done, 10000)
+      })
     })
 
-    const tabAbout = await SettingsPage.aboutTab
-    await tabAbout.waitForDisplayed()
-    await tabAbout.click()
-    await browser.executeAsync((done) => {
-      setTimeout(done, 2000)
+
+
+
+    it("Application should have the same version as in package.json", async () => {
+      const appVersionFromPackageJson = version
+
+      const newsTab = await NavigationTabs.settingsTab
+      await newsTab.waitForDisplayed()
+      await newsTab.click()
+      await browser.executeAsync((done) => {
+        setTimeout(done, 2000)
+      })
+
+      const tabAbout = await SettingsPage.aboutTab
+      await tabAbout.waitForDisplayed()
+      await tabAbout.click()
+      await browser.executeAsync((done) => {
+        setTimeout(done, 2000)
+      })
+      const appVersionFromAppField =
+        await SettingsPage.aboutInstalledVersionTextLabel
+      const appVersionFromAppTextValue = await appVersionFromAppField.getText()
+      const appVersionFromApp = appVersionFromAppTextValue.split(": ")[1]
+      await expect(appVersionFromApp).toEqual(appVersionFromPackageJson)
     })
-    const appVersionFromAppField =
-      await SettingsPage.aboutInstalledVersionTextLabel
-    const appVersionFromAppTextValue = await appVersionFromAppField.getText()
-    const appVersionFromApp = appVersionFromAppTextValue.split(": ")[1]
-    await expect(appVersionFromApp).toEqual(appVersionFromPackageJson)
-  })
+
+  }
+
 })
