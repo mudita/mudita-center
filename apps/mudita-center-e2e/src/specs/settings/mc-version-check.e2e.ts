72452247db0f5d8e50feb2294533da0a19b5fbda
Tomasz Malecki <tomasz.malecki@mudita.com>
2024-03-22 11:13:29 +0100

    Revert mc-version-check WIP solution

--- apps/mudita-center-e2e/src/specs/settings/mc-version-check.e2e.ts
+++ apps/mudita-center-e2e/src/specs/settings/mc-version-check.e2e.ts
@@ -6,50 +6,34 @@ import { version } from "../../../../../package.json"
 import SettingsPage from "../../page-objects/settings.page"
 import NavigationTabs from "../../page-objects/tabs.page"
 
-
 describe("Checking Mudita Center version", () => {
+  before(async () => {
+    // Waiting for device connected through USB
+    await browser.executeAsync((done) => {
+      setTimeout(done, 10000)
+    })
+  })
 
-  console.log("CURRENT PROCESS PLATFORM: " + process.platform)
-  if(process.platform === "linux"){
-    console.log(process.platform + " --------- Test Skipped ")
-    return
-  }
-  else{
+  it("Application should have the same version as in package.json", async () => {
+    const appVersionFromPackageJson = version
 
-    before(async () => {
-
-      // Waiting for device connected through USB
-      await browser.executeAsync((done) => {
-        setTimeout(done, 10000)
-      })
+    const newsTab = await NavigationTabs.settingsTab
+    await newsTab.waitForDisplayed()
+    await newsTab.click()
+    await browser.executeAsync((done) => {
+      setTimeout(done, 2000)
     })
 
-
-
-
-    it("Application should have the same version as in package.json", async () => {
-      const appVersionFromPackageJson = version
-
-      const newsTab = await NavigationTabs.settingsTab
-      await newsTab.waitForDisplayed()
-      await newsTab.click()
-      await browser.executeAsync((done) => {
-        setTimeout(done, 2000)
-      })
-
-      const tabAbout = await SettingsPage.aboutTab
-      await tabAbout.waitForDisplayed()
-      await tabAbout.click()
-      await browser.executeAsync((done) => {
-        setTimeout(done, 2000)
-      })
-      const appVersionFromAppField =
-        await SettingsPage.aboutInstalledVersionTextLabel
-      const appVersionFromAppTextValue = await appVersionFromAppField.getText()
-      const appVersionFromApp = appVersionFromAppTextValue.split(": ")[1]
-      await expect(appVersionFromApp).toEqual(appVersionFromPackageJson)
+    const tabAbout = await SettingsPage.aboutTab
+    await tabAbout.waitForDisplayed()
+    await tabAbout.click()
+    await browser.executeAsync((done) => {
+      setTimeout(done, 2000)
     })
-
-  }
-
+    const appVersionFromAppField =
+      await SettingsPage.aboutInstalledVersionTextLabel
+    const appVersionFromAppTextValue = await appVersionFromAppField.getText()
+    const appVersionFromApp = appVersionFromAppTextValue.split(": ")[1]
+    await expect(appVersionFromApp).toEqual(appVersionFromPackageJson)
+  })
 })
