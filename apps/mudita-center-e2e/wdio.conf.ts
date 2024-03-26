5e0ae014b3923b73a7a1afbe32fd96a811fe3602
Tomasz Malecki <135224481+tomaszmaleckimudita@users.noreply.github.com>
2024-03-26 11:45:19 +0100

    CP-2659 Added a way to skip tests on linux  (#1794)

--- apps/mudita-center-e2e/wdio.conf.ts
+++ apps/mudita-center-e2e/wdio.conf.ts
@@ -69,12 +69,13 @@ export const config: Options.Testrunner = {
     toRelativePath(TestFilesPaths.sarWindowCheckTest),
     toRelativePath(TestFilesPaths.tosPrivacyLicenceWindowsCheckTest),
     toRelativePath(TestFilesPaths.messageSendTest),
-    toRelativePath(TestFilesPaths.mcCheckForUpdatesTest)
+    toRelativePath(TestFilesPaths.mcCheckForUpdatesTest),
   ],
   suites: {
     standalone: [
       toRelativePath(TestFilesPaths.displayInitialOsVersionTest),
       toRelativePath(TestFilesPaths.helpWindowCheckTest),
+      toRelativePath(TestFilesPaths.mcCheckForUpdatesTest),
     ],
     multidevicePureHarmony: [],
     multideviceSingleHarmony: [],
@@ -91,10 +92,7 @@ export const config: Options.Testrunner = {
       toRelativePath(TestFilesPaths.checkForUpdateTest),
       toRelativePath(TestFilesPaths.deviceUpdateTest),
     ],
-    cicd: [
-      toRelativePath(TestFilesPaths.displayInitialOsVersionTest)
-    ],
-
+    cicd: [toRelativePath(TestFilesPaths.displayInitialOsVersionTest)],
   },
   // Patterns to exclude.
   exclude: [
