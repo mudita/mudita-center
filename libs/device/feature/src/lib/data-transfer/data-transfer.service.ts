5b96b29a1c11c25b4a8d5d63402f00fb809ffbff
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2024-04-15 16:07:18 +0200

    add data transfer validators

--- libs/device/feature/src/lib/data-transfer/data-transfer.service.ts
+++ libs/device/feature/src/lib/data-transfer/data-transfer.service.ts
@@ -12,8 +12,12 @@ import { ApiResponse } from "Core/device/types/mudita-os"
 import {
   APIDataTransferServiceEvents,
   DataTransferDomain,
+  DataTransferValidator200,
+  DataTransferValidator202,
   GeneralError,
   PreDataTransfer,
+  PreDataTransferValidator,
+  DataTransfer,
 } from "device/models"
 import random from "lodash/random"
 
@@ -48,7 +52,13 @@ export class APIDataTransferService {
     })
 
     if (response.ok) {
-      return Result.success(response.data.body as PreDataTransfer)
+      const startDataTransferResponse = PreDataTransferValidator(
+        domains
+      ).safeParse(response.data.body)
+
+      return startDataTransferResponse.success
+        ? Result.success(startDataTransferResponse.data)
+        : Result.failed(new AppError(GeneralError.IncorrectResponse, ""))
     }
 
     return Result.failed(new AppError(GeneralError.IncorrectResponse))
@@ -147,9 +157,18 @@ export class APIDataTransferService {
     }
 
     if (response.data.status === 200) {
+      const response200 = DataTransferValidator200.safeParse(response.data.body)
+      if (response200.success) {
+        return Result.success(response200.data)
+      }
       return Result.success(response.data.body as DataTransfer)
     }
     if (response.data.status === 202) {
+      const response202 = DataTransferValidator202.safeParse(response.data.body)
+
+      if (response202.success) {
+        return Result.success(response202.data)
+      }
       return Result.success(response.data.body as DataTransfer)
     }
 
