5e33de351b71d95aa70682958332163c554bf251
Łukasz Kowalczyk <37898730+lkowalczyk87@users.noreply.github.com>
2024-04-16 10:44:49 +0200

    [CP-2733] add data transfer validators (#1837)

--- libs/device/models/src/lib/data-transfer/data-transfer.ts
+++ libs/device/models/src/lib/data-transfer/data-transfer.ts
@@ -3,15 +3,43 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
+import { z } from "zod"
+
 export type DataTransferDomain = "contacts-v1"
 
-export type PreDataTransfer = {
-  dataTransferId: number
-  domains: Record<DataTransferDomain, string>
+export const PreDataTransferValidator = (domains: DataTransferDomain[]) => {
+  const featuresValidator = domains.reduce((acc, key) => {
+    return {
+      ...acc,
+      [key]: z.string().min(1),
+    }
+  }, {} as Record<DataTransferDomain, z.ZodString>)
+
+  return z.object({
+    dataTransferId: z.number().min(1),
+    domains: z.object(featuresValidator),
+  })
 }
 
-export type DataTransfer = {
-  process: number
-  rebootRequired?: boolean
-  message: string
-}
+export type PreDataTransfer = z.infer<
+  ReturnType<typeof PreDataTransferValidator>
+>
+
+export const DataTransferValidator200 = z.object({
+  rebootRequired: z.boolean(),
+  message: z.string().min(1).optional(),
+  progress: z.literal(100).default(100),
+})
+
+export const DataTransferValidator202 = z.object({
+  rebootRequired: z.boolean().optional(),
+  message: z.string().min(1).optional(),
+  progress: z.number().min(0).max(99).default(0),
+})
+
+export const DataTransferValidator = z.union([
+  DataTransferValidator200,
+  DataTransferValidator202,
+])
+
+export type DataTransfer = z.infer<typeof DataTransferValidator>
