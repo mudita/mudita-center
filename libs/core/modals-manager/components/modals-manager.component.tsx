19e77dce17ce47745719003c22af9f37d2cc8210
Daniel Karski <daniel.karski5q@gmail.com>
2024-04-17 12:24:25 +0200

    [CP-2770] Removed `ErrorConnectingModal` as death component

--- libs/core/modals-manager/components/modals-manager.component.tsx
+++ libs/core/modals-manager/components/modals-manager.component.tsx
@@ -8,7 +8,6 @@ import { useSelector } from "react-redux"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
 import ContactSupportFlow from "Core/contact-support/containers/contact-support-flow.container"
 import { UpdateOsInterruptedFlowContainer } from "Core/update/components/update-os-interrupted-flow"
-import ErrorConnectingModal from "Core/connecting/components/error-connecting-modal"
 import ConnectingLoaderModal from "Core/modals-manager/components/connecting-loader-modal.component"
 import DetachedDuringUploadErrorModal from "Core/files-manager/components/dettached-during-upload-error-modal/dettached-during-upload-error-modal.component"
 import { AppUpdateFlow } from "Core/settings/components"
@@ -16,21 +15,14 @@ import { shouldAppUpdateVisibleSelector } from "Core/modals-manager/selectors/sh
 
 type Props = {
   contactSupportFlowShow: boolean
-  deviceInitializationFailedModalShowEnabled: boolean
-  hideModals: () => void
 }
 
 const ModalsManager: FunctionComponent<Props> = ({
   contactSupportFlowShow,
-  deviceInitializationFailedModalShowEnabled,
-  hideModals,
 }) => {
   const appUpdateVisible = useSelector(shouldAppUpdateVisibleSelector)
   return (
     <>
-      {deviceInitializationFailedModalShowEnabled && (
-        <ErrorConnectingModal open closeModal={hideModals} />
-      )}
       {contactSupportFlowShow && <ContactSupportFlow />}
       <UpdateOsInterruptedFlowContainer />
       <ConnectingLoaderModal />
