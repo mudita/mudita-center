7d725e35d2d69ea44ef038cf37545766980aaa43
Daniel Karski <daniel.karski5q@gmail.com>
2024-04-18 10:10:17 +0200

    [CP-2722] Modified manage group in linuks

--- libs/core/desktop/desktop.service.ts
+++ libs/core/desktop/desktop.service.ts
@@ -11,49 +11,42 @@ enum SerialPortGroup {
   uucp = "uucp",
 }
 
-const POTENTIAL_GROUPS = [SerialPortGroup.dialout, SerialPortGroup.uucp];
-
 export class DesktopService {
   public async isLinux(): Promise<boolean> {
     return process.platform === "linux"
   }
 
   public async hasUserSerialPortAccess(): Promise<boolean> {
-    const userGroups = await this.getUserGroups();
-    return POTENTIAL_GROUPS.some(group => userGroups.includes(group));
+    const userGroups = await this.getUserGroups()
+    return userGroups.includes(SerialPortGroup.dialout)
   }
 
   public async addUserToSerialPortGroup(): Promise<void> {
-    const userGroups = await this.getUserGroups();
-    const groupName = POTENTIAL_GROUPS.find(group => !userGroups.includes(group));
+    const command = `usermod -aG ${SerialPortGroup.dialout} $USER & usermod -aG ${SerialPortGroup.uucp} $USER`
+    // Set simpler process.title, otherwise, there is an error from sudoPrompt.exec - 'process.title cannot be used as a valid name.'
+    process.title = "Mudita Center: assign serial port access"
 
-    if (groupName) {
-      const command = `usermod -aG ${groupName} $USER`;
-      // Set simpler process.title, otherwise, there is an error from sudoPrompt.exec - 'process.title cannot be used as a valid name.'
-      process.title = "Mudita Center: assign serial port access";
-
-      return new Promise<void>((resolve, reject) => {
-        sudoPrompt.exec(command, { name: 'User Serial Port Access' }, (error) => {
-          if (error === null) {
-            resolve();
-          } else {
-            reject("Could not add user to serial port group");
-          }
-        });
-      });
-    }
+    return new Promise<void>((resolve, reject) => {
+      sudoPrompt.exec(command, { name: "User Serial Port Access" }, (error) => {
+        if (error === null) {
+          resolve()
+        } else {
+          reject("Could not add user to serial port group")
+        }
+      })
+    })
   }
 
   private async getUserGroups(): Promise<string[]> {
     return new Promise<string[]>((resolve, reject) => {
       exec("groups", (error, stdout, stderr) => {
         if (error || stderr) {
-          reject(`${error?.name} - ${error?.message} - ${stderr}`);
+          reject(`${error?.name} - ${error?.message} - ${stderr}`)
         } else {
-          const groups = stdout.trim().split(/\s+/);
-          resolve(groups);
+          const groups = stdout.trim().split(/\s+/)
+          resolve(groups)
         }
-      });
-    });
+      })
+    })
   }
 }
