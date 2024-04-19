ad4a9b59e56f29c179c1b112015d1f82e3967dfe
Daniel Karski <daniel.karski5q@gmail.com>
2024-04-19 10:08:59 +0200

    [CP-2723] Split files-storage to list and table

--- libs/core/files-manager/components/files-storage-list/files-storage-list.component.tsx
+++ libs/core/files-manager/components/files-storage-list/files-storage-list.component.tsx
@@ -3,70 +3,20 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import React, { useCallback } from "react"
+import React from "react"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
 import { FilesStorageListTestIds } from "Core/files-manager/components/files-storage-list/files-storage-list-test-ids.enum"
 import {
-  Col,
   EmptyState,
   LoadingState,
-  RowSize,
 } from "Core/__deprecated__/renderer/components/core/table/table.component"
-import { IconType } from "Core/__deprecated__/renderer/components/core/icon/icon-type"
-import { intl } from "Core/__deprecated__/renderer/utils/intl"
 import { defineMessages } from "react-intl"
 import { State } from "Core/core/constants"
 import { File } from "Core/files-manager/dto"
-import FilesStorageListTypeCol from "Core/files-manager/components/files-storage-list-type-col/files-storage-list-type-col"
-import { convertBytes } from "Core/core/helpers/convert-bytes/convert-bytes"
-import { DisplayStyle } from "Core/__deprecated__/renderer/components/core/button/button.config"
-import ButtonComponent from "Core/__deprecated__/renderer/components/core/button/button.component"
-import Dropdown from "Core/__deprecated__/renderer/components/core/dropdown/dropdown.component"
-import { IconSize } from "Core/__deprecated__/renderer/components/core/icon/icon.component"
-import useTableScrolling from "Core/__deprecated__/renderer/utils/hooks/use-table-scrolling"
-import { Size } from "Core/__deprecated__/renderer/components/core/input-checkbox/input-checkbox.component"
-import {
-  FilesTable,
-  FirstCol,
-  FileIcon,
-  FilesStorageContainer,
-  Actions,
-  Checkbox,
-  FilesListRow,
-  FilesListLabels,
-  LastEmptyCol,
-  TableWrapper,
-  Ellipsis,
-} from "Core/files-manager/components/files-storage-list/files-storage-list.styled"
-import { DeviceType } from "Core/device/constants"
-import { useDispatch, useSelector } from "react-redux"
-import { Dispatch, ReduxRootState } from "Core/__deprecated__/renderer/store"
-import { Virtuoso } from "react-virtuoso"
-import ElementWithTooltip, {
-  ElementWithTooltipPlace,
-} from "Core/__deprecated__/renderer/components/core/tooltip/element-with-tooltip.component"
-import {
-  TooltipContent,
-  TooltipContentType,
-} from "Core/__deprecated__/renderer/components/core/icon-button-with-tooltip/tooltip-content.style"
-import Text, {
-  TextDisplayStyle,
-} from "Core/__deprecated__/renderer/components/core/text/text.component"
-import { toggleItem } from "Core/files-manager/actions"
+import { FilesStorageContainer } from "Core/files-manager/components/files-storage-list/files-storage-list.styled"
+import FilesStorageListTable from "Core/files-manager/components/files-storage-list-table/files-storage-list-table.component"
 
 const messages = defineMessages({
-  title: {
-    id: "component.filesManagerFilesStorageTitle",
-  },
-  name: {
-    id: "component.filesManagerFilesStorageName",
-  },
-  type: {
-    id: "component.filesManagerFilesStorageType",
-  },
-  size: {
-    id: "component.filesManagerFilesStorageSize",
-  },
   emptyStateTitle: {
     id: "component.filesManagerFilesStorageEmptyStateTitle",
   },
@@ -85,9 +35,6 @@ const messages = defineMessages({
   errorDescription: {
     id: "component.filesManagerFilesStorageErrorStateDescription",
   },
-  deleteAction: {
-    id: "component.filesManagerFilesStorageDelete",
-  },
 })
 
 interface Props {
@@ -98,26 +45,6 @@ interface Props {
   hideCheckbox: boolean
 }
 
-const TruncateText: FunctionComponent<{ text: string }> = ({ text }) => (
-  <TableWrapper>
-    <ElementWithTooltip
-      showIfTextEllipsis
-      place={ElementWithTooltipPlace.TopRight}
-      Element={<Ellipsis>{text}</Ellipsis>}
-    >
-      <TooltipContent type={TooltipContentType.secondary}>
-        <Text
-          displayStyle={TextDisplayStyle.Label}
-          color="primary"
-          element={"p"}
-        >
-          {text}
-        </Text>
-      </TooltipContent>
-    </ElementWithTooltip>
-  </TableWrapper>
-)
-
 const FilesStorageList: FunctionComponent<Props> = ({
   state,
   files = [],
@@ -126,92 +53,19 @@ const FilesStorageList: FunctionComponent<Props> = ({
   hideCheckbox,
   ...rest
 }) => {
-  const dispatch = useDispatch<Dispatch>()
-  const selectedItems = useSelector(
-    (state: ReduxRootState) => state.filesManager.selectedItems.rows
-  )
-
-  const { enableScroll, disableScroll } = useTableScrolling()
-
   const loadedOrInitialState = state === State.Initial || state === State.Loaded
   const noFoundFilesState = loadedOrInitialState && noFoundFiles
   const noFilesState =
     loadedOrInitialState && files.length === 0 && !noFoundFiles
-  const deviceType = useSelector(
-    (state: ReduxRootState) => state.device.deviceType
-  )
-  const filesManagerActionsEnable =
-    deviceType === DeviceType.MuditaPure ||
-    deviceType === DeviceType.MuditaHarmony
-
-  const toggleRow = useCallback(
-    (id: string) => {
-      dispatch(toggleItem(id))
-    },
-    [dispatch]
-  )
 
   return (
     <FilesStorageContainer {...rest}>
       {state === State.Loaded && files.length > 0 && (
-        <FilesTable scrollable={false}>
-          <FilesListLabels
-            filesManagerActionsEnabled={filesManagerActionsEnable}
-            size={RowSize.Tiny}
-            data-testid={FilesStorageListTestIds.Loaded}
-          >
-            <FirstCol>{intl.formatMessage(messages.name)}</FirstCol>
-            <Col />
-            <Col>{intl.formatMessage(messages.type)}</Col>
-            <Col>{intl.formatMessage(messages.size)}</Col>
-            <LastEmptyCol />
-          </FilesListLabels>
-          <Virtuoso
-            data={files}
-            itemContent={(index, file) => {
-              const selected = selectedItems.includes(file.id)
-              const handleCheckboxChange = () => toggleRow(file.id)
-              const handleDelete = () => onDelete([file.id])
-              return (
-                <FilesListRow
-                  key={index}
-                  data-testid={FilesStorageListTestIds.Row}
-                  hideCheckbox={hideCheckbox}
-                >
-                  <Col>
-                    <Checkbox
-                      checked={selected}
-                      onChange={handleCheckboxChange}
-                      size={Size.Medium}
-                      visible={Boolean(selectedItems.length !== 0)}
-                    />
-                    {selectedItems.length === 0 && (
-                      <FileIcon iconType={IconType.MenuMusic} />
-                    )}
-                  </Col>
-                  <Col>
-                    <TruncateText text={file.name} />
-                  </Col>
-                  <FilesStorageListTypeCol file={file} />
-                  <Col>{convertBytes(file.size)}</Col>
-                  <Col>
-                    <Actions>
-                      <Dropdown onOpen={disableScroll} onClose={enableScroll}>
-                        <ButtonComponent
-                          labelMessage={messages.deleteAction}
-                          Icon={IconType.Delete}
-                          onClick={handleDelete}
-                          iconSize={IconSize.Medium}
-                          displayStyle={DisplayStyle.Dropdown}
-                        />
-                      </Dropdown>
-                    </Actions>
-                  </Col>
-                </FilesListRow>
-              )
-            }}
-          />
-        </FilesTable>
+        <FilesStorageListTable
+          files={files}
+          onDelete={onDelete}
+          hideCheckbox={hideCheckbox}
+        />
       )}
       {state === State.Loading && (
         <LoadingState data-testid={FilesStorageListTestIds.Loading} />
