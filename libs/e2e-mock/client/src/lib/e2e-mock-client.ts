bc803b7170c8add3ca34d5d77af67f7b39f6afb6
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2024-04-25 15:10:19 +0200

    add e2e-mock-client lib

---
+++ libs/e2e-mock/client/src/lib/e2e-mock-client.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+export function E2eMockClient() {
+  console.log("E2eMockClient test")
+  return
+}
