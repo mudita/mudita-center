f6c1389316feb39b478f386a5f8cc2f3f97a197d
patrycja-paczkowska <88655668+patrycja-paczkowska@users.noreply.github.com>
2024-05-21 11:49:04 +0200

    [CP-2293] Add Check for MC updates Offline E2E test

--- apps/mudita-center-e2e/src/specs/settings/mc-version-check-for-updates-offline.e2e.ts
+++ apps/mudita-center-e2e/src/specs/settings/mc-version-check-for-updates-offline.e2e.ts
@@ -38,9 +38,37 @@ describe("Checking for Mudita Center updates", () => {
     await browser.executeAsync((done) => {
       setTimeout(done, 10000)
     })
+
     await expect(aboutCheckForUpdateFailedLabel).toBeDisplayed()
     await expect(aboutCheckForUpdateFailedLabel).toHaveText(
       "Checking for updates failed"
+    )
+  })
+
+  it("Check 'Check for updates' button", async () => {
+    const aboutCheckForUpdatesButton =
+      await SettingsPage.aboutCheckForUpdatesButton
+    await aboutCheckForUpdatesButton.waitForDisplayed()
+    await expect(aboutCheckForUpdatesButton).toHaveTextContaining(
+      "CHECK FOR UPDATES"
+    )
+    await aboutCheckForUpdatesButton.click()
+
+    const modalHeader = await ModalPage.modalHeader
+    await expect(modalHeader).toBeDisplayed()
+    await expect(modalHeader).toHaveText("Mudita Center")
+  })
+
+  it("Check for updates", async () => {
+    const checkingFailedUpdateSubtitle =
+      await ModalPage.checkingFailedUpdateSubtitle
+    await expect(checkingFailedUpdateSubtitle).toBeDisplayed()
+    await expect(checkingFailedUpdateSubtitle).toHaveText("Checking failed")
+
+    const checkingFailedUpdateBody = await ModalPage.checkingFailedUpdateBody
+    await expect(checkingFailedUpdateBody).toBeDisplayed()
+    await expect(checkingFailedUpdateBody).toHaveText(
+      "Opps, something went wrong. \nPlease check your internet connection"
     )
   })
 })
