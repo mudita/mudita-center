1956d2427b0c244d1af6738b2fb0c96d5b2de805
Daniel Karski <daniel.karski5q@gmail.com>
2024-06-12 08:37:48 +0200

    [CP-2898] Commit broken png imports to fix

--- libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
+++ libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
@@ -5,8 +5,9 @@
 
 import { ReduxRootState } from "Core/__deprecated__/renderer/store"
 import { createSelector } from "@reduxjs/toolkit"
-import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
-import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
+// TODO: fix imports
+// import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
+// import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
 import { kompaktImg } from "Root/demo-data/kompakt-img"
 import { selectConfiguredDevices } from "./select-configured-devices"
 import { selectActiveApiDeviceId } from "./select-active-api-device-id"
@@ -24,7 +25,8 @@ export const selectDataMigrationSourceDevices = createSelector(
       .filter(({ deviceType }) => deviceType === "MuditaPure")
       .map((device) => ({
         name: getDeviceTypeName(device.deviceType!),
-        image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
+        // image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
+        image: kompaktImg,
         serialNumber: device.serialNumber!,
       }))
 )
