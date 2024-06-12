d133b6120e8114602ff493854024ffdedd6a13d8
Daniel Karski <daniel.karski5q@gmail.com>
2024-06-12 08:37:48 +0200

    [CP-2898] Commit broken png imports to fix

--- libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
+++ libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
@@ -5,8 +5,9 @@
 
 import { ReduxRootState } from "Core/__deprecated__/renderer/store"
 import { createSelector } from "@reduxjs/toolkit"
-import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
-import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
+// TODO: fix imports
+// import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
+// import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
 import { kompaktImg } from "Root/demo-data/kompakt-img"
 
 // FIXME: The device name should be moved to the API config response of API device
@@ -21,7 +22,8 @@ export const selectDataMigrationSourceDevices = createSelector(
       .filter(({ deviceType }) => deviceType === "MuditaPure")
       .map((device) => ({
         name: device.deviceType!,
-        image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
+        // image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
+        image: kompaktImg,
         serialNumber: device.serialNumber!,
       }))
 )
