d9a2494ab7c3401641a7e32fcffeb98efc2f4762
MateuszMudita <165778944+MateuszMudita@users.noreply.github.com>
2024-06-13 13:45:18 +0200

    [CP-2912] Mapper for Outlook contacts (#1920)

---
+++ libs/generic-view/store/src/lib/imports/contacts-mappers/outlook-api/map-outlook-api.ts
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { UnifiedContact } from "device/models"
+import { Contact } from "Core/contacts/reducers/contacts.interface"
+import { getDisplayName } from "../helpers"
+
+export const mapOutlookApi = (contacts: Contact[]): UnifiedContact[] => {
+  return contacts
+    .map((contact): UnifiedContact | null => {
+      try {
+        return {
+          displayName: getDisplayName(contact),
+          id: contact.id,
+          firstName: contact.firstName,
+          lastName: contact.lastName,
+          phoneNumbers: [
+            ...(contact.primaryPhoneNumber
+              ? [
+                  {
+                    value: contact.primaryPhoneNumber,
+                    preference: 1,
+                  },
+                ]
+              : []),
+            ...(contact.secondaryPhoneNumber
+              ? [
+                  {
+                    value: contact.secondaryPhoneNumber,
+                    preference: contact.primaryPhoneNumber ? 2 : 1,
+                  },
+                ]
+              : []),
+          ],
+          emailAddresses: [
+            ...(contact.email
+              ? [
+                  {
+                    value: contact.email,
+                    preference: 1,
+                  },
+                ]
+              : []),
+          ],
+          addresses: [
+            ...(contact.firstAddressLine || contact.secondAddressLine
+              ? [
+                  {
+                    streetAddress:
+                      contact.firstAddressLine || contact.secondAddressLine,
+                    ...(contact.firstAddressLine &&
+                      contact.secondAddressLine && {
+                        extendedAddress: contact.secondAddressLine,
+                      }),
+                  },
+                ]
+              : []),
+          ],
+          note: contact.note,
+          organizations: [],
+          urls: [],
+        }
+      } catch {
+        return null
+      }
+    })
+    .filter((contact): contact is UnifiedContact => contact != null)
+}
