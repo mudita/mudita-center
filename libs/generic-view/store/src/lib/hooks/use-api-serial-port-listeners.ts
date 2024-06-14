813432248b6a5267e5a1da17c0a800613f8ba408
Daniel Karski <daniel.karski5q@gmail.com>
2024-06-14 08:13:11 +0200

    [CP-2898] Add modifiers reducer to manage api devices

--- libs/generic-view/store/src/lib/hooks/use-api-serial-port-listeners.ts
+++ libs/generic-view/store/src/lib/hooks/use-api-serial-port-listeners.ts
@@ -3,14 +3,15 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import { useEffect } from "react"
-import { Device } from "core-device/models"
-import { DeviceProtocolMainEvent, DeviceType } from "device-protocol/models"
+import { useCallback, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { answerMain } from "shared/utils"
-import { detachDevice } from "../views/actions"
-import { getAPIConfig } from "../get-api-config"
+import { answerMain, useDebouncedEventsHandler } from "shared/utils"
+import { DeviceProtocolMainEvent, DeviceType } from "device-protocol/models"
+import { DeviceState } from "generic-view/models"
+import { DeviceBaseProperties } from "device-protocol/models"
 import { Dispatch } from "Core/__deprecated__/renderer/store"
+import { addDevice, removeDevice } from "../views/actions"
+import { getAPIConfig } from "../get-api-config"
 import { setBackupProcessStatus } from "../backup/actions"
 import { closeAllModals } from "../modals/actions"
 import { selectBackupProcessStatus } from "../selectors"
@@ -18,50 +19,66 @@ import { clearDataMigrationDevice } from "../data-migration/clear-data-migration
 
 export const useAPISerialPortListeners = () => {
   const dispatch = useDispatch<Dispatch>()
-  const backupProcess = useSelector(selectBackupProcessStatus)
+  const handleDevicesDetached = useHandleDevicesDetached()
+
+  const batchDeviceDetachedEvents =
+    useDebouncedEventsHandler<DeviceBaseProperties>(handleDevicesDetached)
 
   useEffect(() => {
-    const unregisterFailListener = answerMain<Device>(
+    const unregisterFailListener = answerMain<DeviceBaseProperties>(
       DeviceProtocolMainEvent.DeviceConnectFailed,
       (properties) => {
         const { deviceType } = properties
         if (deviceType !== DeviceType.APIDevice) {
           return
         }
-        // todo: connection error handling
-        console.log(properties)
+        dispatch(addDevice({ ...properties, state: DeviceState.Failed }))
       }
     )
-    const unregisterConnectListener = answerMain<Device>(
+    const unregisterConnectListener = answerMain<DeviceBaseProperties>(
       DeviceProtocolMainEvent.DeviceConnected,
       (properties) => {
         const { id, deviceType } = properties
         if (deviceType !== DeviceType.APIDevice) {
           return
         }
+        dispatch(addDevice(properties))
         dispatch(getAPIConfig({ deviceId: id }))
       }
     )
-    const unregisterDetachedListener = answerMain<Device>(
+    const unregisterDetachedListener = answerMain(
       DeviceProtocolMainEvent.DeviceDetached,
-      async (properties) => {
-        const { id, deviceType } = properties
-        dispatch(clearDataMigrationDevice(id))
-
-        if (deviceType !== DeviceType.APIDevice) {
-          return
-        }
-        dispatch(detachDevice({ deviceId: id }))
-        dispatch(closeAllModals())
-        if (backupProcess) {
-          dispatch(setBackupProcessStatus("FAILED"))
-        }
-      }
+      batchDeviceDetachedEvents
     )
+
     return () => {
       unregisterDetachedListener()
       unregisterConnectListener()
       unregisterFailListener()
     }
-  }, [backupProcess, dispatch])
+  }, [dispatch, batchDeviceDetachedEvents])
+}
+
+const useHandleDevicesDetached = () => {
+  const dispatch = useDispatch<Dispatch>()
+  const backupProcess = useSelector(selectBackupProcessStatus)
+
+  return useCallback(
+    async (deviceDetachedEvents: DeviceBaseProperties[]) => {
+      for (const event of deviceDetachedEvents) {
+        const { deviceType, id } = event
+        dispatch(clearDataMigrationDevice(id))
+
+        if (deviceType !== DeviceType.APIDevice) {
+          return
+        }
+        dispatch(removeDevice(event))
+        dispatch(closeAllModals())
+        if (backupProcess) {
+          dispatch(setBackupProcessStatus("FAILED"))
+        }
+      }
+    },
+    [dispatch, backupProcess]
+  )
 }
