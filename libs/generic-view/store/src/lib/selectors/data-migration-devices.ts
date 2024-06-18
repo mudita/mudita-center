6e6f86b128020f96d694358ff1530334212afb9d
Daniel Karski <daniel.karski5q@gmail.com>
2024-06-18 09:05:55 +0200

    [CP-2898] PNG fixed

--- libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
+++ libs/generic-view/store/src/lib/selectors/data-migration-devices.ts
@@ -3,15 +3,14 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import { ReduxRootState } from "Core/__deprecated__/renderer/store"
 import { createSelector } from "@reduxjs/toolkit"
-// TODO: fix imports
-// import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
-// import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
 import { kompaktImg } from "Root/demo-data/kompakt-img"
+import { ReduxRootState } from "Core/__deprecated__/renderer/store"
+import pureBlackImage from "Core/__deprecated__/renderer/images/pure-black-render.png"
+import pureGreyImage from "Core/__deprecated__/renderer/images/pure-gray-render.png"
+import { getDeviceTypeName } from "Core/discovery-device/utils/get-device-type-name"
 import { selectConfiguredDevices } from "./select-configured-devices"
 import { selectActiveApiDeviceId } from "./select-active-api-device-id"
-import { getDeviceTypeName } from "Core/discovery-device/utils/get-device-type-name"
 
 // FIXME: The device name should be moved to the API config response of API device
 const messages = {
@@ -25,8 +24,7 @@ export const selectDataMigrationSourceDevices = createSelector(
       .filter(({ deviceType }) => deviceType === "MuditaPure")
       .map((device) => ({
         name: getDeviceTypeName(device.deviceType!),
-        // image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
-        image: kompaktImg,
+        image: device.caseColour === "black" ? pureBlackImage : pureGreyImage,
         serialNumber: device.serialNumber!,
       }))
 )
