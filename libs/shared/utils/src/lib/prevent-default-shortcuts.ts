e0ef241ec1adf991ea64e0339548646dd07efc29
Daniel Karski <daniel.karski5q@gmail.com>
2024-06-22 09:24:46 +0200

    [CP-2940] Prevent default DevTools shortcut across all windows

--- libs/shared/utils/src/lib/prevent-default-shortcuts.ts
+++ libs/shared/utils/src/lib/prevent-default-shortcuts.ts
@@ -7,7 +7,18 @@ import { BrowserWindow } from "electron"
 
 export function preventDefaultShortcuts(win: BrowserWindow) {
   win.webContents.on("before-input-event", (event, input) => {
-    if ((input.control || input.meta) && input.key.toLowerCase() === "r") {
+    const { control, meta } = input
+    const key = input.key.toLowerCase()
+
+    // Prevent default refresh shortcut (Ctrl+R or Cmd+R)
+    if ((control || meta) && key === "r") {
+      event.preventDefault()
+    }
+
+    // Prevent default DevTools shortcut (Ctrl+Shift+I or Cmd+Opt+I) and 'dead' key issue
+    // This is necessary because removing the menu in Windows/Linux disables the shortcut,
+    // but on macOS, the shortcut still works. Therefore, we need to explicitly prevent it.
+    if ((control || meta) && (key === "dead" || key === "i")) {
       event.preventDefault()
     }
   })
