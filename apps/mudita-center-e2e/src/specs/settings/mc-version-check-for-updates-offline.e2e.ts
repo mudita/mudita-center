e3c57210070708fd44616403cb2e6b951cf79aab
Tomasz Malecki <tomasz.malecki@mudita.com>
2024-07-03 09:40:07 +0200

    CP-2954 Updated offline tests to work on mock build not on classic build

--- apps/mudita-center-e2e/src/specs/settings/mc-version-check-for-updates-offline.e2e.ts
+++ apps/mudita-center-e2e/src/specs/settings/mc-version-check-for-updates-offline.e2e.ts
@@ -7,17 +7,34 @@ import SettingsPage from "../../page-objects/settings.page"
 import NavigationTabs from "../../page-objects/tabs.page"
 import ModalPage from "../../page-objects/mc-update-modal.page"
 import HomePage from "../../page-objects/home.page"
-import TestHelper from "../../helpers/tests.helper"
+import testsHelper from "../../helpers/tests.helper"
 import dns from "node:dns"
 
 describe("Checking for Mudita Center updates", () => {
   before(async function () {
-    if (TestHelper.isLinux()) {
+    if (testsHelper.isLinux()) {
       this.skip()
     }
 
-    dns.setDefaultResultOrder("ipv4first")
-    await browser.throttle("offline")
+    // Clear browser cache
+    await browser.deleteAllCookies()
+    await browser.execute("window.localStorage.clear();")
+    await browser.execute("window.sessionStorage.clear();")
+
+    // Switch to offline mode before starting the tests
+    await browser.setNetworkConditions({
+      offline: true,
+      latency: 0,
+      download_throughput: 0,
+      upload_throughput: 0,
+    })
+
+    // Add a small delay to ensure network conditions are applied
+    await browser.pause(1000)
+
+    // Verify network conditions
+    const isOnline = await testsHelper.isOnline()
+    await expect(isOnline).toBeFalsy()
 
     const notNowButton = await HomePage.notNowButton
     await notNowButton.waitForDisplayed()
@@ -70,5 +87,22 @@ describe("Checking for Mudita Center updates", () => {
     await expect(checkingFailedUpdateBody).toHaveText(
       "Opps, something went wrong. \nPlease check your internet connection"
     )
+  })
+
+  after(async () => {
+    // Switch back to online mode after finishing the tests
+    await browser.setNetworkConditions({
+      offline: false,
+      latency: 0,
+      download_throughput: -1,
+      upload_throughput: -1,
+    })
+
+    // Add a small delay to ensure network conditions are applied
+    await browser.pause(1000)
+
+    // Verify network conditions
+    const isOnline = await testsHelper.isOnline()
+    await expect(isOnline).toBeTruthy()
   })
 })
