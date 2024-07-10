8a225aa97714f8ac177cf364d311722bb5213a87
robertmudi <158469469+robertmudi@users.noreply.github.com>
2024-07-10 11:26:46 +0200

    [CP-2753] added new test - kompakt-overview  (#1936)
    Use Kompakt mocking system and verify design of an overview page

--- libs/generic-view/ui/src/lib/data-rows/image.tsx
+++ libs/generic-view/ui/src/lib/data-rows/image.tsx
@@ -7,8 +7,12 @@ import { APIFC } from "generic-view/utils"
 import React from "react"
 import { ImageConfig, ImageData } from "generic-view/models"
 
+const dataTestId = {
+  image: "generic-view-image",
+}
+
 export const Image: APIFC<ImageData, ImageConfig> = ({ data, config }) => {
   const { src, alt } = data || config || {}
   if (!src) return null
-  return <img src={src} alt={alt} />
+  return <img src={src} alt={alt} data-testid={dataTestId.image} />
 }
