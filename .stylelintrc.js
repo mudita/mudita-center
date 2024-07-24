6f6a2a59e32d99ede9afc0168dd4a52bf6d98c25
mkurczewski <michal@kurczewski.dev>
2024-07-24 15:32:15 +0200

    Stylelint fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: "@mudita/stylelint-config",
   rules: {
     "no-descending-specificity": null,
+    "value-no-vendor-prefix": null,
     "selector-type-no-unknown": [true, { ignoreTypes: ["$dummyValue"] }],
   },
 }
