5efdd43bcf06f2a65ff057f26566a81ce2876b9c
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2024-07-31 18:55:16 +0200

    add sample test

---
+++ apps/mudita-center-e2e/src/specs/overview/e2e-reset-mock-sample.e2e.ts
@@ -0,0 +1,88 @@
+import { E2EMockClient } from "../../../../../libs/e2e-mock/client/src"
+import {
+  outboxReloadOverview,
+  overviewDataWithoutBadge,
+} from "../../../../../libs/e2e-mock/responses/src"
+import screenshotHelper from "../../helpers/screenshot.helper"
+
+describe("E2E reset mock sample", () => {
+  before(async () => {
+    E2EMockClient.connect()
+    //wait for a connection to be established
+    await browser.waitUntil(() => {
+      return E2EMockClient.checkConnection()
+    })
+  })
+
+  after(() => {
+    E2EMockClient.stopServer()
+    E2EMockClient.disconnect()
+  })
+
+  it("Connect locked device", async () => {
+    E2EMockClient.mockResponse({
+      path: "path-1",
+      body: {},
+      endpoint: "FEATURE_DATA",
+      method: "GET",
+      status: 423,
+    })
+    E2EMockClient.mockResponse({
+      path: "path-1",
+      body: {},
+      endpoint: "FEATURE_CONFIGURATION",
+      method: "GET",
+      status: 423,
+    })
+    E2EMockClient.mockResponse({
+      path: "path-1",
+      body: {},
+      endpoint: "MENU_CONFIGURATION",
+      method: "GET",
+      status: 423,
+    })
+
+    E2EMockClient.mockResponse({
+      path: "path-1",
+      body: {},
+      endpoint: "OUTBOX",
+      method: "GET",
+      status: 423,
+    })
+
+    E2EMockClient.addDevice({
+      path: "path-1",
+      serialNumber: "first-serial-number",
+    })
+
+    await browser.pause(10000)
+    const lockScreen = await $(`//*[text()="Unlock your phone"]`)
+
+    await lockScreen.waitForDisplayed({ timeout: 10000 })
+    await expect(lockScreen).toBeDisplayed()
+    await browser.pause(2000)
+  })
+
+  it("Unlock device", async () => {
+    E2EMockClient.mockReset({
+      path: "path-1",
+      requests: [
+        { endpoint: "FEATURE_DATA", method: "GET" },
+        {
+          endpoint: "FEATURE_CONFIGURATION",
+          method: "GET",
+        },
+        {
+          endpoint: "MENU_CONFIGURATION",
+          method: "GET",
+        },
+        {
+          endpoint: "OUTBOX",
+          method: "GET",
+        },
+      ],
+    })
+
+    await browser.pause(10000)
+  })
+})
