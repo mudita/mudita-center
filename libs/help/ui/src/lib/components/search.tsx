3db978a99c3dca332efc1e44d585efd39d1c0a15
mkurczewski <michal@kurczewski.dev>
2024-08-01 18:33:54 +0200

    Added support for scrolling results while using keyboard arrows

--- libs/help/ui/src/lib/components/search.tsx
+++ libs/help/ui/src/lib/components/search.tsx
@@ -3,7 +3,12 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import React, { KeyboardEvent, useDeferredValue, useEffect } from "react"
+import React, {
+  KeyboardEvent,
+  useDeferredValue,
+  useEffect,
+  useRef,
+} from "react"
 import { intl } from "Core/__deprecated__/renderer/utils/intl"
 import { FunctionComponent } from "Core/core/types/function-component.interface"
 import { defineMessages } from "react-intl"
@@ -38,22 +43,50 @@ export const Search: FunctionComponent = () => {
     cleanSearchPhrase(deferredSearchPhrase)
   const results = useHelpSearch(cleanedSearchPhrase)
   const activeResultIndex = watch("activeResultIndex")
+  const searchResultsRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     register("activeResultIndex", { value: 0 })
   }, [register])
 
+  const onArrowNavigation = (
+    event: KeyboardEvent<HTMLInputElement>,
+    index: number,
+    up?: boolean
+  ) => {
+    event.preventDefault()
+    setValue("activeResultIndex", index)
+
+    const activeElement = searchResultsRef.current?.querySelector(
+      `li.active`
+    ) as HTMLElement
+    const containerScrollHeight = searchResultsRef.current?.scrollHeight || 0
+    const containerScrollTop = searchResultsRef.current?.scrollTop || 0
+    const containerOffsetHeight = searchResultsRef.current?.clientHeight || 0
+    const elementTop = activeElement.offsetTop
+    const elementHeight = activeElement.offsetHeight
+    const scrollDelta = containerScrollHeight - containerOffsetHeight
+
+    if (up) {
+      if (elementTop <= containerScrollTop + elementHeight) {
+        searchResultsRef.current?.scrollTo(0, elementTop - elementHeight)
+      }
+    } else {
+      if (elementTop > scrollDelta + containerScrollTop) {
+        searchResultsRef.current?.scrollTo(0, elementTop - scrollDelta)
+      }
+    }
+  }
+
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "ArrowDown") {
-      event.preventDefault()
-      setValue(
-        "activeResultIndex",
+      onArrowNavigation(
+        event,
         Math.min(activeResultIndex + 1, (results?.hits.length || 1) - 1)
       )
     }
     if (event.key === "ArrowUp") {
-      event.preventDefault()
-      setValue("activeResultIndex", Math.max(activeResultIndex - 1, 0))
+      onArrowNavigation(event, Math.max(activeResultIndex - 1, 0), true)
     }
     if (event.key === "Enter") {
       const activeResult = results?.hits[activeResultIndex]
@@ -84,7 +117,11 @@ export const Search: FunctionComponent = () => {
             label: intl.formatMessage(messages.placeholder),
           }}
         />
-        <SearchResults results={results} phrase={cleanedHighlightPhrase} />
+        <SearchResults
+          results={results}
+          phrase={cleanedHighlightPhrase}
+          ref={searchResultsRef}
+        />
       </InputWrapper>
     </Wrapper>
   )
