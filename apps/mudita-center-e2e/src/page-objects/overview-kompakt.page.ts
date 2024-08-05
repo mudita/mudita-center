b96f848d19a51efe13dc5eb8255c766f0a730150
robertmudi <158469469+robertmudi@users.noreply.github.com>
2024-08-05 16:05:00 +0200

    [CP-2918] [API Device][Multidevice] Switching devices (1st Kompakt, 2nd Kompakt) (#1971)

--- apps/mudita-center-e2e/src/page-objects/overview-kompakt.page.ts
+++ apps/mudita-center-e2e/src/page-objects/overview-kompakt.page.ts
@@ -15,15 +15,19 @@ class OverviewKompaktPage extends OverviewPage {
   }
 
   public get sarInformationPopup() {
-    return $("/html/body/div[10]/div/div")
+    return $('[data-testid="button-text_sarmodal-button"]')
   }
 
   public get sarInformationPopupCloseButton() {
     return $('[data-testid="icon-close"]')
   }
 
+  public get backArrowButton() {
+    return $('[data-testid="icon-ArrowLongLeft"]')
+  }
+
   public get backupInfo() {
-    return $(`//div[@componentkey="backupbackup-info"]/p`)
+    return $(`//div[@data-testid="block-box-backup"]//p`)
   }
 
   public get serialNumberLabel() {
@@ -42,20 +46,26 @@ class OverviewKompaktPage extends OverviewPage {
     return $('[data-testid="icon-network-signal-2"]')
   }
 
+  public getStatusRow(rowIndex: number) {
+    return $(
+      `(//div[@data-testid="block-box-status"]/..//h4[@data-testid="icon-text"])[${rowIndex}]`
+    )
+  }
+
   public get kompaktNetworkName() {
-    return $('[data-testid="icon-text"]')
+    return this.getStatusRow(2)
   }
 
   public get kompaktBatteryIcon() {
-    return $('//div[@data-testid="icon-battery-charging-2"]')
+    return $('//div[@data-testid="icon-battery-charging-5"]')
   }
 
   public get kompaktBatteryLevelValue() {
-    return $('//h3[text()="Status"]/..//h4[@data-testid="icon-text"]')
+    return this.getStatusRow(1)
   }
 
-  public get kompaktSimCard1() {
-    return $('[data-testid="icon-subtext"]')
+  public get kompaktSimCard1Subtext() {
+    return this.getStatusRow(2).$(`//*[@data-testid="icon-subtext"]`)
   }
 
   public get kompaktOsVersion() {
