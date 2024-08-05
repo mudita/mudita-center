57570dc44ad8cdcd4eb481ed87c198eb4a9934f1
robertmudi <robert.rosloniec@mudita.com>
2024-08-05 19:16:25 +0200

    CP-2920 added complete test, modified e2e-mock sample and overview responses

--- apps/mudita-center-e2e/src/specs/stress-tests/connected-devices-stress-test.ts
+++ apps/mudita-center-e2e/src/specs/stress-tests/connected-devices-stress-test.ts
@@ -1,13 +1,26 @@
 import { E2EMockClient } from "../../../../../libs/e2e-mock/client/src"
-import { overviewDataWithOneSimCard } from "../../../../../libs/e2e-mock/responses/src"
-import OverviewPage from "../../page-objects/overview.page"
+import {
+  overviewDataWithOneSimCard,
+  overviewDataWithOneSimCard2nd,
+  overviewDataWithOneSimCard3rd,
+  overviewDataWithOneSimCard4th,
+  overviewDataWithOneSimCard5th,
+  overviewDataWithOneSimCard6th,
+} from "../../../../../libs/e2e-mock/responses/src"
 import OverviewKompaktPage from "../../page-objects/overview-kompakt.page"
 import HomePage from "../../page-objects/home.page"
-import tabsPage from "../../page-objects/tabs.page"
 import { kompaktImageRegex } from "../../consts/regex-const"
-import { addDeviceWithGivenSerialNumber } from "E2E/helpers"
+import overviewPage from "../../page-objects/overview.page"
+import drawerPage from "../../page-objects/drawer.page"
 
-describe("E2E mock sample - overview view", () => {
+describe("Kompakt switching devices", () => {
+  const firstSerialNumber = "KOM1234567890"
+  const secondSerialNumber = "KOM1234567892"
+  const thirdSerialNumber = "KOM1234567893"
+  const fourthSerialNumber = "KOM1234567894"
+  const fifthSerialNumber = "KOM1234567895"
+  const sixthSerialNumber = "KOM1234567896"
+
   before(async () => {
     E2EMockClient.connect()
     //wait for a connection to be established
@@ -21,123 +34,304 @@ describe("E2E mock sample - overview view", () => {
     E2EMockClient.disconnect()
   })
 
-  it("Connect 1st and 2ed device", async () => {
-    addDeviceWithGivenSerialNumber("device-1")
-    addDeviceWithGivenSerialNumber("device-2")
+  it("Connect 1,2 Kompakt devices", async () => {
+    const devices = [
+      {
+        path: "path-1",
+        body: overviewDataWithOneSimCard,
+        serialNumber: firstSerialNumber,
+      },
+      {
+        path: "path-2",
+        body: overviewDataWithOneSimCard2nd,
+        serialNumber: secondSerialNumber,
+      },
+    ]
 
-    await browser.pause(6000)
+    for (const device of devices) {
+      E2EMockClient.mockResponse({
+        path: device.path,
+        body: device.body,
+        endpoint: "FEATURE_DATA",
+        method: "GET",
+        status: 200,
+      })
+      E2EMockClient.addDevice({
+        path: device.path,
+        serialNumber: device.serialNumber,
+      })
 
-    // punkt 4 i 5
-
-    // wybierasz urzadzenie
-
-    // const menuItem = await $(`//a[@href="#/generic/mc-overview"]`)
-
-    // await menuItem.waitForDisplayed({ timeout: 10000 })
-    // await expect(menuItem).toBeDisplayed()
+      await browser.pause(6000)
+    }
   })
 
-  it("Verify 1st device", async () => {
-    const serialNumberElement = await OverviewPage.overviewSerialNumber
-    await expect(serialNumberElement).toHaveText("device-1")
-    //przepiac na 2ed device
+  it("Check Drawer Modal and Switch to 1st device", async () => {
+    const deviceSelectDrawer = await drawerPage.deviceSelectDrawer
+    await expect(deviceSelectDrawer).toBeDisplayed()
+
+    const deviceImageOnDrawer = await drawerPage.deviceImageOnDrawer
+    await expect(deviceImageOnDrawer).toBeDisplayed()
+
+    const firstDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      firstSerialNumber
+    )
+    const secondDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      secondSerialNumber
+    )
+    await expect(firstDeviceOnDrawer).toBeDisplayed()
+    await firstDeviceOnDrawer.waitForClickable()
+    await firstDeviceOnDrawer.click()
+  })
+  it("Verify Overview Page for 1st device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
+
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
+
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
+
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("T-Mobile")
+
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("100%")
   })
 
-  it("Verify 2ed device", async () => {
-    const serialNumberElement = await OverviewPage.overviewSerialNumber
-    await expect(serialNumberElement).toHaveText("device-2")
+  it("Switch to 2nd device", async () => {
+    const secondDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      secondSerialNumber
+    )
+    await expect(secondDeviceOnDrawer).toBeDisplayed()
+    await secondDeviceOnDrawer.waitForClickable()
+    await secondDeviceOnDrawer.click()
   })
 
-  it("Connect 3-6th device", async () => {
-    addDeviceWithGivenSerialNumber("device-3")
-    addDeviceWithGivenSerialNumber("device-4")
-    addDeviceWithGivenSerialNumber("device-5")
-    addDeviceWithGivenSerialNumber("device-6")
+  it("Verify Overview Page for 2nd device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
+
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
+
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
+
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("Play")
+
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("40%")
   })
-  // it("Verify Overview Page", async () => {
-  //   const kompaktImage = await OverviewKompaktPage.kompaktImage
-  //   await expect(kompaktImage).toBeDisplayed
-  //   console.log(kompaktImage)
-  //   await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
 
-  //   const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
-  //   await expect(kompaktOsVersion).toBeDisplayed
+  //PODLACZYC 3 4 5 I 6 KOMPAKT
 
-  //   const kompaktOsVersionLabel =
-  //     await OverviewKompaktPage.kompaktOsVersionLabel
-  //   await expect(kompaktOsVersionLabel).toBeDisplayed
+  it("Connect 3,4,5,6 Kompakt devices", async () => {
+    const devices = [
+      {
+        path: "path-3",
+        body: overviewDataWithOneSimCard3rd,
+        serialNumber: thirdSerialNumber,
+      },
+      {
+        path: "path-4",
+        body: overviewDataWithOneSimCard4th,
+        serialNumber: fourthSerialNumber,
+      },
+      {
+        path: "path-5",
+        body: overviewDataWithOneSimCard5th,
+        serialNumber: fifthSerialNumber,
+      },
+      {
+        path: "path-6",
+        body: overviewDataWithOneSimCard6th,
+        serialNumber: sixthSerialNumber,
+      },
+    ]
 
-  //   const serialNumberLabel = await OverviewKompaktPage.serialNumberLabel
-  //   const serialNumberValue = await OverviewKompaktPage.serialNumberValue
-  //   await expect(serialNumberLabel).toHaveText("Serial number")
-  //   await expect(serialNumberValue).toHaveText(
-  //     overviewDataWithOneSimCard.summary.about.serialNumber.text.toString()
-  //   )
+    for (const device of devices) {
+      E2EMockClient.mockResponse({
+        path: device.path,
+        body: device.body,
+        endpoint: "FEATURE_DATA",
+        method: "GET",
+        status: 200,
+      })
+      E2EMockClient.addDevice({
+        path: device.path,
+        serialNumber: device.serialNumber,
+      })
 
-  //   const aboutYourDevice = await OverviewKompaktPage.aboutYourDevice
-  //   await expect(aboutYourDevice).toBeDisplayed
-  //   console.log(aboutYourDevice)
-  //   await expect(aboutYourDevice).toBeClickable
-  //   await aboutYourDevice.click()
+      await browser.pause(6000)
+    }
+  })
 
-  //   const sarInformationButtonKompakt =
-  //     await OverviewKompaktPage.sarInformationButtonKompakt
-  //   const sarInformationPopup = await OverviewKompaktPage.sarInformationPopup
-  //   await expect(sarInformationButtonKompakt).toBeDisplayed
-  //   await sarInformationButtonKompakt.click()
-  //   await expect(sarInformationPopup).toBeDisplayed
+  it("Check Drawer Modal and Switch to 3rd device", async () => {
+    const deviceSelectDrawer = await drawerPage.deviceSelectDrawer
+    const thirdDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      thirdSerialNumber
+    )
+    await expect(deviceSelectDrawer).toBeDisplayed()
 
-  //   const sarInformationPopupCloseButton =
-  //     await OverviewKompaktPage.sarInformationPopupCloseButton
-  //   await sarInformationPopupCloseButton.click()
+    const deviceImageOnDrawer = await drawerPage.deviceImageOnDrawer
+    await expect(deviceImageOnDrawer).toBeDisplayed()
 
-  //   const createBackupButton = await OverviewPage.createBackupButton
-  //   await expect(createBackupButton).toBeDisplayed
-  //   await expect(createBackupButton).toBeClickable
+    await expect(thirdDeviceOnDrawer).toBeDisplayed()
 
-  //   const backupInfo = await OverviewKompaktPage.backupInfo
-  //   await expect(backupInfo).toBeDisplayed
-  //   const str: string =
-  //     "\tYou haven’t backed up your device yet.\n Create a backup file to protect against data loss.\n"
-  //   const backupInfoText: string = str.trim()
+    await thirdDeviceOnDrawer.waitForClickable()
+    await thirdDeviceOnDrawer.click()
+  })
+  it("Verify Overview Page for 3rd device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
 
-  //   const kompaktBatteryIcon = await OverviewKompaktPage.kompaktBatteryIcon
-  //   await expect(kompaktBatteryIcon).toBeDisplayed
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
 
-  //   const kompaktBatteryLevelValue =
-  //     await OverviewKompaktPage.kompaktBatteryLevelValue
-  //   await expect(kompaktBatteryLevelValue).toBeDisplayed
-  //   const batteryValNum: string = "100%"
-  //   const batteryValue: string = batteryValNum
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
 
-  //   const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
-  //   await expect(kompaktNetworkName).toBeDisplayed
-  //   const strNetwork: string = "T-Mobile"
-  //   const networkName: string = strNetwork
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("Orange")
 
-  //   const kompaktSignalIcon = await OverviewKompaktPage.kompaktSignalIcon
-  //   await expect(kompaktSignalIcon).toBeDisplayed
-  //   const strSignal: string = "network-signal-2"
-  //   const signalValue: string = strSignal
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("60%")
+  })
 
-  //   const kompaktSimCard1 = await OverviewKompaktPage.kompaktSimCard1
-  //   await expect(kompaktSimCard1).toBeDisplayed
-  //   const sim1: string = "SIM 1"
-  //   const simInfo: string = sim1
-  // })
+  it("Switch to 4th device", async () => {
+    const fourthDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      fourthSerialNumber
+    )
+    const selectConnectedDevices = await overviewPage.selectConnectedDevices
+    await selectConnectedDevices.waitForClickable()
+    await expect(selectConnectedDevices).toHaveText("6")
+    await selectConnectedDevices.click()
 
-  // it("Click between Tabs and check them", async () => {
-  //   const muditaNewsTab = await tabsPage.muditaNewsTab
-  //   await muditaNewsTab.waitForClickable()
-  //   await muditaNewsTab.click()
+    await expect(fourthDeviceOnDrawer).toBeDisplayed()
+    await fourthDeviceOnDrawer.waitForClickable()
+    await fourthDeviceOnDrawer.click()
+  })
 
-  //   const overviewKompaktTab = await tabsPage.overviewKompaktTab
-  //   await overviewKompaktTab.waitForClickable()
-  //   await overviewKompaktTab.click()
-  // })
+  it("Verify Overview Page for 4th device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
 
-  // it("Disconnect the device and check if Welcome screen is present", async () => {
-  //   E2EMockClient.removeDevice("path-1")
-  //   await HomePage.homeHeader.waitForDisplayed()
-  // })
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
+
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
+
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("O2")
+
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("80%")
+  })
+
+  it("Switch to 5th device", async () => {
+    const fifthDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      fifthSerialNumber
+    )
+    const selectConnectedDevices = await overviewPage.selectConnectedDevices
+    await expect(selectConnectedDevices).toHaveText("6")
+    await selectConnectedDevices.waitForClickable()
+    await selectConnectedDevices.click()
+
+    await expect(fifthDeviceOnDrawer).toBeDisplayed()
+    await fifthDeviceOnDrawer.waitForClickable()
+    await fifthDeviceOnDrawer.click()
+  })
+
+  it("Verify Overview Page for 5th device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
+
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
+
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
+
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("Vodafone")
+
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("100%")
+  })
+
+  it("Switch to 6th device", async () => {
+    const sixthDeviceOnDrawer = await drawerPage.getDeviceOnDrawer(
+      sixthSerialNumber
+    )
+    const selectConnectedDevices = await overviewPage.selectConnectedDevices
+    await expect(selectConnectedDevices).toHaveText("6")
+    await selectConnectedDevices.waitForClickable()
+    await selectConnectedDevices.click()
+
+    await expect(sixthDeviceOnDrawer).toBeDisplayed()
+    await sixthDeviceOnDrawer.waitForClickable()
+    await sixthDeviceOnDrawer.click()
+  })
+
+  it("Verify Overview Page for 6th device", async () => {
+    const kompaktImage = await OverviewKompaktPage.kompaktImage
+    await expect(kompaktImage).toBeDisplayed()
+    await expect(kompaktImage).toHaveAttribute("src", kompaktImageRegex)
+
+    const kompaktOsVersion = await OverviewKompaktPage.kompaktOsVersion
+    await expect(kompaktOsVersion).toBeDisplayed()
+
+    const kompaktSimCard1Subtext =
+      await OverviewKompaktPage.kompaktSimCard1Subtext
+    await expect(kompaktSimCard1Subtext).toHaveText("SIM 1")
+
+    const kompaktNetworkName = await OverviewKompaktPage.kompaktNetworkName
+    await expect(kompaktNetworkName).toBeDisplayed()
+    await expect(kompaktNetworkName).toHaveText("Telia")
+
+    const kompaktBatteryLevelValue =
+      await OverviewKompaktPage.kompaktBatteryLevelValue
+    await expect(kompaktBatteryLevelValue).toBeDisplayed()
+    await expect(kompaktBatteryLevelValue).toHaveText("20%")
+  })
+
+  it("Disconnect the devices and check if Welcome Page is present", async () => {
+    E2EMockClient.removeDevice("path-1")
+    E2EMockClient.removeDevice("path-2")
+    E2EMockClient.removeDevice("path-3")
+    E2EMockClient.removeDevice("path-4")
+    E2EMockClient.removeDevice("path-5")
+    E2EMockClient.removeDevice("path-6")
+
+    const homeHeader = await HomePage.homeHeader
+    await homeHeader.waitForDisplayed()
+  })
 })
