514db029caaa0fe078449bad53103516aa5bb082
Daniel Karski <daniel.karski5q@gmail.com>
2024-08-06 10:29:46 +0200

    [CP-2880] WIP 4

--- apps/mudita-center-e2e/wdio.conf.ts
+++ apps/mudita-center-e2e/wdio.conf.ts
@@ -157,7 +157,15 @@ export const config: Options.Testrunner = {
       browserName: "chrome",
       "goog:chromeOptions": {
         binary: process.env.TEST_BINARY_PATH,
-        args: ['--no-sandbox', '--disable-dev-shm-usage']
+        args: [
+          "--no-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-gpu",
+          "--disable-software-rasterizer",
+          "--headless",
+          "--no-zygote",
+          "--single-process",
+        ],
       },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
