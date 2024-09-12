b294799a744b4736c11840c32265f3f9f68b3f09
Daniel Karski <daniel.karski5q@gmail.com>
2024-09-12 10:50:49 +0200

    [CP-3108] Add data-testid to Backup modals

--- libs/e2e-test-ids/src/e2e-test-ids.ts
+++ libs/e2e-test-ids/src/e2e-test-ids.ts
@@ -7,6 +7,22 @@ export enum ModalCloseButtonTestIds {
   IconButton = "modal-close-button-icon-button",
 }
 
+export enum InteractiveTextInputTestIds {
+  Text = "interactive-text-input-text",
+  Input = "interactive-text-input-input",
+  IconButton = "interactive-text-input-icon-button",
+  ErrorText = "interactive-text-input-error-text",
+}
+
+export enum PredefinedBackupPasswordTestIds {
+  Title = "predefined-backup-password-title",
+  Description = "predefined-backup-password-description",
+  PasswordPlaceholder = "predefined-backup-password-placeholder",
+  PasswordRepeatPlaceholder = "predefined-backup-password-repeat-placeholder",
+  ConfirmButton = "predefined-backup-password-confirm-button",
+  SkipButton = "predefined-backup-password-skip-button",
+}
+
 export enum ContactSupportModalTestIds {
   Title = "contact-support-modal-success-title",
   Description = "contact-support-modal-success-description",
