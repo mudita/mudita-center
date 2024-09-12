e13d7097a1882ad8d9c68a1827e7b30af6da8259
Daniel Karski <daniel.karski5q@gmail.com>
2024-09-12 09:41:34 +0200

    [CP-3093] Added derived components for ContactSupportModal: title, description, and close button

--- libs/e2e-test-ids/src/e2e-test-ids.ts
+++ libs/e2e-test-ids/src/e2e-test-ids.ts
@@ -4,5 +4,11 @@
  */
 
 export enum E2eTestIds {
+  // Base components
   ModalCloseIconButton = "modal-close-icon-button",
+
+  // Derived components
+  ContactSupportModalSuccessTitle = "contact-support-modal-success-title",
+  ContactSupportModalSuccessDescription = "contact-support-modal-success-description",
+  ContactSupportModalSuccessCloseButton = "contact-support-modal-success-close-button"
 }
