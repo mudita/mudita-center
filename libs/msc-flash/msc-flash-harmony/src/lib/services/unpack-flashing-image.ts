5c3f820f522b17fa9eff530744b6da4d3c76d45b
MateuszMudita <mateusz.bolewski@mudita.com>
2024-09-16 10:44:05 +0200

    Changed exec command for unpacking image

--- libs/msc-flash/msc-flash-harmony/src/lib/services/unpack-flashing-image.ts
+++ libs/msc-flash/msc-flash-harmony/src/lib/services/unpack-flashing-image.ts
@@ -4,6 +4,7 @@
  */
 
 import getAppSettingsMain from "Core/__deprecated__/main/functions/get-app-settings"
+import path from "path"
 import { execPromise } from "shared/utils"
 
 export const unpackFlashingImageService = async (
@@ -11,14 +12,15 @@ export const unpackFlashingImageService = async (
 ): Promise<void> => {
   let command: string = ""
 
-  if (process.platform === "darwin" || process.platform === "linux") {
-    command = `tar -xf ${fileName}`
-  }
-
   const { osDownloadLocation } = await getAppSettingsMain()
 
+  if (process.platform === "darwin" || process.platform === "linux") {
+    const imageFilePath = path.join(osDownloadLocation, fileName)
+    command = `tar -xf "${imageFilePath}" -C "${osDownloadLocation}"`
+  }
+
   try {
-    await execPromise(`cd "${osDownloadLocation}" && tar -xf ${fileName}`)
+    await execPromise(command)
   } catch (error) {
     console.error("Failed to unpack files", error)
     throw error
