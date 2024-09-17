5a38f3eaafaad7bd39719f952935be99baada119
Daniel Karski <daniel.karski5q@gmail.com>
2024-09-17 10:30:32 +0200

    [CP-3093] Add data-testid to Success modal for Contact Support Form sending (#2054)

---
+++ libs/e2e-test-ids/src/e2e-test-ids.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+export enum ModalCloseButtonTestIds {
+  IconButton = "modal-close-button-icon-button",
+}
+
+export enum ContactSupportModalTestIds {
+  Title = "contact-support-modal-success-title",
+  Description = "contact-support-modal-success-description",
+  CloseButton = "contact-support-modal-success-close-button",
+}
