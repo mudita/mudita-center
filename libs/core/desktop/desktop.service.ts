aa646ae3c152134952c7f150076570debf31d2d6
Daniel Karski <daniel.karski5q@gmail.com>
2024-09-18 10:53:25 +0200

    [CP-2914] custom execCommand & execCommandWithSudo replaced with shared utility and error handling simplified

--- libs/core/desktop/desktop.service.ts
+++ libs/core/desktop/desktop.service.ts
@@ -3,8 +3,7 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import { exec } from "child_process"
-import sudoPrompt from "@vscode/sudo-prompt"
+import { execCommand, execCommandWithSudo } from "shared/utils"
 
 enum SerialPortGroup {
   dialout = "dialout",
@@ -26,27 +25,25 @@ export class DesktopService {
     // Set simpler process.title, otherwise, there is an error from sudoPrompt.exec - 'process.title cannot be used as a valid name.'
     process.title = "Mudita Center: assign serial port access"
 
-    return new Promise<void>((resolve, reject) => {
-      sudoPrompt.exec(command, { name: "User Serial Port Access" }, (error) => {
-        if (error === null) {
-          resolve()
-        } else {
-          reject("Could not add user to serial port group")
-        }
-      })
-    })
+    try {
+      await execCommandWithSudo(command, { name: "User Serial Port Access" });
+    } catch (error) {
+      console.error("Could not add user to serial port group");
+    }
   }
 
   private async getUserGroups(): Promise<string[]> {
-    return new Promise<string[]>((resolve, reject) => {
-      exec("groups", (error, stdout, stderr) => {
-        if (error || stderr) {
-          reject(`${error?.name} - ${error?.message} - ${stderr}`)
-        } else {
-          const groups = stdout.trim().split(/\s+/)
-          resolve(groups)
-        }
-      })
-    })
+    try {
+      const result = await execCommand("groups");
+
+      if (typeof result === 'string') {
+        return result.trim().split(/\s+/);
+      } else {
+        throw new Error("No output from command");
+      }
+    } catch (error: unknown) {
+      console.error(error);
+      return [];
+    }
   }
 }
