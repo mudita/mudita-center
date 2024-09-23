62d007fb21daeec8c75295dd29372b228521df2e
robertmudi <robert.rosloniec@mudita.com>
2024-09-23 11:48:31 +0200

    CP-2930 added couple of tests

--- apps/mudita-center-e2e/src/specs/overview/kompakt-backup-getting-initial-info.ts
+++ apps/mudita-center-e2e/src/specs/overview/kompakt-backup-getting-initial-info.ts
@@ -1,7 +1,6 @@
 import { E2EMockClient } from "../../../../../libs/e2e-mock/client/src"
 import { overviewDataWithOneSimCard } from "../../../../../libs/e2e-mock/responses/src"
-import modalBackupPage from "../../page-objects/modal-backup-kompakt.page"
-import HomePage from "../../page-objects/home.page"
+import ModalBackupKompaktPage from "../../page-objects/modal-backup-kompakt.page"
 
 describe("E2E mock sample - overview view", () => {
   before(async () => {
@@ -37,21 +36,147 @@ describe("E2E mock sample - overview view", () => {
     await expect(menuItem).toBeDisplayed()
   })
 
-  it("Verify Overview Page", async () => {
-    const createBackupButton = await modalBackupPage.createBackupButton
+  it("Wait for Overview Page and click Create Backup", async () => {
+    const createBackupButton = await ModalBackupKompaktPage.createBackupButton
     await expect(createBackupButton).toBeDisplayed()
     await expect(createBackupButton).toBeClickable()
     await createBackupButton.click()
+  })
 
+  it("Verify modal in scope of available backup options, verify text and buttons", async () => {
+    const contactList = await ModalBackupKompaktPage.contactList
+    await expect(contactList).toBeDisplayed()
+    const contactListText = await contactList.getProperty("textContent")
+    expect(contactListText).toContain("Contacts list")
+    expect(contactListText).not.toContain("Coming soon!")
+
+    const callLog = await ModalBackupKompaktPage.callLog
+    await expect(callLog).toBeDisplayed()
+    const callLogText = await callLog.getProperty("textContent")
+    expect(callLogText).toContain("Call log")
+    expect(callLogText).not.toContain("Coming soon!")
+
+    const backupModalTitle = ModalBackupKompaktPage.backupModalTitle
+    await expect(backupModalTitle).toBeDisplayed()
+    await expect(backupModalTitle).toHaveText("Create backup")
+
+    const backupModalDescription = ModalBackupKompaktPage.backupModalDescription
+    await expect(backupModalDescription).toBeDisplayed()
+    await expect(backupModalDescription).toHaveText(
+      "All backup data stays on your computer."
+    )
+    //DODAC CANCEL TESTID NA MODALU
+    //const backupModalCancel = ModalBackupKompaktPage.backupModalCancel
+    //await expect(backupModalCancel).toBeClickable()
+
+    const backupModalClose = ModalBackupKompaktPage.backupModalClose
+    await expect(backupModalClose).toBeClickable()
+  })
+
+  it("Click Create backup - verify modal about create password for backup", async () => {
     const createBackupProceedNext =
-      await modalBackupPage.createBackupProceedNext
-    await expect(createBackupProceedNext).toBeDisplayed()
+      await ModalBackupKompaktPage.createBackupProceedNext
     await expect(createBackupProceedNext).toBeClickable()
     await createBackupProceedNext.click()
 
-    await browser.pause(4000)
-    const backupPassword = await modalBackupPage.backupPassword
-    await expect(backupPassword).toBeDisplayed()
-    await expect(backupPassword).toBeClickable()
+    const createBackupPasswordModalTitle =
+      ModalBackupKompaktPage.createBackupPasswordModalTitle
+    await expect(createBackupPasswordModalTitle).toHaveTextContaining(
+      "Create password for backup"
+    )
+
+    const createBackupPasswordOptionalText =
+      ModalBackupKompaktPage.createBackupPasswordOptionalText
+    await expect(createBackupPasswordOptionalText).toHaveText("(optional)")
+
+    const createBackupPasswordModalDescription =
+      ModalBackupKompaktPage.createBackupPasswordModalDescription
+    await expect(createBackupPasswordModalDescription).toHaveTextContaining(
+      "You can protect backup with a new password."
+    )
+
+    const createBackupPasswordModalDescriptionMore =
+      ModalBackupKompaktPage.createBackupPasswordModalDescriptionMore
+    await expect(createBackupPasswordModalDescriptionMore).toHaveText(
+      "* You can't change/recover the password later."
+    )
+
+    const createBackupPasswordPlaceholder =
+      ModalBackupKompaktPage.createBackupPasswordPlaceholder
+    await expect(createBackupPasswordPlaceholder).toBeClickable()
+
+    const createBackupPasswordRepeatPlaceholder =
+      ModalBackupKompaktPage.createBackupPasswordRepeatPlaceholder
+    await expect(createBackupPasswordRepeatPlaceholder).toBeClickable()
+
+    const createBackupPasswordConfirm =
+      ModalBackupKompaktPage.createBackupPasswordConfirm
+    await expect(createBackupPasswordConfirm).not.toBeClickable()
+
+    const createBackupPasswordSkip =
+      ModalBackupKompaktPage.createBackupPasswordSkip
+    await expect(createBackupPasswordSkip).toBeClickable()
+
+    const createBackupPasswordClose =
+      ModalBackupKompaktPage.createBackupPasswordClose
+    await expect(createBackupPasswordClose).toBeClickable()
+  })
+
+  it("Fill password for a backup, unhide it and verify value and design", async () => {
+    const inputPassword = ModalBackupKompaktPage.inputPassword
+    await inputPassword.click()
+    const randomString = Math.random().toString(36).substring(2, 10)
+    await inputPassword.setValue(randomString)
+
+    const checkPassword = await inputPassword.getAttribute("type")
+    await expect(checkPassword).toBe("password")
+
+    const unhidePasswordIcon = ModalBackupKompaktPage.unhidePasswordIcon
+    await unhidePasswordIcon.click()
+
+    //verifying design and value is to check if user can hide password as it was shown
+    const hidePasswordIcon = ModalBackupKompaktPage.hidePasswordIcon
+    await expect(hidePasswordIcon).toBeClickable()
+  })
+
+  it("Fill repeat password for a backup, unhide it and verify value and design, verify (passwords do not match)", async () => {
+    const repeatInputPassword = ModalBackupKompaktPage.repeatInputPassword
+    await repeatInputPassword.click()
+    const randomString2 = Math.random().toString(36).substring(2, 10)
+    await repeatInputPassword.setValue(randomString2)
+
+    const checkPassword = await repeatInputPassword.getAttribute("type")
+    await expect(checkPassword).toBe("password")
+
+    const unhidePasswordIcon = ModalBackupKompaktPage.unhidePasswordIcon
+    await unhidePasswordIcon.click()
+
+    //verifying design and value is to check if user can hide password as it was shown
+    const hidePasswordIcon = ModalBackupKompaktPage.hidePasswordIcon
+    await expect(hidePasswordIcon).toBeClickable()
+
+    const passwordsDoNotMatch = ModalBackupKompaktPage.passwordsDoNotMatch
+    await expect(passwordsDoNotMatch).toHaveText("Passwords do not match")
+  })
+
+  it("Fill repeat password with first filed password and verify if passwords do not match is gone", async () => {
+    const inputPassword = ModalBackupKompaktPage.inputPassword
+    const repeatInputPassword = ModalBackupKompaktPage.repeatInputPassword
+    const randomPassword = Math.random().toString(36).substring(2, 10)
+    await inputPassword.click()
+    await inputPassword.clearValue()
+    await inputPassword.setValue(randomPassword)
+
+    await repeatInputPassword.click()
+    await repeatInputPassword.clearValue()
+    await repeatInputPassword.setValue(randomPassword)
+
+    const passwordsDoNotMatch = ModalBackupKompaktPage.passwordsDoNotMatch
+    await expect(passwordsDoNotMatch).not.toBeDisplayed()
+
+    const createBackupPasswordConfirm =
+      ModalBackupKompaktPage.createBackupPasswordConfirm
+    await expect(createBackupPasswordConfirm).toBeClickable()
+    await createBackupPasswordConfirm.click()
   })
 })
