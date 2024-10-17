190e2a2908cad05943f8f795de00b5cb4c1e9043
Daniel Karski <daniel.karski5q@gmail.com>
2024-10-17 13:44:33 +0200

    [CP-3128] Add removed filter in getUsbDevicesMacOS

--- libs/core/device-manager/services/usb-devices/usb-devices-mac.helper.ts
+++ libs/core/device-manager/services/usb-devices/usb-devices-mac.helper.ts
@@ -5,11 +5,17 @@
 
 import { PortInfo } from "serialport"
 import { MacosUSBPortDeviceParser } from "shared/utils"
+import { ProductID, VendorID } from "Core/device/constants"
 
 export const getUsbDevicesMacOS = async (): Promise<PortInfo | void> => {
   try {
     const devices = await MacosUSBPortDeviceParser.getUSBPortDevices()
-    const device = devices[0]
+    const device = devices.find((device) => {
+      return (
+        device.vendorId?.includes(VendorID.MuditaHarmony) &&
+        device.productId?.includes(ProductID.MuditaHarmonyMsc)
+      )
+    })
 
     if (device !== undefined) {
       return device
