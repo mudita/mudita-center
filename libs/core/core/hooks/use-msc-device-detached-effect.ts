97302e861bbb7eb8aa60f3a1c09a86734fe427ce
MateuszMudita <mateusz.bolewski@mudita.com>
2024-10-29 12:59:49 +0100

    Lint update

--- libs/core/core/hooks/use-msc-device-detached-effect.ts
+++ libs/core/core/hooks/use-msc-device-detached-effect.ts
@@ -14,7 +14,6 @@ import {
 import {
   abortMscFlashing,
   FlashingProcessState,
-  selectFlashingProcessState,
   selectIsFlashingInActivePhases,
   selectIsFlashingInState,
   setFlashingProcessState,
