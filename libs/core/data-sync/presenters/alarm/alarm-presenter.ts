cb7757ab94f15a3d1e2a3c0fd7e910bc3447b302
Daniel Karski <daniel.karski5q@gmail.com>
2024-11-05 10:12:06 +0100

    [CP-3250] serializeToObject method for AlarmPresenter implemented

--- libs/core/data-sync/presenters/alarm/alarm-presenter.ts
+++ libs/core/data-sync/presenters/alarm/alarm-presenter.ts
@@ -5,6 +5,7 @@
 
 import { AlarmEntity, EventInput, AlarmObject } from "Core/data-sync/types"
 import { BasePresenter } from "Core/data-sync/presenters/base-presenter"
+import { UnifiedAlarmRepeatDaysType } from "device/models"
 
 export class AlarmPresenter extends BasePresenter {
   public serializeToObject(input: EventInput): AlarmObject[] {
@@ -17,7 +18,48 @@ export class AlarmPresenter extends BasePresenter {
       input.alarms.columns
     )
 
-    // @ts-ignore
-    return alarms.map((alarm) => alarm)
+    return alarms.map((alarm) => {
+      return {
+        repeatDays: this.parseRRule(alarm.rrule),
+        isEnabled: alarm.enabled === "1",
+        hour: this.parseNumber(alarm.hour),
+        minute: this.parseNumber(alarm.minute),
+        snoozeTime: this.parseNumber(alarm.snooze_duration),
+      }
+    })
+  }
+
+  private parseNumber(value: string | undefined | null): number {
+    return Number(value) || 0
+  }
+
+  private parseRRule(rrule: string): UnifiedAlarmRepeatDaysType {
+    const daysMap: Record<string, 1 | 2 | 3 | 4 | 5 | 6 | 7> = {
+      MO: 1,
+      TU: 2,
+      WE: 3,
+      TH: 4,
+      FR: 5,
+      SA: 6,
+      SU: 7,
+    }
+    const repeatDays: UnifiedAlarmRepeatDaysType = []
+
+    if (rrule?.includes("BYDAY")) {
+      const byDayPart = rrule
+        .split(";")
+        .find((part) => part.startsWith("BYDAY"))
+      if (byDayPart) {
+        const days = byDayPart.replace("BYDAY=", "").split(",")
+        days.forEach((day) => {
+          const mappedDay = daysMap[day]
+          if (mappedDay) {
+            repeatDays.push(mappedDay)
+          }
+        })
+      }
+    }
+
+    return repeatDays
   }
 }
