da03c467e4cb0a5eb34d7f220f414e309059dd1c
Daniel Karski <daniel.karski5q@gmail.com>
2024-11-05 19:39:18 +0100

    [CP-3248] Logic Reduction and Error Logging for Kompakt Backup (#2160)

--- libs/generic-view/store/src/lib/hooks/use-app-events-listeners.ts
+++ libs/generic-view/store/src/lib/hooks/use-app-events-listeners.ts
@@ -4,24 +4,30 @@
  */
 
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { answerMain, AppEvents } from "shared/utils"
 import { Dispatch } from "Core/__deprecated__/renderer/store"
 import { refreshBackupList } from "../backup/refresh-backup-list.action"
+import { selectActiveApiDeviceId } from "../selectors/select-active-api-device-id"
 
 export const useAppEventsListeners = () => {
   const dispatch = useDispatch<Dispatch>()
+  const activeApiDeviceId = useSelector(selectActiveApiDeviceId)
 
   useEffect(() => {
+    if (!activeApiDeviceId) {
+      return
+    }
+
     const unregisterWindowFocusedListener = answerMain(
       AppEvents.WindowFocused,
       () => {
-        dispatch(refreshBackupList())
+        dispatch(refreshBackupList({ disableErrorLog: true }))
       }
     )
 
     return () => {
       unregisterWindowFocusedListener()
     }
-  }, [dispatch])
+  }, [dispatch, activeApiDeviceId])
 }
