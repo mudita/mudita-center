ba252ef345be7e31e802ec11db9a0a4fb26274e4
Katarzyna Stegienko <katarzyna.stegienko@mudita.com>
2024-11-18 13:46:26 +0100

    test

--- libs/e2e-mock/responses/src/lib/feature-configuration-responses.ts
+++ libs/e2e-mock/responses/src/lib/feature-configuration-responses.ts
@@ -627,47 +627,6 @@ export const featureConfigurationContacts = {
   columnEmpty: { component: "table.cell", config: { width: "100%" } },
 }
 
-// export const data = {
-//   main: {
-//     screenTitle: "Contacts",
-//     component: "block-plain",
-//     childrenKeys: ["fullScreenWrapper"],
-//     layout: {
-//       flexLayout: {
-//         direction: "column",
-//         alignItems: "center",
-//         justifyContent: "center",
-//       },
-//     },
-//   },
-//   fullScreenWrapper: {
-//     component: "block-plain",
-//     childrenKeys: ["title", "detailText", "importContactsButton"],
-//     layout: {
-//       flexLayout: {
-//         direction: "column",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         rowGap: "8px",
-//         columnGap: "8px",
-//       },
-//     },
-//   },
-//   title: {
-//     component: "h3-component",
-//     config: { text: "Import your contacts" },
-//   },
-//   detailText: {
-//     component: "p1-component",
-//     config: { text: "Import all your contacts from a singles ource." },
-//   },
-//   importContactsButton: {
-//     component: "mc-import-contacts-button",
-//     config: { text: "import contacts" },
-//     layout: { margin: "16px" },
-//   },
-// }
-
 export const featureConfigurationOverview = {
   title: "Overview",
   summary: {
