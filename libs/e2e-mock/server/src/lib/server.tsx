712471a0a66e19ff4f1dbaec58b44cd055672497
Katarzyna Stegienko <katarzyna.stegienko@mudita.com>
2024-11-19 11:29:19 +0100

    fix server

--- libs/e2e-mock/server/src/lib/server.tsx
+++ libs/e2e-mock/server/src/lib/server.tsx
@@ -75,5 +75,4 @@ export function stopServer() {
   ipc.server.stop()
 }
 
-// export const mockServiceEnabled = process.env.MOCK_SERVICE_ENABLED === "1"
-export const mockServiceEnabled = true
+export const mockServiceEnabled = process.env.MOCK_SERVICE_ENABLED === "1"
