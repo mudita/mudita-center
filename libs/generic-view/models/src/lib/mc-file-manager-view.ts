f99bb25bcc193314aa24e27015e1f50b51be85ad
Daniel Karski <daniel.karski5q@gmail.com>
2024-12-09 13:10:15 +0100

    [CP-3310] Implement Refresh Functionality for FilesManager Data (by feature key) (#2201)

--- libs/generic-view/models/src/lib/mc-file-manager-view.ts
+++ libs/generic-view/models/src/lib/mc-file-manager-view.ts
@@ -5,7 +5,29 @@
 
 import { z } from "zod"
 
-const dataValidator = z.undefined()
+const storageCategoryValidator = z.object({
+  spaceUsedBytes: z.number(),
+  spaceUsedString: z.string(),
+  storageCategory: z.string(),
+})
+
+export const mcFileManagerData = z.object({
+  storageInformation: z.array(
+    z.object({
+      storageType: z.string(),
+      totalSpaceBytes: z.number(),
+      usedSpaceBytes: z.number(),
+      totalSpaceString: z.string(),
+      usedSpaceString: z.string(),
+      categoriesSpaceInformation: z.record(
+        z.string(),
+        storageCategoryValidator
+      ),
+    })
+  ),
+})
+
+export type McFileManagerData = z.infer<typeof mcFileManagerData>
 
 const configValidator = z.object({
   entityTypes: z.array(z.string()).min(1),
@@ -15,6 +37,6 @@ export type McFileManagerView = z.infer<typeof configValidator>
 
 export const mcFileManagerView = {
   key: "mc-file-manager-view",
-  dataValidator,
+  dataValidator: mcFileManagerData,
   configValidator,
 } as const
--- libs/generic-view/store/src/lib/features/transform-generic-components.ts
+++ libs/generic-view/store/src/lib/features/transform-generic-components.ts
@@ -4,12 +4,13 @@
  */
 
 import { View } from "generic-view/utils"
-import { generated } from "../../../../ui/src/lib/generated"
+import { generatedViews } from "../../../../ui/src/lib/generated"
 
 export const transformGenericComponents = (view: View): View => {
   const fullView = { ...view }
   for (const [key, { component, config, layout }] of Object.entries(fullView)) {
-    const transformComponent = generated[component as keyof typeof generated]
+    const transformComponent =
+      generatedViews[component as keyof typeof generatedViews]
     if (transformComponent) {
       Object.assign(
         fullView,
