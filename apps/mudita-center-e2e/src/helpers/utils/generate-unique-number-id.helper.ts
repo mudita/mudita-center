04cdd765a07ce63fb610087e519c56649b4c74be
Daniel Karski <daniel.karski5q@gmail.com>
2024-12-20 10:04:55 +0100

    [CP-3206][API Device][Kompakt] Contacts - mock helpers (#2250)
    Co-authored-by: Katarzyna Stegienko <katarzyna.stegienko@mudita.com>

--- apps/mudita-center-e2e/src/helpers/utils/generate-unique-number-id.helper.ts
+++ apps/mudita-center-e2e/src/helpers/utils/generate-unique-number-id.helper.ts
@@ -3,6 +3,5 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-export * from "./mock-entity-download-process.helper"
-export * from "./sleep.helper"
-export * from "./testid.helper"
+export const generateUniqueNumber = () =>
+  Date.now() + Math.floor(Math.random() * 1000)
