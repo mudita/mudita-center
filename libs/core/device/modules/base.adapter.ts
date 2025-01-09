66977831bc8cd5b00406961b458a59cff4a1a046
Łukasz Kowalczyk <37898730+lkowalczyk87@users.noreply.github.com>
2025-01-09 19:14:55 +0100

    Cp add device tests (#2264)

--- libs/core/device/modules/base.adapter.ts
+++ libs/core/device/modules/base.adapter.ts
@@ -34,6 +34,7 @@ export abstract class BaseAdapter<
     return new Promise((resolve) => {
       const serialPort = new SerialPort(this.path, (error) => {
         if (error) {
+          console.error(error)
           resolve(
             Result.failed(
               new AppError(DeviceError.Initialization, error.message)
