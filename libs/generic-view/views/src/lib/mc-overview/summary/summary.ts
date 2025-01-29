4d777c444d3cc380d90c5f2957d04c6f8ea2ba15
Daniel Karski <daniel.karski5q@gmail.com>
2025-01-29 13:37:25 +0100

    [CP-3432] AboutDivider width set to 280px as a fixed value

--- libs/generic-view/views/src/lib/mc-overview/summary/summary.ts
+++ libs/generic-view/views/src/lib/mc-overview/summary/summary.ts
@@ -71,6 +71,7 @@ export const generateMcOverviewSummaryLayout: ViewGenerator<
             component: "divider",
             layout: {
               margin: "0 -24px",
+              width: "280px",
             },
           },
           [SummaryKeys.About]: {
