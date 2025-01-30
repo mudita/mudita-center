8980a293f7ca8aa61efc0987918756be4d24c8d6
MateuszMudita <mateusz.bolewski@mudita.com>
2025-01-30 11:52:53 +0100

    Removed testing for accordion

--- libs/generic-view/ui/src/lib/generated/mc-contacts-view/delete-modal.ts
+++ libs/generic-view/ui/src/lib/generated/mc-contacts-view/delete-modal.ts
@@ -26,7 +26,6 @@ const generateDeleteModal = ({
         `${id}DeleteModalIcon`,
         `${id}DeleteModalTitle`,
         `${id}DeleteModalContent`,
-        `${id}DeleteModalAccordion`,
         `${id}DeleteModalButtons`,
       ],
     },
@@ -44,43 +43,6 @@ const generateDeleteModal = ({
       component: "typography.p1",
       config: {
         text: "This can't be undone so please make a copy of any important information first.",
-      },
-    },
-    [`${id}DeleteModalAccordion`]: {
-      component: "accordion",
-      config: {
-        collapsedButtonText: "+ More detail",
-        expandedButtonText: "- Less detail",
-      },
-      childrenKeys: [
-        `${id}DeleteModalTestAccordionData1`,
-        `${id}DeleteModalTestAccordionData2`,
-        `${id}DeleteModalTestAccordionData3`,
-        `${id}DeleteModalTestAccordionData4`,
-      ],
-    },
-    [`${id}DeleteModalTestAccordionData1`]: {
-      component: "typography.h3",
-      config: {
-        text: "This is test component nr 1",
-      },
-    },
-    [`${id}DeleteModalTestAccordionData2`]: {
-      component: "typography.h4",
-      config: {
-        text: "This is test component nr 2",
-      },
-    },
-    [`${id}DeleteModalTestAccordionData3`]: {
-      component: "typography.p1",
-      config: {
-        text: "This is test component nr 3",
-      },
-    },
-    [`${id}DeleteModalTestAccordionData4`]: {
-      component: "typography.p2",
-      config: {
-        text: "This is test component nr 4",
       },
     },
     [`${id}DeleteModalButtons`]: {
