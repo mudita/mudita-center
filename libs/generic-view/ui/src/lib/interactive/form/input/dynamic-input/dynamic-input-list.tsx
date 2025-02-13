5705309791b570d8e86eb42440e3e9756c1c7dea
MateuszMudita <mateusz.bolewski@mudita.com>
2025-02-13 08:26:19 +0100

    Finished dynamic input component

--- libs/generic-view/ui/src/lib/interactive/form/input/dynamic-input/dynamic-input-list.tsx
+++ libs/generic-view/ui/src/lib/interactive/form/input/dynamic-input/dynamic-input-list.tsx
@@ -8,25 +8,52 @@ import {
   FormDynamicInputData,
 } from "generic-view/models"
 import { APIFC } from "generic-view/utils"
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { DynamicInputRow } from "./dynamic-input-row"
+import { useFormContext } from "react-hook-form"
 
 export const DynamicInputList: APIFC<
   FormDynamicInputData,
   FormDynamicInputConfig
 > = ({ data, config }) => {
+  const { setValue, watch } = useFormContext()
+  const formValues = watch()
+
+  useEffect(() => {
+    if (data?.values) {
+      data.values.forEach((value, index) => {
+        setValue(`${config.name}-${index}-value`, value.value)
+      })
+    }
+  }, [data, setValue, config.name])
+
+  const handleSetDefault = (index: number) => {
+    // data?.values.forEach((_, i) => {
+    //   setValue(`${config.name}-${i}-isDefault`, i === index)
+    // })
+
+    Object.keys(formValues).forEach((key) => {
+      if (key.endsWith("-isDefault")) {
+        setValue(key, false)
+      }
+    })
+
+    setValue(`${config.name}-${index}-isDefault`, true)
+  }
+
   return (
     <ListWrapper>
       <Label>{config.label}</Label>
       {data?.values.map((value, index) => {
         return (
           <DynamicInputRow
+            key={index}
             name={`${config.name}-${index}`}
             options={config.options}
             type={config.type}
             data={value}
-            key={index}
+            onSetDefault={() => handleSetDefault(index)}
           />
         )
       })}
