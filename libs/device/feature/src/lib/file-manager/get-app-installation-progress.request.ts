5862da23f84caf6467337a9ee612358773ea3b49
MateuszMudita <mateusz.bolewski@mudita.com>
2025-02-14 11:14:30 +0100

    Actions and Requests

---
+++ libs/device/feature/src/lib/file-manager/get-app-installation-progress.request.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { ResultObject } from "Core/core/builder"
+import { DeviceId } from "Core/device/constants/device-id"
+import {
+  AppInstallationServiceEvents,
+  GetAppInstallationProgress,
+  StartAppInstallation,
+} from "device/models"
+import { ipcRenderer } from "electron-better-ipc"
+
+export const getAppInstallationProgressRequest = (
+  installationId: number,
+  deviceId: DeviceId
+): Promise<ResultObject<GetAppInstallationProgress>> => {
+  return ipcRenderer.callMain(
+    AppInstallationServiceEvents.GetAppInstallationProgress,
+    {
+      installationId,
+      deviceId,
+    }
+  )
+}
