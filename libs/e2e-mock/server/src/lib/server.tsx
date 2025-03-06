4be9d164d4f305db3023efae90cba5f48d5ecc07
mkurczewski <michal@kurczewski.dev>
2025-03-06 11:48:28 +0100

    Reverted -> Force enabled mocks

--- libs/e2e-mock/server/src/lib/server.tsx
+++ libs/e2e-mock/server/src/lib/server.tsx
@@ -75,4 +75,4 @@ export function stopServer() {
   ipc.server.stop()
 }
 
-export const mockServiceEnabled = true // process.env.MOCK_SERVICE_ENABLED === "1"
+export const mockServiceEnabled = process.env.MOCK_SERVICE_ENABLED === "1"
