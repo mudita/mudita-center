2fad8e5a2e61bb6854dd030e9f4c7e4ca55c47f6
MateuszMudita <165778944+MateuszMudita@users.noreply.github.com>
2025-03-14 09:38:35 +0100

    [CP-3054] Mock files dialog (#2360)

--- libs/system-utils/feature/src/lib/system-utils.module.ts
+++ libs/system-utils/feature/src/lib/system-utils.module.ts
@@ -3,16 +3,27 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
+import { MockFileDialog } from "e2e-mock-server"
 import { Directory } from "./directory/directory.service"
 import { FileDialog } from "./file-dialog/file-dialog.service"
 import { FileStats } from "./file-stats/file-stats.service"
 
 export class SystemUtilsModule {
-  public directory = new Directory()
-  public fileDialog = new FileDialog()
-  public fileStats = new FileStats()
+  public fileDialog: FileDialog
 
-  constructor() {}
+  constructor(
+    public mockServiceEnabled: boolean,
+    public mockFileDialog: MockFileDialog
+  ) {
+    this.fileDialog = new FileDialog(
+      this.mockFileDialog,
+      this.mockServiceEnabled
+    )
+  }
+
+  public directory = new Directory()
+
+  public fileStats = new FileStats()
 
   public getServices() {
     return [this.directory, this.fileDialog, this.fileStats]
