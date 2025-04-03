0b525e3e6303cb0c66f0a8954b7b3d0a7fe27058
Daniel Karski <daniel.karski5q@gmail.com>
2025-04-03 07:44:13 +0200

    [CP-3527][M1] Implemen Method to Upload File to First Available Device on Phone Storage (Node.js) (#2379)
    Co-authored-by: slawomir-werner <slawomir.werner@mudita.com>

--- libs/app-mtp/src/lib/node-mtp/node-mtp-device-manager.ts
+++ libs/app-mtp/src/lib/node-mtp/node-mtp-device-manager.ts
@@ -3,11 +3,61 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
+import { usb, UsbDetect } from "./usb"
 import { NodeMtpDevice } from "./node-mtp-device"
+import { MtpDevice, MTPError } from "../app-mtp.interface"
+import { AppError } from "../../../../core/core/errors/app-error"
 
 export class NodeMtpDeviceManager {
-  getDevice(): NodeMtpDevice {
-    // mock implementation
-    return new NodeMtpDevice()
+  private nodeMtpDevices: Map<string, NodeMtpDevice> = new Map()
+
+  constructor() {
+    this.startDetachedDeviceWatcher()
+  }
+
+  async getDevices(): Promise<MtpDevice[]> {
+    const devices = await usb.getDevices()
+
+    return devices.map((device) => ({
+      id: device.serialNumber,
+      name: device.productName,
+    }))
+  }
+
+  async getNodeMtpDevice({
+    id,
+  }: {
+    id: MtpDevice["id"]
+  }): Promise<NodeMtpDevice> {
+    if (this.nodeMtpDevices.has(id)) {
+      return this.nodeMtpDevices.get(id)!
+    }
+
+    const devices = await usb.getDevices()
+    const device = devices.find(({ serialNumber }) => serialNumber === id)
+
+    if (!device) {
+      throw new AppError(MTPError.MTP_DEVICE_NOT_FOUND)
+    }
+
+    const nodeMtpDevice = new NodeMtpDevice(device)
+
+    await nodeMtpDevice.initialize()
+    this.nodeMtpDevices.set(id, nodeMtpDevice)
+
+    return nodeMtpDevice
+  }
+
+  private startDetachedDeviceWatcher() {
+    UsbDetect.ondetach = async () => {
+      const attachedDevices = await this.getDevices()
+      const attachedDeviceIds = new Set(attachedDevices.map(({ id }) => id))
+
+      this.nodeMtpDevices.forEach((_, id) => {
+        if (!attachedDeviceIds.has(id)) {
+          this.nodeMtpDevices.delete(id)
+        }
+      })
+    }
   }
 }
---
+++ libs/app-mtp/src/lib/node-mtp/utils/with-timeout.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+/**
+ * Helper function to add a timeout to any async operation.
+ * @param {Promise} asyncFunction - The async function that returns a Promise.
+ * @param {number} timeoutMs - The timeout in milliseconds.
+ * @param {string} error - The custom error message to be used if timeout occurs.
+ * @returns {Promise} - Resolves with the result of the async function or rejects with the timeout error.
+ */
+
+export const withTimeout = <T>(
+  asyncFunction: Promise<T>,
+  timeoutMs: number = 5000,
+  error: Error = new Error("Timeout exceeded")
+): Promise<T> => {
+  const timeout = new Promise<never>((_, reject) =>
+    setTimeout(() => reject(error), timeoutMs)
+  )
+
+  return Promise.race([asyncFunction, timeout])
+}
