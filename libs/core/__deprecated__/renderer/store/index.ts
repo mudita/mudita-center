520c7201bfe48ce49ec8e09d7b211fa2d2bb4227
MateuszMudita <mateusz.bolewski@mudita.com>
2025-04-03 08:36:59 +0200

    server.tsx change

--- libs/core/__deprecated__/renderer/store/index.ts
+++ libs/core/__deprecated__/renderer/store/index.ts
@@ -20,11 +20,9 @@ import { combinedReducers, reducers } from "./reducers"
 
 const middlewares: Middleware[] = [thunk]
 
-// if (process.env.DEV_REDUX_LOGGER_ENABLED !== "0") {
-//   middlewares.push(logger)
-// }
-
-middlewares.push(logger)
+if (process.env.DEV_REDUX_LOGGER_ENABLED !== "0") {
+  middlewares.push(logger)
+}
 
 const config: InitConfig<RootModel> = {
   models,
