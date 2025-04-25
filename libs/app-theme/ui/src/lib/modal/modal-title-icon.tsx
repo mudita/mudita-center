a7e04f4ea9be3b5d5acdef10269042e24f0e73bf
mkurczewski <michal@kurczewski.dev>
2025-04-25 13:22:36 +0200

    Removed spin animation from modal-title-icon.tsx

--- libs/app-theme/ui/src/lib/modal/modal-title-icon.tsx
+++ libs/app-theme/ui/src/lib/modal/modal-title-icon.tsx
@@ -4,7 +4,7 @@
  */
 
 import React, { FunctionComponent } from "react"
-import styled, { css, keyframes } from "styled-components"
+import styled from "styled-components"
 import { IconType } from "app-theme/models"
 import { Icon } from "../icon/icon"
 
@@ -14,10 +14,9 @@ interface Props {
 
 export const ModalTitleIcon: FunctionComponent<Props> = ({ type, ...rest }) => {
   if (!type) return null
-  const spin = type === IconType.Spinner
   return (
     <TitleIconWrapper {...rest}>
-      <TitleIcon type={type} $spin={spin} />
+      <TitleIcon type={type} />
     </TitleIconWrapper>
   )
 }
@@ -36,22 +35,8 @@ export const TitleIconWrapper = styled.div`
   align-self: center;
 `
 
-const TitleIcon = styled(Icon)<{ $spin?: boolean }>`
+const TitleIcon = styled(Icon)`
   height: 4.08rem;
   width: 4.08rem;
-  ${({ $spin }) => $spin && spinAnimation};
   color: ${({ theme }) => theme.app.color.black};
-`
-
-const spin = keyframes({
-  "0%": {
-    transform: "rotate(0deg)",
-  },
-  "100%": {
-    transform: "rotate(360deg)",
-  },
-})
-
-const spinAnimation = css`
-  animation: ${spin} 1s steps(12) infinite;
 `
