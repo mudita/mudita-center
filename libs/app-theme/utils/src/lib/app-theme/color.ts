d5d745d83db014dfd43fba3ccafbd5a001bda8b3
mkurczewski <michal@kurczewski.dev>
2025-04-25 19:15:17 +0200

    Added stories for theme colors

--- libs/app-theme/utils/src/lib/app-theme/color.ts
+++ libs/app-theme/utils/src/lib/app-theme/color.ts
@@ -5,16 +5,6 @@
 
 export const color = {
   white: "#FFFFFF",
-  black: "#000000",
-  blackAlpha: {
-    light: "rgba(0, 0, 0, 0.08)",
-  },
-  blue1: "#40749A",
-  blue2: "#6D9BBC",
-  blue3: "#AEBEC9",
-  blue4: "#E3F3FF",
-  blue5: "#F2F7FA",
-  blue6: "#6D9BBC",
   grey1: "#3B3F42",
   grey2: "#6A6A6A",
   grey3: "#A5A5A5",
@@ -23,7 +13,17 @@ export const color = {
   grey6: "#FBFBFB",
   grey7: "#EDEDED",
   grey8: "#E7E7E9",
+  blue1: "#40749A",
+  blue2: "#6D9BBC",
+  blue3: "#AEBEC9",
+  blue4: "#E3F3FF",
+  blue5: "#F2F7FA",
+  blue6: "#6D9BBC",
   red: "#E96A6A",
   green: "#DFEFDE",
   orange: "#DD802A",
+  black: "#000000",
+  blackAlpha: {
+    light: "rgba(0, 0, 0, 0.08)",
+  },
 } as const
