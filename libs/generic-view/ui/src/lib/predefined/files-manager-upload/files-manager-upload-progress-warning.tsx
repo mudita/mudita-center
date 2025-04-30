20e477d59651b3ce43303f843c7c438677e04232
Daniel Karski <daniel.karski5q@gmail.com>
2025-04-30 10:42:55 +0200

    [CP-3529] Initialized `FilesManagerUploadProgressWarning` without tooltip info

---
+++ libs/generic-view/ui/src/lib/predefined/files-manager-upload/files-manager-upload-progress-warning.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import React, { FunctionComponent } from "react"
+import styled from "styled-components"
+import { Typography } from "../../typography/typography"
+import { typographyConfig } from "../../typography/typography.config"
+
+export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
+  return (
+    <Wrapper>
+      <Typography.P3>
+        Some apps slow down transfer, it’s better <br></br> to close them if you
+        have them open.
+      </Typography.P3>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  background: ${({ theme }) => theme.color.grey5};
+  padding: 12px;
+  margin: -10px 12px 0 12px;
+
+  p {
+    color: ${({ theme }) => theme.color.black};
+    font-size: ${typographyConfig["typography.p3"].fontSize};
+    line-height: ${typographyConfig["typography.p3"].fontHeight};
+    font-weight: ${typographyConfig["typography.p3"].fontWeight};
+    letter-spacing: ${typographyConfig["typography.p3"].letterSpacing};
+  }
+`
