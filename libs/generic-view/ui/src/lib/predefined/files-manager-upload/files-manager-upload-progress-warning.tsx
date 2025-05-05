80264b58d2fbebf67ab04da94f0a539a2c2947dc
Daniel Karski <daniel.karski5q@gmail.com>
2025-05-05 15:56:16 +0200

    [CP-3529] Added `InterferingAppsTooltip` for `ParagraphIcon`

--- libs/generic-view/ui/src/lib/predefined/files-manager-upload/files-manager-upload-progress-warning.tsx
+++ libs/generic-view/ui/src/lib/predefined/files-manager-upload/files-manager-upload-progress-warning.tsx
@@ -4,10 +4,12 @@
  */
 
 import React, { FunctionComponent } from "react"
+import * as process from "process"
 import styled from "styled-components"
 import { IconType } from "generic-view/utils"
 import { Typography } from "../../typography/typography"
 import { Icon } from "../../icon/icon"
+import Tooltip from "../../interactive/tooltip/tooltip"
 
 export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
   return (
@@ -17,7 +19,8 @@ export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
         config={{ color: "black" }}
       >
         Some apps slow down transfer, it’s better <TextAlignPlaceholder />{" "}
-        <br /> to close them if you have them open. <ParagraphIcon />
+        <br /> to close them if you have them open.{" "}
+        <InterferingAppsTooltipIcon />
       </Typography.P3>
     </Wrapper>
   )
@@ -49,4 +52,77 @@ const ParagraphIcon = styled(Icon).attrs({
 const TextAlignPlaceholder = styled(ParagraphIcon)`
   opacity: 0;
   user-select: none;
+`
+
+const WindowsInterferingApps: string[] = [
+  "Slack",
+  "Google Chrome",
+  "Google Drive",
+  "Dropbox",
+  "OneDrive",
+  "WhatsApp",
+  "Lightroom",
+]
+
+const MacInterferingApps: string[] = [
+  "Android File Transfer",
+  "Google Chrome",
+  "Google Drive",
+  "PhotoSync",
+  "Preview",
+  "Dropbox",
+  "Photos",
+  "Image Capture",
+  "OneDrive",
+  "WhatsApp",
+  "Slack",
+  "Lightroom",
+]
+const interferingApps =
+  process.platform === "win32" ? WindowsInterferingApps : MacInterferingApps
+
+const InterferingAppsTooltipIcon: FunctionComponent = () => {
+  return (
+    <Tooltip
+      config={{
+        placement: "bottom-right",
+        strategy: "element-oriented",
+        offset: { x: 14, y: 2 },
+      }}
+    >
+      <Tooltip.Anchor>
+        <ParagraphIcon />
+      </Tooltip.Anchor>
+      <Tooltip.Content>
+        <TooltipContentWrapper>
+          {interferingApps.map((app) => (
+            <TooltipContentItem key={app}>
+              <TooltipContentItemTextMarker>▪</TooltipContentItemTextMarker>
+              <TooltipContentItemText>{app}</TooltipContentItemText>
+            </TooltipContentItem>
+          ))}
+        </TooltipContentWrapper>
+      </Tooltip.Content>
+    </Tooltip>
+  )
+}
+
+const TooltipContentWrapper = styled.div`
+  padding: 0.6rem 0;
+`
+
+const TooltipContentItem = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const TooltipContentItemText = styled(Typography.P5).attrs({
+  className: "modal-content-original-paragraph",
+  config: {
+    color: "grey1",
+  },
+})``
+
+const TooltipContentItemTextMarker = styled(TooltipContentItemText)`
+  margin: 0 0.6rem;
 `
