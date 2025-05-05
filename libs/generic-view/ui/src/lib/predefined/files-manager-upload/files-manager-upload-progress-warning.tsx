c0b9f48a02f352674e25e6bcf6f3ab253dd5d900
Daniel Karski <daniel.karski5q@gmail.com>
2025-05-05 13:57:50 +0200

    [CP-3529] Added `ParagraphIcon` with align placeholder to FilesManagerUploadProgressWarning

--- libs/generic-view/ui/src/lib/predefined/files-manager-upload/files-manager-upload-progress-warning.tsx
+++ libs/generic-view/ui/src/lib/predefined/files-manager-upload/files-manager-upload-progress-warning.tsx
@@ -5,7 +5,9 @@
 
 import React, { FunctionComponent } from "react"
 import styled from "styled-components"
+import { IconType } from "generic-view/utils"
 import { Typography } from "../../typography/typography"
+import { Icon } from "../../icon/icon"
 
 export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
   return (
@@ -14,8 +16,8 @@ export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
         className="modal-content-original-paragraph"
         config={{ color: "black" }}
       >
-        Some apps slow down transfer, it’s better <br /> to close them if you
-        have them open.
+        Some apps slow down transfer, it’s better <TextAlignPlaceholder />{" "}
+        <br /> to close them if you have them open. <ParagraphIcon />
       </Typography.P3>
     </Wrapper>
   )
@@ -23,11 +25,28 @@ export const FilesManagerUploadProgressWarning: FunctionComponent = () => {
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.color.grey5};
-  padding: 1.2rem;
+  padding: 1.2rem 0 1.2rem 1.2rem;
   margin: -1rem 1.2rem 0 1.2rem;
 
   p {
     text-align: center;
     white-space: pre-line;
   }
+`
+
+const ParagraphIcon = styled(Icon).attrs({
+  config: {
+    type: IconType.Information2,
+    size: "tiny",
+  },
+})`
+  display: inline-block;
+  position: relative;
+  user-select: none;
+  top: 0.3rem;
+`
+
+const TextAlignPlaceholder = styled(ParagraphIcon)`
+  opacity: 0;
+  user-select: none;
 `
