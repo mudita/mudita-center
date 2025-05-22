5cea798f312207496a712d10a84e1fbe860676d8
slawomir-werner <slawomir.werner@mudita.com>
2025-05-22 08:23:58 +0200

    Fixed entities tests

--- libs/api-devices-testing/src/lib/entities.spec.tsx
+++ libs/api-devices-testing/src/lib/entities.spec.tsx
@@ -6,13 +6,21 @@
 import { DeviceProtocol } from "device-protocol/feature"
 import { setKompaktConnection } from "./helpers/set-connection"
 import { setActiveDevice } from "./helpers/protocol-validator"
-import { APIConfigService, APIEntitiesService, ServiceBridge } from "device/feature"
-import { ApiConfig } from "device/models"
+import {
+  APIConfigService,
+  APIEntitiesService,
+  ServiceBridge,
+} from "device/feature"
+import { ApiConfig, EntitiesFileData, EntitiesJsonData } from "device/models"
+import { ResponseStatus } from "Core/device"
 
 jest.mock("shared/utils", () => {
-  return { 
-    callRenderer: () => {}, 
-    delay: () => { return new Promise((resolve) => setTimeout(resolve, 500))} }
+  return {
+    callRenderer: () => {},
+    delay: () => {
+      return new Promise((resolve) => setTimeout(resolve, 500))
+    },
+  }
 })
 jest.mock("Core/device-manager/services/usb-devices/usb-devices.helper", () => {
   return { getUsbDevices: () => {} }
@@ -29,7 +37,7 @@ describe("Entities configuration, metadata and data", () => {
   let deviceProtocol: DeviceProtocol
   let entityTypes: string[]
 
-  async function fetchSupportedEntities(){
+  async function fetchSupportedEntities() {
     const apiConfigService = new APIConfigService(deviceProtocol)
     const result = await apiConfigService.getAPIConfig()
     const apiConfig = result.data as ApiConfig
@@ -37,13 +45,11 @@ describe("Entities configuration, metadata and data", () => {
     expect(entityTypes.length).toBeGreaterThan(0)
   }
 
-
-
   beforeAll(async () => {
     deviceProtocol = setActiveDevice(await setKompaktConnection())
     await fetchSupportedEntities()
     await deviceProtocol.activeDevice?.disconnect()
-  },10000)
+  }, 10000)
 
   beforeEach(async () => {
     deviceProtocol = setActiveDevice(await setKompaktConnection())
@@ -56,12 +62,16 @@ describe("Entities configuration, metadata and data", () => {
   it("should return successful response for entity configuration", async () => {
     expect(entityTypes).toBeDefined()
     expect(entityTypes.length).toBeGreaterThan(0)
-  
+
     for (const entityType of entityTypes) {
-     
-      const service = new APIEntitiesService(deviceProtocol, new ServiceBridge())
-      const result = await service.getEntitiesConfiguration({ entitiesType: entityType })
-  
+      const service = new APIEntitiesService(
+        deviceProtocol,
+        new ServiceBridge()
+      )
+      const result = await service.getEntitiesConfiguration({
+        entitiesType: entityType,
+      })
+
       expect(result).toBeDefined()
       expect(result.ok).toBeTruthy()
     }
@@ -70,27 +80,60 @@ describe("Entities configuration, metadata and data", () => {
   it("should return successful response for entity metadata", async () => {
     expect(entityTypes).toBeDefined()
     expect(entityTypes.length).toBeGreaterThan(0)
-  
+
     for (const entityType of entityTypes) {
-      const service = new APIEntitiesService(deviceProtocol, new ServiceBridge())
-      const result = await service.getEntitiesMetadata({ entitiesType: entityType })
-  
+      const service = new APIEntitiesService(
+        deviceProtocol,
+        new ServiceBridge()
+      )
+      const result = await service.getEntitiesMetadata({
+        entitiesType: entityType,
+      })
+
       expect(result).toBeDefined()
       expect(result.ok).toBeTruthy()
     }
-  })
+  }, 10_000)
 
   it("should return successful response for entity data", async () => {
     expect(entityTypes).toBeDefined()
     expect(entityTypes.length).toBeGreaterThan(0)
-  
+
     for (const entityType of entityTypes) {
-      const service = new APIEntitiesService(deviceProtocol, new ServiceBridge())
-      const result = await service.getEntitiesData({ entitiesType: entityType, responseType: "file" })
-      
-      expect(result).toBeDefined()
-      expect(result.ok).toBeTruthy()
+      let progress = 0
+      let status = 0
+
+      const service = new APIEntitiesService(
+        deviceProtocol,
+        new ServiceBridge()
+      )
+      const createEntitiesResult = await service.getEntitiesData({
+        entitiesType: entityType,
+        responseType: "file",
+        action: "create",
+      })
+
+      expect(createEntitiesResult).toBeDefined()
+      expect(createEntitiesResult.ok).toBeTruthy()
+
+      while (status != ResponseStatus.Ok) {
+        const getEntitiesResult = await service.getEntitiesData({
+          entitiesType: entityType,
+          responseType: "file",
+          action: "get",
+        })
+        expect(getEntitiesResult).toBeDefined()
+        expect(getEntitiesResult.ok).toBeTruthy()
+        if (getEntitiesResult.ok) {
+          const data = getEntitiesResult.data as EntitiesFileData & {
+            status: ResponseStatus
+          }
+          progress = data.progress!
+          status = data.status
+        }
+      }
+      expect(progress).toBe(100)
+      expect(status).toBe(ResponseStatus.Ok)
     }
-  },20000)
-  
+  }, 30_000)
 })
