274c716b90f89d731b9fe86cdc6d4fa1a294df56
MateuszMudita <mateusz.bolewski@mudita.com>
2025-06-09 00:42:32 +0200

    Configuration for Contact Support

---
+++ libs/contact-support/feature/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+export * from "./lib/store/contact-support.reducer"
+export * from "./lib/store/contact-support.actions"
+export * from "../../models/src/lib/contact-support.types"
