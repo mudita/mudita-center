c4bb9a225bac3d48349476d1dc92c6b8889e955c
robertmudi <158469469+robertmudi@users.noreply.github.com>
2025-06-09 15:14:50 +0200

    Update apps/mudita-center-e2e/src/specs/overview/kompakt-manage-files-remove-files.ts
    Co-authored-by: Daniel Karski <daniel.karski5q@gmail.com>

--- apps/mudita-center-e2e/src/specs/overview/kompakt-manage-files-remove-files.ts
+++ apps/mudita-center-e2e/src/specs/overview/kompakt-manage-files-remove-files.ts
@@ -1,5 +1,4 @@
 import { E2EMockClient } from "../../../../../libs/e2e-mock/client/src"
-import { mockEntityDownloadProcess } from "../../helpers"
 import { selectedContactsEntities } from "../../helpers/entity-fixtures"
 import { expect } from "@wdio/globals"
 import NavigationTabs from "../../page-objects/tabs.page"
