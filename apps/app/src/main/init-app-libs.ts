9d49640b8a50b0c94cd56a5a175f80291be4b303
MateuszMudita <mateusz.bolewski@mudita.com>
2025-06-09 12:44:02 +0200

    Fixes after code review

--- apps/app/src/main/init-app-libs.ts
+++ apps/app/src/main/init-app-libs.ts
@@ -7,8 +7,9 @@ import { initSerialPort } from "app-serialport/main"
 import { ipcMain, WebContents } from "electron"
 import { initSql } from "app-sql/main"
 import { initNews } from "news/main"
+import { initAppHelp } from "help/main"
 import { initAppSettings } from "app-settings/main"
-import { initAppActions, initAppHelp } from "app-utils/main"
+import { initAppActions } from "app-utils/main"
 
 export const initAppLibs = (webContents: WebContents) => {
   initAppActions(ipcMain)
