8127076189c1d0a523c3a51efc986e0792c94754
Daniel Karski <daniel.karski5q@gmail.com>
2025-06-12 13:29:24 +0200

    [CP-3637] Basic structure of Analytics domain initialized (#2511)

--- libs/app-utils/renderer/src/lib/analytics/analytics-mutation-keys.ts
+++ libs/app-utils/renderer/src/lib/analytics/analytics-mutation-keys.ts
@@ -3,7 +3,6 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-export * from "./lib/analytics/use-analytics"
-export * from "./lib/app-actions"
-export * from "./lib/app-help"
-export * from "./lib/app-updater"
+export const analyticsMutationKeys = {
+  track: ["analytics", "track"],
+}
