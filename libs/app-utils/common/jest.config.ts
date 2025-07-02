94b7fca32bfc2f383deeefcfd7d52a6e6ffd24f4
Daniel Karski <daniel.karski5q@gmail.com>
2025-07-02 14:33:43 +0200

    [CP-XXX] empty app-utils/common generated

---
+++ libs/app-utils/common/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: "app-utils/common",
+  preset: "../../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../../coverage/libs/app-utils/common",
+}
