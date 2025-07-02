502512a38e635e3960f368f5b5928e6be14f3bf2
Daniel Karski <daniel.karski5q@gmail.com>
2025-07-02 14:38:53 +0200

    [CP-XXX] Delay functionality was moved

---
+++ libs/app-utils/common/src/delay.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+/**
+ * Creates a promise that resolves after a specified delay.
+ *
+ * @param {number} [ms=500] The delay in milliseconds.
+ * @param  {AbortSignal} [signal] Optional AbortSignal to cancel the delay
+ * @returns {Promise<void>} A promise that resolves after the delay.
+ */
+
+export const delay = (ms = 500, signal?: AbortSignal): Promise<void> => {
+  return new Promise<void>((resolve, reject) => {
+    if (signal?.aborted) {
+      resolve()
+      return
+    }
+
+    const timer = setTimeout(() => {
+      cleanup()
+      resolve()
+    }, ms)
+
+    const onAbort = () => {
+      clearTimeout(timer)
+      cleanup()
+      resolve()
+    }
+
+    const cleanup = () => {
+      signal?.removeEventListener("abort", onAbort)
+    }
+
+    signal?.addEventListener("abort", onAbort)
+  })
+}
