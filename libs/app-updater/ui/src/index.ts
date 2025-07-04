b1e9f3e2e8350c737c5cb64b990e1e9f88b68e68
mkurczewski <michal@kurczewski.dev>
2025-07-04 10:45:52 +0200

    Refactored name of modal for update checking

--- libs/app-updater/ui/src/index.ts
+++ libs/app-updater/ui/src/index.ts
@@ -3,7 +3,7 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-export * from "./lib/checking-for-update-modal"
+export * from "./lib/update-checking-modal"
 export * from "./lib/update-not-available-modal"
 export * from "./lib/update-available-modal"
 export * from "./lib/update-in-progress-modal"
