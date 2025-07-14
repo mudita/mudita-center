eef0db204fcd6d4bcdb2a3415681a03a93ee89ee
Daniel Karski <daniel.karski5q@gmail.com>
2025-07-14 08:47:52 +0200

    [CP-3598] app-e2e-mock/client empty lib generated

---
+++ libs/e2e-mock/client/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: "app-e2e-mock/client",
+  preset: "../../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../../coverage/libs/e2e-mock/client",
+}
