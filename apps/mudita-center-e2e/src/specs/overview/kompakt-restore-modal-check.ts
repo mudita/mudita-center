eae2f8685b6a556e811c3a59e88667185c6cc8b1
robertmudi <robert.rosloniec@mudita.com>
2025-07-14 22:03:31 +0200

    fixed test

--- apps/mudita-center-e2e/src/specs/overview/kompakt-restore-modal-check.ts
+++ apps/mudita-center-e2e/src/specs/overview/kompakt-restore-modal-check.ts
@@ -2,13 +2,13 @@ import { E2EMockClient } from "../../../../../libs/e2e-mock/client/src"
 import { overviewDataWithOneSimCard } from "../../../../../libs/e2e-mock/responses/src"
 import { createMockBackup } from "./../../helpers/mock-backup"
 import OverviewKompaktPage from "../../page-objects/overview-kompakt.page"
-import ModalBackupKompaktPage from "../../page-objects/modal-backup-kompakt.page"
-import exp from "constants"
 
 describe("Check restore modal", () => {
   const firstSerialNumber = "0123456789ABCDEF"
 
   before(async () => {
+    await createMockBackup(firstSerialNumber)
+
     E2EMockClient.connect()
     //wait for a connection to be established
     await browser.waitUntil(() => {
@@ -25,27 +25,30 @@ describe("Check restore modal", () => {
     E2EMockClient.mockResponses([
       {
         path: "path-1",
-        body: overviewDataWithOneSimCard,
         endpoint: "FEATURE_DATA",
         method: "GET",
         status: 200,
-      },
-      {
-        path: "path-1",
-        endpoint: "FEATURE_DATA", //mock backup file for Restore check
-        method: "GET",
-        status: 200,
         body: {
-          fileName: "1752426740400_0123456789ABCDEF.mcbackup",
-          serialNumber: "0123456789ABCDEF",
-          vendorId: "3310",
-          productId: "2006",
-          timestamp: 1752426740400,
+          ...overviewDataWithOneSimCard,
+          sections: {
+            ...overviewDataWithOneSimCard.sections,
+            backup: {
+              ...overviewDataWithOneSimCard.sections.backup,
+              show: true,
+              details: [
+                {
+                  fileName: "1752426740400_0123456789ABCDEF.mcbackup",
+                  serialNumber: "0123456789ABCDEF",
+                  vendorId: "3310",
+                  productId: "2006",
+                  timestamp: 1752426740400,
+                },
+              ],
+            },
+          },
         },
       },
     ])
-
-    await createMockBackup(firstSerialNumber)
 
     E2EMockClient.addDevice({
       path: "path-1",
@@ -56,9 +59,9 @@ describe("Check restore modal", () => {
     const menuItem = await $(`//a[@href="#/generic/mc-overview"]`)
     await menuItem.waitForDisplayed({ timeout: 10000 })
     await expect(menuItem).toBeDisplayed()
-
     await browser.pause(7000) //wait until Restore button will appear (it takes about 5-7 seconds to load)
   })
+
   it("Click Restore button", async () => {
     const kompaktRestoreButton = OverviewKompaktPage.kompaktRestoreButton
     await expect(kompaktRestoreButton).toBeDisplayed()
