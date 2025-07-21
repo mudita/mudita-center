3bcc3257b5b38b798aca1950a5ad5d43bad2b6ab
Daniel Karski <daniel.karski5q@gmail.com>
2025-07-21 15:03:02 +0200

    Revert "[CP-XXX] copilot improvements - vol.2"
    This reverts commit 84bd45a62ce7dd66cb65dc0e83f0591c2c6c54e4.

--- apps/app-e2e/src/helpers/mock-app-settings.ts
+++ apps/app-e2e/src/helpers/mock-app-settings.ts
@@ -8,7 +8,8 @@ import * as fs from "fs-extra"
 import * as path from "node:path"
 import { AppSettings } from "app-settings/models"
 import { NestedPartial } from "app-utils/models"
-import { version } from "@app/package.json"
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { version } from "../../../../package.json"
 
 const INITIAL_MOCK_APP_SETTINGS: AppSettings = {
   version,
