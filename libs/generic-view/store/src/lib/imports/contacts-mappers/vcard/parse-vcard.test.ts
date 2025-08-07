4ad922a55aec09ec9c8274a6da311cf902aa9c25
slawomir-werner <slawomir.werner@mudita.com>
2025-08-07 08:51:21 +0200

    Lint fix

--- libs/generic-view/store/src/lib/imports/contacts-mappers/vcard/parse-vcard.test.ts
+++ libs/generic-view/store/src/lib/imports/contacts-mappers/vcard/parse-vcard.test.ts
@@ -3,7 +3,6 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import vCard from "vcf"
 import { parseVcard } from "./parse-vcard"
 
 describe("parseVcard", () => {
