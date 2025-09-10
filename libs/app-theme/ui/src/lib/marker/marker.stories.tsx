2ada6db5f1d64d9b204410912542b4cca3fe566f
Daniel Karski <daniel.karski5q@gmail.com>
2025-09-10 12:24:33 +0200

    [CP-3674][App Refactor][Harmony] Sounds - UI Components (#2656)

---
+++ libs/app-theme/ui/src/lib/marker/marker.stories.tsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import type { Meta, StoryObj } from "@storybook/react"
+import { Marker } from "./marker"
+
+const meta: Meta<typeof Marker> = {
+  title: "UI/Marker",
+  component: Marker,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The `<Marker />` component renders a simple circular marker with a given color.",
+      },
+    },
+  },
+}
+export default meta
+
+type Story = StoryObj<typeof Marker>
+
+export const Default: Story = {
+  args: {
+    $color: "#E38577",
+    style: { width: "1rem" }
+  },
+}
