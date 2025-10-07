b23bdbc8257db34d1dd1b1c1720743933537823a
Daniel Karski <daniel.karski5q@gmail.com>
2025-10-07 10:54:01 +0200

    [CP-3672] waitUntilHarmonyBackupFinished added

---
+++ libs/devices/harmony/feature/src/lib/usecases/wait-until-harmony-backup-finished.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { delay } from "app-utils/common"
+import {
+  BackupState,
+  Harmony,
+  HarmonyEndpointNamed,
+  HarmonyMethodNamed,
+} from "devices/harmony/models"
+import { OKResponse } from "devices/harmony/adapters"
+import { getHarmonyBackupStatus } from "../api/get-harmony-backup-status"
+
+export const waitUntilHarmonyBackupFinished = async (
+  device: Harmony,
+  id: string,
+  delayMs = 100,
+  iteration = 0
+): Promise<OKResponse<HarmonyEndpointNamed.Backup, HarmonyMethodNamed.Get>> => {
+  const response = await getHarmonyBackupStatus({ device, id })
+
+  if (!response.ok || response.body?.state === BackupState.Error) {
+    throw new Error(`Error getting backup status: ${response.status}`)
+  } else if (response.body?.state === BackupState.Finished) {
+    return response
+  } else if (iteration === 10) {
+    throw new Error("Timeout waiting for backup to finish")
+  } else {
+    await delay(delayMs)
+    return waitUntilHarmonyBackupFinished(device, id, delayMs, iteration + 1)
+  }
+}
