b437a87414d62d28f3f39b005b8092f1d452690a
slawomir-werner <slawomir.werner@mudita.com>
2025-10-08 10:32:44 +0200

    Clear device data system action

--- libs/device/models/src/lib/renderer-to-main-events/device-system-actions-service-events.ts
+++ libs/device/models/src/lib/renderer-to-main-events/device-system-actions-service-events.ts
@@ -7,4 +7,5 @@ export enum DeviceSystemActionsServiceEvents {
   Reboot = "apiservice_device_system_actions-reboot",
   Lock = "apiservice_device_system_actions-lock",
   PowerOff = "apiservice_device_system_actions-power-off",
+  ClearData = "apiservice_device_system_actions-clear-data",
 }
