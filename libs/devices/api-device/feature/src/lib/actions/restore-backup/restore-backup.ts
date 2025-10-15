1c5b06c9c7c31094b2d1f97232a33b8bdd01436a
Michał Kurczewski <michal@kurczewski.dev>
2025-10-15 16:25:07 +0200

    Implemented backup restore action

---
+++ libs/devices/api-device/feature/src/lib/actions/restore-backup/restore-backup.ts
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { ApiDevice, PreRestoreRequest } from "devices/api-device/models"
+import { random } from "lodash"
+import { uploadFiles } from "../upload-files/upload-files"
+import { deleteRestore } from "../../api/delete-restore"
+import { preRestoreStep } from "./pre-restore-step"
+import { restoreStep } from "./restore-step"
+
+export interface RestoreBackupParams {
+  device: ApiDevice
+  features: (PreRestoreRequest["features"][number] & { data: unknown })[]
+  onProgress: (progress: number) => void
+  abortController: AbortController
+}
+export const restoreBackup = async ({
+  device,
+  features,
+  onProgress,
+  abortController,
+}: RestoreBackupParams) => {
+  onProgress(0)
+
+  const restoreId = random(1, 100000)
+  let restoreProgress = 0
+  let uploadProgress = 0
+
+  const handleProgress = () => {
+    const totalProgress = restoreProgress * 0.7 + uploadProgress * 0.3
+    onProgress(Math.round(totalProgress))
+  }
+
+  try {
+    const featuresTargets = await preRestoreStep({
+      device,
+      restoreId,
+      features: features.map(({ feature, key }) => ({ feature, key })),
+    })
+    if (abortController.signal.aborted) {
+      throw new Error("Restore aborted")
+    }
+
+    const sourceData = featuresTargets.map(({ feature }) => {
+      const featureData = features.find((f) => f.feature === feature)
+      if (!featureData) {
+        throw new Error(`No data for feature ${feature}`)
+      }
+      return btoa(JSON.stringify(featureData.data))
+    })
+
+    await uploadFiles({
+      device,
+      targetFilePaths: featuresTargets.map(({ filePath }) => filePath),
+      sourceData,
+      onProgress: (progress) => {
+        uploadProgress = progress
+        handleProgress()
+      },
+      abortController,
+    })
+
+    await restoreStep({
+      device,
+      restoreId,
+      onProgress: (progress) => {
+        restoreProgress = progress
+        handleProgress()
+      },
+    })
+  } catch (error) {
+    await deleteRestore(device, { restoreId })
+    if (abortController.signal.aborted) {
+      throw abortController.signal
+    }
+    throw error
+  } finally {
+    onProgress(100)
+  }
+}
