879f1e5a74b0a00771721397aa1a844a537845bf
Michał Kurczewski <michal@kurczewski.dev>
2025-12-16 11:03:17 +0100

    Implemented import flow UI

---
+++ libs/devices/common/ui/src/lib/views/contacts-import-flow/import-cancelled-modal.tsx
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) Mudita sp. z o.o. All rights reserved.
+ * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
+ */
+
+import { FunctionComponent } from "react"
+import { Button, Modal, Typography } from "app-theme/ui"
+import { defineMessages } from "app-localize/utils"
+import { ButtonType, IconType } from "app-theme/models"
+
+const messages = defineMessages({
+  title: {
+    id: "apiDevice.contacts.import.cancelledModal.title",
+  },
+  description: {
+    id: "apiDevice.contacts.import.cancelledModal.description",
+  },
+  closeButtonText: {
+    id: "general.app.closeButton.text",
+  },
+})
+
+interface Props {
+  opened: boolean
+  onClose: VoidFunction
+}
+
+export const ImportCancelledModal: FunctionComponent<Props> = ({
+  opened,
+  onClose,
+}) => {
+  return (
+    <Modal opened={opened}>
+      <Modal.CloseButton onClick={onClose} />
+      <Modal.TitleIcon type={IconType.Failed} />
+      <Modal.Title message={messages.title.id} />
+      <Typography.P1 message={messages.description.id} />
+      <Modal.Buttons>
+        <Button
+          type={ButtonType.Secondary}
+          onClick={onClose}
+          message={messages.closeButtonText.id}
+        />
+      </Modal.Buttons>
+    </Modal>
+  )
+}
