a52ff22bfab00e6f25555bd29767ae1628241a16
Michał Kurczewski <michal@kurczewski.dev>
2026-02-05 14:09:01 +0100

    Added serial number recognition for Kompakt on Windows

--- libs/app-serialport/main/src/lib/app-serialport-device-scanner.ts
+++ libs/app-serialport/main/src/lib/app-serialport-device-scanner.ts
@@ -5,9 +5,13 @@
 
 import { SerialPort } from "serialport"
 import { getUsbDevices } from "./usb-devices/get-usb-devices"
-import { SerialPortDeviceInfo } from "app-serialport/models"
+import {
+  SerialPortDeviceInfo,
+  SerialPortDeviceSubtype,
+} from "app-serialport/models"
 import { devices, SerialPortDevice } from "app-serialport/devices"
 import { PortInfo } from "@serialport/bindings-interface"
+import { execPromise } from "app-utils/main"
 
 export class AppSerialportDeviceScanner {
   private static readonly supportedDevices = devices
@@ -32,6 +36,27 @@ export class AppSerialportDeviceScanner {
     })
   }
 
+  static async getSerialNumberOnWindows(deviceInfo: SerialPortDeviceInfo) {
+    try {
+      const stdout = await execPromise(
+        `Get-PnpDevice -InstanceId '${deviceInfo.pnpId}' | Get-PnpDeviceProperty -KeyName 'DEVPKEY_Device_Parent' | Select-Object -ExpandProperty Data`,
+        "powershell.exe"
+      )
+      if (!stdout) {
+        return
+      }
+
+      const serialNumber = stdout.trim().split("\\").pop() || ""
+
+      if (!/[a-z0-9]+/i.test(serialNumber)) {
+        return
+      }
+      return serialNumber
+    } catch {
+      return
+    }
+  }
+
   /**
    * Scans for connected serial port and usb devices and returns a list of recognized devices.
    */
@@ -39,7 +64,7 @@ export class AppSerialportDeviceScanner {
     const serialportDevices = await SerialPort.list()
     const usbDevices = await getUsbDevices()
 
-    return [
+    const devices = [
       ...serialportDevices.filter((portInfo) => {
         return this.getMatchingInstance(portInfo) !== undefined
       }),
@@ -90,6 +115,20 @@ export class AppSerialportDeviceScanner {
         }
         return internalDeviceInfo
       })
-      .filter((device) => device !== null)
+      .filter((device): device is SerialPortDeviceInfo => device !== null)
+
+    if (process.platform === "win32") {
+      for (const device of devices) {
+        if (device.deviceSubtype === SerialPortDeviceSubtype.Kompakt) {
+          const serialNumber = await this.getSerialNumberOnWindows(device)
+          if (serialNumber) {
+            device.id = serialNumber
+            device.serialNumber = serialNumber
+          }
+        }
+      }
+    }
+
+    return devices
   }
 }
