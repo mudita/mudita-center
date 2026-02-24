74541c77913abbf4af8a6c75f9d109c9ab9bab17
Michał Kurczewski <michalkurczewski94@gmail.com>
2026-02-24 09:52:51 +0100

    Dependencies update (#2831)

--- libs/app-mtp/renderer/src/lib/app-mtp.ts
+++ libs/app-mtp/renderer/src/lib/app-mtp.ts
@@ -3,7 +3,7 @@
  * For licensing, see https://github.com/mudita/mudita-center/blob/master/LICENSE.md
  */
 
-import "types-preload"
+import type {} from "types-preload"
 
 export const AppMtp = {
   getMtpDeviceId: window.api.appMtp.getMtpDeviceId,
